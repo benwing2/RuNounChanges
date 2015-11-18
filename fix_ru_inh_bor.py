2f84d6660c94961bb1c0d86d3f4618267f669571
Ben Wing <ben@benwing.com>
2015-11-18 01:41:47 -0600

    fix_ru_inh_bor: Do multiple-level inheritance chains, and do borrowings (NOT TESTED)

--- fix_ru_inh_bor.py
+++ fix_ru_inh_bor.py
@@ -8,6 +8,8 @@ from blib import getparam, rmparam
 
 site = pywikibot.Site()
 
+borrowed_langs = {}
+
 def msg(text):
   print text.encode("utf-8")
 
@@ -19,18 +21,23 @@ def process_page(index, page, save, verbose):
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagetitle, txt))
 
-  def do_inh(m, langcode, has_prefix_text=False):
-    if has_prefix_text:
-      prefix_text, text = m.groups()
-    else:
-      prefix_text = ""
-      text = m.group(1)
+  # re.sub() substitution function for replacing {{etyl|*|ru}} {{m|*|FOO}}
+  # with either {{inh|ru|*|FOO}} or {{bor|ru|*|FOO}}, depending on the
+  # language.
+  def do_inh_bor(m, do_bor=False):
+    prefix_text, text, langcode = m.groups()
+    if do_bor:
+      if langcode in ["orv", "sla-pro", "ine-bsl-pro", "ine-pro"]:
+        pagemsg("Not creating {{bor}} for inherited language %s: %s" % (
+          langcode, text))
+        return m.group(0)
+      borrowed_langs[langcode] = borrowed_langs.get(langcode, 0) + 1
     parsed = blib.parse_text(text)
     for t in parsed.filter_templates():
       targs = ""
       if unicode(t.name) == "m" and getparam(t, "1") == langcode:
         targs = re.sub(r"^\{\{\s*m\s*\|\s*%s\s*" % langcode, "", unicode(t))
-      if unicode(t.name) == "term" and getparam(t, "lang") == langcode:
+      elif unicode(t.name) == "term" and getparam(t, "lang") == langcode:
         rmparam(t, "lang")
         targs = re.sub(r"^\{\{\s*term\s*", "", unicode(t))
       if targs:
@@ -38,24 +45,46 @@ def process_page(index, page, save, verbose):
           pagemsg("WARNING: Something went wrong in substitution with %s: %s" % (
               targs, text))
           return m.group(0)
-        new_text = prefix_text + "{{inh|ru|%s%s" % (langcode, targs)
+        if do_bor:
+          new_text = "{{bor|ru|%s%s" % (langcode, targs)
+        else:
+          new_text = prefix_text + "{{inh|ru|%s%s" % (langcode, targs)
         pagemsg("Replacing <%s> with <%s>" % (m.group(0), new_text))
         return new_text
-    pagemsg("WARNING: Something went wrong, can't find {{m|...}} or {{term|...}}: %s" % (
+    pagemsg("WARNING: Something went wrong, can't find {{m|...}} or {{term|...}}, or wrong langcode: %s" % (
       text))
     return m.group(0)
 
   text = unicode(page.text)
   orig_text = text
-  text = re.sub(r"^((?:[^{}\n]*[Ff]rom )?)(\{\{etyl\|orv\|ru\}\} \{\{(?:term|m)[^{}\n]*\}\})",
-      lambda x:do_inh(x, "orv", has_prefix_text=True), text, 0, re.M)
-  text = re.sub(r"^((?:[^{}\n]*[Ff]rom )?)(\{\{etyl\|sla-pro\|ru\}\} \{\{(?:term|m)[^{}]*\}\})",
-      lambda x:do_inh(x, "sla-pro", has_prefix_text=True), text, 0, re.M)
-  text = re.sub(r"^((?:[^{}\n]*[Ff]rom )?\{\{inh\|ru\|orv\|[^{}\n]*}\}, from )(\{\{etyl\|sla-pro\|ru\}\} \{\{(?:term|m)[^{}]*\}\})",
-      lambda x:do_inh(x, "sla-pro", has_prefix_text=True), text, 0, re.M)
+
+  # Do inherited cases. We look for a line beginning with either nothing or
+  # some non-template text ending in [Ff]rom, followed by an inheritance chain
+  # of zero or more {{inh|ru|*|...}} templates followed by ",? from ",
+  # followed by {{etyl|*|ru}} {{m...}} or {{etyl|*|ru}} {{term...}}, where
+  # in all cases * must be one of the languages in the inheritance chain
+  # (orv, sla-pro, ine-bsl-pro or ine-pro) and the language inside of
+  # {{m|...}} or {{term|...}} must match the * in the previous {{etyl}}
+  # template. Repeat until no more substitutions, to handle chains of
+  # inheritance.
+  while true:
+    new_text = re.sub(r"^((?:[^{}\n]*[Ff]rom +)?(?:\{\{inh\|ru\|(?:orv|sla-pro|ine-bsl-pro|ine-pro)(?:\|[^{}\n]*)}\},? +from +)*)(\{\{etyl\|(orv|sla-pro|ine-bsl-pro|ine-pro)\|ru\}\} +\{\{(?:term|m)[^{}\n]*\}\})",
+      do_inh_bor, text, 0, re.M)
+    if new_text == text:
+      break
+    text = new_text
+
+  # Do borrowings. We look for a line beginning with either [Ff]rom or nothing,
+  # followed by {{etyl|*|ru}} {{m...}} or {{etyl|*|ru}} {{term...}}, where
+  # * must not be one of the inheritance-chain languages (orv, sla-pro,
+  # ine-bsl-pro or ine-pro) and the language inside of {{m|...} or
+  # {{term|...}} must match the * in the previous {{etyl}} template.
+  # There should only be one such substitution.
+  text = re.sub(r"^((?:[Ff]rom +)?)(\{\{etyl\|([^|{}\n]*)\|ru\}\} +\{\{(?:term|m)[^{}\n]*\}\})",
+    lambda m:do_inh_bor(m, do_bor=True), text, 0, re.M)
 
   if text != orig_text:
-    comment = "Use {{inh}} in Russian for terms inherited from orv and sla-pro"
+    comment = "Use {{inh}}/{{bor}} in Russian for terms inherited or borrowed"
     if save:
       pagemsg("Saving with comment = %s" % comment)
       page.text = text
@@ -78,3 +107,8 @@ for cat in ["Russian terms derived from Old East Slavic", "Russian terms derived
   for i, page in blib.cat_articles(cat, start, end):
     msg("Page %s %s: Processing" % (i, unicode(page.title())))
     process_page(i, page, args.save, args.verbose)
+
+msg("")
+msg("Processed borrowed languages:")
+for lang, count in sorted(borrowed_langs.items(), key=lambda x:-int(x[1])):
+  msg("%s = %s" % (lang, count))
