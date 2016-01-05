c1e7697f9c3c63c15f763b6b9ac3ec24754d26e8
Ben Wing <ben@benwing.com>
2016-01-04 20:54:53 -0600

    ru-translit: Latest from Wiktionary (Cyrillic е after dash at beginning of word should become plain e, for use in suffixes, make э become ɛ after consonants, in мягк- лёгк-, легк-, convert to хк, Decompose composed-grave chars early to avoid problems with e.g. воѐнно-морско́й, also transliterate гч as хч in мягч, легч, лёгч)

--- ru-translit.lua
+++ ru-translit.lua
@@ -6,53 +6,68 @@ local GR = u(0x0300) -- grave =  ̀
 local tab = {
 	["А"]="A", ["Б"]="B", ["В"]="V", ["Г"]="G", ["Д"]="D", ["Е"]="E", ["Ё"]="Jó", ["Ж"]="Ž", ["З"]="Z", ["И"]="I", ["Й"]="J",
 	["К"]="K", ["Л"]="L", ["М"]="M", ["Н"]="N", ["О"]="O", ["П"]="P", ["Р"]="R", ["С"]="S", ["Т"]="T", ["У"]="U", ["Ф"]="F",
-	["Х"]="X", ["Ц"]="C", ["Ч"]="Č", ["Ш"]="Š", ["Щ"]="Šč", ["Ъ"]="ʺ", ["Ы"]="Y", ["Ь"]="ʹ", ["Э"]="E", ["Ю"]="Ju", ["Я"]="Ja",
+	["Х"]="X", ["Ц"]="C", ["Ч"]="Č", ["Ш"]="Š", ["Щ"]="Šč", ["Ъ"]="ʺ", ["Ы"]="Y", ["Ь"]="ʹ", ["Э"]="Ɛ", ["Ю"]="Ju", ["Я"]="Ja",
 	['а']='a', ['б']='b', ['в']='v', ['г']='g', ['д']='d', ['е']='e', ['ё']='jó', ['ж']='ž', ['з']='z', ['и']='i', ['й']='j',
 	['к']='k', ['л']='l', ['м']='m', ['н']='n', ['о']='o', ['п']='p', ['р']='r', ['с']='s', ['т']='t', ['у']='u', ['ф']='f',
-	['х']='x', ['ц']='c', ['ч']='č', ['ш']='š', ['щ']='šč', ['ъ']='ʺ', ['ы']='y', ['ь']='ʹ', ['э']='e', ['ю']='ju', ['я']='ja',
+	['х']='x', ['ц']='c', ['ч']='č', ['ш']='š', ['щ']='šč', ['ъ']='ʺ', ['ы']='y', ['ь']='ʹ', ['э']='ɛ', ['ю']='ju', ['я']='ja',
 	-- Russian style quotes
 	['«']='“', ['»']='”',
 	-- archaic, pre-1918 letters
 	['І']='I', ['і']='i', ['Ѳ']='F', ['ѳ']='f',
 	['Ѣ']='Ě', ['ѣ']='ě', ['Ѵ']='I', ['ѵ']='i',
-	-- composed combinations with grave accents map to uncomposed letters
-	-- for consistency with other char+grave combinations
-	['ѐ'] = 'e' .. GR, ['Ѐ'] = 'E' .. GR, ['ѝ'] = 'i' .. GR, ['Ѝ'] = 'I' .. GR,
 }
 
-local function replace_e(pre, e)
+-- FIXME! Doesn't work with ɣ, which gets included in this character set
+local non_consonants = "[АОУҮЫЭЯЁЮИЕЪЬІѢѴаоуүыэяёюиеъьіѣѵAEIOUYĚƐaeiouyěɛʹʺ%A]"
+
+local map_to_plain_e_map = {["Е"] = "E", ["е"] = "e", ["Ѣ"] = "Ě", ["ѣ"] = "ě", ["Э"] = "Ɛ", ["э"] = "ɛ"}
+local function map_to_plain_e(pre, e)
+	return pre .. map_to_plain_e_map[e]
+end
+
+local map_to_je_map = {["Е"] = "Je", ["е"] = "je", ["Ѣ"] = "Jě", ["ѣ"] = "jě", ["Э"] = "E", ["э"] = "e"}
+local function map_to_je(pre, e)
 	if e == nil then
 		e = pre
 		pre = ""
 	end
-	e = mw.ustring.gsub(e, '.', {["Е"] = "Je", ["е"] = "je", ["Ѣ"] = "Jě", ["ѣ"] = "jě"})
-	if pre == "" or mw.ustring.find(pre, "[АОУҮЫЭЯЁЮИЕЪЬІѢѴаоуүыэяёюиеъьіѣѵAEIOUYĚƐaeiouyěɛʹʺ%A]") then
-		return pre .. e
-	else
-		return pre .. mw.ustring.sub(e, 2)
-	end
+	return pre .. map_to_je_map[e]
 end
+
+-- decompose composed grave chars; they will map to uncomposed Latin letters for
+-- consistency with other char+grave combinations, and we do this early to
+-- avoid problems converting to e or je
+local decompose_grave_map = {['ѐ'] = 'е' .. GR, ['Ѐ'] = 'Е' .. GR, ['ѝ'] = 'и' .. GR, ['Ѝ'] = 'И' .. GR}
 
 -- Transliterates text, which should be a single word or phrase. It should
 -- include stress marks, which are then preserved in the transliteration.
 function export.tr(text, lang, sc)
 	-- Remove word-final hard sign
 	text = mw.ustring.gsub(text, "[Ъъ]$", "")
-	text = mw.ustring.gsub(text, "[Ъъ]([%A])", "%1")
-
-	-- Ё needs converting if is decomposed
-	-- won't have any effect
-	-- text = text:gsub("ё","ё"):gsub("Ё","Ё")
+	text = mw.ustring.gsub(text, "[Ъъ]([- ])", "%1")
 
 	-- ё after a "hushing" consonant becomes ó (ё is mostly stressed)
 	text = mw.ustring.gsub(text, "([жшчщЖШЧЩ])ё","%1ó")
 	-- ю after ж and ш becomes u (e.g. брошюра, жюри)
 	text = mw.ustring.gsub(text, "([жшЖШ])ю","%1u")
 
-	-- е after a vowel or at the beginning of a word becomes je
-	text = mw.ustring.gsub(text, "^([ЕеѢѣ]+)", replace_e)
-	text = mw.ustring.gsub(text, "(.)([ЕеѢѣ]+)", replace_e)
+	-- decompose composed grave characters before we convert Cyrillic е to
+	-- Latin e or je
+	text = mw.ustring.gsub(text, "[ѐЀѝЍ]", decompose_grave_map)
 
+	-- е after a dash at the beginning of a word becomes e, and э becomes ɛ
+	-- (like after a consonant)
+	text = mw.ustring.gsub(text, "^(%-)([ЕеѢѣЭэ])", map_to_plain_e)
+	text = mw.ustring.gsub(text, "(%s%-)([ЕеѢѣЭэ])", map_to_plain_e)
+
+	-- е after a vowel or at the beginning of a word becomes je, and э becomes e
+	text = mw.ustring.gsub(text, "^([ЕеѢѣЭэ])", map_to_je)
+	text = mw.ustring.gsub(text, "(" .. non_consonants .. ")([ЕеѢѣЭэ])", map_to_je)
+	-- need to do it twice in case of sequences of such vowels
+	text = mw.ustring.gsub(text, "^([ЕеѢѣЭэ])", map_to_je)
+	text = mw.ustring.gsub(text, "(" .. non_consonants .. ")([ЕеѢѣЭэ])", map_to_je)
+
+	text = mw.ustring.gsub(text, "([МмЛл][яеё][́̀]?)г([кч])", "%1х%2")
 	return (mw.ustring.gsub(text,'.',tab))
 end
 
@@ -64,9 +79,11 @@ function export.tr_adj(text)
 
 	local tr = export.tr(text)
 
-	--handle genitive/accusative endings, which are spelled -ого/-его (-ogo/-ego) but transliterated -ovo/-evo
-	-- only for adjectives and pronouns, excluding words like много, ого
-	local pattern = "([oeóéOEÓÉ][\204\129\204\128]?)([gG])([oO][\204\129\204\128]?)"
+	--handle genitive/accusative endings, which are spelled -ого/-его/-аго
+	-- (-ogo/-ego/-ago) but transliterated -ovo/-evo/-avo; only for adjectives
+	-- and pronouns, excluding words like много, ого (-аго occurs in
+	-- pre-reform spelling)
+	local pattern = "([oeaóéáOEAÓÉÁ][\204\129\204\128]?)([gG])([oO][\204\129\204\128]?)"
 	local reflexive = "([sS][jJ][aáAÁ][\204\129\204\128]?)"
 	local v = {["g"] = "v", ["G"] = "V"}
 	local repl = function(e, g, o, sja) return e .. v[g] .. o .. (sja or "") end
