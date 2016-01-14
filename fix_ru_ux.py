4d33f6bfca7bb745c974a00867e35a520fd8e1f7
Ben Wing <ben@benwing.com>
2016-01-14 16:02:55 -0600

    fix_ru_ux: New script

---
+++ fix_ru_ux.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Remove adj= and shto= from ru-ux.
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site
+
+import rulib as ru
+import runounlib as runoun
+
+def process_page(index, page, save, verbose):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+
+  text = unicode(page.text)
+  parsed = blib.parse(page)
+  notes = []
+  for t in parsed.filter_templates():
+    if unicode(t.name) == "ru-ux":
+      origt = unicode(t)
+      if t.has("adj"):
+        pagemsg("Removing adj=")
+        notes.append("remove adj= from ru-ux")
+        rmparam(t, "adj")
+      if t.has("shto"):
+        pagemsg("Removing shto=")
+        notes.append("remove shto= from ru-ux")
+        rmparam(t, "shto")
+      newt = unicode(t)
+      if origt != newt:
+        pagemsg("Replaced %s with %s" % (origt, newt))
+
+  new_text = unicode(parsed)
+
+  if new_text != text:
+    if verbose:
+      pagemsg("Replacing <%s> with <%s>" % (text, new_text))
+    assert notes
+    comment = "; ".join(notes)
+    if save:
+      pagemsg("Saving with comment = %s" % comment)
+      page.text = new_text
+      page.save(comment=comment)
+    else:
+      pagemsg("Would save with comment = %s" % comment)
+
+parser = blib.create_argparser(u"Remove adj= and shto= from ru-ux")
+args = parser.parse_args()
+start, end = blib.get_args(args.start, args.end)
+
+for i, page in blib.references("Template:ru-ux", start, end):
+  process_page(i, page, args.save, args.verbose)
