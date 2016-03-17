debb1ac134c17df7dbeb539bcc3bf1ed7a553b90
Ben Wing <ben@benwing.com>
2016-03-17 13:13:44 -0500

    purge_pages: New script

---
+++ purge_pages.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site
+
+parser = blib.create_argparser(u"Purge (null-save) pages in category or references")
+parser.add_argument('--cat', help="Category to purge")
+parser.add_argument('--ref', help="References to purge")
+args = parser.parse_args()
+start, end = blib.get_args(args.start, args.end)
+
+pages = []
+if args.cat:
+  pages_to_list = blib.cat_articles(args.cat, start, end)
+else:
+  pages_to_list = blib.references(args.ref, start, end)
+for i, page in pages_to_list:
+  # msg("Page %s %s: Null-saving" % (i, unicode(page.title())))
+  page.save(comment="null save")
