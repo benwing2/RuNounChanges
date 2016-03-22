2c952140188026e067351f58d8b53adf74cbcae5
Ben Wing <ben@benwing.com>
2016-03-21 21:59:51 -0500

    fix_3a: add --delete-bad, --fix-verbs so we can do these separately (they need different users); only change 3olda templates

--- fix_3a.py
+++ fix_3a.py
@@ -8,7 +8,7 @@ from blib import getparam, rmparam, msg, site
 
 import rulib
 
-def process_page(index, page, direc, save, verbose):
+def process_page(index, page, direc, delete_bad, fix_verbs, save, verbose):
   pagetitle = unicode(page.title())
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagetitle, txt))
@@ -26,6 +26,8 @@ def process_page(index, page, direc, save, verbose):
     origt = unicode(t)
     if unicode(t.name) in ["ru-conj"]:
       conjtype = getparam(t, "1")
+      if not conjtype.startswith("3olda"):
+        continue
       if conjtype.startswith("3olda") and conjtype != "3olda":
         pagemsg("WARNING: Found 3a-old with variant, can't process: %s" % unicode(t))
         continue
@@ -44,40 +46,41 @@ def process_page(index, page, direc, save, verbose):
       if not result:
         pagemsg("WARNING: Error generating forms, skipping")
         continue
-      newargs = rulib.split_generate_args(result)
-      for form in ["past_m", "past_f", "past_n", "past_pl", "past_m_short",
-          "past_f_short", "past_n_short", "past_pl_short"]:
-        oldforms = re.split(",", oldargs[form]) if form in oldargs else []
-        newforms = re.split(",", newargs[form]) if form in newargs else []
-        for oldform in oldforms:
-          if oldform not in newforms:
-            formpagename = rulib.remove_accents(oldform)
-            formpage = pywikibot.Page(site, formpagename)
-            if not formpage.exists():
-              pagemsg("WARNING: Form page %s doesn't exist, skipping" % formpagename)
-            elif formpagename == pagetitle:
-              pagemsg("WARNING: Attempt to delete dictionary form, skipping")
-            else:
-              text = unicode(formpage.text)
-              if "Etymology 1" in text:
-                pagemsg("WARNING: Found 'Etymology 1', skipping form %s" % formpagename)
-              elif "----" in text:
-                pagemsg("WARNING: Multiple languages apparently in form, skippin form %s" % formpagename)
+      if delete_bad:
+        newargs = rulib.split_generate_args(result)
+        for form in ["past_m", "past_f", "past_n", "past_pl", "past_m_short",
+            "past_f_short", "past_n_short", "past_pl_short"]:
+          oldforms = re.split(",", oldargs[form]) if form in oldargs else []
+          newforms = re.split(",", newargs[form]) if form in newargs else []
+          for oldform in oldforms:
+            if oldform not in newforms:
+              formpagename = rulib.remove_accents(oldform)
+              formpage = pywikibot.Page(site, formpagename)
+              if not formpage.exists():
+                pagemsg("WARNING: Form page %s doesn't exist, skipping" % formpagename)
+              elif formpagename == pagetitle:
+                pagemsg("WARNING: Attempt to delete dictionary form, skipping")
               else:
-                numinfls = len(re.findall(r"\{\{inflection of\|", text))
-                if numinfls < 1:
-                  pagemsg("WARNING: Something wrong, no 'inflection of' templates on page for form %s" % formpagename)
-                elif numinfls > 1:
-                  pagemsg("WARNING: Multiple 'inflection of' templates on page for form %s, skipping" % formpagename)
+                text = unicode(formpage.text)
+                if "Etymology 1" in text:
+                  pagemsg("WARNING: Found 'Etymology 1', skipping form %s" % formpagename)
+                elif "----" in text:
+                  pagemsg("WARNING: Multiple languages apparently in form, skippin form %s" % formpagename)
                 else:
-                  comment = "Delete erroneously created long form of %s" % pagetitle
-                  pagemsg("Existing text for form %s: [[%s]]" % (
-                    formpagename, text))
-                  if save:
-                    formpage.delete(comment)
+                  numinfls = len(re.findall(r"\{\{inflection of\|", text))
+                  if numinfls < 1:
+                    pagemsg("WARNING: Something wrong, no 'inflection of' templates on page for form %s" % formpagename)
+                  elif numinfls > 1:
+                    pagemsg("WARNING: Multiple 'inflection of' templates on page for form %s, skipping" % formpagename)
                   else:
-                    pagemsg("Would delete page %s with comment=%s" %
-                        (formpagename, comment))
+                    comment = "Delete erroneously created long form of %s" % pagetitle
+                    pagemsg("Existing text for form %s: [[%s]]" % (
+                      formpagename, text))
+                    if save:
+                      formpage.delete(comment)
+                    else:
+                      pagemsg("Would delete page %s with comment=%s" %
+                          (formpagename, comment))
 
       notes.append("fix 3olda -> %s" % direc)
     newt = unicode(t)
@@ -86,7 +89,7 @@ def process_page(index, page, direc, save, verbose):
 
   new_text = unicode(parsed)
 
-  if new_text != text:
+  if new_text != text and fix_verbs:
     if verbose:
       pagemsg("Replacing <%s> with <%s>" % (text, new_text))
     assert notes
@@ -100,6 +103,8 @@ def process_page(index, page, direc, save, verbose):
 
 parser = blib.create_argparser(u"Fix up class 3a")
 parser.add_argument('--direcfile', help="File containing pages to fix and directives.")
+parser.add_argument('--delete-bad', action="store_true", help="Delete bad forms.")
+parser.add_argument('--fix-verbs', action="store_true", help="Fix verb conjugation.")
 args = parser.parse_args()
 start, end = blib.get_args(args.start, args.end)
 
@@ -110,4 +115,4 @@ for i, line in blib.iter_items(lines, start, end):
     msg("Skipping comment: %s" % line)
   else:
     page, direc = re.split(" ", line)
-    process_page(i, pywikibot.Page(site, page), direc, args.save, args.verbose)
+    process_page(i, pywikibot.Page(site, page), direc, args.delete_bad, args.fix_verbs, args.save, args.verbose)
