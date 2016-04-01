d3e502e7b35153efef8c9fe2ce05727372d581ba
Ben Wing <ben@benwing.com>
2016-04-01 01:34:24 -0500

    generate_adjs: New script

---
+++ generate_adjs.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site
+
+import rulib as ru
+
+parser = argparse.ArgumentParser(description="Generate adjective stubs.")
+parser.add_argument('--direcfile', help="File containing directives.")
+args = parser.parse_args()
+
+for line in codecs.open(args.direcfile, "r", "utf-8"):
+  line = line.strip()
+  els = re.split(r"\s+", line)
+  adj, etym, short, defns = els[0], els[1], els[2], els[3]
+  assert re.search(u"(ый|ий|о́й)$", adj)
+  if etym == "-":
+    etymtext = "{{rfe|lang=ru}}"
+  else:
+    prefix = ""
+    suffix = ""
+    if etym.startswith("?"):
+      prefix = "Perhaps from "
+      suffix = "."
+      etym = re.sub(r"^\?", "", etym)
+    elif etym.startswith("<<"):
+      prefix = "Ultimately from "
+      suffix = "."
+      etym = re.sub(r"^<<", "", etym)
+    m = re.search(r"^(de|la|en):(.*?)\+(.*)$", etym)
+    if m:
+      prefix += "{{der|ru|%s|%s}} + " % (m.group(1), m.group(2))
+      etym = m.group(3)
+    etymtext = "%s{{affix|ru|%s}}%s" % (prefix,
+        "|".join(re.split(r"\+", etym)), suffix)
+
+  # Create declension
+  short = re.sub(r"\?.*$", "", short) # eliminate uncertainty notations
+  if short == "-":
+    shorttext = ""
+  else:
+    shorttext = "|%s" % short
+  decltext = "{{ru-decl-adj|%s%s}}" % (adj, shorttext)
+
+  # Create definition
+  defnlines = []
+  for defn in re.split(";", defns):
+    if defn == "-":
+      defnlines.append("# {{rfdef|lang=ru}}\n")
+    else:
+      prefix = ""
+      if defn.startswith("+"):
+        prefix = "{{lb|ru|attributive}} "
+        defn = re.sub(r"^\+", "", defn)
+      elif defn.startswith("#"):
+        prefix = "{{lb|ru|figurative}} "
+        defn = re.sub(r"^#", "", defn)
+      elif defn.startswith("!"):
+        prefix = "{{lb|ru|colloquial}} "
+        defn = re.sub(r"^!", "", defn)
+      defnlines.append("# %s%s\n" % (prefix, defn))
+  defntext = "".join(defnlines)
+
+  syntext = ""
+  anttext = ""
+  dertext = ""
+  reltext = """====Related terms====
+* {{l|ru|}}
+* {{l|ru|}}
+* {{l|ru|}}
+* {{l|ru|}}
+* {{l|ru|}}
+* {{l|ru|}}
+
+"""
+  for synantrel in els[4:]:
+    m = re.search(r"^(syn|ant|der|rel):(.*)", synantrel)
+    if not m:
+      msg("Element %s doesn't start with syn:, ant:, der: or rel:" % synantrel)
+    assert m
+    sartype, vals = m.groups()
+    if sartype in ["syn", "ant"]:
+      lines = []
+      for synantgroup in re.split(";", vals):
+        sensetext = ""
+        if synantgroup.startswith("*"):
+          sensetext = "{{sense|FIXME}} "
+          synantgroup = re.sub(r"\*", "", synantgroup)
+        else:
+          sensetext = ""
+        links = []
+        for synant in re.split(",", synantgroup):
+          links.append("{{l|ru|%s}}" % synant)
+        lines.append("* %s%s\n" % (sensetext, ", ".join(links)))
+      synantguts = "====%s====\n%s\n" % (
+          "Synonyms" if sartype == "syn" else "Antonyms",
+          "".join(lines))
+      if sartype == "syn":
+        syntext = synantguts
+      else:
+        anttext = synantguts
+    else: # derived or related terms
+      lines = []
+      for derrelgroup in re.split(",", vals):
+        if "/" in derrelgroup:
+          impf, pf = re.split("/", derrelgroup)
+          lines.append("* {{l|ru|%s|g=impf}}, {{l|ru|%s|g=pf}}\n" % (impf, pf))
+        else:
+          links = []
+          for derrel in re.split(":", derrelgroup):
+            links.append("{{l|ru|%s}}" % derrel)
+          lines.append("* %s\n" % ", ".join(links))
+      derrelguts = "====%s terms====\n%s\n" % (
+          "Derived" if sartype == "der" else "Related", "".join(lines))
+      if sartype == "der":
+        dertext = derrelguts
+      else:
+        reltext = derrelguts
+
+  msg("""%s
+
+==Russian==
+
+===Etymology===
+%s
+
+===Pronunciation===
+* {{ru-IPA|%s}}
+
+===Adjective===
+{{ru-adj|%s}}
+
+%s
+====Declension====
+%s
+
+%s%s%s%s[[ru:%s]]
+
+""" % (ru.remove_accents(adj), etymtext, adj, adj, defntext,
+  decltext, syntext, anttext, dertext, reltext, ru.remove_accents(adj)))
