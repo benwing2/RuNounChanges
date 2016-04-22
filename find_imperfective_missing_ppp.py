ef0d8396e9edd85f7c72b16b30ebd14654a88b67
Ben Wing <ben@benwing.com>
2016-04-22 03:33:26 -0500

    find_imperfective_missing_ppp.py: Code to fix missing PPP's

--- find_imperfective_missing_ppp.py
+++ find_imperfective_missing_ppp.py
@@ -24,7 +24,7 @@ from blib import getparam, rmparam, msg, site
 
 import rulib
 
-def process_page(index, page, save, verbose):
+def process_page(index, page, save, verbose, fixdirecs):
   pagetitle = unicode(page.title())
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagetitle, txt))
@@ -34,7 +34,9 @@ def process_page(index, page, save, verbose):
 
   pagemsg("Processing")
 
+  text = unicode(page.text)
   parsed = blib.parse(page)
+  notes = []
   saw_paired_verb = False
   for t in parsed.filter_templates():
     if unicode(t.name) == "ru-verb":
@@ -53,11 +55,56 @@ def process_page(index, page, save, verbose):
         pass
       else:
         pagemsg("Apparent unpaired transitive imperfective without PPP")
+        if pagetitle in fixdirecs:
+          direc = fixdirecs[pagetitle]
+          assert direc in ["fixed", "paired", "intrans", "+p", "|ppp=-"]
+          origt = unicode(t)
+          if direc == "+p":
+            t.add("1", getparam(t, "1") + "+p")
+            notes.append("add missing past passive participle to transitive unpaired imperfective verb")
+            pagemsg("Add missing PPP, replace %s with %s" % (origt, unicode(t)))
+          elif direc == "|ppp=-":
+            t.add("ppp", "-")
+            notes.append("note transitive unpaired imperfective verb as lacking past passive participle")
+            pagemsg("Note no PPP, replace %s with %s" % (origt, unicode(t)))
+          elif direc == "paired":
+            pagemsg("Verb actually is paired")
+          elif direc == "fixed":
+            pagemsg("WARNING: Unfixed verb marked as fixed")
+          elif direc == "intrans":
+            pagemsg("WARNING: Transitive verb marked as intrans")
+
+  new_text = unicode(parsed)
+
+  if new_text != text:
+    if verbose:
+      pagemsg("Replacing <%s> with <%s>" % (text, new_text))
+    assert notes
+    comment = "; ".join(notes)
+    if save:
+      pagemsg("Saving with comment = %s" % comment)
+      page.text = new_text
+      page.save(comment=comment)
+    else:
+      pagemsg("Would save with comment = %s" % comment)
 
 parser = blib.create_argparser(u"Find Russian terms without a proper headword line")
+parser.add_argument('--fix-pagefile', help="File containing pages to fix.")
 args = parser.parse_args()
 start, end = blib.get_args(args.start, args.end)
 
-for category in ["Russian verbs"]:
-  for i, page in blib.cat_articles(category, start, end):
-    process_page(i, page, args.save, args.verbose)
+if args.fix_pagefile:
+  fixdireclines = [
+    x.strip() for x in codecs.open(args.fix_pagefile, "r", "utf-8")]
+  fixdirecs = {}
+  fixpages = []
+  for line in fixdireclines:
+    verb, direc = re.split(" ", line)
+    fixdirecs[verb] = direc
+    fixpages.append(verb)
+  for i, page in blib.iter_items(fixpages, start, end):
+    process_page(i, pywikibot.Page(site, page), args.save, args.verbose, fixdirecs)
+else:
+  for category in ["Russian verbs"]:
+    for i, page in blib.cat_articles(category, start, end):
+      process_page(i, page, args.save, args.verbose, {})
