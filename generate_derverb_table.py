55a3b834daef3c48e991aad607731da629f66b4d
Ben Wing <ben@benwing.com>
2016-05-01 16:59:52 -0500

    generate_derverb_table: Handle bare prefixes

--- generate_derverb_table.py
+++ generate_derverb_table.py
@@ -10,18 +10,41 @@ parser = argparse.ArgumentParser(description="Generate derived-verb tables.")
 parser.add_argument('--direcfile', help="File containing directives.")
 args = parser.parse_args()
 
+def combine_prefix(prefix, suffix):
+  if ru.is_stressed(prefix):
+    return "* {{l|ru|" + prefix + ru.make_unstressed(suffix) + "}}"
+  else:
+    return "* {{l|ru|" + prefix + suffix + "}}"
 # Each group is delineated by a line containing only a hyphen in the
 # directive file, and consists of a list of (pf, impf) pairs.
 groups = []
 group = []
+pfsuffix = None
+impfsuffix = None
 for line in codecs.open(args.direcfile, "r", "utf-8"):
   line = line.strip()
+  if not line:
+    continue
   if line == "-":
     if group:
       groups.append(group)
     group = []
+  elif " " not in line:
+    group.append((combine_prefix(line, pfsuffix),
+        combine_prefix(ru.make_unstressed(line), impfsuffix)))
+  elif "!" in line:
+    pf, impf = re.split(r"\s+", line)
+    assert pf == "!" or impf == "!"
+    if pf == "!":
+      group.append(("* (no equivalent)", combine_prefix(ru.make_unstressed(impf), impfsuffix)))
+    else:
+      group.append((combine_prefix(pf, pfsuffix), "* (no equivalent)"))
   else:
     pf, impf = re.split(r"\s+", line)
+    if pf.startswith("-") and impf.startswith("-"):
+      pfsuffix = re.sub("^-", "", pf)
+      impfsuffix = re.sub("^-", "", impf)
+      continue
     def do_line(direc, aspect):
       links = []
       if direc == "-":
