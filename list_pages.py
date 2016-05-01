6555ba166dc9e8d458c3c20ecfd63a8f28f2cd17
Ben Wing <ben@benwing.com>
2016-05-01 17:00:27 -0500

    list_pages: Change --cats to directly specify categories

--- list_pages.py
+++ list_pages.py
@@ -7,20 +7,12 @@ import blib
 from blib import getparam, rmparam, msg, site
 
 parser = blib.create_argparser(u"List pages, lemmas and/or non-lemmas")
-parser.add_argument('--cats', default="lemma,nonlemma", help="Categories to do (lemma, nonlemma or comma-separated list)")
+parser.add_argument('--cats', default="lemmas", help="Categories to do (can be comma-separated list)")
 args = parser.parse_args()
 start, end = blib.get_args(args.start, args.end)
 
-categories = []
-for cattype in re.split(",", args.cats):
-  if cattype == "lemma":
-    categories.append("Russian lemmas")
-  elif cattype == "nonlemma":
-    categories.append("Russian non-lemma forms")
-  else:
-    raise RuntimeError("Invalid value %s, should be 'lemma' or 'nonlemma'" %
-        cattype)
-for category in categories:
-  msg("Processing category: %s" % category)
-  for i, page in blib.cat_articles(category, start, end):
+for cat in re.split(",", args.cats):
+  cat = "Russian " + cat
+  msg("Processing category: %s" % cat)
+  for i, page in blib.cat_articles(cat, start, end):
     msg("Page %s %s: Processing" % (i, unicode(page.title())))
