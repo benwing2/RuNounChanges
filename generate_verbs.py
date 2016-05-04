c7ab2a27f74b4ac6aa394b5570cd69ad3bb2a0c2
Ben Wing <ben@benwing.com>
2016-05-04 03:02:49 -0500

    generate_verbs: Check for properly stressed words, fix handling of etymologies from other languages

--- generate_verbs.py
+++ generate_verbs.py
@@ -4,11 +4,17 @@
 import re, sys, codecs, argparse
 
 from blib import msg
-import rulib as ru
+import rulib
 
 parser = argparse.ArgumentParser(description="Generate verb stubs.")
 parser.add_argument('--direcfile', help="File containing directives.")
 args = parser.parse_args()
+
+def check_stress(word):
+  word = re.sub(r"|.*", "", word)
+  if rulib.needs_accents(word):
+    msg("Word %s missing an accent" % word)
+    assert False
 
 for line in codecs.open(args.direcfile, "r", "utf-8"):
   line = line.strip()
@@ -18,6 +24,7 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
   els = [el if i == 4 else el.replace("_", " ") for i, el in enumerate(els)]
   verb, etym, aspect, corverbs, conj = els[0], els[1], els[2], els[3], els[4]
   assert re.search(u"(ть(ся)?|ти́?(сь)?|чь(ся)?)$", verb)
+  check_stress(verb)
   isrefl = re.search(u"(ся|сь)$", verb)
   if etym == "-":
     etymtext = "{{rfe|lang=ru}}"
@@ -36,22 +43,28 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
       assert isrefl
       etymtext = re.sub(u"^(.*?)(ся|сь)$", r"{{affix|ru|\1|-\2}}", verb)
     else:
-      m = re.search(r"^(de|fr|la|en):(.*?)\+(.*)$", etym)
+      m = re.search(r"^([a-z-]+):(.*)", etym)
       if m:
-        prefix += "{{der|ru|%s|%s}} + " % (m.group(1), m.group(2))
-        etym = m.group(3)
-      etymtext = "%s{{affix|ru|%s}}%s" % (prefix,
-          "|".join(re.split(r"\+", etym)), suffix)
+        langtext = "|lang1=%s" % m.group(1)
+        etym = m.group(2)
+      else:
+        langtext = ""
+      etymtext = "%s{{affix|ru|%s%s}}%s" % (prefix,
+          "|".join(re.split(r"\+", etym)), langtext, suffix)
   headword_aspect = re.sub("-.*", "", aspect)
   assert headword_aspect in ["pf", "impf", "both"]
+  if corverbs == "-":
+    corverbs = []
+  else:
+    corverbs = re.split(",", corverbs)
   corverbtext = ""
-  if corverbs != "-":
-    corverbno = 1
-    for corverb in re.split(",", corverbs):
-      corverbtext += "|%s%s=%s" % (
-          "impf" if headword_aspect == "pf" else "pf",
-          "" if corverbno == 1 else str(corverbno), corverb)
-      corverbno += 1
+  corverbno = 1
+  for corverb in corverbs:
+    check_stress(corverb)
+    corverbtext += "|%s%s=%s" % (
+        "impf" if headword_aspect == "pf" else "pf",
+        "" if corverbno == 1 else str(corverbno), corverb)
+    corverbno += 1
   verbbase = re.sub(u"(ся|сь)$", "", verb)
   passivetext = ("\n# {{passive of|lang=ru|%s}}" % verbbase
       if etym == "r" else "")
@@ -61,41 +74,41 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
       assert re.search(u"[ая]ть$", verbbase)
       conjargs = re.sub(u"[ая]ть$", "", verbbase)
     elif conj.startswith("6b") or conj.startswith(u"6°b") or conj.startswith("6ob"):
-      assert ru.is_monosyllabic(verbbase) or re.search(u"[ая]́ть$", verbbase)
+      assert rulib.is_monosyllabic(verbbase) or re.search(u"[ая]́ть$", verbbase)
       conjargs = re.sub(u"[ая]́?ть$", "", verbbase)
     elif conj.startswith("6c") or conj.startswith(u"6°c") or conj.startswith("6oc"):
-      assert ru.is_monosyllabic(verbbase) or re.search(u"[ая]́ть$", verbbase)
-      conjargs = ru.make_ending_stressed(re.sub(u"[ая]́?ть$", "", verbbase))
+      assert rulib.is_monosyllabic(verbbase) or re.search(u"[ая]́ть$", verbbase)
+      conjargs = rulib.make_ending_stressed(re.sub(u"[ая]́?ть$", "", verbbase))
     elif conj.startswith("5a"):
       assert re.search(u"[еая]ть$", verbbase)
       conjargs = "%s|%s" % (re.sub(u"[еая]ть$", "", verbbase),
           re.sub(u"ть$", "", verbbase))
     elif conj.startswith("5b"):
-      assert ru.is_monosyllabic(verbbase) or re.search(u"[еая]́ть$", verbbase)
+      assert rulib.is_monosyllabic(verbbase) or re.search(u"[еая]́ть$", verbbase)
       conjargs = "%s|%s" % (re.sub(u"[еая]́?ть$", "", verbbase),
           re.sub(u"ть$", "", verbbase))
     elif conj.startswith("5c"):
-      assert ru.is_monosyllabic(verbbase) or re.search(u"[еая]́ть$", verbbase)
-      conjargs = "%s|%s" % (ru.make_ending_stressed(re.sub(u"[еая]́?ть$", "", verbbase)),
+      assert rulib.is_monosyllabic(verbbase) or re.search(u"[еая]́ть$", verbbase)
+      conjargs = "%s|%s" % (rulib.make_ending_stressed(re.sub(u"[еая]́?ть$", "", verbbase)),
           re.sub(u"ть$", "", verbbase))
     elif conj.startswith("4a"):
       assert verbbase.endswith(u"ить")
       conjargs = re.sub(u"ить", "", verbbase)
     elif conj.startswith("4b"):
-      assert ru.is_monosyllabic(verbbase) or verbbase.endswith(u"и́ть")
+      assert rulib.is_monosyllabic(verbbase) or verbbase.endswith(u"и́ть")
       conjargs = re.sub(u"и́?ть", "", verbbase)
     elif conj.startswith("4c"):
-      assert ru.is_monosyllabic(verbbase) or verbbase.endswith(u"и́ть")
-      conjargs = ru.make_ending_stressed(re.sub(u"и́?ть", "", verbbase))
+      assert rulib.is_monosyllabic(verbbase) or verbbase.endswith(u"и́ть")
+      conjargs = rulib.make_ending_stressed(re.sub(u"и́?ть", "", verbbase))
     elif conj.startswith("3a") or conj.startswith(u"3°a") or conj.startswith("3oa"):
       assert verbbase.endswith(u"нуть")
       conjargs = re.sub(u"нуть$", "", verbbase)
     elif conj.startswith("3b"):
-      assert ru.is_monosyllabic(verbbase) or verbbase.endswith(u"ну́ть")
+      assert rulib.is_monosyllabic(verbbase) or verbbase.endswith(u"ну́ть")
       conjargs = re.sub(u"у́?ть$", "", verbbase)
     elif conj.startswith("3c"):
-      assert ru.is_monosyllabic(verbbase) or verbbase.endswith(u"ну́ть")
-      conjargs = ru.make_ending_stressed(re.sub(u"у́?ть", "", verbbase))
+      assert rulib.is_monosyllabic(verbbase) or verbbase.endswith(u"ну́ть")
+      conjargs = rulib.make_ending_stressed(re.sub(u"у́?ть", "", verbbase))
     elif conj.startswith("2a") or conj.startswith("2b"):
       assert re.search(u"ва́?ть$", verbbase)
       conjargs = re.sub(u"ть$", "", verbbase)
@@ -154,6 +167,7 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
           sensetext = ""
         links = []
         for synant in re.split(",", synantgroup):
+          check_stress(synant)
           links.append("{{l|ru|%s}}" % synant)
         lines.append("* %s%s\n" % (sensetext, ", ".join(links)))
       synantguts = "====%s====\n%s\n" % (
@@ -168,6 +182,7 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
     elif sartype == "alt":
       lines = []
       for altform in re.split(",", vals):
+        check_stress(altform)
         lines.append("* {{l|ru|%s}}\n" % altform)
       alttext = "===Alternative forms===\n%s\n" % "".join(lines)
     else: # derived or related terms
@@ -187,21 +202,19 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
               if isrefl:
                 refverb = re.sub(u"с[ья]$", "", verb) + "|g=" + headword_aspect
                 correfverbs = []
-                if corverbs != "-":
-                  for corverb in re.split(",", corverbs):
-                    correfverbs.append("%s|g=%s" % (
-                      re.sub(u"с[ья]$", "", corverb),
-                      "impf" if headword_aspect == "pf" else "pf"))
+                for corverb in corverbs:
+                  correfverbs.append("%s|g=%s" % (
+                    re.sub(u"с[ья]$", "", corverb),
+                    "impf" if headword_aspect == "pf" else "pf"))
               else:
                 refverb = (re.search(u"и́?$", verb) and verb + u"сь" or
                     verb + u"ся") + "|g=" + headword_aspect
                 correfverbs = []
-                if corverbs != "-":
-                  for corverb in re.split(",", corverbs):
-                    correfverbs.append("%s|g=%s" % (
-                        (re.search(u"и́?$", corverb) and corverb + u"сь" or
-                          corverb + u"ся"),
-                        "impf" if headword_aspect == "pf" else "pf"))
+                for corverb in corverbs:
+                  correfverbs.append("%s|g=%s" % (
+                      (re.search(u"и́?$", corverb) and corverb + u"сь" or
+                        corverb + u"ся"),
+                      "impf" if headword_aspect == "pf" else "pf"))
               if headword_aspect == "impf":
                 refverbs = [refverb] + correfverbs
               else:
@@ -210,6 +223,8 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
 
             if "/" in derrel:
               impfpfverbs = re.split("/", derrel)
+              for impfpfverb in impfpfverbs:
+                check_stress(impfpfverb)
               if "|" in impfpfverbs[0]:
                 links.append("{{l|ru|%s}}" % impfpfverbs[0])
               else:
@@ -220,6 +235,7 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
                 else:
                   links.append("{{l|ru|%s|g=pf}}" % pf)
             else:
+              check_stress(derrel)
               links.append("{{l|ru|%s}}" % derrel)
           lines.append("* %s\n" % ", ".join(links))
         derrelguts = "====%s terms====\n%s\n" % (
@@ -249,6 +265,6 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
 
 %s%s%s%s[[ru:%s]]
 
-""" % (ru.remove_accents(verb), alttext, etymtext, prontext, verb, headword_aspect,
+""" % (rulib.remove_accents(verb), alttext, etymtext, prontext, verb, headword_aspect,
   corverbtext, passivetext, conjtext, syntext, anttext, dertext, reltext,
-  ru.remove_accents(verb)))
+  rulib.remove_accents(verb)))
