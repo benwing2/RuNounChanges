1336dda9a0f3293cc5cfd04c0b951f1b4b136e4d
Ben Wing <ben@benwing.com>
2016-07-07 23:53:51 -0500

    find_red_links.py: Support lists form Sergei Sharoff

--- find_red_links.py
+++ find_red_links.py
@@ -23,6 +23,7 @@ from blib import getparam, rmparam, msg, site
 
 parser = blib.create_argparser(u"Find red links")
 parser.add_argument("--pagefile", help="File containing pages to check")
+parser.add_argument("--with-freq", help="Words come with frequencies and possibly occur multiple times.", action="store_true")
 args = parser.parse_args()
 start, end = blib.get_args(args.start, args.end)
 
@@ -31,18 +32,40 @@ msg("Reading Russian lemmas")
 for i, page in blib.cat_articles("Russian lemmas", start, end):
   lemmas.add(unicode(page.title()))
 
+words_freq = {}
+
 lines = [x.strip() for x in codecs.open(args.pagefile, "r", "utf-8")]
-for i, pagename in blib.iter_items(lines, start, end):
+if args.with_freq:
+  for line in lines:
+    freq, word = re.split(r"\s", line)
+    freq = int(freq)
+    if word in words_freq:
+      words_freq[word] += freq
+    else:
+      words_freq[word] = freq
+  words = [x[0] for x in sorted(words_freq.items(), key=lambda y:-y[1])]
+else:
+  words = lines
+
+for i, pagename in blib.iter_items(words, start, end):
   if pagename in lemmas:
     msg("Page %s [[%s]]: exists" % (i, pagename))
   else:
-    page = pywikibot.Page(site, pagename)
-    if page.exists():
-      if re.search("#redirect", unicode(page.text), re.I):
-        msg("Page %s [[%s]]: exists as redirect" % (i, pagename))
-      elif re.search(r"\{\{superlative of", unicode(page.text)):
-        msg("Page %s [[%s]]: exists as superlative" % (i, pagename))
-      else:
-        msg("Page %s [[%s]]: exists as non-lemma" % (i, pagename))
+    m = re.search(u"[^-Ѐ-џҊ-ԧꚀ-ꚗ]", pagename)
+    if m:
+      msg("Page %s [[%s]]: skipped due to non-Cyrillic characters" % (i, pagename))
     else:
-      msg("Page %s [[%s]]: does not exist" % (i, pagename))
+      for pagenm, pagetype in [(pagename, ""),
+          (pagename.capitalize(), " (capitalized)"),
+          (pagename.upper(), " (uppercased)")]:
+        page = pywikibot.Page(site, pagenm)
+        if page.exists():
+          if re.search("#redirect", unicode(page.text), re.I):
+            msg("Page %s [[%s]]: exists%s as redirect" % (i, pagename, pagetype))
+          elif re.search(r"\{\{superlative of", unicode(page.text)):
+            msg("Page %s [[%s]]: exists%s as superlative" % (i, pagename, pagetype))
+          else:
+            msg("Page %s [[%s]]: exists%s as non-lemma" % (i, pagename, pagetype))
+          break
+      else:
+        msg("Page %s [[%s]]: does not exist" % (i, pagename))
