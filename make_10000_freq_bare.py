c6d369cd5c9f4cd1bb0d2d4144f6ac3e5b36f881
Ben Wing <ben@benwing.com>
2016-07-14 23:51:11 -0500

    make_10000_freq_bare.py: Generate bare list of words in conjunction with 10000-entry frequency list

---
+++ make_10000_freq_bare.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re, codecs, argparse
+from blib import msg
+import rulib
+
+parser = argparse.ArgumentParser(description="Make bare and list versions of 10,000-word frequency list from the Internet.")
+parser.add_argument('--file', help="File containing original list.")
+args = parser.parse_args()
+
+for line in codecs.open(args.file, "r", "utf-8"):
+  line = line.strip()
+  line = re.sub(" .*", "", line)
+  line = rulib.remove_accents(line)
+  if "/" in line:
+    els = re.split("/", line)
+    impf = els[0]
+    msg(impf)
+    for pf in els[1:]:
+      if pf.endswith("-"):
+        pf = re.sub("-$", impf, pf)
+      msg(pf)
+  else:
+    msg(line)
