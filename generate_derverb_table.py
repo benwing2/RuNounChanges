2b559d36ed18038d994c628d23b933d7452369df
Ben Wing <ben@benwing.com>
2016-07-15 05:35:27 -0500

    generate_derverb_table.py: Support multiple tables in one file, support comments

--- generate_derverb_table.py
+++ generate_derverb_table.py
@@ -10,22 +10,79 @@ parser = argparse.ArgumentParser(description="Generate derived-verb tables.")
 parser.add_argument('--direcfile', help="File containing directives.")
 args = parser.parse_args()
 
+def render_groups(groups):
+  def is_noequiv(x):
+    return x == "* (no equivalent)"
+  def compare_aspect_pair(xpf, ximpf, ypf, yimpf):
+    if not is_noequiv(xpf) and not is_noequiv(ypf):
+      return cmp(xpf, ypf)
+    elif not is_noequiv(ximpf) and not is_noequiv(yimpf):
+      return cmp(ximpf, yimpf)
+    elif not is_noequiv(xpf) and not is_noequiv(yimpf):
+      return cmp(xpf, yimpf)
+    elif not is_noequiv(ximpf) and not is_noequiv(ypf):
+      return cmp(ximpf, ypf)
+    else:
+      return 0
+  def sort_aspect_pair(x, y):
+    xpf, ximpf = x
+    ypf, yimpf = y
+    # First compare ignoring accents, so that влить goes before вли́ться,
+    # then compare with accents so e.g. рассы́пать and рассыпа́ть are ordered
+    # consistently.
+    retval = compare_aspect_pair(ru.remove_accents(xpf), ru.remove_accents(ximpf),
+      ru.remove_accents(ypf), ru.remove_accents(yimpf))
+    if retval == 0:
+      return compare_aspect_pair(xpf, ximpf, ypf, yimpf)
+    else:
+      return retval
+
+  pfs = []
+  impfs = []
+  for gr in groups:
+    gr = sorted(gr, cmp=sort_aspect_pair)
+    for pf, impf in gr:
+      pfs.append(pf)
+      impfs.append(impf)
+
+  msg("""
+====Derived terms====
+{{top2}}
+''imperfective''
+%s
+{{mid2}}
+''perfective''
+%s
+{{bottom2}}
+""" % ("\n".join(impfs), "\n".join(pfs)))
+
 def combine_prefix(prefix, suffix):
   if ru.is_stressed(prefix):
     return "* {{l|ru|" + prefix + ru.make_unstressed(suffix) + "}}"
   else:
     return "* {{l|ru|" + prefix + suffix + "}}"
 # Each group is delineated by a line containing only a hyphen in the
-# directive file, and consists of a list of (pf, impf) pairs.
+# directive file, and consists of a list of (pf, impf) pairs. Multiple tables
+# are delineated by a line containing two or more hyphens.
 groups = []
 group = []
 pfsuffix = None
 impfsuffix = None
 for line in codecs.open(args.direcfile, "r", "utf-8"):
   line = line.strip()
-  if not line:
-    continue
-  if line == "-":
+  if not line or line.startswith("#"):
+    pass # Skip blank and comment lines
+  elif re.search("^--+$", line):
+    # End of table; other tables may follow
+    if group:
+      groups.append(group)
+    if groups:
+      render_groups(groups)
+    groups = []
+    group = []
+    pfsuffix = None
+    impfsuffix = None
+  elif line == "-":
     if group:
       groups.append(group)
     group = []
@@ -77,50 +134,8 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
               notes and " {{i|%s}}" % ", ".join(notes) or ""))
         return "* " + ", ".join(links)
     group.append((do_line(pf, "pf"), do_line(impf, "impf")))
+
 if group:
   groups.append(group)
-
-def is_noequiv(x):
-  return x == "* (no equivalent)"
-def compare_aspect_pair(xpf, ximpf, ypf, yimpf):
-  if not is_noequiv(xpf) and not is_noequiv(ypf):
-    return cmp(xpf, ypf)
-  elif not is_noequiv(ximpf) and not is_noequiv(yimpf):
-    return cmp(ximpf, yimpf)
-  elif not is_noequiv(xpf) and not is_noequiv(yimpf):
-    return cmp(xpf, yimpf)
-  elif not is_noequiv(ximpf) and not is_noequiv(ypf):
-    return cmp(ximpf, ypf)
-  else:
-    return 0
-def sort_aspect_pair(x, y):
-  xpf, ximpf = x
-  ypf, yimpf = y
-  # First compare ignoring accents, so that влить goes before вли́ться,
-  # then compare with accents so e.g. рассы́пать and рассыпа́ть are ordered
-  # consistently.
-  retval = compare_aspect_pair(ru.remove_accents(xpf), ru.remove_accents(ximpf),
-    ru.remove_accents(ypf), ru.remove_accents(yimpf))
-  if retval == 0:
-    return compare_aspect_pair(xpf, ximpf, ypf, yimpf)
-  else:
-    return retval
-
-pfs = []
-impfs = []
-for gr in groups:
-  gr = sorted(gr, cmp=sort_aspect_pair)
-  for pf, impf in gr:
-    pfs.append(pf)
-    impfs.append(impf)
-
-msg("""
-====Derived terms====
-{{top2}}
-''imperfective''
-%s
-{{mid2}}
-''perfective''
-%s
-{{bottom2}}
-""" % ("\n".join(impfs), "\n".join(pfs)))
+if groups:
+  render_groups(groups)
