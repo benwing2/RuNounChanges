803812a022f27ee7b6c09804abe92672bc5f42da
Ben Wing <ben@benwing.com>
2016-09-05 15:51:48 -0500

    list_pages: Generalize past Russian

--- list_pages.py
+++ list_pages.py
@@ -7,12 +7,11 @@ import blib
 from blib import getparam, rmparam, msg, site
 
 parser = blib.create_argparser(u"List pages, lemmas and/or non-lemmas")
-parser.add_argument('--cats', default="lemmas", help="Categories to do (can be comma-separated list)")
+parser.add_argument('--cats', default="Russian lemmas", help="Categories to do (can be comma-separated list)")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
 for cat in re.split(",", args.cats):
-  cat = "Russian " + cat
   msg("Processing category: %s" % cat)
   for i, page in blib.cat_articles(cat, start, end):
     msg("Page %s %s: Processing" % (i, unicode(page.title())))
