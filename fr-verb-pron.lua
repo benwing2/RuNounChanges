6fbdf7d67f83ecb072af28a0ec61d89d7399696b
Ben Wing <ben@benwing.com>
2016-09-14 04:32:36 -0500

    fr-verb-pron: Latest from Wiktionary

--- fr-verb-pron.lua
+++ fr-verb-pron.lua
@@ -3,40 +3,45 @@ local pron = {}
 -- Combine stem pronunciation and suffix pronunciation. The stem can actually
 -- consist of multiple stems separated by /, in which case the suffix will
 -- be added to each stem separately and the return value will be a list of
--- pronunciations; otherwise the return value will be a single string. The
--- combination procedure mostly just appends the two, but handles changing
--- ".jj" to "j.j" (moving the syllable boundary) and converting ".C[lʁ]j"
--- to ".C[lʁ]i".
-local function add(source,appendix)
+-- pronunciations (unless CONCAT is true, in which case the return value will
+-- be a string with separate pronunciations separated by /); otherwise the
+-- return value will be a single string. The combination procedure mostly
+-- just appends the two, but handles changing ".jj" to "j.j" (moving the
+-- syllable boundary) and converting ".C[lʁ]j" to ".C[lʁ]i".
+local function add(source, appendix, concat)
 	if mw.ustring.match(source,"/") then
 		source = mw.text.split(source,"/",true)
 		for i,val in ipairs(source) do
 			source[i] = mw.ustring.gsub(mw.ustring.gsub(val..appendix, "%.jj", "j.j"),"(%..[lʁ])(j[ɔe]̃?)","%1i.%2")
 		end
+		if concat then
+			source = table.concat(source, "/")
+		end
+		return source
 	else
 		return mw.ustring.gsub(mw.ustring.gsub(source..appendix, "%.jj", "j.j"),"(%..[lʁ])(j[ɔe]̃?)","%1i.%2")
 	end
-	return source
 end
 
 -- Construct the pronunciation of the entire present tense (indicative,
 -- subjunctive, imperative and participle), plus the imperfect indicative,
--- given three stems:
--- STEM = singular stem of present indicative/imperative;
--- STEM2 = pre-vocalic stem (used for pres indic/subj/imper 1p/2p,
---         entire imperfect, and pres part);
--- STEM3 = 3p stem of pres indicative/subjunctive (also used for singular of
---         pres subj).
---
--- STEM and STEM2 are required but STEM3 defaults to STEM.
+-- given up to four stems:
+-- STEM = short pre-final stem (used for pres indic/imper 1s/2s/3s, required);
+-- STEM2 = pre-vocalic stem (used for pres indic/imper 1p/2p,
+--         impf indic 1s/2s/3s/3p, pres part) (required);
+-- STEM3 = long pre-final stem (used for pres indic 3p and pres subj 1s/2s/3s/3p,
+--         defaults to STEM);
+-- STEM4 = pre-/j/ stem (used for 1p/2p of pres subj and impf indic,
+--         defaults to STEM2).
 --
 -- The value of any of these stem arguments can actually consist of multiple
 -- stems separated by a /; see add().
 --
 -- Note that this will not override an already-existing value for the
 -- present participle, but will override all the rest.
-pron["ind_p"] = function(data, stem, stem2, stem3)
+pron["ind_p"] = function(data, stem, stem2, stem3, stem4)
 	stem3 = stem3 or stem
+	stem4 = stem4 or stem2
 	data.prons.ind_p_1s = add(stem,"")
 	data.prons.ind_p_2s = add(stem,"")
 	data.prons.ind_p_3s = add(stem,"")
@@ -44,8 +49,8 @@ pron["ind_p"] = function(data, stem, stem2, stem3)
 	data.prons.ind_p_2p = add(stem2,"e")
 	data.prons.ind_p_3p = add(stem3,"")
 	
-	data = pron["ind_i"](data, stem2)
-	data = pron["sub_p"](data, stem3, stem2)
+	data = pron["ind_i"](data, stem2, stem4)
+	data = pron["sub_p"](data, stem3, stem4)
 	
 	data.prons.imp_p_2s = add(stem,"")
 	data.prons.imp_p_1p = add(stem2,"ɔ̃")
@@ -56,14 +61,16 @@ pron["ind_p"] = function(data, stem, stem2, stem3)
 	return data
 end
 
--- Construct the pronunciation of the imperfect indicative given the stem
--- (which may be multipart, see add()).
-pron["ind_i"] = function(data, stem)
+-- Construct the pronunciation of the imperfect indicative given STEM
+-- (used for the 1s/2s/3s/3p parts) and STEM2 (used for the 1p/2p parts).
+-- STEM2 defaults to STEM, and either or both may be multipart, see add().
+pron["ind_i"] = function(data, stem, stem2)
+	stem2 = stem2 or stem
 	data.prons.ind_i_1s = add(stem,"ɛ")
 	data.prons.ind_i_2s = add(stem,"ɛ")
 	data.prons.ind_i_3s = add(stem,"ɛ")
-	data.prons.ind_i_1p = add(stem,"jɔ̃")
-	data.prons.ind_i_2p = add(stem,"je")
+	data.prons.ind_i_1p = add(stem2,"jɔ̃")
+	data.prons.ind_i_2p = add(stem2,"je")
 	data.prons.ind_i_3p = add(stem,"ɛ")
 	
 	return data
@@ -88,25 +95,10 @@ pron["ind_ps"] = function(data, stem)
 	return data
 end
 
--- Construct the pronunciation of future and conditional for verbs where a
--- schwa needs to be inserted before the endings (e.g. 'montrer'). The stem
--- passed in should not include the schwa and may be multipart (see add()).
-pron["future_with_schwa"] = function(data, stem)
-	data.prons.ind_f_1s = add(stem,"ə.ʁe")
-	data.prons.ind_f_2s = add(stem,"ə.ʁa")
-	data.prons.ind_f_3s = add(stem,"ə.ʁa")
-	data.prons.ind_f_1p = add(stem,"ə.ʁɔ̃")
-	data.prons.ind_f_2p = add(stem,"ə.ʁe")
-	data.prons.ind_f_3p = add(stem,"ə.ʁɔ̃")
-	
-	data = pron["cond_p"](data, mw.ustring.match(stem,"/") and table.concat(add(stem,"ə."),"/") or add(stem,"ə."))
-	
-	return data
-end
-
--- Construct the pronunciation of future and conditional. The stem
--- passed in may be multipart (see add()).
-pron["ind_f"] = function(data, stem)
+-- Construct the pronunciation of future and conditional. STEM is used for
+-- all parts but 1p/2p of the conditional, which uses STEM2 (which defaults to
+-- STEM). Either or both stems may be multipart (see add()).
+pron["ind_f"] = function(data, stem, stem2)
 	data.prons.ind_f_1s = add(stem, "ʁe")
 	data.prons.ind_f_2s = add(stem, "ʁa")
 	data.prons.ind_f_3s = add(stem, "ʁa")
@@ -114,22 +106,30 @@ pron["ind_f"] = function(data, stem)
 	data.prons.ind_f_2p = add(stem, "ʁe")
 	data.prons.ind_f_3p = add(stem, "ʁɔ̃")
 	
-	data = pron["cond_p"](data, stem)
+	data = pron["cond_p"](data, stem, stem2)
 	
 	return data
 end
 
 -- Construct the pronunciation of the conditional. The stem
 -- passed in may be multipart (see add()).
-pron["cond_p"] = function(data, stem)
+pron["cond_p"] = function(data, stem, stem2)
+	stem2 = stem2 or stem
 	data.prons.cond_p_1s = add(stem,"ʁɛ")
 	data.prons.cond_p_2s = add(stem,"ʁɛ")
 	data.prons.cond_p_3s = add(stem,"ʁɛ")
-	data.prons.cond_p_1p = add(stem,"ʁjɔ̃")
-	data.prons.cond_p_2p = add(stem,"ʁje")
+	data.prons.cond_p_1p = add(stem2,"ʁjɔ̃")
+	data.prons.cond_p_2p = add(stem2,"ʁje")
 	data.prons.cond_p_3p = add(stem,"ʁɛ")
 	
 	return data
+end
+
+-- Construct the pronunciation of future and conditional for verbs where a
+-- schwa needs to be inserted before the endings (e.g. 'montrer'). The stem
+-- passed in should not include the schwa and may be multipart (see add()).
+pron["future_with_schwa"] = function(data, stem)
+	return pron["ind_f"](data, add(stem,"ə.", "concat"))
 end
 
 -- Construct the pronunciation of the present subjunctive given two stems
@@ -159,22 +159,27 @@ pron["sub_pa"] = function(data, stem)
 	return data
 end
 
--- Construct the pronunciation of all verb parts for -er verbs given two stems
--- (which may be multipart, see add()). STEM is used for the singular and 3p
--- of the present indicative/subjunctive/imperative. STEM2 is used for all
--- other forms.
-pron["er"] = function(data, stem, stem2)
-	data.prons.ppr = data.prons.ppr or add(stem2,"ɑ̃")
-	data.prons.pp = data.prons.pp or add(stem2,"e")
-	
-	data = pron.ind_p(data, stem, stem2, stem)
-	
-	data = pron.ind_i(data, stem2)
-	
-	data = pron.future_with_schwa(data, stem2)
-	
-	data = pron.ind_ps_a(data, stem2)
-	
+-- Construct the pronunciation of all verb parts for -er verbs given the
+-- following stems (which may be multipart, see add()):
+--   STEM_FINAL is used for those forms without a syllabic suffix (singular
+--     and 3p of the present indicative/subjunctive/imperative);
+--   STEM_NONFINAL is used for the remaining forms, except that:
+--   STEM_NONFINAL_I is used for those forms beginning with /j/ (1p and 2p of
+--     present subjunctive and imperfect indicative), and should not contain
+--     a final /j/;
+--   STEM_FUT is used for the future and conditional (and should not contain
+--     a final /ʁ/), except that:
+--   STEM_FUT_I is used for 1p and 2p of the conditional (and should not
+--     contain a final /ʁj/).
+pron["er"] = function(data, stem_final, stem_nonfinal, stem_nonfinal_i, stem_fut, stem_fut_i)
+	stem_fut = stem_fut or add(stem_nonfinal, "ə.", "concat")
+	data.prons.ppr = data.prons.ppr or add(stem_nonfinal,"ɑ̃")
+	data.prons.pp = data.prons.pp or add(stem_nonfinal,"e")
+
+	data = pron.ind_p(data, stem_final, stem_nonfinal, stem_final, stem_nonfinal_i)
+	data = pron.ind_f(data, stem_fut, stem_fut_i)
+	data = pron.ind_ps_a(data, stem_nonfinal)
+
 	return data
 end
 
@@ -188,7 +193,7 @@ pron["ind_ps_a"] = function(data, stem)
 	data.prons.ind_ps_2p = add(stem,"at")
 	data.prons.ind_ps_3p = add(stem,"ɛʁ")
 	
-	data = pron.sub_pa(data, mw.ustring.match(stem,"/") and table.concat(add(stem,"a"),"/") or add(stem,"a"))
+	data = pron.sub_pa(data, add(stem, "a", "concat"))
 	
 	return data
 end
