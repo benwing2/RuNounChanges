71af277dc9c82ad4368357c617f9caa683fef91f
Ben Wing <ben@benwing.com>
2016-09-18 16:50:28 -0500

    list_pages: Support --refs

--- list_pages.py
+++ list_pages.py
@@ -8,10 +8,17 @@ from blib import getparam, rmparam, msg, site
 
 parser = blib.create_argparser(u"List pages, lemmas and/or non-lemmas")
 parser.add_argument('--cats', default="Russian lemmas", help="Categories to do (can be comma-separated list)")
+parser.add_argument('--refs', help="References to do (can be comma-separated list)")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
-for cat in re.split(",", args.cats):
-  msg("Processing category: %s" % cat)
-  for i, page in blib.cat_articles(cat, start, end):
-    msg("Page %s %s: Processing" % (i, unicode(page.title())))
+if args.refs:
+  for ref in re.split(",", args.refs):
+    msg("Processing references to: %s" % ref)
+    for i, page in blib.references(ref, start, end):
+      msg("Page %s %s: Processing" % (i, unicode(page.title())))
+else:
+  for cat in re.split(",", args.cats):
+    msg("Processing category: %s" % cat)
+    for i, page in blib.cat_articles(cat, start, end):
+      msg("Page %s %s: Processing" % (i, unicode(page.title())))
