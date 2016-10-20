13a843faba4775128555176fda9caf54f3bd8c9c
Ben Wing <ben@benwing.com>
2016-10-19 23:35:03 -0500

    fr_find_no_heads: new script, working

---
+++ fr_find_no_heads.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+#    fr_find_no_heads.py is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Go through all the French terms we can find looking for pages that are
+# missing a headword declaration.
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site
+
+fr_head_templates = ["fr-noun", "fr-proper noun", "fr-proper-noun",
+  "fr-verb", "fr-adj", "fr-adv", "fr-phrase", "fr-adj form", "fr-adj-form",
+  "fr-abbr", "fr-diacritical mark", "fr-intj", "fr-letter",
+  "fr-past participle", "fr-prefix", "fr-prep", "fr-pron",
+  "fr-punctuation mark", "fr-suffix", "fr-verb form", "fr-verb-form"]
+fr_heads_to_warn_about = ["abbreviation", "acronym", "initialism", "idiom",
+    "phrase", "adverb", "adjective", "adjective form", "verb", "noun",
+    "proper noun", "prefix", "suffix", "interjection", "diacritical mark",
+    "letter", "past participle", "preposition", "pronoun", "punctuation mark"]
+
+overall_head_count = {}
+cat_head_count = {}
+
+def output_heads_seen(overall=False):
+  if overall:
+    dic = overall_head_count
+    msg("Overall templates seen:")
+  else:
+    dic = cat_head_count
+    msg("Templates seen per category:")
+  for head, count in sorted(dic.items(), key=lambda x:-x[1]):
+    msg("  %s = %s" % (head, count))
+
+def process_page(index, page, save, verbose):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+
+  parsed = blib.parse(page)
+  found_page_head = False
+  for t in parsed.filter_templates():
+    found_this_head = False
+    tname = unicode(t.name)
+    if tname in fr_head_templates:
+      headname = tname
+      found_this_head = True
+    elif tname == "head" and getparam(t, "1") == "fr":
+      headtype = getparam(t, "2")
+      headname = "head|fr|%s" % headtype
+      if headtype in fr_heads_to_warn_about:
+        pagemsg("WARNING: Found %s" % unicode(t))
+      found_this_head = True
+    if found_this_head:
+      cat_head_count[headname] = cat_head_count.get(headname, 0) + 1
+      overall_head_count[headname] = overall_head_count.get(headname, 0) + 1
+      found_page_head = True
+  if not found_page_head:
+    pagemsg("WARNING: No head")
+  if index % 100 == 0:
+    output_heads_seen()
+
+parser = blib.create_argparser("Find French terms without a proper headword line")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+for category in ["French nouns", "French proper nouns", "French pronouns", "French determiners", "French adjectives", "French verbs", "French participles", "French adverbs", "French prepositions", "French conjunctions", "French interjections", "French idioms", "French phrases", "French abbreviations", "French acronyms", "French initialisms", "French noun forms", "French proper noun forms", "French pronoun forms", "French determiner forms", "French verb forms", "French adjective forms", "French participle forms", "French proverbs", "French prefixes", "French suffixes", "French diacritical marks", "French punctuation marks"]:
+  cat_head_count = {}
+  msg("Processing category: %s" % category)
+  for i, page in blib.cat_articles(category, start, end):
+    process_page(i, page, args.save, args.verbose)
+  output_heads_seen()
+output_heads_seen(overall=True)
