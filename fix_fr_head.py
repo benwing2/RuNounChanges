7113b4aa01b1d471b0f59daf9125567078b4df27
Ben Wing <ben@benwing.com>
2016-10-23 18:26:38 -0500

    fix_fr_head: Fix bugs, support various head types

--- fix_fr_head.py
+++ fix_fr_head.py
@@ -66,11 +66,18 @@ def process_page(index, page, save, verbose):
           t.add("g2", g2)
         if plural:
           t.add("2", plural)
-      elif headtype == "adjective form":
+      elif headtype in ["proper noun", "proper nouns"]:
+        head = getparam(t, "head")
+        g = getparam(t, "g")
+        g2 = getparam(t, "g2")
+        remove_3 = False
+        if not g and getparam(t, "3") in ["m", "f", "m-p", "f-p"]:
+          g = getparam(t, "3")
+          remove_3 = True
         unrecognized_params = False
         for param in t.params:
           pname = unicode(param.name)
-          if pname in ["1", "2"]:
+          if pname in ["1", "2", "head", "g", "g2", "sort"] or remove_3 and pname in ["3"]:
             pass
           else:
             unrecognized_params = True
@@ -79,9 +86,74 @@ def process_page(index, page, save, verbose):
           pagemsg("WARNING: Unrecognized parameters in %s, skipping"
               % unicode(t))
           continue
+        if not g:
+          pagemsg("WARNING: No gender given in %s, skipping" % unicode(t))
+          continue
+        rmparam(t, "3")
         rmparam(t, "2")
         rmparam(t, "1")
-        t.name = "fr-adj form"
+        rmparam(t, "head")
+        rmparam(t, "g")
+        rmparam(t, "g2")
+        rmparam(t, "sort")
+        t.name = "fr-proper noun"
+        if head:
+          t.add("head", head)
+        t.add("1", g)
+        if g2:
+          t.add("g2", g2)
+      elif headtype in ["adjective", "adjectives"]:
+        if getparam(t, "3") in ["invariable", "invariant"]:
+          params = dict((unicode(p.name), unicode(p.value)) for p in t.params)
+          del params["1"]
+          del params["2"]
+          del params["3"]
+          if getparam(t, "g") == "m" and getparam(t, "g2") == "f":
+            del params["g"]
+            del params["g2"]
+          if not params:
+            rmparam(t, "g2")
+            rmparam(t, "g")
+            rmparam(t, "3")
+            rmparam(t, "2")
+            rmparam(t, "1")
+            t.name = "fr-adj"
+            t.add("inv", "y")
+          else:
+            pagemsg("WARNING: Unrecognized parameters in %s, skipping" %
+                unicode(t))
+        else:
+          pagemsg("WARNING: Unrecognized parameters in %s, skipping" %
+              unicode(t))
+      elif headtype in ["adjective form", "verb form", "verb forms",
+          "interjection", "preposition", "prefix", "prefixes",
+          "suffix", "suffixes", "proverb", "proverbs"]:
+        head = getparam(t, "head")
+        unrecognized_params = False
+        for param in t.params:
+          pname = unicode(param.name)
+          if pname in ["1", "2", "head", "sort"]:
+            pass
+          else:
+            unrecognized_params = True
+            break
+        if unrecognized_params:
+          pagemsg("WARNING: Unrecognized parameters in %s, skipping"
+              % unicode(t))
+          continue
+        rmparam(t, "sort")
+        rmparam(t, "head")
+        rmparam(t, "2")
+        rmparam(t, "1")
+        t.name = ("fr-adj-form" if headtype == "adjective form" else
+            "fr-verb-form" if headtype in ["verb form", "verb forms"] else
+            "fr-intj" if headtype == "interjection" else
+            "fr-prep" if headtype == "preposition" else
+            "fr-prefix" if headtype in ["prefix", "prefixes"] else
+            "fr-suffix" if headtype in ["suffix", "suffixes"] else
+            "fr-proverb")
+        if head:
+          t.add("head", head)
 
       newt = unicode(t)
       if origt != newt:
@@ -100,11 +172,11 @@ def process_page(index, page, save, verbose):
     else:
       pagemsg("Would save with comment = %s" % comment)
 
-parser = blib.create_argparser("Convert head|fr|* fr-*")
+parser = blib.create_argparser("Convert head|fr|* to fr-*")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
-for cat in ["French nouns"]:
+for cat in ["French nouns", "French proper nouns", "French pronouns", "French determiners", "French adjectives", "French verbs", "French participles", "French adverbs", "French prepositions", "French conjunctions", "French interjections", "French idioms", "French phrases", "French abbreviations", "French acronyms", "French initialisms", "French noun forms", "French proper noun forms", "French pronoun forms", "French determiner forms", "French verb forms", "French adjective forms", "French participle forms", "French proverbs", "French prefixes", "French suffixes", "French diacritical marks", "French punctuation marks"]:
   msg("Processing category: %s" % cat)
   for i, page in blib.cat_articles(cat, start, end):
     process_page(i, page, args.save, args.verbose)
