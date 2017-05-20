265fe4c86d1ea0a71fd3d9e97a7e3fa9eb00424a
Ben Wing <ben@benwing.com>
2017-05-20 18:09:26 -0500

    find_no_decl: Handle invariant terms properly, check all nouns instead of just multi-word nouns

--- find_no_decl.py
+++ find_no_decl.py
@@ -17,21 +17,35 @@ def process_page(index, page):
   parsed = blib.parse(page)
 
   found_headword_template = False
+  headword_templates = []
+  found_invariant_headword_template = False
   found_decl_template = False
   for t in parsed.filter_templates():
     if unicode(t.name) in ["ru-noun", "ru-proper noun"]:
       found_headword_template = True
+      if getparam(t, "3") == "-":
+        found_invariant_headword_template = True
+      else:
+        headword_templates.append(unicode(t))
     if unicode(t.name) in ["ru-noun-table", "ru-decl-noun-see"]:
       found_decl_template = True
-  if found_headword_template and not found_decl_template:
-    pagemsg("Found headword template without decl")
+  if found_headword_template and not found_invariant_headword_template:
+    if found_decl_template:
+      pagemsg("Found old-style headword template(s) %s with decl" % ", ".join(headword_templates))
+    else:
+      pagemsg("Found old-style headword template(s) %s without decl" % ", ".join(headword_templates))
 
 parser = blib.create_argparser("Find nouns without declension")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
-for pos in ["nouns", "proper nouns"]:
-  tracking_page = "Template:tracking/ru-headword/space-in-headword/" + pos
-  msg("Processing references to %s" % tracking_page)
-  for index, page in blib.references(tracking_page, start, end):
+#for pos in ["nouns", "proper nouns"]:
+#  Do multi-word nouns
+#  tracking_page = "Template:tracking/ru-headword/space-in-headword/" + pos
+#  msg("Processing references to %s" % tracking_page)
+#  for index, page in blib.references(tracking_page, start, end):
+#    process_page(index, page)
+#  Do all nouns with {{ru-noun}} or {{ru-proper noun}}
+for template in ["ru-noun", "ru-proper noun"]:
+  for index, page in blib.references("Template:%s" % template, start, end):
     process_page(index, page)
