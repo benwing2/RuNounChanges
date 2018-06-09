1d83c31b7a21592170ff99b10811399dab69e6d2
Ben Wing <ben@benwing.com>
2018-06-09 02:19:14 -0500

    find_ru_of_or_related.py: New script

---
+++ find_ru_of_or_related.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+#    find_ru_of_or_related.py is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Find pages that need definitions among a set list (e.g. most frequent words).
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site
+
+def process_page(index, page, save, verbose):
+  pagetitle = unicode(page.title())
+  subpagetitle = re.sub("^.*:", "", pagetitle)
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+
+  if ":" in pagetitle:
+    pagemsg("WARNING: Colon in page title, skipping page")
+    return
+
+  text = unicode(page.text)
+  notes = []
+
+  foundrussian = False
+  sections = re.split("(^==[^=]*==\n)", text, 0, re.M)
+
+  for j in xrange(2, len(sections), 2):
+    if sections[j-1] == "==Russian==\n":
+      if foundrussian:
+        pagemsg("WARNING: Found multiple Russian sections, skipping page")
+        return
+      foundrussian = True
+
+      if re.search("[Oo]f or related", sections[j]):
+        pagemsg("Found likely of-or-related")
+
+parser = blib.create_argparser(u"Find pages that need definitions")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+for category in ["Russian adjectives"]:
+  for i, page in blib.cat_articles(category, start, end):
+    process_page(i, page, args.save, args.verbose)
