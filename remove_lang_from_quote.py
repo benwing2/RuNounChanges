bb96c809c29e2a5906481941f528d0a10be528b3
Ben Wing <ben@benwing.com>
2019-02-03 15:09:08 -0600

    remove_lang_from_quote: New script

---
+++ remove_lang_from_quote.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site
+
+import rulib as ru
+
+def process_page(index, page, save, verbose):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+
+  text = unicode(page.text)
+  parsed = blib.parse(page)
+  notes = []
+  for t in parsed.filter_templates():
+    origt = unicode(t)
+    if unicode(t.name) in ["quote-book", "quote-hansard", "quote-journal",
+          "quote-newsgroup", "quote-song", "quote-us-patent", "quote-video",
+          "quote-web", "quote-wikipedia"] and getparam(t, "lang") == "ru":
+      passage = getparam(t, "passage")
+      m = re.search(r"^\{\{lang\|ru\|(.*)\}\}$", passage)
+      if m:
+        t.add("passage", m.group(1))
+        notes.append("remove {{lang|ru|...}} from passage= in quote-*")
+    newt = unicode(t)
+    if origt != newt:
+      pagemsg("Replaced %s with %s" % (origt, newt))
+
+  new_text = unicode(parsed)
+
+  if new_text != text:
+    if verbose:
+      pagemsg("Replacing <%s> with <%s>" % (text, new_text))
+    assert notes
+    comment = "; ".join(notes)
+    if save:
+      pagemsg("Saving with comment = %s" % comment)
+      page.text = new_text
+      page.save(comment=comment)
+    else:
+      pagemsg("Would save with comment = %s" % comment)
+
+parser = blib.create_argparser(u"Remove {{lang|ru|...}} from passage= in quote-*")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+for i, page in blib.cat_articles("Russian_terms_with_quotations", start, end):
+  process_page(i, page, args.save, args.verbose)
