b5bb05559020aa6a557e61c1566b58aa6b084a61
Ben Wing <ben@benwing.com>
2019-03-27 01:26:33 -0500

    form-of-doc*.lua: Near-final versions

--- form-of-doc-templates.lua
+++ form-of-doc-templates.lua
@@ -1,17 +1,84 @@
 local export = {}
 
-function export.paramdoc_t(frame)
-	local params = {
-		["sgdescwithart"] = {},
+local m_parameters = require("Module:parameters")
+local m_form_of_doc = require("Module:form of doc")
+
+local function create_introdoc_params()
+	return {
+		["lang"] = {},
+		["exlang"] = {list = true},
+		["pldesc"] = {},
+		["primaryentrytext"] = {},
+		["cat"] = {list = true},
+		["addlintrotext"] = {},
+		["withdot"] = {type = "boolean"},
+		["withcap"] = {type = "boolean"},
+	}
+end
+
+function export.introdoc_t(frame)
+	local params = create_introdoc_params()
+	local args = m_parameters.process(frame:getParent().args, params)
+	return m_form_of_doc.introdoc(args)
+end
+
+local function create_paramdoc_params()
+	return {
+		["lang"] = {},
+		["sgdescof"] = {},
 		["art"] = {},
 		["withfrom"] = {type = "boolean"},
 		["withdot"] = {type = "boolean"},
 		["withcap"] = {type = "boolean"},
 	}
-	
-	local args = require("Module:parameters").process(frame:getParent().args, params)
+end
 
-	return require("Module:form of doc").paramdoc(args)
+function export.paramdoc_t(frame)
+	local params = create_paramdoc_params()
+	local args = m_parameters.process(frame:getParent().args, params)
+	return m_form_of_doc.paramdoc(args)
+end
+
+local function create_usagedoc_params()
+	local params = create_paramdoc_params()
+	params["exlang"] = {list = true}
+	return params
+end
+
+function export.usagedoc_t(frame)
+	local params = create_usagedoc_params()
+	local args = m_parameters.process(frame:getParent().args, params)
+	return m_form_of_doc.usagedoc(args)
+end
+
+local function create_fulldoc_params()
+	local params = create_introdoc_params()
+	local usageparams = create_usagedoc_params()
+	for k, v in pairs(usageparams) do
+		params[k] = v
+	end
+	params["shortcut"] = {list = true}
+	return params
+end
+
+function export.fulldoc_t(frame)
+	local params = create_fulldoc_params()
+	local args = m_parameters.process(frame:getParent().args, params)
+	return m_form_of_doc.fulldoc(args)
+end
+
+local function create_infldoc_params()
+	local params = create_fulldoc_params()
+	params["pldesc"] = nil
+	params["sgdesc"] = {}
+	params["form"] = {}
+	return params
+end
+
+function export.infldoc_t(frame)
+	local params = create_infldoc_params()
+	local args = m_parameters.process(frame:getParent().args, params)
+	return m_form_of_doc.infldoc(args)
 end
 
 return export
--- form-of-doc.lua
+++ form-of-doc.lua
@@ -2,6 +2,8 @@ local export = {}
 
 local m_template_link = require("Module:template link")
 local m_languages = require("Module:languages")
+local m_table = require("Module:table")
+local strutils = require("Module:string utilities")
 
 local usub = mw.ustring.sub
 local uupper = mw.ustring.upper
@@ -28,63 +30,58 @@ local function template_name()
 	return rsub(PAGENAME, "/documentation$", "")
 end
 
-local function join_list_with_comma_plus_and(els)
-	if #els == 1 then
-		return els[1]
-	elseif #els == 2 then
-		return els[1] .. " and " .. els[2]
-	else
-		local retval = {}
-		for i, el in ipairs(els) do
-			table.insert(retval, el)
-			if i <= #els - 2 then
-				table.insert(retval, ", ")
-			elseif i == #els - 1 then
-				table.insert(retval, "<span class=\"serial-comma\">,</span><span class=\"serial-and\"> and</span> ")
-			end
-		end
-		return table.concat(retval, "")
-	end
-end
-
 function export.introdoc(args)
 	local langname = args.lang and lang_name(args.lang, "lang")
-	local exlangname = args.exlang and lang_name(args.exlang, "exlang")
-	parts = {}
-	parts.append("This template creates a definition line for ")
-	parts.append(args.pldesc or rsub(template_name(), " of$", "") .. "s")
-	parts.append(" ")
-	parts.append(args.primaryentrytext or "of primary entries")
-	if args.lang then
-		parts.append(" in " .. langname)
-	elseif args.exlang then
-		parts.append(", e.g. in " .. exlangname)
+	local exlangnames = {}
+	for _, exlang in ipairs(args.exlang) do
+		table.insert(exlangnames, lang_name(args.exlang, "exlang"))
 	end
-	parts.append(".")
+	parts = {}
+	table.insert(parts, "This template creates a definition line for ")
+	table.insert(parts, args.pldesc or rsub(template_name(), " of$", "") .. "s")
+	table.insert(parts, " ")
+	table.insert(parts, args.primaryentrytext or "of primary entries")
+	if args.lang then
+		table.insert(parts, " in " .. langname)
+	elseif #args.exlang > 0 then
+		table.insert(parts, ", e.g. in " .. m_table.serialCommaJoin(exlangnames, {conj = "or"}))
+	end
+	table.insert(parts, ".")
 	if #args.cat > 0 then
-		parts.append(" It also categorizes the page into ")
+		table.insert(parts, " It also categorizes the page into ")
 		local catparts = {}
 		for _, cat in ipairs(args.cat) do
 			if args.lang then
-				catparts.append("[[:Category:" .. langname .. " " .. cat .. "]]")
+				table.insert(catparts, "[[:Category:" .. langname .. " " .. cat .. "]]")
 			else
-				catparts.append("the proper language-specific subcategory of [[:Category:" .. ucfirst(cat) .. " by language]] (e.g. [[:Category:" .. (exlangname or "English") .. " " .. cat .. "]])")
+				table.insert(catparts, "the proper language-specific subcategory of [[:Category:" .. ucfirst(cat) .. " by language]] (e.g. [[:Category:" .. (exlangnames[1] or "English") .. " " .. cat .. "]])")
 			end
 		end
-		parts.append(join_list_with_comma_plus_and(catparts))
-		parts.append(".")
+		table.insert(parts, m_table.serialCommaJoin(catparts))
+		table.insert(parts, ".")
 	end
 	if args.addlintrotext then
-		parts.append(" ")
-		parts.append(args.addlintrotext)
+		table.insert(parts, " ")
+		table.insert(parts, args.addlintrotext)
 	end
-	parts.append("\n")
-	parts.append([===[
+	table.insert(parts, "\n")
+	if args.withcap and args.withdot then
+		table.insert(parts, [===[
+
+By default, this template displays its output as a full sentence, with an initial capital letter and a trailing period (full stop). This can be overridden using <code>|nocap=1</code> and/or <code>|nodot=1</code> (see below).
+]===])
+	elseif args.withcap then
+		table.insert(parts, [===[
+
+By default, this template displays its output with an initial capital letter. This can be overridden using <code>|nocap=1</code> (see below).
+]===])
+	end
+	table.insert(parts, [===[
 
 This template is '''not''' meant to be used in etymology sections.
 
 Note that users can customize how the output of this template displays by modifying their monobook.css files. See [[:Category:Form-of templates|“Form of” templates]] for details.
-]===]
+]===])
 	return table.concat(parts)
 end
 
@@ -95,15 +92,15 @@ local function param(params, list, required)
 	end
 	for _, p in ipairs(params) do
 		local listparts = {}
-		listparts.append("<code>|" .. p .. "=</code>")
+		table.insert(listparts, "<code>|" .. p .. "=</code>")
 		if list then
-			listparts.append(", <code>|" .. p .. "2=</code>")
-			listparts.append(", <code>|" .. p .. "3=</code>")
-			listparts.append(", etc.")
+			table.insert(listparts, ", <code>|" .. p .. "2=</code>")
+			table.insert(listparts, ", <code>|" .. p .. "3=</code>")
+			table.insert(listparts, ", etc.")
 		end
-		paramparts.append(table.concat(listparts))
+		table.insert(paramparts, table.concat(listparts))
 	end
-	local reqtext = required and "'''required'''" or "''(optional)''"
+	local reqtext = required and "'''(required)'''" or "''(optional)''"
 	return table.concat(paramparts, " or ") .. " " .. reqtext
 end
 
@@ -111,22 +108,27 @@ function export.paramdoc(args)
 	local parts = {}
 	
 	local function param_and_doc(params, list, required, doc)
-		parts.append(": ")
-		parts.append(param(params, list, required))
-		parts.append("\n")
-		parts.append("; ")
-		parts.append(doc)
-		parts.append("\n")
+		table.insert(parts, "; ")
+		table.insert(parts, param(params, list, required))
+		table.insert(parts, "\n")
+		table.insert(parts, ": ")
+		table.insert(parts, doc)
+		table.insert(parts, "\n")
 	end
 
-	local art = args.art or rfind(PAGENAME, "^[aeiouAEIOU]") and "an" or "a"
-	local sgdescwithart = args.sgdescwithart or art .. " " .. template_name()
-	parts.append("''Positional (unnamed) parameters:''\n")
-	param_and_doc("1", false, true, "The language code of the term linked to (which this page is " .. sgdescwithart .. "). See [[Wiktionary:Languages]].")
-	param_and_doc("2", false, true, "The term to link to (which this page is " .. sgdescwithart .. "). This should include diacritics as appropriate to the language (e.g. accents in Russian to mark the stress, vowel diacritics in Arabic, macrons in Latin to indicate vowel length, etc.). These diacritics will automatically be stripped out in a language-specific fashion in order to create the link to the page.")
-	param_and_doc("3", false, false, "The text to be shown in the link to the term. If empty or omitted, the term specified by the second parameter will be used. This parameter is normally not necessary, and should not be used solely to indicate diacritics; instead, put the diacritics in the second parameter.")
-	parts.append("''Named parameters:''")
-	param_and_doc({"t", "4"}, false, false, "A gloss or short translation of the term linked to.")
+	local tempname = template_name()
+	local art = args.art or rfind(tempname, "^[aeiouAEIOU]") and "an" or "a"
+	local sgdescof = args.sgdescof or art .. " " .. tempname
+	table.insert(parts, "''Positional (unnamed) parameters:''\n")
+	if args.lang then
+		param_and_doc("1", false, true, "The term to link to (which this page is " .. sgdescof .. "). This should include any needed diacritics as appropriate to " .. lang_name(args.lang, "lang") .. ". These diacritics will automatically be stripped out in the appropriate fashion in order to create the link to the page.")
+	else
+		param_and_doc("1", false, true, "The [[WT:LANGCODE|language code]] of the term linked to (which this page is " .. sgdescof .. "). See [[Wiktionary:Languages]]. <small>The parameter <code>|lang=</code> is a deprecated synonym; please do not use. If this is used, all numbered parameters move down by one.</small>")
+		param_and_doc("2", false, true, "The term to link to (which this page is " .. sgdescof .. "). This should include diacritics as appropriate to the language (e.g. accents in Russian to mark the stress, vowel diacritics in Arabic, macrons in Latin to indicate vowel length, etc.). These diacritics will automatically be stripped out in a language-specific fashion in order to create the link to the page.")
+	end
+	param_and_doc(args.lang and "2" or "3", false, false, "The text to be shown in the link to the term. If empty or omitted, the term specified by the second parameter will be used. This parameter is normally not necessary, and should not be used solely to indicate diacritics; instead, put the diacritics in the second parameter.")
+	table.insert(parts, "''Named parameters:''\n")
+	param_and_doc({"t", args.lang and "3" or "4"}, false, false, "A gloss or short translation of the term linked to. <small>The parameter <code>|gloss=</code> is a deprecated synonym; please do not use.</small>")
 	param_and_doc("tr", false, false, "Transliteration for non-Latin-script terms, if different from the automatically-generated one.")
 	param_and_doc("ts", false, false, "Transcription for non-Latin-script terms whose transliteration is markedly different from the actual pronunciation. Should not be used for IPA pronunciations.")
 	param_and_doc("sc", false, false, "Script code to use, if script detection does not work. See [[Wiktionary:Scripts]].")
@@ -146,39 +148,47 @@ end
 
 function export.usagedoc(args)
 	local exlangs = {}
-	if args.exlang then
-		table.insert(exlangs, args.exlang)
+	for _, exlang in ipairs(args.exlang) do
+		table.insert(exlangs, exlang)
 	end
-	ut.insert_if_not(exlangs, 'en')
-	ut.insert_if_not(exlangs, 'de')
-	ut.insert_if_not(exlangs, 'ja')
+	table.insert(exlangs, 'en')
+	table.insert(exlangs, 'de')
+	table.insert(exlangs, 'ja')
+	exlangs = m_table.removeDuplicates(exlangs)
 	local sub = {}
 	local langparts = {}
 	for i, langcode in ipairs(exlangs) do
 		table.insert(langparts, '<code>' .. langcode .. '</code> for ' .. lang_name(langcode, "exlang"))
 	end
-	sub.exlangs = join_list_with_comma_plus_and(langparts)
+	sub.exlangs = m_table.serialCommaJoin(langparts, {conj = "or"})
 	sub.tempname = template_name()
 	
-	return [===[
+	return strutils.format([===[
 ==Usage==
 Use in the definition line, most commonly as follows:
- <nowiki># {\op}{\op}</nowiki>{tempname}|<var><langcode></var>|<var><primary entry goes here></var>{\cl}{\cl}
+ # {\op}{\op}{tempname}|<var><langcode></var>|<var><primary entry goes here></var>{\cl}{\cl}
 where <code><var><langcode></var></code> is the [[Wiktionary:Languages|language code]], e.g. {exlangs}.
 
 ===Parameters===
-]===]] .. export.paramdoc(args)
+]===], sub) .. export.paramdoc(args)
 end
 
 function export.fulldoc(args)
 	local docsubpage = mw.getCurrentFrame():expandTemplate{title="documentation subpage", args={}}
-	local shortcuts = #args.shortcuts > 0 and require("Module:shortcut box").show(args.shortcuts) or ""
+	local shortcuts = #args.shortcut > 0 and require("Module:shortcut box").show(args.shortcut) or ""
 	local introdoc = export.introdoc(args)
 	local usagedoc = export.usagedoc(args)
-	return docsubpage .. "\n" .. shortcuts .. introdoc .. "\n\n" .. usagedoc
+	return docsubpage .. "\n" .. shortcuts .. introdoc .. "\n" .. usagedoc
 end
 
 function export.infldoc(args)
-
+	args = require("Module:table").shallowcopy(args)
+	args.sgdesc = args.sgdesc or (args.art or "the") .. " " ..
+		rsub(template_name(), " of$", "") .. (args.form and " " .. args.form or "")
+	args.pldesc = args.sgdesc
+	args.sgdescof = args.sgdescof or args.sgdesc .. " of"
+	args.primaryentrytext = args.primaryentrytext or "of a primary entry"
+	return export.fulldoc(args)	
+end
 
 return export
