84251cf51a4a3eca5da53e4e89f36791cebf9dd5
Ben Wing <ben@benwing.com>
2019-03-28 15:41:51 -0500

    rewrite_form_of_templates: New script (not tested)

---
+++ rewrite_form_of_templates.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site, tname
+
+from form_of_templates import (
+  language_specific_alt_form_of_templates,
+  alt_form_of_templates,
+  language_specific_form_of_templates,
+  form_of_templates
+)
+
+templates_to_process = form_of_templates + alt_form_of_templates + (
+  language_specific_form_of_templates + language_specific_alt_form_of_templates
+)
+
+def process_page(page, index, parsed):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+  notes = []
+
+  for t in parsed.filter_templates():
+    origt = unicode(t)
+    tn = tname(t)
+    if tn == "#invoke:form of" and getparam(t, "1") in ["form_of_t", "alt_form_of_t"]:
+      ignorelist = blib.fetch_param_chain(t, "ignorelist")
+      if ignorelist:
+        ignore = blib.fetch_param_chain(t, "ignore")
+        for il in ignorelist:
+          ignore.append(il + ":list")
+        blib.set_param_chain(t, ignore, "ignore", "ignore", before="ignorelist")
+        blib.remove_param_chain(t, "ignorelist", "ignorelist")
+      blib.set_template_name(t, "#invoke:form of/templates")
+    if tn == "#invoke:form of" and getparam(t, "1") == "alt_form_of_t":
+      t.add("2", getparam(t, "text"), before="text")
+      rmparam(t, "text")
+      if t.has("nocap"):
+        rmparam(t, "nocap")
+      else:
+        t.add("withcap", "1")
+      if t.has("nodot"):
+        rmparam(t, "nodot")
+      else:
+        t.add("withdot", "1")
+      t.add("1", "form_of_t")
+
+    if unicode(t) != origt:
+      pagemsg("Replaced <%s> with <%s>" % (origt, unicode(t)))
+
+  return unicode(parsed), notes
+
+parser = blib.create_argparser("Convert form_of_t and alt_form_of_t invocations in [[Module:form of]] to form_of_t in [[Module:form of/templates]]")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+for template in blib.iter_items(templates_to_process, start, end):
+  page = pywikibot.Page(site, "Template:%s" % template)
+  blib.do_edit(page, i, process_page, save=args.save, verbose=args.verbose)
