af120e7e2795925b6797a415f179c581a9050639
Ben Wing <ben@benwing.com>
2019-04-05 18:48:56 -0500

    make_form_of_table: Support cat=

--- make_form_of_table.py
+++ make_form_of_table.py
@@ -9,7 +9,7 @@ parser.add_argument('--direcfile', help="File containing directives.")
 args = parser.parse_args()
 
 msg('{|class="wikitable"')
-msg("! Template !! Initial capital !! Final period !! Supports from=, from2=, ... || Supports POS=")
+msg("! Template !! Category !! Initial capital !! Final period !! Supports from=, from2=, ... || Supports POS=")
 for line in codecs.open(args.direcfile, "r", "utf-8"):
   direcs = line.rstrip('\n').split(',')
   template = direcs[0]
@@ -17,6 +17,7 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
   withdot = False
   withfrom = False
   withPOS = False
+  cat = None
   for direc in direcs[1:]:
     if direc == "cap":
       withcap = True
@@ -26,11 +27,14 @@ for line in codecs.open(args.direcfile, "r", "utf-8"):
       withfrom = True
     elif direc == "POS":
       withPOS = True
+    elif direc.startswith("cat="):
+      cat = re.sub("^cat=", "", direc)
     else:
       assert False, "Unrecognized directive %s" % direc
   def dobool(val):
     return val and "'''yes'''" or "no"
   msg("|-")
-  msg("| %-50s || %-9s || %-9s || %-9s || %-9s" % ("[[%s]]" % template, dobool(withcap),
-    dobool(withdot), dobool(withfrom), dobool(withPOS)))
+  msg("| %-50s || %-50s || %-9s || %-9s || %-9s || %-9s" % ("[[%s]]" % template,
+    "<code><nowiki>LANG %s</nowiki></code>" % cat if cat else "",
+    dobool(withcap), dobool(withdot), dobool(withfrom), dobool(withPOS)))
 msg("|}")
