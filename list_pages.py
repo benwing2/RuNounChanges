4a0011fd7a6c8803cbd58a5bed652f5fbdd882c3
Ben Wing <ben@benwing.com>
2019-04-05 18:48:30 -0500

    list_pages: Support --namespace

--- list_pages.py
+++ list_pages.py
@@ -7,8 +7,9 @@ import blib
 from blib import getparam, rmparam, msg, site
 
 parser = blib.create_argparser(u"List pages, lemmas and/or non-lemmas")
-parser.add_argument('--cats', default="Russian lemmas", help="Categories to do (can be comma-separated list)")
+parser.add_argument('--cats', help="Categories to do (can be comma-separated list)")
 parser.add_argument('--refs', help="References to do (can be comma-separated list)")
+parser.add_argument('--namespace', help="List all pages in namespace")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
@@ -17,8 +18,16 @@ if args.refs:
     msg("Processing references to: %s" % ref)
     for i, page in blib.references(ref, start, end):
       msg("Page %s %s: Processing" % (i, unicode(page.title())))
-else:
+elif args.cats:
   for cat in re.split(",", args.cats):
     msg("Processing category: %s" % cat)
     for i, page in blib.cat_articles(cat, start, end):
+      msg("Page %s %s: Processing" % (i, unicode(page.title())))
+elif args.namespace:
+  ns = args.namespace
+  if re.search('^[0-9]+$', ns):
+    ns = int(ns)
+  for i, page in blib.iter_items(site.allpages(
+    start=start if isinstance(start, basestring) else '!', namespace=ns,
+    filterredir=False), start, end):
       msg("Page %s %s: Processing" % (i, unicode(page.title())))
