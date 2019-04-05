2f653dc565dcf98cfaa78b9fe089b0a12359d222
Ben Wing <ben@benwing.com>
2019-04-05 18:49:35 -0500

    find_template_refs: Support --table-of-uses

--- find_template_refs.py
+++ find_template_refs.py
@@ -4,7 +4,7 @@
 import pywikibot, re, sys, codecs, argparse
 
 import blib
-from blib import getparam, rmparam, msg, site, tname
+from blib import getparam, rmparam, msg, errmsg, site, tname
 
 def process_page(page, index):
   pagetitle = unicode(page.title())
@@ -18,9 +18,11 @@ parser = blib.create_argparser("Find templates transcluding a given page")
 parser.add_argument("--pages",
     help=u"""Comma-separated list of pages to check.""")
 parser.add_argument("--pagefile",
-    help=u"""Comma-separated list of pages to check.""")
+    help=u"""File containing pages to check.""")
 parser.add_argument("--redirects-only",
     help=u"""Only output redirects.""", action='store_true')
+parser.add_argument("--table-of-uses", action='store_true',
+    help=u"""Output in table_of_uses.py input format.""")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
@@ -28,7 +30,15 @@ if args.pages:
   pages = args.pages.split(",")
 else:
   pages = [x.strip() for x in codecs.open(args.pagefile, "r", "utf-8")]
-  for page in pages:
-    msg("Processing references to %s" % page)
-    for i, page in blib.references(page, start, end, namespaces=["Template"], only_template_inclusion=False, filter_redirects=args.redirects_only):
-      process_page(page, i)
+  for pagename in pages:
+    errmsg("Processing references to %s" % pagename)
+    if not args.table_of_uses:
+      msg("Processing references to %s" % pagename)
+    aliases = []
+    for i, page in blib.references(pagename, start, end, namespaces=["Template"], only_template_inclusion=False, filter_redirects=args.redirects_only):
+      aliases.append(unicode(page.title()))
+      if not args.table_of_uses:
+        process_page(page, i)
+    if args.table_of_uses:
+      msg("%s%s" % (pagename.replace("Template:", ""),
+        aliases and "," + ",".join(x.replace("Template:", "") for x in aliases) or ""))
