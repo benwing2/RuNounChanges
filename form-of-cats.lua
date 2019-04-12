d401d9ac4560730302c718ab9c3a51bd0f1f3c23
Ben Wing <ben@benwing.com>
2019-04-11 22:23:05 -0500

    form-of-cats.lua: Add liv

--- form-of-cats.lua
+++ form-of-cats.lua
@@ -1,12 +1,41 @@
 local cats = {}
 
+cats["liv"] = {
+	{"hasany", {"1", "2", "3"},
+		{"cond",
+			{"hasall", {"pres", "ind"}, "verb forms (present indicative)"},
+			{"hasall", {"past", "ind"}, "verb forms (past indicative)"},
+			{"hasall", {"imp", "neg"}, "verb forms (imperative negative)"},
+			{"has", "imp", "verb forms (imperative)"},
+			{"has", "neg", "verb forms (negative)"},
+			{"has", "cond", "verb forms (conditional)"},
+			{"has", "juss", "verb forms (jussive)"},
+			{"has", "quot", "verb forms (quotative)"},
+		}
+	},
+	{"has", "part",
+		{"cond",
+			{"hasall", {"pres", "act"}, "present active participles"},
+			{"hasall", {"pres", "pass"}, "present passive participles"},
+			{"hasall", {"past", "act"}, "past active participles"},
+			{"hasall", {"past", "pass"}, "past passive participles"},
+		},
+	},
+	{"cond",
+		{"has", "ger", "gerunds"},
+		{"hasall", {"sup", "abe"}, "supine abessives"},
+		{"has", "sup", "supines"},
+		{"has", "deb", "debitives"},
+	},
+}
+
 cats["lv"] = {
 	{"has", "neg", "negative verb forms"},
 }
 
 cats["sl"] = {
-	{"has", "participle", "participles"},
-	{"hasany", {"supine", "gerund"}, "verbal nouns"},
+	{"has", "part", "participles"},
+	{"hasany", {"sup", "ger"}, "verbal nouns"},
 }
 
 return cats
