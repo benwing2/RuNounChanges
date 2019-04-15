a5df61f562db669db762a84c809f5aeb8cacbb42
Ben Wing <ben@benwing.com>
2019-04-14 21:52:40 -0500

    list_pages: Support --recursive for listing recursively in subcategories

--- list_pages.py
+++ list_pages.py
@@ -8,6 +8,8 @@ from blib import getparam, rmparam, msg, site
 
 parser = blib.create_argparser(u"List pages, lemmas and/or non-lemmas")
 parser.add_argument('--cats', help="Categories to do (can be comma-separated list)")
+parser.add_argument('--recursive', help="In conjunction with --cats, recursively list pages in subcategories.",
+  action="store_true")
 parser.add_argument('--refs', help="References to do (can be comma-separated list)")
 parser.add_argument('--namespace', help="List all pages in namespace")
 args = parser.parse_args()
@@ -23,6 +25,12 @@ elif args.cats:
     msg("Processing category: %s" % cat)
     for i, page in blib.cat_articles(cat, start, end):
       msg("Page %s %s: Processing" % (i, unicode(page.title())))
+    if args.recursive:
+      for i, subcat in blib.cat_subcats(cat, start, end, recurse=True):
+        msg("Processing subcategory: %s" % unicode(subcat.title()))
+        for j, page in blib.cat_articles(subcat, start, end):
+          msg("Page %s %s: Processing" % (j, unicode(page.title())))
+
 elif args.namespace:
   ns = args.namespace
   if re.search('^[0-9]+$', ns):
