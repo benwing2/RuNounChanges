095e3337b5934b191157891bb07abf03b13061a5
Ben Wing <ben@benwing.com>
2019-04-21 13:13:07 -0500

    clean_lang_form_of: Include processing to add final period, post-template colon

--- clean_lang_form_of.py
+++ clean_lang_form_of.py
@@ -39,12 +39,12 @@ def init_all_templates(move_dot):
   global all_templates
 
   all_templates = (
-    sv_templates_with_plural_of +
-    sv_templates_with_obsoleted_by +
-    ca_templates_with_val +
-    nl_templates_with_comp_of_sup_of +
-    el_templates_with_active +
-    (el_templates_to_move_dot if move_dot else []) +
+#    sv_templates_with_plural_of +
+#    sv_templates_with_obsoleted_by +
+#    ca_templates_with_val +
+#    nl_templates_with_comp_of_sup_of +
+#    el_templates_with_active +
+#    (el_templates_to_move_dot if move_dot else []) +
     []
   )
 
@@ -136,9 +136,48 @@ def process_page(page, index, parsed):
 
   return text, notes
 
+def process_page_2(page, index, parsed):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+  notes = []
+
+  text = unicode(page.text)
+
+  if ":" in pagetitle and not re.search(
+      "^(Citations|Appendix|Reconstruction|Transwiki|Talk|Wiktionary|[A-Za-z]+ talk):", pagetitle):
+    pagemsg("WARNING: Colon in page title and not a recognized namespace to include, skipping page")
+    return None, None
+
+  newtext = re.sub(r"(\{\{en-third-person singular of.*?\}\}.*?)\.$", r"\1", text, 0, re.M)
+  if newtext != text:
+    notes.append("remove final period after {{en-third-person singular of}}")
+    text = newtext
+  newtext = re.sub(r"(\{\{el-form-of-nounadj.*?\}\}.*?)\.$", r"\1", text, 0, re.M)
+  if newtext != text:
+    notes.append("remove final period after {{el-form-of-nounadj}}")
+    text = newtext
+  newtext = re.sub(r"(\{\{fi-verb form of.*?\}\}.*?)\.$", r"\1", text, 0, re.M)
+  if newtext != text:
+    notes.append("remove final period after {{fi-verb form of}}")
+    text = newtext
+  newtext = re.sub(r"(\{\{ro-form-noun.*?\}\})([^:\n])", r"\1:\2", text, 0, re.M)
+  if newtext != text:
+    notes.append("add colon after non-final {{ro-form-noun}}")
+    text = newtext
+  newtext = re.sub(r"(\{\{ro-form-verb.*?\}\})([^:\n])", r"\1:\2", text, 0, re.M)
+  if newtext != text:
+    notes.append("add colon after non-final {{ro-form-verb}}")
+    text = newtext
+
+  return text, notes
+
 parser = blib.create_argparser("Clean up various form-of templates needing text moved outside of template into separate template")
 parser.add_argument('--move-dot', help="Move .= outside of template",
     action="store_true")
+parser.add_argument("--pagefile", help="List of pages to process.")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
@@ -148,3 +187,9 @@ for template in all_templates:
   errandmsg("Processing references to Template:%s" % template)
   for i, page in blib.references("Template:%s" % template, start, end):
     blib.do_edit(page, i, process_page, save=args.save, verbose=args.verbose)
+
+if args.pagefile:
+  pages = [x.rstrip('\n') for x in codecs.open(args.pagefile, "r", "utf-8")]
+  for i, page in blib.iter_items(pages, start, end):
+    blib.do_edit(pywikibot.Page(site, page), i, process_page_2, save=args.save,
+        verbose=args.verbose)
