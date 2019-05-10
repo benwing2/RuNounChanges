09e56bfb8ceccfa9c1338834e9b0056746c1b875
Ben Wing <ben@benwing.com>
2019-05-10 05:17:21 -0500

    fix_de_numerals: New script

---
+++ fix_de_numerals.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site
+
+def process_page(page, index, parsed):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+  notes = []
+
+  text = unicode(page.text)
+  text = re.sub(r"\n(===+)Adjective(===+)\n\{\{head\|de\|adjective form\}\}", "\n" + r"\1Numeral\2" + "\n{{head|de|numeral form}}",
+      text)
+  notes.append("change headword from adjective form to numeral form")
+  return text, notes
+
+parser = blib.create_argparser(u"Change ordinal numeral form headwords from adjective to numeral")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+endings = ["en", "er", "em", "es"]
+
+for index, page in blib.cat_articles("German ordinal numbers", start, end):
+  pagetitle = unicode(page.title())
+  if not pagetitle.endswith("e"):
+    continue
+  for ending in endings:
+    page = pywikibot.Page(site, pagetitle[:-1] + ending)
+    if page.exists():
+      blib.do_edit(page, index, process_page, save=args.save, verbose=args.verbose)
