4c053f9044dda68580de7c3fb88c40ef85d22aa8
Ben Wing <ben@benwing.com>
2019-06-12 03:22:45 -0500

    fix_bad_latin_forms: Major work, works now

--- fix_bad_latin_forms.py
+++ fix_bad_latin_forms.py
@@ -9,20 +9,34 @@ from blib import getparam, rmparam, msg, errandmsg, site, tname, pname
 import lalib
 from lalib import remove_macrons
 
-def process_form(index, page, lemma, formind, formval, goodstem, badstem):
+def process_form(index, page, lemma, formind, formval, subs):
   pagetitle = unicode(page.title())
 
   def pagemsg(txt):
     msg("Page %s %s: form %s %s: %s" % (index, lemma, formind, formval, txt))
 
   notes = []
+
   parsed = blib.parse(page)
-  pagetitle = unicode(page.title())
-  def pagemsg(txt):
-    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
   for t in parsed.filter_templates():
     origt = unicode(t)
     tn = tname(t)
+
+    def fix_head(headparam, head, tn):
+      for badstem, goodstem in subs:
+        if head.startswith(badstem):
+          newhead = goodstem + head[len(badstem):]
+          t.add(headparam, newhead)
+          notes.append("correct stem %s -> %s in {{%s}}" % (
+            badstem, goodstem, tn))
+          return newhead
+      else:
+        # no break
+        pagemsg("WARNING: Head %s not same as page title and doesn't begin with bad stem %s: %s" % (
+          head, " or ".join(badstem for badstem, goodstem in subs), unicode(t)))
+        return False
+
     # la-suffix-form has its own format, don't handle
     if tn in lalib.la_nonlemma_headword_templates and tn != "la-suffix-form":
       headparam = "head"
@@ -31,14 +45,11 @@ def process_form(index, page, lemma, formind, formval, goodstem, badstem):
         headparam = "1"
         head = getparam(t, headparam)
       if remove_macrons(head) != pagetitle:
-        if head.startswith(badstem):
-          t.add(headparam, goodstem + head[len(badstem):])
-          notes.append("correct stem %s -> %s in {{%s}}" % (
-            badstem, goodstem, tn))
-        else:
-          pagemsg("WARNING: Head %s not same as page title and doesn't begin with bad stem %s: %s" % (
-            head, badstem, unicode(t)))
-    elif tn in la_infl_of_templates:
+        newhead = fix_head(headparam, head, tn)
+        if newhead and remove_macrons(newhead) != pagetitle:
+          pagemsg("WARNING: Replacement head %s not same as page title: %s" % (
+            newhead, unicode(t)))
+    elif tn in lalib.la_infl_of_templates:
       langparam = "lang"
       headparam = "1"
       altparam = "2"
@@ -52,24 +63,24 @@ def process_form(index, page, lemma, formind, formval, goodstem, badstem):
         link = getparam(t, headparam)
         alt = getparam(t, altparam)
         head = alt or link
-        if remove_macrons(head) != pagetitle:
-          if head.startswith(badstem):
-            t.add(headparam, goodstem + head[len(badstem):])
+        if remove_macrons(head) != remove_macrons(lemma):
+          if subs:
+            newhead = fix_head(headparam, head, tn + "|la")
+            if newhead:
+              t.add(altparam, "")
+              if remove_macrons(newhead) != remove_macrons(lemma):
+                pagemsg("WARNING: Replacement lemma %s not same as lemma %s: %s" % (
+                  newhead, lemma, unicode(t)))
+        else:
+          if link != lemma or alt != "":
+            t.add(headparam, lemma)
             t.add(altparam, "")
-            notes.append("correct stem %s -> %s in {{%s|la}}" % (
-              badstem, goodstem, tn))
-          else:
-            pagemsg("WARNING: Head %s not same as page title and doesn't begin with bad stem %s: %s" % (
-              head, badstem, unicode(t)))
-        elif alt:
-          t.add(headparam, head)
-          t.add(altparam, "")
-          notes.append("move alt text to link text in {{%s|la}}" % tn)
+            notes.append("correct lemma and/or move alt text to link text in {{%s|la}}" % tn)
     if origt != unicode(t):
       pagemsg("Replaced %s with %s" % (origt, unicode(t)))
   return unicode(parsed), notes
 
-def process_page(index, pos, lemma, goodstem, badstem, infl, save, verbose):
+def process_page(index, pos, lemma, subs, infl, save, verbose):
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, lemma, txt))
   def errandpagemsg(txt):
@@ -90,11 +101,11 @@ def process_page(index, pos, lemma, goodstem, badstem, infl, save, verbose):
 
   for formind, form in blib.iter_items(forms_to_delete):
     def handler(page, formind, parsed):
-      return process_form(index, page, lemma, formind, form, goodstem, badstem)
-    blib.do_edit(pywikibot.Page(site, remove_macrons(formval)), formind, handler, save=save, verbose=verbose)
+      return process_form(index, page, lemma, formind, form, subs)
+    blib.do_edit(pywikibot.Page(site, remove_macrons(form)), formind, handler, save=save, verbose=verbose)
 
 parser = blib.create_argparser(u"Fix up bad Latin forms")
-parser.add_argument('--declfile', help="File containing pos, lemma, goodstem, badstem, infl.")
+parser.add_argument('--declfile', help="File containing pos lemma bad:good,... infl")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
@@ -102,8 +113,11 @@ if not args.declfile:
   raise ValueError("--declfile required")
 lines = [x.strip() for x in codecs.open(args.declfile, "r", "utf-8")]
 for index, line in blib.iter_items(lines, start, end):
+  if line.startswith("#"):
+    continue
   if "!!!" in line:
-    pos, lemma, goodstem, badstem, infl = re.split("!!!", line)
+    pos, lemma, subs, infl = re.split("!!!", line)
   else:
-    pos, lemma, goodstem, badstem, infl = re.split(" ", line, 2)
-  process_page(index, pos, lemma, goodstem, badstem, args.save, args.verbose)
+    pos, lemma, subs, infl = re.split(" ", line, 4)
+  subs = [] if subs == "-" else [x.split(":") for x in subs.split(",")]
+  process_page(index, pos, lemma, subs, infl, args.save, args.verbose)
