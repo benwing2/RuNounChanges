97e4c86e963876682721fd9238237e91e19429e4
Ben Wing <ben@benwing.com>
2019-06-16 15:10:25 -0500

    delete_bad_latin_terms,remove_it_stress: New scripts

---
+++ delete_bad_latin_terms.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, errandmsg, site, tname, pname
+
+import lalib
+
+pages_to_delete = []
+
+def delete_term(index, term, expected_head_templates, save, verbose):
+  notes = []
+
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, term, txt))
+  page = pywikibot.Page(site, term)
+  if not page.exists():
+    pagemsg("Skipping form value %s, page doesn't exist" % term)
+    return
+
+  text = unicode(page.text)
+  origtext = text
+
+  retval = lalib.find_latin_section(text, pagemsg)
+  if retval is None:
+    return
+
+  sections, j, secbody, sectail, has_non_latin = retval
+
+  subsections = re.split("(^==+[^=\n]+==+\n)", secbody, 0, re.M)
+  saw_lemma_in_etym = False
+  saw_wrong_lemma_in_etym = False
+  saw_head = False
+  infl_template = None
+  saw_bad_template = False
+  for k in xrange(2, len(subsections), 2):
+    parsed = blib.parse_text(subsections[k])
+    for t in parsed.filter_templates():
+      tn = tname(t)
+      if tn in expected_head_templates:
+        saw_head = True
+      elif tn in ["inflection of", "rfdef", "la-IPA"]:
+        pass
+      else:
+        pagemsg("WARNING: Saw unrecognized template in subsection #%s %s: %s" % (
+          k // 2, subsections[k - 1].strip(), unicode(t)))
+        saw_bad_template = True
+
+  delete = False
+  if saw_head:
+    if saw_bad_template:
+      pagemsg("WARNING: Would delete but saw unrecognized template, not deleting")
+    else:
+      delete = True
+
+  if not delete:
+    return
+
+  if "==Etymology" in sections[j]:
+    pagemsg("WARNING: Found Etymology subsection, don't know how to handle")
+    return
+  if "==Pronunciation " in sections[j]:
+    pagemsg("WARNING: Found Pronunciation N subsection, don't know how to handle")
+    return
+
+  #### Now, we can maybe delete the whole section or page
+
+  if subsections[0].strip():
+    pagemsg("WARNING: Whole Latin section deletable except that there's text above all subsections: <%s>" % subsections[0].strip())
+    return
+  if "[[Category:" in sectail:
+    pagemsg("WARNING: Whole Latin section deletable except that there's a category at the end: <%s>" % sectail.strip())
+    return
+  if not has_non_latin:
+    # Can delete the whole page, but check for non-blank section 0
+    cleaned_sec0 = re.sub("^\{\{also\|.*?\}\}\n", "", sections[0])
+    if cleaned_sec0.strip():
+      pagemsg("WARNING: Whole page deletable except that there's text above all sections: <%s>" % cleaned_sec0.strip())
+      return
+    pagetitle = unicode(page.title())
+    pagemsg("Page %s should be deleted" % pagetitle)
+    pages_to_delete.append(pagetitle)
+    return
+  del sections[j]
+  del sections[j-1]
+  notes.append("removed Latin section for bad term")
+  if j > len(sections):
+    # We deleted the last section, remove the separator at the end of the
+    # previous section.
+    sections[-1] = re.sub(r"\n+--+\n*\Z", "", sections[-1])
+  text = "".join(sections)
+
+  if text != origtext:
+    if verbose:
+      pagemsg("Replacing <%s> with <%s>" % (origtext, text))
+    assert notes
+    comment = "; ".join(blib.group_notes(notes))
+    if save:
+      pagemsg("Saving with comment = %s" % comment)
+      page.text = text
+      page.save(comment=comment)
+    else:
+      pagemsg("Would save with comment = %s" % comment)
+
+parser = blib.create_argparser(u"Delete bad Latin terms")
+parser.add_argument('--headtemp', required=True, help="Name(s) of expected headword template(s).")
+parser.add_argument('--pagefile', required=True, help="File containing terms to delete.")
+parser.add_argument('--output-pages-to-delete', help="File to write pages to delete.")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+terms = [x.strip() for x in codecs.open(args.pagefile, "r", "utf-8")]
+headtemp = args.headtemp.decode('utf-8').split(',')
+for index, term in blib.iter_items(terms, start, end):
+  delete_term(index, term, args.headtemp, args.save, args.verbose)
+msg("The following pages need to be deleted:")
+for page in pages_to_delete:
+  msg(page)
+if args.output_pages_to_delete:
+  with codecs.open(args.output_pages_to_delete, "w", "utf-8") as fp:
+    for page in pages_to_delete:
+      print >> fp, page
---
+++ remove_it_stress.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, errandmsg, site, tname, pname
+
+import lalib
+from lalib import remove_macrons
+
+def process_form(index, page, lemma, formind, formval, subs):
+  pagetitle = unicode(page.title())
+
+  def pagemsg(txt):
+    msg("Page %s %s: form %s %s: %s" % (index, lemma, formind, formval, txt))
+
+  notes = []
+
+  parsed = blib.parse(page)
+
+  for t in parsed.filter_templates():
+    origt = unicode(t)
+    tn = tname(t)
+
+    def fix_head(headparam, head, tn):
+      for badstem, goodstem in subs:
+        if head.startswith(badstem):
+          newhead = goodstem + head[len(badstem):]
+          t.add(headparam, newhead)
+          notes.append("correct stem %s -> %s in {{%s}}" % (
+            badstem, goodstem, tn))
+          return newhead
+      else:
+        # no break
+        pagemsg("WARNING: Head %s not same as page title and doesn't begin with bad stem %s: %s" % (
+          head, " or ".join(badstem for badstem, goodstem in subs), unicode(t)))
+        return False
+
+    # la-suffix-form has its own format, don't handle
+    if tn in lalib.la_nonlemma_headword_templates and tn != "la-suffix-form":
+      headparam = "head"
+      head = getparam(t, headparam)
+      if not head:
+        headparam = "1"
+        head = getparam(t, headparam)
+      if remove_macrons(head) != pagetitle:
+        newhead = fix_head(headparam, head, tn)
+        if newhead and remove_macrons(newhead) != pagetitle:
+          pagemsg("WARNING: Replacement head %s not same as page title: %s" % (
+            newhead, unicode(t)))
+    elif tn in lalib.la_infl_of_templates:
+      langparam = "lang"
+      headparam = "1"
+      altparam = "2"
+      lang = getparam(t, langparam)
+      if not lang:
+        langparam = "1"
+        headparam = "2"
+        altparam = "3"
+        lang = getparam(t, langparam)
+      if lang == "la":
+        link = getparam(t, headparam)
+        alt = getparam(t, altparam)
+        head = alt or link
+        if remove_macrons(head) != remove_macrons(lemma):
+          if subs:
+            newhead = fix_head(headparam, head, tn + "|la")
+            if newhead:
+              t.add(altparam, "")
+              if remove_macrons(newhead) != remove_macrons(lemma):
+                pagemsg("WARNING: Replacement lemma %s not same as lemma %s: %s" % (
+                  newhead, lemma, unicode(t)))
+        else:
+          if link != lemma or alt != "":
+            t.add(headparam, lemma)
+            t.add(altparam, "")
+            notes.append("correct lemma and/or move alt text to link text in {{%s|la}}" % tn)
+    if origt != unicode(t):
+      pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+  return unicode(parsed), notes
+
+remove_stress_table = {
+  u'à': 'a',
+  u'á': 'a',
+  u'è': 'e',
+  u'é': 'e',
+  u'ì': 'i',
+  u'í': 'i',
+  u'ò': 'o',
+  u'ó': 'o',
+  u'ù': 'u',
+  u'ú': 'u',
+  u'ỳ': 'y',
+  u'ý': 'y',
+  u'À': 'A',
+  u'Á': 'A',
+  u'È': 'E',
+  u'É': 'E',
+  u'Ì': 'I',
+  u'Í': 'I',
+  u'Ò': 'O',
+  u'Ó': 'O',
+  u'Ù': 'U',
+  u'Ú': 'U',
+  u'Ỳ': 'Y',
+  u'Ý': 'Y',
+}
+
+def remove_stress(char):
+  if char in remove_stress_table:
+    return remove_stress_table[char]
+  return char
+
+def synchronize(term, hyphenation, pagemsg):
+  i = 0
+  hyph = "|".join(hyphenation)
+  j = 0
+  transfer_j = None
+  transfer_char = None
+  while i < len(term) and j < len(hyph):
+    while j < len(hyph) and hyph[j] == '|':
+      j += 1
+    if j >= len(hyph):
+      break
+    if term[i] == hyph[j]:
+      i += 1
+      j += 1
+      continue
+    if remove_stress(term[i]) == hyph[j]:
+      if transfer_j is not None:
+        pagemsg("WARNING: Two potential accents to transfer, #%s=%s and #%s=%s" % (
+          transfer_j, transfer_char, j, term[i]))
+        return None
+      transfer_j = j
+      transfer_char = term[i]
+      i += 1
+      j += 1
+      continue
+    if term[i] == " ":
+      i += 1
+      continue
+    pagemsg("WARNING: Unable to match it-stress term %s against hyphenation %s at it-stress char #%s=%s, hyphenation char #%s=%s" % (
+      term, hyph, i, term[i], j, hyph[j]))
+    return None
+  if i < len(term):
+    pagemsg("WARNING: Trailing it-stress chars %s in term %s when matching against hyphenation %s" % (
+      term[i:], term, hyph))
+    return None
+  if j < len(hyph):
+    pagemsg("WARNING: Trailing hyphenation chars %s in term %s when matching against it-stress %s" % (
+      hyph[j:], hyph, term))
+    return None
+  if transfer_char is None:
+    pagemsg("Stress already transferred from %s to %s" % (term, hyph))
+    return hyphenation
+  return (hyph[0:transfer_j] + transfer_char + hyph[transfer_j+1:]).split("|")
+
+def process_page(index, page):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+  def errandpagemsg(txt):
+    errandmsg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+
+  text = unicode(page.text)
+
+  notes = []
+
+  retval = blib.find_modifiable_lang_section(text, "Italian", pagemsg)
+  if retval is None:
+    return None, None
+  sections, j, secbody, sectail, has_non_lang = retval
+
+  subsections = re.split("(^===+[^=\n]+===+\n)", secbody, 0, re.M)
+
+  for k in xrange(2, len(subsections), 2):
+    parsed = blib.parse_text(subsections[k])
+    it_stress_template = None
+    it_hyph_template = None
+    do_continue = False
+    for t in parsed.filter_templates():
+      tn = tname(t)
+      if tn == "it-stress":
+        if it_stress_template:
+          pagemsg("WARNING: Saw multiple it-stress templates: %s and %s" % (
+            unicode(it_stress_template), unicode(t)))
+          do_continue = True
+          break
+        else:
+          it_stress_template = t
+      elif tn in ["hyph", "hyphenation"]:
+        lang = getparam(t, "lang")
+        if not lang:
+          lang = getparam(t, "1")
+        if lang == "it":
+          if it_hyph_template:
+            pagemsg("WARNING: Saw multiple hyph|it templates: %s and %s" % (
+              unicode(it_hyph_template), unicode(t)))
+            do_continue = True
+            break
+          else:
+            it_hyph_template = t
+    if do_continue:
+      continue
+    if not it_stress_template:
+      if "==Phonology==" in subsections[k - 1]:
+        pagemsg("No it-stress template in Phonology section")
+      continue
+    if not it_hyph_template:
+      pagemsg("WARNING: Saw it-stress template %s but no hyphenation template" %
+          unicode(it_stress_template))
+      continue
+    if getparam(it_hyph_template, "lang"):
+      first_hyph_param = 1
+    else:
+      first_hyph_param = 2
+    hyph_params = []
+    i = first_hyph_param
+    while getparam(it_hyph_template, str(i)):
+      hyph_params.append(getparam(it_hyph_template, str(i)))
+      i += 1
+    new_hyph = synchronize(getparam(it_stress_template, "1"), hyph_params,
+        pagemsg)
+    if new_hyph is None:
+      continue
+    assert len(hyph_params) == len(new_hyph)
+    orig_hyph_template = unicode(it_hyph_template)
+    i = first_hyph_param
+    for param in new_hyph:
+      it_hyph_template.add(str(i), param)
+      i += 1
+    if orig_hyph_template != unicode(it_hyph_template):
+      pagemsg("Replaced %s with %s" % (orig_hyph_template, unicode(it_hyph_template)))
+    else:
+      pagemsg("No changes to hyph template %s" % (orig_hyph_template))
+    subsec_k = unicode(parsed)
+    subsec_k, modified = blib.replace_in_text(subsec_k,
+      "* %s\n" % unicode(it_stress_template), "", pagemsg, no_found_repl_check=True)
+    if not modified:
+      continue
+    subsections[k] = subsec_k
+    notes.append("transfer accent from {{it-stress}} to {{hyph|it}} and remove {{it-stress}}")
+
+  secbody = "".join(subsections)
+  sections[j] = secbody + sectail
+  return "".join(sections), notes
+
+  for formind, form in blib.iter_items(forms_to_delete):
+    def handler(page, formind, parsed):
+      return process_form(index, page, lemma, formind, form, subs)
+    blib.do_edit(pywikibot.Page(site, remove_macrons(form)), formind, handler, save=save, verbose=verbose)
+
+parser = blib.create_argparser(u"Transfer accent from {{it-stress}} to {{hyph|it}} and remove {{it-stress}}")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+def handler(page, index, parsed):
+  return process_page(index, page)
+
+for index, page in blib.references("Template:it-stress", start, end):
+  blib.do_edit(page, index, handler, save=args.save, verbose=args.verbose)
