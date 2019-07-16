d6e96e72ccd949f1d9f6d2f8ad50d41d4bec1a9b
Ben Wing <ben@benwing.com>
2019-07-16 10:12:19 -0500

    delete_bad_latin_forms: Support allbutlemma

--- delete_bad_latin_forms.py
+++ delete_bad_latin_forms.py
@@ -471,6 +471,12 @@ def process_page(index, lemma, pos, infl, forms, pages_to_delete, preserve_diaer
       forms_to_delete.append((form, args[form]))
     elif form == "all":
       add_bad_forms(lambda key: True)
+    elif form == "allbutlemma":
+      for key, val in args.iteritems():
+        vals = val.split(",")
+        if remove_macrons(lemma) not in [remove_macrons(v) for v in vals]:
+          tag_sets_to_delete.append(lalib.form_key_to_tag_set(key))
+          forms_to_delete.append((key, val))
     elif form == "firstpart":
       add_bad_forms(lambda key: (
         key not in ["futr_actv_ptc", "futr_actv_inf", "futr_pasv_inf"] and (
