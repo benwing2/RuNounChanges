4fc4b4078ef2ed35630e8fb7567a9848b2b9e6c8
Ben Wing <ben@benwing.com>
2019-07-18 15:14:31 -0500

    fix_latin_superlative: new script

---
+++ fix_latin_superlative.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, msg, site
+
+import lalib
+
+def process_page(page, index, parsed):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+
+  notes = []
+
+  lemma = None
+  for t in parsed.filter_templates():
+    if tname(t) == "la-adecl":
+      if lemma:
+        pagemsg("WARNING: Saw more than one declension table, first lemma=%s, second template=%s" % (
+          lemma, unicode(t)))
+      lemma = getparam(t, "1")
+  if not lemma:
+    pagemsg("WARNING: Couldn't find declension template")
+    return None, None
+  for t in parsed.filter_templates():
+    if tname(t) == "head" and getparam(t, "1") == "la" and getparam(t, "2") == "adjective superlative form":
+      origt = unicode(t)
+      if getparam(t, "3") == "superlative of":
+        base_lemma = getparam(t, "4")
+        rmparam(t, "head")
+        rmparam(t, "4")
+        rmparam(t, "3")
+        t.add("1", lemma)
+        t.add("2", base_lemma)
+        blib.set_template_name(t, "la-adj-superlative")
+        pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+        notes.append("Use {{la-adj-superlative}} instead of {{head|la|...}}")
+      else:
+        pagemsg("WARNING: Head template doesn't include base form: %s" % unicode(t))
+
+  return unicode(parsed), notes
+
+parser = blib.create_argparser("Fix Latin superlatives formatted using {{head|la|...}}")
+parser.add_argument("--pagefile", help="List of pages to process.")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+if args.pagefile:
+  pages = [x.rstrip('\n') for x in codecs.open(args.pagefile, "r", "utf-8")]
+  for i, page in blib.iter_items(pages, start, end):
+    blib.do_edit(pywikibot.Page(site, page), i, process_page, save=args.save,
+        verbose=args.verbose, diff=args.diff)
+else:
+  for i, page in blib.cat_articles("Latin adjective superlative forms", start, end):
+    blib.do_edit(page, i, process_page, save=args.save, verbose=args.verbose, diff=args.diff)
