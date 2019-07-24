d70ef92cc515aa1dc4f116787cd5d6f4559c9c09
Ben Wing <ben@benwing.com>
2019-07-23 19:39:18 -0500

    delete_obsolete_pages: Catch APIError and retry

--- delete_obsolete_pages.py
+++ delete_obsolete_pages.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import pywikibot, re, sys, codecs, argparse
+from pywikibot.data.api import APIError
 
 import blib
 from blib import getparam, rmparam, msg, errandmsg, site
@@ -18,15 +19,25 @@ pages_to_delete = [x.strip() for x in codecs.open(args.pagefile, "r", "utf-8")]
 comment = args.comment and args.comment.decode('utf-8') or "Delete obsolete page"
 doc_comment = "Delete documentation page of " + re.sub("^([Dd]elete|[Rr]emove) ", "", comment)
 
+def delete_page(page, comment):
+  for i in range(11):
+    try:
+      page.delete(comment)
+      return
+    except APIError as e:
+      if i == 10:
+        raise e
+      errandmsg("APIError, try #%s: %s" % (i + 1, e))
+
 for i, pagename in blib.iter_items(pages_to_delete, start, end):
   page = pywikibot.Page(site, pagename)
   if page.exists():
     msg("Deleting %s (comment=%s)" % (page.title(), comment))
-    page.delete('%s (content was "%s")' % (comment, unicode(page.text)))
+    delete_page(page, '%s (content was "%s")' % (comment, unicode(page.text)))
     errandmsg("Page [[%s]] deleted" % page.title())
   if args.delete_docs:
     doc_page = pywikibot.Page(site, "%s/documentation" % pagename)
     if doc_page.exists():
       msg("Deleting %s (comment=%s)" % (doc_page.title(), doc_comment))
-      doc_page.delete('%s (content was "%s")' % (doc_comment, unicode(doc_page.text)))
+      delete_page(doc_page, '%s (content was "%s")' % (doc_comment, unicode(doc_page.text)))
       errandmsg("Page [[%s]] deleted" % doc_page.title())
