541b2a0a4712c23b197738b808741199f06f45a3
Ben Wing <ben@benwing.com>
2019-07-30 01:21:35 -0500

    fix_latin_3rd_decl_plural_nouns: Update to compare old against new while fixing

--- fix_latin_3rd_decl_plural_nouns.py
+++ fix_latin_3rd_decl_plural_nouns.py
@@ -4,72 +4,133 @@
 import pywikibot, re, sys, codecs, argparse
 
 import blib
-from blib import getparam, rmparam, tname, msg, site
+from blib import getparam, rmparam, tname, msg, errandmsg, site
 
 import lalib
+
+def compare_new_and_old_templates(oldt, newt, pagetitle, pagemsg, errandpagemsg):
+  global args
+  def expand_text(tempcall):
+    return blib.expand_text(tempcall, pagetitle, pagemsg, args.verbose)
+
+  def generate_old_forms():
+    old_generate_template = re.sub(r"^\{\{la-ndecl\|", "{{la-generate-noun-forms|", oldt)
+    old_generate_template = re.sub(r"^\{\{la-adecl\|", "{{la-generate-adj-forms|", old_generate_template)
+    old_result = expand_text(old_generate_template)
+    if not old_result:
+      return None
+    return old_result
+
+  def generate_new_forms():
+    new_generate_template = re.sub(r"^\{\{la-ndecl\|", "{{User:Benwing2/la-new-generate-noun-forms|", newt)
+    new_generate_template = re.sub(r"^\{\{la-adecl\|", "{{User:Benwing2/la-new-generate-adj-forms|", new_generate_template)
+    new_result = expand_text(new_generate_template)
+    if not new_result:
+      return None
+    return new_result
+
+  return blib.compare_new_and_old_template_forms(oldt, newt, generate_old_forms,
+    generate_new_forms, pagemsg, errandpagemsg)
 
 def process_page(page, index, parsed):
   pagetitle = unicode(page.title())
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagetitle, txt))
+  def errandpagemsg(txt):
+    errandmsg("Page %s %s: %s" % (index, pagetitle, txt))
 
   notes = []
 
-  if " " in pagetitle:
-    pagemsg("WARNING: Space in page title, skipping")
-    return None, None
   pagemsg("Processing")
+
+  bad_compare = False
 
   for t in parsed.filter_templates():
     origt = unicode(t)
     tn = tname(t)
     if tn == "la-ndecl":
-      lemmaspec = getparam(t, "1")
-      m = re.search("^(.*)<(.*)>$", lemmaspec)
-      if not m:
-        pagemsg("WARNING: Unable to parse lemma+spec %s, skipping: %s" % (
-          lemmaspec, origt))
-        continue
-      lemma, spec = m.groups()
-      decl, *subtypes = spec.split(".")
-      if "pl" not in subtypes:
-        pagemsg("WARNING: .pl not in lemma spec %s, skipping: %s" % (
-          spec, origt))
-        continue
-      if "Greek" in subtypes:
-        pagemsg("WARNING: .Greek and .pl in lemma spec %s, not able to handle, skipping: %s" % (
-          lemmaspec, origt))
-        continue
-      if "/" in lemma:
-        base, stem2 = lemma.split("/")
-      else:
-        base = lemma
-        stem2 = lalib.infer_3rd_decl_stem(base)
-      if "N" in subtypes and "I" in subtypes:
-        newlemma = stem2 + "ia"
-        subtypes = [x for x in subtypes if x != "N" and x != "I"]
-        if "pure" in subtypes:
-          subtypes = [x for x in subtypes if x != "pure"]
+      while True:
+        lemmaspec = getparam(t, "1")
+        if " " in lemmaspec:
+          pagemsg("Space in lemma+spec, skipping")
+          break
+        m = re.search("^(.*)<(.*)>$", lemmaspec)
+        if not m:
+          pagemsg("WARNING: Unable to parse lemma+spec %s, skipping: %s" % (
+            lemmaspec, origt))
+          break
+        lemma, spec = m.groups()
+        split_spec = spec.split(".")
+        decl = split_spec[0]
+        subtypes = split_spec[1:]
+        if decl != "3" or "pl" not in subtypes:
+          break
+        if "Greek" in subtypes:
+          pagemsg("WARNING: .Greek and .pl in lemma spec %s, not able to handle, skipping: %s" % (
+            lemmaspec, origt))
+          break
+        if "/" in lemma:
+          base, stem2 = lemma.split("/")
         else:
-          subtypes = subtypes + ["-pure"]
-      elif "N" in subtypes:
-        newlemma = stem2 + "a"
-        subtypes = [x for x in subtypes if x != "N"]
-      else:
-        newlemma = stem2 + u"ēs"
-      newspec = ".".join([decl] + subtypes)
-      t.add("1", "%s<%s>" % (newlemma, newspec))
-      pagemsg("Replaced %s with %s" % (origt, unicode(t)))
-      notes.append("convert 3rd-declension plural term to have plural lemma in {{la-ndecl}}")
+          base = lemma
+          stem2 = lalib.infer_3rd_decl_stem(base)
 
+        # implement autodetection of the types we care about (N, I, pure)
+        if "N" not in subtypes and "-I" not in subtypes:
+          if (base.endswith("is") and base[:-2] == stem2 or
+              base.islower() and base.endswith(u"ēs") and base[:-2] == stem2):
+            subtypes += ["I"]
+        if ("-N" not in subtypes and "M" not in subtypes and "F" not in subtypes and
+            "-I" not in subtypes and "-pure" not in subtypes):
+          if (base.endswith("e") and base[:-1] == stem2 or
+              base.endswith("al") and base[:-2] + u"āl" == stem2 or
+              base.endswith("ar") and base[:-2] + u"ār" == stem2):
+            subtypes += ["N", "I", "pure"]
+        if ("-N" not in subtypes and "M" not in subtypes and "F" not in subtypes):
+          if (base.endswith("us") and base[:-2] + "or" == stem2 or
+              base.endswith("us") and base[:-2] + "er" == stem2 or
+              base.endswith("ma") and base[:-2] + "mat" == stem2 or
+              base.endswith("men") and base[:-2] + "min" == stem2):
+            subtypes += ["N"]
+
+        if "N" in subtypes and "I" in subtypes:
+          newlemma = stem2 + "ia"
+          subtypes = [x for x in subtypes if x != "N" and x != "I"]
+          if "pure" in subtypes:
+            subtypes = [x for x in subtypes if x != "pure"]
+          else:
+            subtypes = subtypes + ["-pure"]
+        elif "N" in subtypes:
+          newlemma = stem2 + "a"
+          subtypes = [x for x in subtypes if x != "N"]
+        else:
+          newlemma = stem2 + u"ēs"
+        subtypes = [x for x in subtypes if x != "-I"]
+        newspec = ".".join([decl] + subtypes)
+        t.add("1", "%s<%s>" % (newlemma, newspec))
+        pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+        notes.append("convert 3rd-declension plural term to have plural lemma in {{la-ndecl}}")
+        break
+      if not compare_new_and_old_templates(origt, unicode(t), pagetitle, pagemsg, errandpagemsg):
+        bad_compare = True
+
+  if bad_compare:
+    return None, None
   return unicode(parsed), notes
 
-parser = blib.create_argparser("Fix Latin 3rd-decl plural nouns to specify plural lemma")
-parser.add_argument("--pagefile", help="List of pages to process.", required=True)
+parser = blib.create_argparser("Fix Latin 3rd-decl plural nouns to specify plural lemma, and check new against old {{la-ndecl}} code")
+parser.add_argument("--pagefile", help="List of pages to process.")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
-pages = [x.rstrip('\n') for x in codecs.open(args.pagefile, "r", "utf-8")]
-for i, page in blib.iter_items(pages, start, end):
-  blib.do_edit(pywikibot.Page(site, page), i, process_page, save=args.save,
-    verbose=args.verbose, diff=args.diff)
+if args.pagefile:
+  pages = [x.rstrip('\n') for x in codecs.open(args.pagefile, "r", "utf-8")]
+  for i, page in blib.iter_items(pages, start, end):
+    blib.do_edit(pywikibot.Page(site, page), i, process_page, save=args.save,
+      verbose=args.verbose, diff=args.diff)
+else:
+  refs = ["Template:la-ndecl", "Template:la-adecl"]
+  for ref in refs:
+    for i, page in blib.references(ref, start, end):
+      blib.do_edit(page, i, process_page, save=args.save, verbose=args.verbose,
+          diff=args.diff)
