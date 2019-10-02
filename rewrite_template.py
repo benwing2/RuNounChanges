34ece92f344f740f9657404342c1afba3d633e52
Ben Wing <ben@benwing.com>
2019-10-02 03:44:32 -0500

    rewrite_template: use blib.do_pagefile_cats_refs

--- rewrite_template.py
+++ rewrite_template.py
@@ -19,65 +19,57 @@ import pywikibot, re, sys, codecs, argparse
 import blib
 from blib import getparam, rmparam, msg, site, tname
 
-def rewrite_pages(template, new_name, params_to_remove, params_to_rename,
-    pages, filters, comment, startFrom, upTo):
-  global args
-  def process_page(page, index, parsed):
-    pagetitle = unicode(page.title())
-    def pagemsg(txt):
-      msg("Page %s %s: %s" % (index, pagetitle, txt))
+def process_page(index, page, template, new_name, params_to_remove,
+    params_to_rename, filters, comment):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
 
-    pagemsg("Processing")
-    notes = []
+  pagemsg("Processing")
+  notes = []
 
-    for t in parsed.filter_templates():
-      origt = unicode(t)
-      tn = tname(t)
-      if tn == template:
-        for filt in filters:
-          m = re.search("^(.*)=(.*)$", filt)
+  parsed = blib.parse(page)
+
+  for t in parsed.filter_templates():
+    origt = unicode(t)
+    tn = tname(t)
+    if tn == template:
+      for filt in filters:
+        m = re.search("^(.*)=(.*)$", filt)
+        if m:
+          if getparam(t, m.group(1)) != m.group(2):
+            pagemsg("Skipping %s because filter %s doesn't match" %
+              origt, filt)
+          continue
+        else:
+          m = re.search("^(.*)~(.*)$", filt)
           if m:
-            if getparam(t, m.group(1)) != m.group(2):
+            if not re.search(m.group(2), getparam(t, m.group(1))):
               pagemsg("Skipping %s because filter %s doesn't match" %
                 origt, filt)
             continue
           else:
-            m = re.search("^(.*)~(.*)$", filt)
-            if m:
-              if not re.search(m.group(2), getparam(t, m.group(1))):
-                pagemsg("Skipping %s because filter %s doesn't match" %
-                  origt, filt)
-              continue
-            else:
-              raise ValueError("Unrecognized filter %s" % filt)
-        for old_param, new_param in params_to_rename:
-          if t.has(old_param):
-            t.add(new_param, getparam(t, old_param), before=old_param)
-            rmparam(t, old_param)
-            notes.append("rename %s= to %s= in {{%s}}" % (old_param, new_param, tn))
-        for param in params_to_remove:
-          if t.has(param):
-            rmparam(t, param)
-            notes.append("remove %s= from {{%s}}" % (param, tn))
-        if new_name:
-          blib.set_template_name(t, new_name)
-          notes.append("rename {{%s}} to {{%s}}" % (template, new_name))
+            raise ValueError("Unrecognized filter %s" % filt)
+      for old_param, new_param in params_to_rename:
+        if t.has(old_param):
+          t.add(new_param, getparam(t, old_param), before=old_param)
+          rmparam(t, old_param)
+          notes.append("rename %s= to %s= in {{%s}}" % (old_param, new_param, tn))
+      for param in params_to_remove:
+        if t.has(param):
+          rmparam(t, param)
+          notes.append("remove %s= from {{%s}}" % (param, tn))
+      if new_name:
+        blib.set_template_name(t, new_name)
+        notes.append("rename {{%s}} to {{%s}}" % (template, new_name))
 
-      if unicode(t) != origt:
-        pagemsg("Replaced <%s> with <%s>" % (origt, unicode(t)))
+    if unicode(t) != origt:
+      pagemsg("Replaced <%s> with <%s>" % (origt, unicode(t)))
 
-    return unicode(parsed), comment or notes
+  return unicode(parsed), comment or notes
 
-  if pages:
-    for i, page in blib.iter_items(pages, startFrom, upTo):
-      blib.do_edit(pywikibot.Page(site, page), i, process_page, save=args.save,
-          verbose=args.verbose, diff=args.diff)
-  else:
-    for index, page in blib.references("Template:%s" % template, startFrom, upTo):
-      blib.do_edit(page, index, process_page, save=args.save, verbose=args.verbose,
-          diff=args.diff)
-
-pa = blib.init_argparser("Rewrite templates, possibly renaming params or the template itself, or removing params")
+pa = blib.create_argparser("Rewrite templates, possibly renaming params or the template itself, or removing params",
+  include_pagefile=True)
 pa.add_argument("-t", "--template", help="Name of template", required=True)
 pa.add_argument("-n", "--new-name", help="New name of template")
 pa.add_argument("-r", "--remove", help="Param to remove, can be specified multiple times",
@@ -88,10 +80,9 @@ pa.add_argument("--to", help="New name of param, can be specified multiple times
     action="append")
 pa.add_argument("--filter", help="Only take action on templates matching the filter, which should be either PARAM=VALUE meaning the parameter must have the given value, or PARAM~REGEXP meaning the parameter must match the given regular expression (unanchored). Can be specified multiple times and all must match.",
     action="append")
-pa.add_argument("--pagefile", help="List of pages to process.")
 pa.add_argument("-c", "--comment", help="Comment to use in place of auto-generated ones.")
 args = pa.parse_args()
-startFrom, upTo = blib.parse_start_end(args.start, args.end)
+start, end = blib.parse_start_end(args.start, args.end)
 
 template = args.template.decode("utf-8")
 new_name = args.new_name and args.new_name.decode("utf-8")
@@ -106,10 +97,9 @@ if len(from_) != len(to):
 
 params_to_rename = zip(from_, to)
 
-if args.pagefile:
-  pages = [x.rstrip('\n') for x in codecs.open(args.pagefile, "r", "utf-8")]
-else:
-  pages = None
+def do_process_page(page, index, parsed):
+  return process_page(index, page, template, new_name, params_to_remove,
+    params_to_rename, filters, comment)
 
-rewrite_pages(template, new_name, params_to_remove, params_to_rename,
-    pages, filters, comment, startFrom, upTo)
+blib.do_pagefile_cats_refs(args, start, end, do_process_page, edit=True,
+  default_refs=["Template:%s" % template])
