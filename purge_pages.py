a83b779acd468a2d6b684c7b7d6f8e95a42f2e3a
Ben Wing <ben@benwing.com>
2019-10-22 00:40:05 -0500

    purge_pages: Support --cat-subcats

--- purge_pages.py
+++ purge_pages.py
@@ -8,6 +8,7 @@ from blib import getparam, rmparam, msg, site
 
 parser = blib.create_argparser(u"Purge (null-save) pages in category or references")
 parser.add_argument('--cat', help="Category to purge")
+parser.add_argument('--cat-subcats', help="Category whose subcategories to purge")
 parser.add_argument('--ref', help="References to purge")
 parser.add_argument('--ignore-non-mainspace', help="Ignore pages not in the mainspace",
     action="store_true")
@@ -17,6 +18,8 @@ start, end = blib.parse_start_end(args.start, args.end)
 pages = []
 if args.cat:
   pages_to_list = blib.cat_articles(args.cat, start, end)
+elif args.cat_subcats:
+  pages_to_list = blib.cat_subcats(args.cat_subcats, start, end)
 else:
   pages_to_list = blib.references(args.ref, start, end)
 for i, page in pages_to_list:
