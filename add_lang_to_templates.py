abe5fd269ae5fe1b7d6ff289c54b4c42621d7e0d
Ben Wing <ben@benwing.com>
2019-10-22 00:38:34 -0500

    add_lang_to_templates: support templates_to_rename, generalize past name templates

--- add_lang_to_templates.py
+++ add_lang_to_templates.py
@@ -8,11 +8,20 @@ from blib import getparam, rmparam, msg, site, tname
 
 borrowed_langs = {}
 
-#name_templates = ["given name", "surname"]
-#name_templates = ["rfdatek", "rfquotek"]
-#name_templates = ["rfdate"] # requires --lang-as-1
-#name_templates = ["rfc-header", "tea room"]
-name_templates = ["rfc"] # requires --lang-as-1
+#templates_to_process = ["given name", "surname"]
+#templates_to_process = ["rfdatek", "rfquotek"]
+#templates_to_process = ["rfdate"] # requires --lang-as-1
+#templates_to_process = ["rfc-header", "tea room"]
+#templates_to_process = ["rfc"] # requires --lang-as-1
+#templates_to_process = ["SeeCites", "seecites"]
+templates_to_rename = {
+  "SeeCites": "seeCites",
+  "seecites": "seeCites",
+}
+#templates_to_process = ["elements"]
+#templates_to_process = ["abbreviated", "cuneiform", "patronymic", "IPA letters",
+#  "seeSynonyms", "SI-unit"]
+templates_to_process = ["SI-unit-np"]
 blib.getData()
 
 def process_page(page, index, parsed, lang_in_1):
@@ -38,34 +47,39 @@ def process_page(page, index, parsed, lang_in_1):
       tn = tname(t)
       if tn in ["citation", "citations"] and is_citation:
         langnamecode = getparam(t, "lang") or getparam(t, "1")
-      elif tn in name_templates:
+      if tn in templates_to_process:
         if getparam(t, langparam):
-          continue
-        if not langnamecode:
+          pass
+        elif not langnamecode:
           pagemsg("WARNING: Unrecognized language %s, unable to add language to %s" % (langname, origt))
-          continue
-        notes.append("infer %s=%s for {{%s}} based on section it's in" % (
-          langparam, langnamecode, tn))
-        newline = "\n" if "\n" in unicode(t.name) else ""
-        if langparam == "1":
-          if t.has("lang"):
-            pagemsg("WARNING: Template has lang=, removing: %s" % origt)
-            notes.append("remove lang= from {{%s}}" % tn)
-            rmparam(t, "lang")
-          t.add(langparam, langnamecode + newline, preserve_spacing=False)
         else:
-          # Fetch all params.
-          params = []
-          for param in t.params:
-            pname = unicode(param.name)
-            params.append((pname, param.value, param.showkey))
-          # Erase all params.
-          del t.params[:]
-          t.add(langparam, langnamecode + newline, preserve_spacing=False)
-          # Put remaining parameters in order.
-          for name, value, showkey in params:
-            t.add(name, value, showkey=showkey, preserve_spacing=False)
-        pagemsg("Replaced <%s> with <%s>" % (origt, unicode(t)))
+          notes.append("infer %s=%s for {{%s}} based on section it's in" % (
+            langparam, langnamecode, tn))
+          newline = "\n" if "\n" in unicode(t.name) else ""
+          if langparam == "1":
+            if t.has("lang"):
+              pagemsg("WARNING: Template has lang=, removing: %s" % origt)
+              notes.append("remove lang= from {{%s}}" % tn)
+              rmparam(t, "lang")
+            t.add(langparam, langnamecode + newline, preserve_spacing=False)
+          else:
+            # Fetch all params.
+            params = []
+            for param in t.params:
+              pname = unicode(param.name)
+              params.append((pname, param.value, param.showkey))
+            # Erase all params.
+            del t.params[:]
+            t.add(langparam, langnamecode + newline, preserve_spacing=False)
+            # Put remaining parameters in order.
+            for name, value, showkey in params:
+              t.add(name, value, showkey=showkey, preserve_spacing=False)
+      if tn in templates_to_rename:
+        blib.set_template_name(t, templates_to_rename[tn])
+        notes.append("rename {{%s}} to {{%s}}" % (tn, templates_to_rename[tn]))
+      newt = unicode(t)
+      if newt != origt:
+        pagemsg("Replaced <%s> with <%s>" % (origt, newt))
 
     return langnamecode
 
@@ -112,4 +126,4 @@ def do_process_page(page, index, parsed):
   return process_page(page, index, parsed, args.lang_in_1)
 
 blib.do_pagefile_cats_refs(args, start, end, do_process_page, edit=True,
-  default_refs=["Template:%s" % template for template in name_templates])
+  default_refs=["Template:%s" % template for template in templates_to_process])
