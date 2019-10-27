0d800f76b9fd429385bdfee08970159c341b4ec3
Ben Wing <ben@benwing.com>
2019-10-27 01:37:52 -0500

    delete: rewrite using blib.do_pagefile_cats_refs

--- delete.py
+++ delete.py
@@ -17,66 +17,33 @@
 import blib, re, codecs
 from blib import msg, errmsg, site
 import pywikibot
-from arabiclib import reorder_shadda
 
-def delete_pages(refs, page_and_refs, cat, pages, pagefile,
-    comment, filter_pages, save, verbose, startFrom, upTo):
-  if pages:
-    pages = ((index, pywikibot.Page(site, page)) for index, page in blib.iter_items(pages, startFrom, upTo))
-  elif pagefile:
-    lines = [x.strip() for x in codecs.open(pagefile, "r", "utf-8")]
-    pages = ((index, pywikibot.Page(site, page)) for index, page in blib.iter_items(lines, startFrom, upTo))
-  elif refs:
-    pages = blib.references(refs, startFrom, upTo, only_template_inclusion=False)
-  elif page_and_refs:
-    pages = blib.references(page_and_refs, startFrom, upTo, only_template_inclusion=False, include_page=True)
-  else:
-    pages = blib.cat_articles(cat, startFrom, upTo)
-  for index, page in pages:
-    pagetitle = unicode(page.title())
-    def pagemsg(txt):
-      msg("Page %s %s: %s" % (index, pagetitle, txt))
-    def errandpagemsg(txt):
-      msg("Page %s %s: %s" % (index, pagetitle, txt))
-      errmsg("Page %s %s: %s" % (index, pagetitle, txt))
-    if filter_pages and not re.search(filter_pages, pagetitle):
-      pagemsg("Skipping because doesn't match --filter-pages regex %s" %
-          filter_pages)
+def process_page(page, index, args, comment):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+  def errandpagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+    errmsg("Page %s %s: %s" % (index, pagetitle, txt))
+  if args.verbose:
+    pagemsg("Processing")
+  this_comment = comment or 'delete page'
+  if page.exists():
+    if args.save:
+      page.delete('%s (content was "%s")' % (this_comment, unicode(page.text)))
+      errandpagemsg("Deleted (comment=%s)" % this_comment)
     else:
-      if verbose:
-        pagemsg("Processing")
-      this_comment = comment or 'delete page'
-      if page.exists():
-        if save:
-          page.delete('%s (content was "%s")' % (this_comment, unicode(page.text)))
-          errandpagemsg("Deleted (comment=%s)" % this_comment)
-        else:
-          pagemsg("Would delete (comment=%s)" % this_comment)
-      else:
-        pagemsg("Skipping, page doesn't exist")
+      pagemsg("Would delete (comment=%s)" % this_comment)
+  else:
+    pagemsg("Skipping, page doesn't exist")
 
-pa = blib.init_argparser("Rename pages")
-pa.add_argument("-r", "--references", "--refs",
-    help="Do pages with references to this page")
-pa.add_argument("--page-and-refs", help="Do page and references to this page")
-pa.add_argument("-c", "--category", "--cat",
-    help="Do pages in this category")
-pa.add_argument("--comment", help="Specify the change comment to use")
-pa.add_argument('--filter-pages', help="Regex to use to filter page names.")
-pa.add_argument('--pages', help="List of pages to delete, comma-separated.")
-pa.add_argument('--pagefile', help="File containing pages to delete.")
-params = pa.parse_args()
-startFrom, upTo = blib.parse_start_end(params.start, params.end)
+params = blib.create_argparser("Delete pages", include_pagefile=True)
+params.add_argument("--comment", help="Specify the change comment to use")
+args = params.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
 
-if not params.references and not params.page_and_refs and not params.category and not params.pages and not params.pagefile:
-  raise ValueError("--references, --category, --pages, page-and-refs or --pagefile must be present")
+comment = args.comment and args.comment.decode("utf-8")
 
-references = params.references and params.references.decode("utf-8")
-page_and_refs = params.page_and_refs and params.page_and_refs.decode("utf-8")
-category = params.category and params.category.decode("utf-8")
-pages = params.pages and re.split(",", params.pages.decode("utf-8"))
-comment = params.comment and params.comment.decode("utf-8")
-filter_pages = params.filter_pages and params.filter_pages.decode("utf-8")
-
-delete_pages(references, page_and_refs, category, pages, params.pagefile,
-    comment, filter_pages, params.save, params.verbose, startFrom, upTo)
+def do_process_page(page, index):
+  return process_page(page, index, args, comment)
+blib.do_pagefile_cats_refs(args, start, end, do_process_page)
