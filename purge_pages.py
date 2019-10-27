dbeb54afac48d60fc38fee6943730be46eaf2d40
Ben Wing <ben@benwing.com>
2019-10-27 01:39:44 -0500

    purge_pages: rewrite using blib.do_pagefile_cats_refs

--- purge_pages.py
+++ purge_pages.py
@@ -6,24 +6,18 @@ import pywikibot, re, sys, codecs, argparse
 import blib
 from blib import getparam, rmparam, msg, site
 
-parser = blib.create_argparser(u"Purge (null-save) pages in category or references")
-parser.add_argument('--cat', help="Category to purge")
-parser.add_argument('--cat-subcats', help="Category whose subcategories to purge")
-parser.add_argument('--ref', help="References to purge")
+parser = blib.create_argparser(u"Purge (null-save) pages in category or references",
+  include_pagefile=True)
 parser.add_argument('--ignore-non-mainspace', help="Ignore pages not in the mainspace",
     action="store_true")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
-pages = []
-if args.cat:
-  pages_to_list = blib.cat_articles(args.cat, start, end)
-elif args.cat_subcats:
-  pages_to_list = blib.cat_subcats(args.cat_subcats, start, end)
-else:
-  pages_to_list = blib.references(args.ref, start, end)
-for i, page in pages_to_list:
+def process_page(page, index):
+  global args
   if args.ignore_non_mainspace and ':' in unicode(page.title()):
-    continue
-  # msg("Page %s %s: Null-saving" % (i, unicode(page.title())))
+    return
+  # msg("Page %s %s: Null-saving" % (index, unicode(page.title())))
   page.save(comment="null save")
+
+blib.do_pagefile_cats_refs(args, start, end, process_page)
