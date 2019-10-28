d576cae72eae2a9a303b7e4a50b91d5417673b37
Ben Wing <ben@benwing.com>
2019-10-28 00:32:12 -0500

    remove_redundant_fr_pronun,push_manual_fr_pronun_fixes: new scripts

---
+++ push_manual_fr_pronun_fixes.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, errandmsg, site
+
+def process_page(page, index, line, respelling, orig_template, repl_template,
+    args):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  if respelling == "-":
+    pagemsg("Skipping line with respelling '-': %s" % line)
+    return
+
+  if respelling == "":
+    pagemsg("WARNING: Skipping blank respelling: %s" % line)
+    return
+
+  notes = []
+
+  text = unicode(page.text)
+  if orig_template not in text:
+    pagemsg("WARNING: Can't find original template %s in text" % orig_template)
+    return
+
+  m = re.search("^.*?%s.*$" % re.escape(orig_template), text, re.M)
+  if not m:
+    pagemsg("WARNING: Couldn't find template %s in page text" % orig_template)
+    textline = "(unknown)"
+  else:
+    textline = m.group(0)
+
+  m = re.search(r"(\|pos=[a-z]+)", repl_template)
+  if m:
+    posarg = m.group(1)
+  else:
+    posarg = ""
+  if respelling == "y":
+    respellingarg = ""
+  else:
+    respellingarg = "|" + "|".join(respelling.split(","))
+  real_repl = "{{fr-IPA%s%s}}" % (respellingarg, posarg)
+
+  if "{{a|" in textline:
+    pagemsg("WARNING: Replacing %s with %s and saw accent spec on line: %s" % (
+      orig_template, real_repl, textline))
+
+  newtext, did_replace = blib.replace_in_text(text, orig_template,
+      real_repl, pagemsg)
+  text = newtext
+  if did_replace:
+    notes.append("semi-manually replace %s with %s" % (orig_template, real_repl))
+  if respelling != "y":
+    parsed = blib.parse_text(text)
+    saw_fr_conj_auto = False
+    for t in parsed.filter_templates():
+      tn = tname(t)
+      if tn == "fr-conj-auto":
+        if saw_fr_conj_auto:
+          pagemsg("WARNING: Saw {{fr-conj-auto}} twice, first=%s, second=%s" % (
+            saw_fr_conj_auto, unicode(t)))
+        saw_fr_conj_auto = unicode(t)
+        if getparam(t, "pron"):
+          pagemsg("WARNING: Already saw pron= param: %s" % unicode(t))
+          continue
+        pronarg = ",".join(pron or pagetitle for pron in respelling.split(","))
+        origt = unicode(t)
+        t.add("pron", pronarg)
+        pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+        notes.append("add pron=%s to {{fr-conj-auto}}" % pronarg)
+    text = unicode(parsed)
+
+  return text, notes
+
+parser = blib.create_argparser("Push manual {{fr-IPA}} replacements for {{IPA|fr}}")
+parser.add_argument("--direcfile", help="File of directives", required=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+for index, line in blib.iter_items(codecs.open(args.direcfile, "r", encoding="utf-8"), start, end):
+  line = line.strip()
+  m = re.search(r"^(.*?)\|Page [0-9]+ (.*?): WARNING: Can't replace (\{\{IPA\|fr\|.*?\}\}) with (\{\{.*?\}\}) because auto-generated pron .*$", line)
+  if not m:
+    errandmsg("Unrecognized line: %s" % line)
+    continue
+  respelling, page, orig_template, repl_template = m.groups()
+  def do_process_page(page, index, parsed):
+    return process_page(page, index, line, respelling, orig_template,
+        repl_template, args)
+  blib.do_edit(pywikibot.Page(site, page), index, do_process_page,
+    save=args.save, verbose=args.verbose, diff=args.diff)
---
+++ remove_redundant_fr_pronun.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, site
+
+def process_page(page, index, parsed):
+  global args
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+  def expand_text(tempcall):
+    return blib.expand_text(tempcall, pagetitle, pagemsg, args.verbose)
+
+  notes = []
+
+  pagemsg("Processing")
+
+  for t in parsed.filter_templates():
+    tn = tname(t)
+    if tn == "fr-IPA":
+      maxindex = 1
+      for i in xrange(2, 11):
+        if getparam(t, str(i)):
+          maxindex = i
+      params = []
+      redundant = []
+      pos = getparam(t, "pos")
+      pos_arg = "|pos=%s" % pos if pos else ""
+      default_autopron = expand_text("{{#invoke:fr-pron|show|%s%s}}" % (pagetitle, pos_arg))
+      if not default_autopron:
+        continue
+      origt = unicode(t)
+      for i in xrange(1, maxindex + 1):
+        pron = getparam(t, str(i))
+        if pron:
+          autopron = expand_text("{{#invoke:fr-pron|show|%s%s}}" % (
+            pron, pos_arg))
+          if not autopron:
+            continue
+          if autopron == default_autopron:
+            if maxindex == 1:
+              rmparam(t, "1")
+              notes.append("remove redundant respelling %s from {{fr-IPA}}" %
+                  pron)
+            elif i < maxindex:
+              t.add(str(i), "")
+              notes.append("set redundant respelling %s in {{fr-IPA}} to blank" %
+                  pron)
+            else:
+              pagemsg("WARNING: Can't remove redundant last pronunciation when multiple pronunciations: %s" %
+                  unicode(t))
+      if unicode(t) != origt:
+        pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+
+  return unicode(parsed), notes
+
+parser = blib.create_argparser("Remove redundant French respelling from {{fr-IPA}}",
+  include_pagefile=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_page, edit=True,
+  default_refs=["Template:tracking/fr-pron/redundant-pron"])
