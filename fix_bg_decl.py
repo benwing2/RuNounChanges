07f4980dafb2fdbae21d64d25b702d8a65a65e0b
Ben Wing <ben@benwing.com>
2020-04-06 22:09:08 -0500

    fix_bg_decl: Support adjectives, renamed from fix_bg_noun_decl

--- fix_bg_decl.py
+++ fix_bg_decl.py
@@ -10,7 +10,7 @@ import lalib
 
 import find_regex
 
-def process_page(index, pagename, text):
+def process_page(index, pagename, text, adj):
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagename, txt))
   def errandpagemsg(txt):
@@ -29,16 +29,16 @@ def process_page(index, pagename, text):
   headword = None
   for t in parsed.filter_templates():
     tn = tname(t)
-    if tn == "bg-noun" or tn == "bg-proper noun":
+    if tn in (adj and ["bg-adj"] or ["bg-noun", "bg-proper noun"]):
       headword = getparam(t, "1")
-    if tn.startswith("bg-noun-"):
+    if (tn == "bg-decl-adj" if adj else tn.startswith("bg-noun-")):
       origt = unicode(t)
       if not headword:
-        pagemsg("WARNING: Saw %s without {{bg-noun}} headword" % origt)
+        pagemsg("WARNING: Saw %s without {{%s}} headword" % (origt, "bg-adj" if adj else "bg-noun"))
         continue
       del t.params[:]
       t.add("1", "%s<>" % headword)
-      blib.set_template_name(t, "bg-ndecl")
+      blib.set_template_name(t, "bg-adecl" if adj else "bg-ndecl")
       pagemsg("Replacing %s with %s" % (origt, unicode(t)))
 
   text = unicode(parsed)
@@ -46,8 +46,9 @@ def process_page(index, pagename, text):
   msg(text.rstrip('\n'))
   msg("------- end text --------")
 
-parser = blib.create_argparser("Use {{bg-ndecl}} for Bulgarian declensions")
+parser = blib.create_argparser("Use {{bg-ndecl}}/{{bg-adecl}} for Bulgarian declensions")
 parser.add_argument('--direcfile', help="File containing output from find_regex.py.")
+parser.add_argument('--adj', help="Do adjectives instead of nouns", action="store_true")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
@@ -56,4 +57,4 @@ lines = codecs.open(args.direcfile, "r", "utf-8")
 pagename_and_text = find_regex.yield_text_from_find_regex(lines, args.verbose)
 for index, (pagename, text) in blib.iter_items(pagename_and_text, start, end,
     get_name=lambda x:x[0]):
-  process_page(index, pagename, text)
+  process_page(index, pagename, text, args.adj)
