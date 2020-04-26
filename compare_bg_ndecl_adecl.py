cf464ee08c954c4f4d0fcf45d07530059b16e1af
Ben Wing <ben@benwing.com>
2020-04-26 01:43:00 -0500

    compare_bg_ndecl_adecl,find_bg_red_links: new scripts

---
+++ compare_bg_ndecl_adecl.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, msg, errandmsg, site
+
+import lalib
+
+def compare_new_and_old_templates(t, pagetitle, pagemsg, errandpagemsg):
+  global args
+  def expand_text(tempcall):
+    return blib.expand_text(tempcall, pagetitle, pagemsg, args.verbose)
+
+  def generate_old_forms():
+    old_generate_template = re.sub(r"^\{\{bg-ndecl\|", "{{bg-generate-noun-forms|", t)
+    old_generate_template = re.sub(r"^\{\{bg-adecl\|", "{{bg-generate-adj-forms|", old_generate_template)
+    old_result = expand_text(old_generate_template)
+    if not old_result:
+      return None
+    return old_result
+
+  def generate_new_forms():
+    new_generate_template = re.sub(r"^\{\{bg-ndecl\|", "{{User:Benwing2/bg-generate-noun-forms|", t)
+    new_generate_template = re.sub(r"^\{\{bg-adecl\|", "{{User:Benwing2/bg-generate-adj-forms|", new_generate_template)
+    new_result = expand_text(new_generate_template)
+    if not new_result:
+      return None
+    return new_result
+
+  return blib.compare_new_and_old_template_forms(t, t, generate_old_forms,
+    generate_new_forms, pagemsg, errandpagemsg)
+
+def process_page(page, index):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+  def errandpagemsg(txt):
+    errandmsg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+
+  parsed = blib.parse_text(unicode(page.text))
+
+  for t in parsed.filter_templates():
+    tn = tname(t)
+    if tn == "bg-ndecl" or tn == "bg-adecl":
+      compare_new_and_old_templates(unicode(t), pagetitle, pagemsg, errandpagemsg)
+
+parser = blib.create_argparser("Check potential changes to {{bg-ndecl}} or {{bg-adecl}} implementation",
+    include_pagefile=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_page,
+    default_refs=["Template:bg-ndecl", "Template:bg-adecl"])
---
+++ find_bg_red_links.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+#    find_bg_red_links.py is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Find redlinks (non-existent pages).
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site
+
+parser = blib.create_argparser(u"Find red links")
+parser.add_argument("--pagefile", help="File containing pages to check")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+lemmas = set()
+msg("Reading Bulgarian lemmas")
+for i, page in blib.cat_articles("Bulgarian lemmas", start, end):
+  lemmas.add(unicode(page.title()))
+
+lines = [x.strip() for x in codecs.open(args.pagefile, "r", "utf-8")]
+words = lines
+
+for i, line in blib.iter_items(words, start, end):
+  pagename, freq = line.split("\t")
+  m = re.search(u"[^-Ѐ-џҊ-ԧꚀ-ꚗ]", pagename)
+  def fmsg(txt):
+    msg("Page %s [[%s]]: %s (freq %s)" % (i, pagename, txt, freq))
+  if m:
+    fmsg("skipped due to non-Cyrillic characters")
+  else:
+    for pagenm, pagetype in [(pagename, ""),
+        (pagename.capitalize(), " (capitalized)"),
+        (pagename.upper(), " (uppercased)")]:
+      if pagenm in lemmas:
+        fmsg("exists%s" % pagetype)
+        break
+      else:
+        page = pywikibot.Page(site, pagenm)
+        if page.exists():
+          text = unicode(page.text)
+          if re.search("#redirect", text, re.I):
+            fmsg("exists%s as redirect" % pagetype)
+          elif re.search(r"\{\{superlative of\|bg\|", text):
+            fmsg("exists%s as superlative" % pagetype)
+          elif "==Bulgarian==" in text:
+            fmsg("exists%s as non-lemma" % pagetype)
+          else:
+            fmsg("exists%s only in some other language" % pagetype)
+          break
+    else:
+      fmsg("does not exist")
