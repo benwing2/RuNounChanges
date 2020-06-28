2e0c0cb6cab8584e4154edbe0ef2685425b7c080
Ben Wing <ben@benwing.com>
2020-06-27 21:19:58 -0500

    uklib: remove_non_primary_accents, remove_monosyllabic_stress, needs_accent -> needs_accents

--- uklib.py
+++ uklib.py
@@ -4,7 +4,25 @@
 import re, sys
 
 AC = u"\u0301"
-vowel = u"аеиоуіїяєюАЕИОУІЇЯЄЮ"
+GR = u"\u0300" # grave =  ̀
+
+# non-primary accents (i.e. excluding acute) that indicate pronunciation
+# (not counting diaeresis, which indicates a completely different vowel,
+# and caron, which is used in translit as ě to indicate the yat vowel)
+non_primary_pron_accents = GR
+# accents that indicate pronunciation (not counting diaresis, which indicates
+# a completely different vowel)
+pron_accents = AC + non_primary_pron_accents
+# all accents
+accents = pron_accents
+# accents indicating stress (primary or otherwise)
+stress_accents = AC + GR
+
+# regex for any optional accent(s)
+opt_accent = "[" + accents + "]*"
+
+composed_grave_vowel = u"ѐЀѝЍ"
+vowel = u"аеиоуіїяєюАЕИОУІЇЯЄЮ" + composed_grave_vowel
 vowel_c = "[" + vowel + "]"
 non_vowel_c = "[^" + vowel + "]"
 cons_except_hushing_or_ts = u"бдфгґйклмнпрствхзь'БДФГҐЙКЛМНПРСТВХЗЬ"
@@ -22,8 +40,26 @@ velar_c = "[" + velar + "]"
 uppercase = u"АЕИОУІЇЯЄЮБЦДФГҐЧЙКЛМНПРСТВШХЗЖЬЩ"
 uppercase_c = "[" + uppercase + "]"
 
+grave_deaccenter = {
+    GR:"", # grave accent
+    u"ѐ":u"е", # composed Cyrillic chars w/grave accent
+    u"Ѐ":u"Е",
+    u"ѝ":u"и",
+    u"Ѝ":u"И",
+}
+
+deaccenter = grave_deaccenter.copy()
+deaccenter[AC] = "" # acute accent
+
 def remove_accents(word):
-  return word.replace(AC, "")
+  # remove pronunciation accents
+  return re.sub("([" + pron_accents + u"ѐЀѝЍ])",
+    lambda m: deaccenter[m.group(1)], word)
+
+def remove_non_primary_accents(word):
+  # remove all pronunciation accents except acute
+  return re.sub("([" + non_primary_pron_accents + u"ѐЀѝЍ])",
+    lambda m: deaccenter[m.group(1)], word)
 
 def is_unstressed(word):
   return AC not in word
@@ -44,8 +80,30 @@ def add_monosyllabic_stress(word):
   else:
     return word
 
-def needs_accent(word):
-  return not is_monosyllabic(word) and is_unstressed(word)
+def remove_monosyllabic_stress(word):
+  if is_monosyllabic(word) and not word.startswith("-"):
+    return remove_accents(word)
+  return word
+
+# Does a phrase of connected text need accents? We need to split by word
+# and check each one.
+def needs_accents(text, split_dash=False):
+  # A word needs accents if it is unstressed and contains more than one vowel;
+  # but if split_dash, allow cases like динь-динь with multiple monosyllabic
+  # words separated by a hyphen. We don't just split on hyphens at top level
+  # otherwise a word like Али-Баба́ will "need accents".
+  def word_needs_accents(word):
+    if not is_unstressed(word):
+      return False
+    for sw in re.split(r"-", word) if split_dash else [word]:
+      if not is_monosyllabic(sw):
+        return True
+    return False
+  words = re.split(r"\s", text)
+  for word in words:
+    if word_needs_accents(word):
+      return True
+  return False
 
 def is_end_stressed(word, possible_endings=[]):
   for ending in possible_endings:
