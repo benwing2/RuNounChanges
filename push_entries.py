c4147339961e1ca6725eafe82411a19203e56588
Ben Wing <ben@benwing.com>
2020-06-27 21:22:39 -0500

    push_entries: renamed from push_bg_entries and modified to support all langs

--- push_entries.py
+++ push_entries.py
@@ -8,7 +8,7 @@ import pywikibot, re, sys, codecs, argparse
 
 import find_regex
 
-def process_page(index, page, contents, verbose, comment):
+def process_page(index, page, contents, lang, verbose, comment):
   pagetitle = unicode(page.title())
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagetitle, txt))
@@ -33,10 +33,10 @@ def process_page(index, page, contents, verbose, comment):
         errandpagemsg("WARNING: Saw bad second-level header: %s" % sections[j - 1].strip())
         return
       foundlang = m.group(1)
-      if foundlang == "Bulgarian":
-        errandpagemsg("WARNING: Already found Bulgarian section")
+      if foundlang == lang:
+        errandpagemsg("WARNING: Already found %s section" % lang)
         return
-      if foundlang > "Bulgarian":
+      if foundlang > lang:
         insert_before = j - 1
         break
     if insert_before == 0:
@@ -47,9 +47,10 @@ def process_page(index, page, contents, verbose, comment):
     return "".join(sections), comment
 
 if __name__ == "__main__":
-  parser = blib.create_argparser("Push new entries from generate_bg_entries.py")
+  parser = blib.create_argparser("Push new entries from generate_entries.py")
   parser.add_argument('--direcfile', help="File containing entries.")
   parser.add_argument('--comment', help="Comment to use.", required="true")
+  parser.add_argument('--lang', help="Language of entries.", required="true")
   args = parser.parse_args()
   start, end = blib.parse_start_end(args.start, args.end)
 
@@ -59,6 +60,6 @@ if __name__ == "__main__":
   for index, (pagename, text) in blib.iter_items(pagename_and_text, start, end,
       get_name=lambda x:x[0]):
     def do_process_page(page, index, parsed):
-      return process_page(index, page, text, args.verbose, args.comment.decode('utf-8'))
+      return process_page(index, page, text, args.lang, args.verbose, args.comment.decode('utf-8'))
     blib.do_edit(pywikibot.Page(site, pagename), index, do_process_page,
         save=args.save, verbose=args.verbose, diff=args.diff)
