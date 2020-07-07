b2f7bf57820a3dab87173d206970f00f52691670
Ben Wing <ben@benwing.com>
2020-07-07 00:27:01 -0500

    find_red_links: rewrite

--- find_red_links.py
+++ find_red_links.py
@@ -23,50 +23,50 @@ from blib import getparam, rmparam, msg, site
 
 parser = blib.create_argparser(u"Find red links")
 parser.add_argument("--pagefile", help="File containing pages to check")
-parser.add_argument("--with-freq", help="Words come with frequencies and possibly occur multiple times.", action="store_true")
+parser.add_argument("--lang", help="Language of terms")
+parser.add_argument("--field", help="Field containing terms", type=int, default=1)
+parser.add_argument("--output-orig", help="Output original lines", action="store_true")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
 lemmas = set()
-msg("Reading Russian lemmas")
-for i, page in blib.cat_articles("Russian lemmas", start, end):
+msg("Reading %s lemmas" % args.lang)
+for i, page in blib.cat_articles("%s lemmas" % args.lang, start, end):
   lemmas.add(unicode(page.title()))
 
 words_freq = {}
 
-lines = [x.strip() for x in codecs.open(args.pagefile, "r", "utf-8")]
-if args.with_freq:
-  for line in lines:
-    freq, word = re.split(r"\s", line)
-    freq = int(freq)
-    if word in words_freq:
-      words_freq[word] += freq
-    else:
-      words_freq[word] = freq
-  words = [x[0] for x in sorted(words_freq.items(), key=lambda y:-y[1])]
-else:
-  words = lines
+lines = [re.split(r"\s", x.strip()) for x in codecs.open(args.pagefile, "r", "utf-8")]
+lines = [(x[args.field - 1], x) for x in lines]
 
-for i, pagename in blib.iter_items(words, start, end):
-  m = re.search(u"[^-Ѐ-џҊ-ԧꚀ-ꚗ]", pagename)
+for i, (pagename, origline) in blib.iter_items(lines, start, end):
+  m = re.search(u"[^-'Ѐ-џҊ-ԧꚀ-ꚗ]", pagename)
   if m:
-    msg("Page %s [[%s]]: skipped due to non-Cyrillic characters" % (i, pagename))
+    outtext = "skipped due to non-Cyrillic characters"
   else:
     for pagenm, pagetype in [(pagename, ""),
         (pagename.capitalize(), " (capitalized)"),
         (pagename.upper(), " (uppercased)")]:
       if pagenm in lemmas:
-        msg("Page %s [[%s]]: exists%s" % (i, pagename, pagetype))
+        outtext = "exists%s" % pagetype
         break
       else:
         page = pywikibot.Page(site, pagenm)
         if page.exists():
-          if re.search("#redirect", unicode(page.text), re.I):
-            msg("Page %s [[%s]]: exists%s as redirect" % (i, pagename, pagetype))
-          elif re.search(r"\{\{superlative of", unicode(page.text)):
-            msg("Page %s [[%s]]: exists%s as superlative" % (i, pagename, pagetype))
+          text = unicode(page.text)
+          if re.search("#redirect", text, re.I):
+            outtext = "exists%s as redirect" % pagetype
+          elif re.search(r"\{\{superlative of", text):
+            outtext = "exists%s as superlative" % pagetype
+          elif "==%s==" % args.lang in text:
+            outtext = "exists%s as non-lemma" % pagetype
           else:
-            msg("Page %s [[%s]]: exists%s as non-lemma" % (i, pagename, pagetype))
+            outtext = "exists%s only in some other language" % pagetype
           break
     else:
-      msg("Page %s [[%s]]: does not exist" % (i, pagename))
+      outtext = "does not exist"
+  if args.output_orig:
+    msg("| %s || %s || %s" % (i, " || ".join(origline), outtext))
+    msg("|-")
+  else:
+    msg("Page %s [[%s]]: %s" % (i, pagename, outtext))
