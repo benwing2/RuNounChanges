8a7e1425385950665199bee9f33a8e9ac6561b41
Ben Wing <ben@benwing.com>
2020-07-21 00:27:51 -0500

    format_be_decl_noun: new script

---
+++ format_be_decl_noun.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, site
+
+import belib
+
+def process_page(page, index, parsed):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  notes = []
+
+  pagemsg("Processing")
+
+  head = None
+  for t in parsed.filter_templates():
+    origt = unicode(t)
+    tn = tname(t)
+    if tn == "be-decl-noun":
+      t.name = "be-decl-noun\n"
+      for i in [2, 4, 6, 8, 10, 12]:
+        val = getparam(t, str(i)).strip()
+        if val:
+          t.add(str(i), val + "\n", preserve_spacing=False)
+    if origt != unicode(t):
+      pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+      notes.append("format {{be-decl-noun}} using newlines")
+
+  return unicode(parsed), notes
+
+parser = blib.create_argparser(u"Format be-decl-noun using newlines",
+    include_pagefile=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_page,
+    default_refs=["Template:be-decl-noun"], edit=True)
