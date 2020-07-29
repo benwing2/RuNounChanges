c27131aa5a8c37958eb99886721e711d2700d43e
Ben Wing <ben@benwing.com>
2020-07-28 20:22:06 -0500

    convert_uk_noun_headword -> convert_uk_be_noun_headword, support Belarusian

--- convert_uk_be_noun_headword.py
+++ convert_uk_be_noun_headword.py
@@ -7,6 +7,7 @@ import blib
 from blib import getparam, rmparam, msg, site, tname
 
 import uklib as uk
+import belib as be
 
 AC = u"\u0301"
 
@@ -29,7 +30,7 @@ def process_page(page, index, parsed):
   plurals = None
   for t in parsed.filter_templates():
     tn = tname(t)
-    if tn in ["uk-noun", "uk-proper noun"]:
+    if tn in [args.lang + "-noun", args.lang + "-proper noun"]:
       if heads:
         pagemsg("WARNING: Encountered headword twice without declension: %s" % unicode(t))
         return
@@ -39,11 +40,12 @@ def process_page(page, index, parsed):
       gender_and_animacy = blib.fetch_param_chain(t, "2", "g")
       genitives = blib.fetch_param_chain(t, "3", "gen")
       plurals = blib.fetch_param_chain(t, "4", "pl")
-    if tn == "uk-ndecl":
+      genitive_plurals = blib.fetch_param_chain(t, "5", "genpl")
+    if tn == args.lang + "-ndecl":
       if not heads:
         pagemsg("WARNING: Encountered decl without headword: %s" % unicode(t))
         return
-      generate_template = re.sub(r"^\{\{uk-ndecl\|", "{{User:Benwing2/uk-generate-prod-noun-props|",
+      generate_template = re.sub(r"^\{\{%s-ndecl\|" % args.lang, "{{User:Benwing2/%s-generate-prod-noun-props|" % args.lang,
           unicode(t))
       result = expand_text(generate_template)
       if not result:
@@ -51,9 +53,15 @@ def process_page(page, index, parsed):
       new_forms = blib.split_generate_args(result)
       new_g = new_forms["g"].split(",")
       def compare(old, new, stuff, nocanon=False):
+        if not old:
+          return True
         if not nocanon:
-          old = [uk.remove_monosyllabic_stress(x) for x in old]
-          new = [uk.remove_monosyllabic_stress(x) for x in new]
+          remove_monosyllabic_accents = (
+            uk.remove_monosyllabic_stress if args.lang == "uk" else
+            be.remove_monosyllabic_accents
+          )
+          old = [remove_monosyllabic_accents(blib.remove_links(x)) for x in old]
+          new = [remove_monosyllabic_accents(x) for x in new]
         if set(old) != set(new):
           pagemsg("WARNING: Old %ss %s disagree with new %ss %s: head=%s, decl=%s" % (
             stuff, ",".join(old), stuff, ",".join(new), unicode(headt), unicode(t)))
@@ -76,8 +84,9 @@ def process_page(page, index, parsed):
       else:
         if (not compare(heads, new_forms.get("nom_s", "-").split(","), "nom sg") or
             not compare(genitives, new_forms.get("gen_s", "-").split(","), "gen sg") or
-            # 'uk-proper noun' headwords don't have nominative plural set
-            headtn == "uk-noun" and not compare(plurals, new_forms.get("nom_p", "-").split(","), "nom pl")):
+            # 'uk/be-proper noun' headwords don't have nominative plural set
+            headtn == args.lang + "-noun" and not compare(plurals, new_forms.get("nom_p", "-").split(","), "nom pl") or
+            headtn == args.lang + "-noun" and not compare(genitive_plurals, new_forms.get("gen_p", "-").split(","), "gen pl")):
           heads = None
           continue
       decl = getparam(t, "1")
@@ -85,13 +94,19 @@ def process_page(page, index, parsed):
       blib.remove_param_chain(headt, "2", "g")
       blib.remove_param_chain(headt, "3", "gen")
       blib.remove_param_chain(headt, "4", "pl")
-      notes.append("convert {{uk-noun}} to new style using decl %s" % decl)
+      blib.remove_param_chain(headt, "5", "genpl")
+      notes.append("convert {{%s-noun}} to new style using decl %s" % (args.lang, decl))
       heads = None
   return unicode(parsed), notes
 
-parser = blib.create_argparser("Convert {{uk-noun}} to new style", include_pagefile=True)
+parser = blib.create_argparser("Convert {{uk-noun}}/{{be-noun}} to new style", include_pagefile=True)
+parser.add_argument("--lang", required=True, help="Language (uk or be)")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
+if args.lang not in ["uk", "be"]:
+  raise ValueError("Unrecognized language: %s" % args.lang)
+langname = "Ukrainian" if args.lang == "uk" else "Belarusian"
+
 blib.do_pagefile_cats_refs(args, start, end, process_page,
-    default_cats=["Ukrainian proper nouns"], edit=True)
+    default_cats=[langname + " nouns", langname + " proper nouns"], edit=True)
