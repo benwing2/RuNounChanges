68888ac3aca3fd503ab22f8b07cc31fe3fbc46f5
Ben Wing <ben@benwing.com>
2020-08-06 22:55:01 -0500

    fix_bg_grave: new script

---
+++ fix_bg_grave.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site, tname, pname
+
+import bglib
+from bglib import AC, GR
+
+def process_page(page, index, parsed):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  pagemsg("Processing")
+
+  notes = []
+
+  text = unicode(page.text)
+  parsed = blib.parse_text(text)
+  for t in parsed.filter_templates():
+    tn = tname(t)
+    origt = unicode(t)
+    param = None
+    if tn in ["bg-noun", "bg-proper noun", "bg-verb", "bg-adj", "bg-adv",
+        "bg-part", "bg-part form", "bg-verbal noun", "bg-verbal noun form",
+        "bg-phrase"]:
+      param = "1"
+    elif tn == "head" and getparam(t, "1") == "bg":
+      param = "head"
+    if param:
+      val = getparam(t, param)
+      val = bglib.decompose(val)
+      if GR in val:
+        val = val.replace(GR, AC)
+        t.add(param, val)
+        notes.append("convert grave to acute in {{%s}}" % tn)
+    if unicode(t) != origt:
+      pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+  return unicode(parsed), notes
+
+parser = blib.create_argparser("Change grave to acute in Bulgarian headwords",
+    include_pagefile=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_page,
+    default_cats=["Bulgarian lemmas", "Bulgarian non-lemma forms"], edit=1)
