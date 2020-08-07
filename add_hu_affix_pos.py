b02cc71e2263804e05107c81bcbad2b01dd71ccc
Ben Wing <ben@benwing.com>
2020-08-06 22:54:27 -0500

    add_hu_affix_pos: new script

---
+++ add_hu_affix_pos.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, site
+
+import belib
+
+def process_page(page, index, parsed):
+  pagetitle = unicode(page.title())
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  notes = []
+
+  pagemsg("Processing")
+  text = unicode(page.text)
+
+  retval = blib.find_modifiable_lang_section(text, "Hungarian", pagemsg)
+  if retval is None:
+    pagemsg("WARNING: Couldn't find Hungarian section")
+    return
+  sections, j, secbody, sectail, has_non_lang = retval
+  if "==Alternative forms==" in secbody:
+    pagemsg("WARNING: Skipping page with 'Alternative forms' section")
+    return
+
+  parsed = blib.parse_text(secbody)
+  for t in parsed.filter_templates():
+    origt = unicode(t)
+    tn = tname(t)
+    if tn in ["compound", "affix", "af"] and getparam(t, "1") == "hu" and not getparam(t, "pos"):
+      t.add("pos", "noun")
+    if origt != unicode(t):
+      pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+      notes.append("add pos=noun to {{%s|hu}}" % tn)
+  sections[j] = unicode(parsed) + sectail
+  text = "".join(sections)
+  return text, notes
+
+parser = blib.create_argparser(u"Add pos=noun to Hungarian compound words",
+    include_pagefile=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_page,
+    default_cats=["Hungarian compound words"], edit=True)
