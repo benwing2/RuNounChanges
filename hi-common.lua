fbe13bb9e2834a7c96462d36fa3d0b80e8e5b071
Ben Wing <ben@benwing.com>
2020-09-01 01:03:46 -0500

    hi-common: latest from Wiktionary

--- hi-common.lua
+++ hi-common.lua
@@ -18,8 +18,38 @@ local rsplit = mw.text.split
 local rfind = mw.ustring.find
 local rmatch = mw.ustring.match
 local rsubn = mw.ustring.gsub
+local usub = mw.ustring.sub
 
+-- vowel diacritics; don't display nicely on their own
+local AA = u(0x093e)
+local AI = u(0x0948)
+local AU = u(0x094c)
+local E = u(0x0947)
+local I = u(0x093f)
+local II = u(0x0940)
+local O = u(0x094b)
+local U = u(0x0941)
+local UU = u(0x0942)
+local R = u(0x0943)
 local VIRAMA = u(0x094d)
+local TILDE = u(0x0303)
+
+local diacritic_to_independent = {
+	[AA] = "आ", [I] = "इ", [II] = "ई", [U] = "उ", [UU] = "ऊ",
+	[R] = "ऋ", [E] = "ए", [AI] = "ऐ", [O] = "ओ", [AU] = "औ",
+}
+
+local diacritic_list = {}
+local independent_list = {}
+for dia, ind in pairs(diacritic_to_independent) do
+	table.insert(independent_list, ind)
+	table.insert(diacritic_list, dia)
+end
+
+local diacritics = table.concat(diacritic_list)
+local independents = table.concat(independent_list) .. "अ"
+local vowels = diacritics .. independents
+local transliterated_diacritics = "aāãeẽiīĩoõuūũṛ" .. TILDE
 
 
 -- version of rsubn() that discards all but the first return value
@@ -70,26 +100,35 @@ function export.add_form(base, stem, translit_stem, slot, ending, footnotes)
 		-- '-a' dropped. A third case we need to handle correctly is इंटरव्यू "interview";
 		-- if we truncate the final ू  '-ū' and then transliterate, we get 'iṇṭarvya'
 		-- with extra '-a' that may appear in the transliteration if we're not careful.
-		elseif rfind(stem, "a$") and rfind(base.lemma_translit, "a$") then
-			return rsub(stem, "a$", "") .. ending
+		--
+		-- HACK! Handle प्रातः correctly by checking specially for lemma_translit ending
+		-- in -a or -aḥ and ending starting with a vowel. The proper way to do this
+		-- correctly that handles all of the above cases requires access to the original
+		-- (Devanagari) ending, and checks to see if the stem ends in '-a' and the ending
+		-- begins with a Devanagari diacritic; in this case, it's correct to elide the '-a'.
+		elseif rfind(stem, "a$") and rfind(base.lemma_translit, "aḥ?$") and
+			rfind(ending, "^[" .. transliterated_diacritics .. "]") then
+			stem = rsub(stem, "a$", "")
 		elseif rfind(stem, VIRAMA .. "$") and rfind(base.lemma, VIRAMA .. "$") then
-			return rsub(stem, VIRAMA .. "$", "") .. ending
-		else
-			return stem .. ending
+			stem = rsub(stem, VIRAMA .. "$", "")
 		end
+		if stem == "" or rfind(stem, "[" .. vowels .. "]$") then
+			-- A diacritic at the beginning of the ending should be converted to its independent form
+			-- if the stem does not end in a consonant.
+			if rfind(ending, "^[" .. diacritics .. "]") then
+				local ending_first = usub(ending, 1, 1)
+				ending = (diacritic_to_independent[ending_first] or ending_first) .. usub(ending, 2)
+			end
+		end
+		-- Don't convert independent letters to diacritics after consonants because of cases like मई
+		-- where the independent letter belongs after the consonant.
+		return stem .. ending
 	end
 
 	footnotes = iut.combine_footnotes(base.footnotes, footnotes)
 	local ending_obj = iut.generate_form(ending, footnotes)
 	if translit_stem then
-		-- Check to see if manual translit for form would be same as auto translit
-		-- and if so, remove it, so it doesn't get propagated to accelerators.
-		local form_with_ending = combine_stem_ending(stem, ending)
-		local form_with_ending_translit = lang:transliterate(form_with_ending)
-		local translit_with_ending = combine_stem_ending(translit_stem, lang:transliterate(ending))
-		if form_with_ending_translit ~= translit_with_ending then
-			stem = {form = stem, translit = translit_stem}
-		end
+		stem = {form = stem, translit = translit_stem}
 	end
 	iut.add_forms(base.forms, slot, stem, ending_obj, combine_stem_ending, lang,
 		combine_stem_ending)
@@ -101,10 +140,10 @@ function export.strip_ending(base, ending)
 	if not stem then
 		error("Internal error: Lemma " .. base.lemma .. " should end in " .. ending)
 	end
-	local ending_translit = lang:transliterate(ending)
+	ending_translit = lang:transliterate(ending)
 	local translit_stem = rmatch(base.lemma_translit, "^(.*)" .. ending_translit .. "$")
 	if not translit_stem then
-		error("Internal error: Unable to strip ending " .. ending .. " (transliterated " .. ending_translit .. ") from transliteration " .. base.lemma_translit)
+		error("Unable to strip ending " .. ending .. " (transliterated " .. ending_translit .. ") from transliteration " .. base.lemma_translit)
 	end
 	return stem, translit_stem	
 end
@@ -128,6 +167,36 @@ function export.normalize_all_lemmas(alternant_multiword_spec)
 			base.lemma_translit = lang:transliterate(base.lemma)
 		end
 	end)
+end
+
+
+--[=[
+Remove redundant translit. We need to do all declension using manual translit
+because declined forms of words like अचकन "jacket" and कालाधन "blood money" may
+need manual translit, even though the base form itself doesn't need manual
+translit. For example, अचकन has default translit ''ackan'', which is correct,
+but the oblique plural अचकनों has default translit ''acaknõ'', which is incorrect
+(correct is ''ackanõ'', with stem translit as in the base). In general, such
+words need manual translit that forces the stem of the inflected form to have
+the same translit as the stem of the base form. We want to remove redundant
+manual translit so that accelerator-generated entries don't have unnecessary
+manual translit in them. Finally, we need to remove redundant manual translit at
+the very end, not as we decline each part of a multiword expression (which we
+used to do), because of cases like कालाधन, which declines as if written काला धन.
+Hence it has oblique plural कालेधनों, which has incorrect default translit
+''kaledhnõ'' instead of correct ''kaledhanõ'', even though the individual parts
+काले and धनों both have correct default translits.
+]=]
+function export.remove_redundant_translit(alternant_multiword_spec)
+	for slot, forms in pairs(alternant_multiword_spec.forms) do
+		alternant_multiword_spec.forms[slot] = iut.map_forms(forms, function(form, translit)
+			if translit and lang:transliterate(form) == translit then
+				return form, nil
+			else
+				return form, translit
+			end
+		end)
+	end
 end
 
 
