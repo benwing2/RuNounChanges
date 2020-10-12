84db8f3648278f6b8de21ebf983627428e22acff
Ben Wing <ben@benwing.com>
2020-10-12 00:58:48 -0500

    rename_lang_of_entry,remove_cats: new scripts

---
+++ remove_cats.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site, tname
+
+blib.getLanguageData()
+
+topics_templates = ["topics", "topic", "top", "c", "C", "catlangcode"]
+catlangname_templates = ["catlangname", "cln"]
+categorize_templates = ["categorize", "cat"]
+
+def process_text_on_page(index, pagetitle, text):
+  global args
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  if ":" in pagetitle and not re.search("^(Appendix|Reconstruction|Citations):", pagetitle):
+    return
+
+  origtext = text
+  pagemsg("Processing")
+  notes = []
+  removed_cats = []
+
+  def should_remove_cat(cat):
+    return re.match(args.regex + "$", cat)
+
+  parsed = blib.parse_text(text)
+
+  text_to_remove = []
+  for t in parsed.filter_templates():
+    tn = tname(t)
+    if tn in topics_templates or tn in catlangname_templates or tn in categorize_templates:
+      lang = getparam(t, "1").strip()
+      cats = []
+      for paramno in xrange(2, 30):
+        cat = getparam(t, str(paramno)).strip()
+        if cat:
+          cats.append(cat)
+      filtered_cats = []
+      for cat in cats:
+        if tn in topics_templates:
+          full_cat = "%s:%s" % (lang, cat)
+        elif tn in categorize_templates:
+          full_cat = cat
+        else:
+          if lang not in blib.languages_byCode:
+            pagemsg("WARNING: Saw unrecognized language code '%s'" % lang)
+            return
+          else:
+            full_cat = "%s %s" % (blib.languages_byCode[lang]["canonicalName"], cat)
+        if should_remove_cat(full_cat):
+          if full_cat not in removed_cats:
+            removed_cats.append(full_cat)
+        else:
+          filtered_cats.append(cat)
+      if cats == filtered_cats:
+        continue
+      non_numbered_params = []
+      for param in t.params:
+        pname = unicode(param.name).strip()
+        pval = unicode(param.value).strip()
+        showkey = param.showkey
+        if not re.search("^[0-9]+$", pname):
+          non_numbered_params.append((pname, pval, showkey))
+      if filtered_cats:
+        origt = unicode(t)
+        # Erase all params.
+        del t.params[:]
+        # Put back new params.
+        t.add("1", lang)
+        for catind, cat in enumerate(filtered_cats):
+          t.add(str(catind + 2), cat)
+        for pname, pval, showkey in non_numbered_params:
+          t.add(pname, pval, showkey=showkey, preserve_spacing=False)
+        if origt != unicode(t):
+          pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+      else:
+        text_to_remove.append(unicode(t))
+  text = unicode(parsed)
+
+  for m in re.finditer(r"\[\[(?:Category|CAT):(.*?)\]\]\n?", text):
+    cat = m.group(1)
+    cat = re.sub(r"\|.*", "", cat)
+    if should_remove_cat(cat):
+      text_to_remove.append(m.group(0))
+      if m.group(1) not in removed_cats:
+        removed_cats.append(m.group(1))
+
+  for remove_it in text_to_remove:
+    text, did_replace = blib.replace_in_text(text, remove_it, "", pagemsg, no_found_repl_check=True)
+    if not did_replace:
+      return
+    pagemsg("Removed %s" % remove_it.replace("\n", r"\n"))
+
+  text = re.sub(r"\n\n+", "\n\n", text)
+  if removed_cats:
+    notes.append("remove categories: %s" % ",".join(removed_cats))
+  if text != origtext and not notes:
+    notes.append("condense 3+ newlines")
+  return text, notes
+
+parser = blib.create_argparser("Remove categories based on a regex", include_pagefile=True, include_stdin=True)
+parser.add_argument("--regex", required=True, help="Regex matching full category name to remove.")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True)
---
+++ rename_lang_of_entry.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site, tname
+
+def process_text_on_page(index, pagetitle, text):
+  global args
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  if ":" in pagetitle and not re.search("^(Appendix|Reconstruction|Citations):", pagetitle):
+    return
+
+  origtext = text
+  pagemsg("Processing")
+  notes = []
+
+  # Split into sections
+  splitsections = re.split(r"(^[ \t]*==[ \t]*[^=\n]+?[ \t]*==[ \t]*?\n)", text, 0, re.M)
+  pagehead = splitsections[0]
+
+  # Convert to a list of three items: language name, section header, section text minus separator.
+  keyed_sections = []
+  for i in range(1, len(splitsections), 2):
+    m = re.search(r"=\s*([^=\n]+?)\s*=", splitsections[i])
+    assert m
+    langname = m.group(1)
+    secheader = splitsections[i]
+    sectext = splitsections[i + 1]
+    m = re.search(r"^(.*?\n)(\n*--+\n*)$", sectext, re.S)
+    if m:
+      sectext = m.group(1)
+    keyed_sections.append([langname, secheader, sectext])
+
+  # Make sure new language section not already present.
+  for i in range(len(keyed_sections)):
+    if keyed_sections[i][0] == args.tolang:
+      pagemsg("WARNING: Already saw %s section, skipping" % args.tolang)
+      return
+
+  # Change language name.
+  for i in range(len(keyed_sections)):
+    if keyed_sections[i][0] == args.fromlang:
+      keyed_sections[i][0] = args.tolang
+      keyed_sections[i][1] = re.sub("=[ \t]*%s[ \t]*=" % re.escape(args.fromlang), "=%s=" % args.tolang, keyed_sections[i][1])
+
+  # Re-sort add combine with separators.
+  def lang_sort_key(langname):
+    if langname == "Translingual":
+      return (0, langname)
+    elif langname == "English":
+      return (1, langname)
+    else:
+      return (2, langname)
+
+  separator = "\n----\n\n"
+  text = pagehead + separator.join(
+    secheader + sectext for langname, secheader, sectext in sorted(keyed_sections, key=lambda sec: lang_sort_key(sec[0]))
+  )
+
+  if text != origtext:
+    notes.append("move %s section to %s" % (args.fromlang, args.tolang))
+  return text, notes
+
+parser = blib.create_argparser("Move entries from one language to another", include_pagefile=True, include_stdin=True)
+parser.add_argument("--fromlang", required=True, help="Existing language to rename.")
+parser.add_argument("--tolang", required=True, help="New name of language.")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True)
