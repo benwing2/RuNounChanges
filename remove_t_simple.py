8218d5a0cbfd81a5d9807c8c9855f2853cac6b5a
Ben Wing <ben@benwing.com>
2020-12-15 01:02:38 -0600

    remove_t_simple: bug fixes, support --no-remove-redundant-translit

--- remove_t_simple.py
+++ remove_t_simple.py
@@ -10,6 +10,10 @@ def process_text_on_page(index, pagetitle, text):
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagetitle, txt))
 
+  global args
+  def expand_text(tempcall):
+    return blib.expand_text(tempcall, pagetitle, pagemsg, args.verbose)
+
   notes = []
 
   pagemsg("Processing")
@@ -26,6 +30,26 @@ def process_text_on_page(index, pagetitle, text):
       rmparam(t, "g")
       if g:
         t.add("3", g)
+      if t.has("3") and not getparam(t, "3"):
+        rmparam(t, "3")
+      lang = getparam(t, "1")
+      link = getparam(t, "2")
+      alt = getparam(t, "alt")
+      trans = alt or link
+      tr = getparam(t, "tr")
+      if tr and lang and trans and not args.no_remove_redundant_translit:
+        autotr = expand_text("{{xlit|%s|%s}}" % (lang, trans))
+        if autotr and autotr == tr:
+          pagemsg("Removing redundant translit %s of %s for lang %s" % (tr, trans, lang))
+          rmparam(t, "tr")
+          notes.append("remove redundant translit from {{t-simple}}")
+      if alt and link:
+        autolink = expand_text("{{#invoke:languages/templates|makeEntryName|%s|%s}}" % (lang, alt))
+        if autolink and autolink == link:
+          pagemsg("Removing redundant alt form %s of %s for lang %s" % (alt, link, lang))
+          t.add("2", alt)
+          rmparam(t, "alt")
+          notes.append("move redundant alt= to 2= in {{t-simple}}")
       if interwiki:
         tempname = "t+"
       else:
@@ -40,6 +64,8 @@ def process_text_on_page(index, pagetitle, text):
 
 parser = blib.create_argparser("Convert {{t-simple}} to {{t}} or {{t+}}",
     include_pagefile=True, include_stdin=True)
+parser.add_argument("--no-remove-redundant-translit", help="Don't remove redundant transliterations",
+    action="store_true")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
