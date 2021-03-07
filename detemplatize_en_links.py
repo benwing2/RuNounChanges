24b31b61f79343fe913f429d63f0c0d3bcc601ce
Ben Wing <ben@benwing.com>
2021-03-07 17:44:17 -0600

    fix_self_links -> detemplatize_english_links and make it normally detemplatize rather than fix raw links

--- detemplatize_en_links.py
+++ detemplatize_en_links.py
@@ -14,25 +14,39 @@ def process_text_on_page(index, pagetitle, text):
   notes = []
 
   def fix_sec_self_links(sectext):
-    template_split_re = r'''(\{\{(?:[^{}]|\{\{[^{}]*\}\})*\}\})'''
     lines = sectext.split("\n")
     new_lines = []
     changed = False
+    template_split_re = r'''(\{\{(?:[^{}]|\{\{[^{}]*\}\})*\}\})'''
     for line in lines:
       if line.startswith("#"):
-        # Split templates and only change non-template text
-        split_templates = re.split(template_split_re, line)
-        for l in xrange(0, len(split_templates), 2):
-          while True:
-            newtext = re.sub(r"^#(.*?)\[\[%s\]\]" % pagetitle, r"#\1[[#English|%s]]" % pagetitle, split_templates[l], 0, re.M)
-            if newtext == split_templates[l]:
-              break
+        if args.do_raw:
+          # Split templates and only change non-template text
+          split_templates = re.split(template_split_re, line)
+          for l in xrange(0, len(split_templates), 2):
+            while True:
+              newtext = re.sub(r"^#(.*?)\[\[%s\]\]" % pagetitle, r"#\1[[#English|%s]]" % pagetitle, split_templates[l], 0, re.M)
+              if newtext == split_templates[l]:
+                break
+              changed = True
+              split_templates[l] = newtext
+          line = "".join(split_templates)
+        else:
+          def replace_templated(m):
+            if m.group(1) == pagetitle:
+              return "[[#English|%s]]" % pagetitle
+            else:
+              return "[[%s]]" % m.group(1)
+          new_line = re.sub(r"\{\{l\|en\|([^{}|]*?)\}\}", replace_templated, line)
+          if new_line != line:
             changed = True
-            split_templates[l] = newtext
-        line = "".join(split_templates)
+            line = new_line
       new_lines.append(line)
     if changed:
-      notes.append("fix raw self links to English terms")
+      if args.do_raw:
+        notes.append("fix raw self links to English terms")
+      else:
+        notes.append("replace templated links to English terms in defns with raw ones")
     return "\n".join(new_lines)
 
   if args.lang:
@@ -53,6 +67,7 @@ def process_text_on_page(index, pagetitle, text):
 parser = blib.create_argparser("Fix raw self links to English terms on the same page",
   include_pagefile=True, include_stdin=True)
 parser.add_argument("--lang", help="Language to do (optional)")
+parser.add_argument("--do-raw", help="Convert raw links rather than templated links", action="store_true")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
