af23876527fb6d8e6336c8f3551c7f77c76216c5
Ben Wing <ben@benwing.com>
2021-05-10 23:32:30 -0500

    add_default_it_pronun: new script

---
+++ add_default_it_pronun.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse, json
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, errandmsg, site
+import unicodedata
+
+AC = u"\u0301"
+GR = u"\u0300"
+CFLEX = u"\0302"
+vowel = u"aeiouɛɔy"
+vowel_c = "[" + vowel + "]"
+vocalic_c = "[^" + vowel + "jw]"
+not_vowel_c = "[^" + vowel + "]"
+stress_c = "[" + AC + GR + "]"
+
+def process_text_on_page(index, pagetitle, text):
+  global args
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+  def errandpagemsg(txt):
+    errandmsg("Page %s %s: %s" % (index, pagetitle, txt))
+  def expand_text(tempcall):
+    return blib.expand_text(tempcall, pagetitle, pagemsg, args.verbose)
+
+  notes = []
+
+  if "it-IPA" not in text:
+    return
+
+  if ":" in pagetitle:
+    pagemsg("Skipping non-mainspace title")
+    return
+
+  pagemsg("Processing")
+
+  parsed = blib.parse_text(text)
+
+  headt = None
+  saw_decl = False
+
+  for t in parsed.filter_templates():
+    origt = unicode(t)
+    tn = tname(t)
+
+    if tn == "it-IPA":
+      pagemsg("Saw %s" % unicode(t))
+      if getparam(t, "voiced2"):
+        pagemsg("WARNING: Can't yet handle voiced2=%s" % getparam(t, "voiced2"))
+        continue
+      pronuns = blib.fetch_param_chain(t, "1", "") or [pagetitle]
+      print "pronuns: %s" % pronuns
+      frobbed_pronuns = []
+      must_continue = False
+      for ipa in pronuns:
+        ipa = unicodedata.normalize("NFD", ipa)
+        if AC not in ipa and GR not in ipa:
+          vowel_count = len([x for x in ipa if x in vowel])
+          if vowel_count > 1:
+            ipa = re.sub("(" + vowel_c + ")(" + not_vowel_c + "*[iyu]?" + vowel_c + not_vowel_c + "*)$",
+                lambda m: m.group(1) + (AC if m.group(1) in u"eoɛɔ" else GR) + m.group(2), ipa)
+        if "z" in ipa:
+          frobbed_ipa = re.sub("i(" + vowel_c + ")", r"j\1", ipa)
+          frobbed_ipa = re.sub("u(" + vowel_c + ")", r"w\1", frobbed_ipa)
+          split_frobbed_ipa = re.split("(z+)", frobbed_ipa)
+          split_z = re.split("(z+)", ipa)
+          voiced = getparam(t, "voiced")
+          if voiced not in ["y", "yes", "1", ""]:
+            pagemsg("WARNING: Unrecognized voiced=%s" % voiced)
+            must_continue = True
+            break
+          for i in xrange(1, len(split_z), 2):
+            default_voiced = False
+            if voiced in ["y", "yes"] or i == 2 and voiced == "1":
+              default_voiced = True
+            elif i == 1 and split_frobbed_ipa[0] == "":
+              if re.search("^[ij]" + stress_c + "?" + vowel_c, split_frobbed_ipa[2]):
+                default_voiced = False
+              elif re.search("^" + vowel_c + stress_c + "?" + vowel_c, split_frobbed_ipa[2]):
+                default_voiced = True
+            else:
+              if (split_frobbed_ipa[i] == "z" and
+                  re.search(vowel_c + stress_c + "?$", split_frobbed_ipa[i - 1]) and
+                  re.search("^" + vowel_c, split_frobbed_ipa[i + 1])):
+                default_voiced = True
+              if re.search("i" + CFLEX, split_frobbed_ipa[i + 1]):
+                default_voiced = False
+            if default_voiced:
+              z_to_voiced = {"z": "dz", "zz": "ddz"}
+              split_z[i] = z_to_voiced.get(split_z[i], split_z[i])
+            else:
+              z_to_voiceless = {"z": "ts", "zz": "tts"}
+              split_z[i] = z_to_voiceless.get(split_z[i], split_z[i])
+          ipa = "".join(split_z)
+        ipa = ipa.replace(u"ʦ", "ts")
+        ipa = ipa.replace(u"ʣ", "dz")
+        ipa = unicodedata.normalize("NFC", ipa)
+        # module special-cases -izzare
+        ipa = re.sub(u"iddz[àá]re", "izzare", ipa)
+        frobbed_pronuns.append(ipa)
+      if must_continue:
+        continue
+      if frobbed_pronuns == [pagetitle]:
+        frobbed_pronuns = []
+      blib.set_param_chain(t, frobbed_pronuns, "1", "")
+
+    if origt != unicode(t):
+      pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+      notes.append("add stress to defaulted {{it-IPA}} pronun")
+
+  return unicode(parsed), notes
+
+parser = blib.create_argparser("Add missing stress and z resolution to {{it-IPA}}",
+  include_pagefile=True, include_stdin=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True,
+    default_refs=["Template:it-IPA"])
