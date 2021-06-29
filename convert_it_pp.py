9202c8badc409e0164c63a9fb074ec38ba9ad911
Ben Wing <ben@benwing.com>
2021-06-29 02:15:26 -0500

    convert_it_pp.py: new script

---
+++ convert_it_pp.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, site
+
+def process_text_on_page(index, pagetitle, text):
+  global args
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  notes = []
+
+  if "it-pp" not in text:
+    return
+
+  if ":" in pagetitle:
+    pagemsg("Skipping non-mainspace title")
+    return
+
+  pagemsg("Processing")
+
+  parsed = blib.parse_text(text)
+
+  for t in parsed.filter_templates():
+    tn = tname(t)
+    def getp(param):
+      return getparam(t, param)
+    if tn == "it-pp":
+      origt = unicode(t)
+      if getp("2") == "-":
+        rmparam(t, "2")
+        t.add("inv", "1")
+      rmparam(t, "1")
+      notes.append("convert {{it-pp}} to new form")
+      if origt != unicode(t):
+        pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+
+  return unicode(parsed), notes
+
+parser = blib.create_argparser("Convert {{it-pp}} templates to new format",
+  include_pagefile=True, include_stdin=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True,
+    default_refs=["Template:it-pp"])
