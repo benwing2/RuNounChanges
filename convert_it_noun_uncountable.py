15a70033c436f655d5d7d77177cbe6fcb2a02e92
Ben Wing <ben@benwing.com>
2021-11-26 01:40:59 -0600

    move_it_wikipedia,convert_it_noun_inv,convert_it_noun_uncountable: new scripts

---
+++ convert_it_noun_uncountable.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, site
+
+def process_text_on_page(index, pagetitle, text):
+  global args
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  notes = []
+
+  if "it-noun" not in text:
+    return
+
+  parsed = blib.parse_text(text)
+
+  for t in parsed.filter_templates():
+    tn = tname(t)
+    def getp(param):
+      return getparam(t, param)
+    if tn == "it-noun":
+      origt = unicode(t)
+      if getp("2") == "~":
+        t.add("2", "-")
+        notes.append("convert ~ in {{it-noun}} to -")
+      if origt != unicode(t):
+        pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+
+  return unicode(parsed), notes
+
+parser = blib.create_argparser("Convert ~ in {{it-noun}} to -",
+  include_pagefile=True, include_stdin=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True,
+    default_refs=["Template:it-noun"])
---
+++ move_it_wikipedia.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, site
+
+def process_text_on_page(index, pagetitle, text):
+  global args
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  notes = []
+
+  retval = blib.find_modifiable_lang_section(text, None if args.partial_page else "Italian", pagemsg,
+    force_final_nls=True)
+  if retval is None:
+    return
+  sections, j, secbody, sectail, has_non_lang = retval
+
+  subsections = re.split("(^==+[^=\n]+==+\n)", secbody, 0, re.M)
+
+  sect_for_wiki = 0
+  seen_lemmas = []
+  for k in xrange(1, len(subsections), 2):
+    if re.search(r"==\s*Etymology [0-9]+\s*==", subsections[k]):
+      sect_for_wiki = k + 1
+      seen_lemmas = []
+    else:
+      lines = subsections[k + 1].strip().split("\n")
+      lines = [line for line in lines]
+      lines_so_far = []
+      for lineind, line in enumerate(lines):
+        if re.search(r"^\{\{(wp|wiki|wikipedia|Wikipedia)\|[^{}]*\}\}$", line):
+          if len(seen_lemmas) >= 1:
+            pagemsg("Already saw preceding lemma(s) %s, not moving wikipedia line %s" % (
+              ",".join(seen_lemmas), line))
+            lines_so_far.append(line)
+          else:
+            # Put after any other wikipedia lines.
+            m = re.search(r"\A(.*?)(\n*)\Z", subsections[sect_for_wiki], re.S)
+            stripped_sect_for_wiki, sect_for_wiki_endlines = m.groups()
+            sect_for_wiki_lines = stripped_sect_for_wiki.split("\n")
+            for i in range(len(sect_for_wiki_lines)):
+              if not re.search(r"^\{\{(wp|wiki|wikipedia|Wikipedia)\|[^{}]*\}\}$", sect_for_wiki_lines[i]):
+                break
+            sect_for_wiki_lines[i:i] = [line]
+            subsections[sect_for_wiki] = "\n".join(sect_for_wiki_lines) + sect_for_wiki_endlines
+            subsections[k + 1] = "%s\n\n" % "\n".join(lines_so_far + lines[lineind + 1:])
+            notes.append("move {{wikipedia}} line to top of etym section")
+        else:
+          lines_so_far.append(line)
+      parsed = blib.parse_text(subsections[k + 1])
+      for t in parsed.filter_templates():
+        tn = tname(t)
+        def getp(param):
+          return getparam(t, param)
+        if tn in ["it-noun", "it-proper noun", "it-verb", "it-adj", "it-adv", "it-card-adj", "it-card-noun",
+            "it-card-inv", "it-prefix", "it-prepositional phrase", "it-letter", "it-det", "it-pron-adj",
+            "it-phrase", "it-plural noun", "it-pp"] or (
+            tn == "head" and getp("1") == "it" and getp("2") in ["noun", "verb", "adjective", "adverb", "proper noun",
+              "determiner", "pronoun", "participle", "past participle", "gerund", "preposition",
+              "prepositional phrase", "phrase", "interjection", "conjunction", "numeral", "cardinal numeral",
+              "cardinal number", "circumfix", "idiom", "interfix", "letter", "prefix", "suffix", "proverb"]):
+          if tn == "head":
+            lemma = "head|it|%s" % getp("2")
+          else:
+            lemma = tn
+          seen_lemmas.append(lemma)
+
+  secbody = "".join(subsections)
+  # Strip extra newlines added to secbody
+  sections[j] = secbody.rstrip("\n") + sectail
+  newsecj = re.sub(r"(\{\{it-noun[^{}]*\}\}\n)([^\n])", r"\1" + "\n" + r"\2", sections[j])
+  if newsecj != sections[j]:
+    notes.append("add missing newline after {{it-noun}}")
+    sections[j] = newsecj
+  text = "".join(sections)
+  newtext = re.sub(r"\n\n\n+", "\n\n", text)
+  if text != newtext:
+    notes.append("convert 3+ newlines to 2 newlines")
+  text = newtext
+  return text, notes
+
+parser = blib.create_argparser("Move {{wikipedia}} lines to top of etym section",
+  include_pagefile=True, include_stdin=True)
+parser.add_argument("--partial-page", action="store_true", help="Input was generated with 'find_regex.py --lang LANG' and has no ==LANG== header.")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True,
+    default_cats="Italian lemmas")
