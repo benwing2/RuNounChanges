de50316a2b69a8843a6350ffe1a0bf0466befcbf
Ben Wing <ben@benwing.com>
2021-11-28 17:51:52 -0600

    snarf_it_pron: handle {{it-pr}}, add --include-defns

--- snarf_it_pron.py
+++ snarf_it_pron.py
@@ -195,6 +195,7 @@ def process_text_on_page(index, pagetitle, text):
   saw_existing_pron_this_etym_section = False
 
   etymsection = "top" if has_etym_sections else "all"
+  etymsections_to_first_subsection = {}
   if etymsection == "top":
     after_etym_1 = False
     for k in xrange(2, len(subsections), 2):
@@ -205,6 +206,9 @@ def process_text_on_page(index, pagetitle, text):
           split_pronun_sections = True
         else:
           saw_pronun_section_at_top = True
+      m = re.search("==Etymology ([0-9]*)==", subsections[k - 1])
+      if m:
+        etymsections_to_first_subsection[int(m.group(1))] = k
 
   msgs = []
 
@@ -250,6 +254,7 @@ def process_text_on_page(index, pagetitle, text):
 
       respellings = []
       prev_it_IPA_t = None
+      prev_it_pr_t = None
       must_continue = False
       for t in parsed.filter_templates():
         tn = tname(t)
@@ -292,22 +297,82 @@ def process_text_on_page(index, pagetitle, text):
             append_msg("EXISTING_DEFAULTED")
             this_respellings.extend(apply_default_pronun_to_pagetitle())
           respellings.extend(this_respellings)
+        if tn == "it-pr":
+          saw_existing_pron = True
+          saw_existing_pron_this_etym_section = True
+          if prev_it_pr_t:
+            pronun_lines = re.findall(r"^.*\{\{it-pr.*$", subsections[k], re.M)
+            pagemsg("WARNING: Saw multiple {{it-pr}} templates in a single Pronunciation section: %s" %
+              " ||| ".join(pronun_lines))
+            must_continue = True
+            break
+          prev_it_pr_t = t
+          this_respellings = []
+          saw_pronun = False
+          for param in t.params:
+            pn = pname(param)
+            pv = unicode(param.value).strip().replace(" ", "_")
+            if re.search("^[0-9]+$", pn):
+              saw_pronun = True
+              #if pv == "+":
+              #  append_msg("EXISTING_DEFAULTED")
+              #  this_respellings.extend(apply_default_pronun_to_pagetitle())
+              #else:
+              def fix_ref(m):
+                refname, refparams = m.groups()
+                refname = "Treccani" if refname == "Trec" else refname
+                return "<r:%s%s>" % (refname, refparams or "")
+              pv = re.sub(r"<ref:\{\{R:it:(DiPI|Olivetti|Treccani|Trec|DOP)(\|[^{}]*)?\}\}>", fix_ref, pv)
+              append_msg("EXISTING")
+              this_respellings.append(pv)
+            else:
+              this_respellings.append("%s=%s" % (pn, pv))
+          if not saw_pronun:
+            append_msg("EXISTING_DEFAULTED")
+            #this_respellings.extend(apply_default_pronun_to_pagetitle())
+            this_respellings.append("+")
+          respellings.extend(this_respellings)
       if must_continue:
         continue
+
+      if args.include_defns and etymsection not in ["top", "all"]:
+        first_etym_subsec = etymsections_to_first_subsection.get(int(etymsection), None)
+        next_etym_subsec = etymsections_to_first_subsection.get(1 + int(etymsection), None)
+        if first_etym_subsec is None:
+          pagemsg("WARNING: Internal error: Unknown first etym section for =Etymology %s=" % etymsection)
+        else:
+          if next_etym_subsec is None:
+            next_etym_subsec = len(subsections)
+          defns = blib.find_defns("".join(subsections[first_etym_subsec:next_etym_subsec]), "it")
+          append_msg("defns: %s" % ";".join(defns))
 
       if respellings:
         pagemsg("<respelling> %s: %s <end> %s" % (etymsection, " ".join(respellings), " ".join(msgs)))
 
   check_missing_pronun(etymsection)
   if not saw_existing_pron:
-    msgs = []
-    append_msg("NEW_DEFAULTED")
-    respellings = apply_default_pronun_to_pagetitle()
-    pagemsg("<respelling> %s: %s <end> %s" % ("top" if has_etym_sections else "all", " ".join(respellings), " ".join(msgs)))
+    if args.include_defns and has_etym_sections:
+      for etymsec in sorted(list(etymsections_to_first_subsection.keys())):
+        msgs = []
+        first_etym_subsec = etymsections_to_first_subsection[etymsec]
+        next_etym_subsec = etymsections_to_first_subsection.get(1 + etymsec, None)
+        if next_etym_subsec is None:
+          next_etym_subsec = len(subsections)
+        append_msg("NEW_DEFAULTED")
+        defns = blib.find_defns("".join(subsections[first_etym_subsec:next_etym_subsec]), "it")
+        append_msg("defns: %s" % ";".join(defns))
+        respellings = apply_default_pronun_to_pagetitle()
+        pagemsg("<respelling> %s: %s <end> %s" % (etymsec, " ".join(respellings), " ".join(msgs)))
+    else:
+      msgs = []
+      append_msg("NEW_DEFAULTED")
+      respellings = apply_default_pronun_to_pagetitle()
+      pagemsg("<respelling> %s: %s <end> %s" % ("top" if has_etym_sections else "all", " ".join(respellings), " ".join(msgs)))
 
 if __name__ == "__main__":
   parser = blib.create_argparser("Snarf Italian pronunciations for fixing", include_pagefile=True, include_stdin=True)
   parser.add_argument("--partial-page", action="store_true", help="Input was generated with 'find_regex.py --lang LANG' and has no ==LANG== header.")
+  parser.add_argument("--include-defns", action="store_true", help="Include defns of snarfed terms (helps with multi-etym sections).")
   args = parser.parse_args()
   start, end = blib.parse_start_end(args.start, args.end)
 
