b85b9d0a905750b6940f12a14c61178d50f21a3f
Ben Wing <ben@benwing.com>
2022-01-08 21:55:57 -0600

    fix_mn_variant: new script

---
+++ fix_mn_variant.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, site
+
+def process_text_on_page(index, pagetitle, text):
+  global args
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  notes = []
+
+  parsed = blib.parse_text(text)
+
+  for t in parsed.filter_templates():
+    tn = tname(t)
+    def getp(param):
+      return getparam(t, param)
+    if tn == "mn-variant":
+      origt = unicode(t)
+      m = getp("m")
+      if m:
+        t.add("1", m, before="m")
+        t.add("2", m, before="m")
+      c = getp("c")
+      if c:
+        t.add("3", c, before="c")
+      rmparam(t, "m")
+      rmparam(t, "c")
+      if origt != unicode(t):
+        pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+        notes.append("Convert m=/c= in {{mn-variant}} to numbered params")
+
+  return unicode(parsed), notes
+
+parser = blib.create_argparser("Convert m=/c= in {{mn-variant}} to numbered params",
+  include_pagefile=True, include_stdin=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True,
+    default_refs=["Template:mn-variant"])
