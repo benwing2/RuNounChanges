091efdf00c1bb2c0ade0452dbc4420553acbed68
Ben Wing <ben@benwing.com>
2022-05-17 00:32:39 -0500

    remove remaining GPL 3 headers

--- find_red_links.py
+++ find_red_links.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#    find_red_links.py is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 # Find redlinks (non-existent pages).
 
 import pywikibot, re, sys, codecs, argparse
