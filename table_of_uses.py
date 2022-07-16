04c3bc640ec766e4e7acd5e8fbcec1ad6efe164e
Ben Wing <ben@benwing.com>
2022-07-16 03:01:25 -0500

    table_of_uses: support multiple --ref-namespaces, add strikethrough around deleted templates

--- table_of_uses.py
+++ table_of_uses.py
@@ -4,7 +4,7 @@
 import pywikibot, re, sys, codecs, argparse
 
 import blib
-from blib import getparam, rmparam, msg, errmsg, site
+from blib import getparam, rmparam, msg, errmsg, errandmsg, site
 
 parser = blib.create_argparser(u"List pages, lemmas and/or non-lemmas")
 parser.add_argument('--tempfile', help="Templates and aliases to do")
@@ -15,7 +15,7 @@ parser.add_argument('--include-disposition', help="Include column for dispositio
     action="store_true")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
-ref_namespaces = args.ref_namespaces and args.ref_namespaces.decode("utf-8") or None
+ref_namespaces = args.ref_namespaces and args.ref_namespaces.decode("utf-8").split(",") or None
 
 msg('{|class="wikitable"')
 msg("! Aliased template !! Canonical template !! #Uses%s%s" %
@@ -29,12 +29,17 @@ for lineno, ref_and_aliases in blib.iter_items_from_file(args.tempfile):
   for alias in aliases:
     refs.append(("Template:%s" % alias, mainref))
   for alias, mainref in refs:
+    def errandpagemsg(txt):
+      errandmsg("Page %s %s: %s" % (lineno, alias, txt))
     errmsg("Processing references to: %s" % alias)
+    exists = blib.safe_page_exists(pywikibot.Page(site, alias), errandpagemsg)
+    def maybe_strikethru(txt):
+      return txt if exists else "<s>%s</s>" % txt
     template_refs = list(blib.references(alias, start, end, namespaces=ref_namespaces))
     num_refs = len(template_refs)
     msg("|-")
     msg("| %s || %s || %s%s%s" % (
-      "[[%s]]" % alias if mainref else "'''[[%s]]'''" % alias,
+      maybe_strikethru("[[%s]]" % alias if mainref else "'''[[%s]]'''" % alias),
       "[[%s]]" % mainref if mainref else "'''[[%s]]'''" % alias,
       num_refs,
       " || %s" % ", ".join("[[%s]]" % unicode(ref.title()) for i, ref in template_refs) if args.include_refs else "",
