536b8f89065da1b59ba764fd98e3412f07c8fd7e
Ben Wing <ben@benwing.com>
2022-07-21 00:36:47 -0500

    convert_es_verb_form_of,remove_cu_pronunciation: new scripts

---
+++ convert_es_verb_form_of.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site, tname, pname
+
+conj_table = {}
+def lookup_conjugation(inf, pagemsg, errandpagemsg):
+  if inf in conj_table:
+    conjs, warning = conj_table[inf]
+    if warning:
+      pagemsg("%s: No conjugation because '%s' (cached)" % (inf, warning))
+    else:
+      pagemsg("%s: Returning %s (cached)" % (inf, ", ".join("'%s'" % conj for conj in conjs)))
+    return conj_table[inf]
+  conjpage = pywikibot.Page(site, inf)
+  conjtext = blib.safe_page_text(conjpage, errandpagemsg)
+  if not conjtext:
+    if blib.safe_page_exists(conjpage, errandpagemsg):
+      warning = "WARNING: Infinitive page exists but is blank"
+    else:
+      warning = "WARNING: Infinitive page doesn't exist"
+  else:
+    parsed = blib.parse_text(conjtext)
+    conjs = []
+    for t in parsed.filter_templates():
+      tn = tname(t)
+      if tn == "es-conj":
+        arg1 = getparam(t, "1")
+        if arg1 == "":
+          newconj = inf
+        elif re.search("^<[^<>]*>$", arg1):
+          newconj = "%s%s" % (inf, arg1)
+        else:
+          newconj = arg1
+        if newconj == arg1:
+          pagemsg("%s: Conjugation already has infinitive in it: %s" % (inf, arg1))
+        else:
+          pagemsg("%s: Converting conjugation '%s' to '%s'" % (inf, arg1, newconj))
+          arg1 = newconj
+        m = re.search("^([^<>]+)(<[^<>]*>)$", arg1)
+        if m:
+          conjinf, rawconj = m.groups()
+        elif "<" not in arg1:
+          conjinf = arg1
+          rawconj = ""
+        else:
+          conjinf = None
+          pagemsg("%s: WARNING: Can't parse out infinitive from conjugation '%s'" % (inf, arg1))
+        if conjinf is not None:
+          if conjinf.endswith("se") and not inf.endswith("se"):
+            newarg1 = conjinf[:-2] + rawconj
+            pagemsg("%s: Converting reflexive conjugation '%s' for non-reflexive infinitive to non-reflexive '%s'" %
+                (inf, arg1, newarg1))
+            arg1 = newarg1
+        if arg1 not in conjs:
+          conjs.append(arg1)
+    if len(conjs) == 0:
+      warning = "WARNING: Infinitive page exists but has no conjugations"
+    else:
+      if len(conjs) > 1:
+        warning = "WARNING: Multiple conjugations %s" % ", ".join(conjs)
+      conj = conjs[0]
+      conj_table[inf] = (conjs, None)
+      pagemsg("%s: Returning %s" % (inf, ", ".join("'%s'" % conj for conj in conjs)))
+      return conj_table[inf]
+  pagemsg("%s: %s" % (inf, warning))
+  conj_table[inf] = (None, warning)
+  pagemsg("%s: No conjugation because '%s'" % (inf, warning))
+  return conj_table[inf]
+
+def escape_newlines(text):
+  return text.replace("\n", r"\n")
+
+def process_text_on_page(index, pagetitle, pagetext):
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+  def errandpagemsg(txt):
+    errandmsg("Page %s %s: %s" % (index, pagetitle, txt))
+  def expand_text(tempcall):
+    return blib.expand_text(tempcall, pagetitle, pagemsg, args.verbose)
+
+  notes = []
+
+  if blib.page_should_be_ignored(pagetitle):
+    return
+
+  m = re.search(r"\A(.*?)(\n*)\Z", pagetext, re.S)
+  pagetext_nonl, finalnl = m.groups()
+  pagetext = pagetext_nonl + "\n\n"
+
+  chunks = re.split(r"^((?:# \{\{es-verb form of\|.*\n)+)", pagetext, 0, re.M)
+  for k in xrange(1, len(chunks), 2):
+    verb_form_chunk = chunks[k]
+    extra_text = ""
+    if not re.search(r"\A((?:# \{\{es-verb form of\|.*\}\}\n)+)\Z", verb_form_chunk):
+      m = re.search(r"\A# \{\{es-verb form of\|.*\}\}(.*)\n\Z", verb_form_chunk)
+      if m:
+        pagemsg("WARNING: Extraneous text after {{es-verb form of}}, adding after new {{es-verb form of}}: <%s>"
+          % escape_newlines(verb_form_chunk))
+        extra_text = re.sub(r"\.$", "", m.group(1))
+      else:
+        pagemsg("WARNING: Multiple calls to {{es-verb form of}} with extraneous text, skipping: <%s>"
+          % escape_newlines(verb_form_chunk))
+        continue
+    parsed = blib.parse_text(verb_form_chunk)
+    must_continue = False
+    seen_infs = set()
+    parts = []
+    for t in parsed.filter_templates():
+      origt = unicode(t)
+      def getp(param):
+        return getparam(t, param)
+      tn = tname(t)
+      if tn != "es-verb form of":
+        pagemsg("WARNING: Saw non-{{es-verb form of}} template mixed in with {{es-verb form of}} templates, skipping: %s" % origt)
+        must_continue = True
+        break
+      is_old = False
+      oldparams = ["verb", "inf", "infinitive", "end", "ending", "nocap", "nodot", "mood", "tense", "num", "number",
+          "pers", "person", "formal", "sense", "sera", "gen", "gender", "par", "part", "participle", "voseo", "region"]
+      for oldparam in oldparams:
+        if t.has(oldparam):
+          is_old = True
+          break
+      if not is_old:
+        pagemsg("Saw new-style {{es-verb form of}}, skipping: %s" % origt)
+        must_continue = True
+        break
+      inf = getp("1") or getp("verb") or getp("inf") or getp("infinitive")
+      if not inf:
+        pagemsg("WARNING: No infinitive in {{es-verb form of}}, skipping: %s" % origt)
+        must_continue = True
+        break
+      if inf in seen_infs:
+        continue
+      seen_infs.add(inf)
+      conjs, bad_reason = lookup_conjugation(inf, pagemsg, errandpagemsg)
+      if conjs is None:
+        pagemsg("WARNING: Can't find conjugation for infinitive '%s', skipping: %s" % (inf, origt))
+        must_continue = True
+        break
+      expansions = []
+      expansion_conjugations = []
+      for conj in conjs:
+        del t.params[:]
+        t.add("1", conj)
+        newtemp = unicode(t)
+        expansion = expand_text(newtemp)
+        if expansion is not False and expansion not in expansions:
+          expansions.append(expansion)
+          expansion_conjugations.append(conj)
+      if len(expansions) == 0:
+        pagemsg("WARNING: No expansions, can't replace old-style {{es-verb form of}} with %s, skipping: %s"
+          % (newtemp, origt))
+        must_continue = True
+        break
+      if len(expansions) > 1:
+        pagemsg("WARNING: Multiple conjugations with differing expansions, can't replace old-style {{es-verb form of}}, skipping: %s"
+            % ", ".join("%s=%s" % (expansion_conjugations[i], expansion) for i, expansion in enumerate(expansions)))
+        must_continue = True
+        break
+      conj = expansion_conjugations[0]
+      notes.append("replace {{es-verb form of}} conjugation(s) for infinitive [[%s]] with '%s'" % (inf, conj))
+      del t.params[:]
+      t.add("1", conj)
+      newtemp = unicode(t)
+      parts.append("# " + newtemp + extra_text + "\n")
+    if must_continue:
+      continue
+    chunks[k] = "".join(parts)
+    pagemsg("Replaced <%s> with <%s>" % (escape_newlines(verb_form_chunk), escape_newlines(chunks[k])))
+  pagetext = "".join(chunks).rstrip("\n") + finalnl
+
+  return pagetext, notes
+  
+parser = blib.create_argparser(u"Convert {{es-verb form of}} to new format", include_pagefile=True, include_stdin=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True,
+    default_refs=["Template:es-verb form of"])
---
+++ remove_cu_pronunciation.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, msg, site, tname, pname
+
+def process_text_on_page(index, pagetitle, pagetext):
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  if not args.stdin:
+    pagemsg("Processing")
+
+  notes = []
+
+  retval = blib.find_modifiable_lang_section(pagetext, None if args.partial_page else "Old Church Slavonic", pagemsg,
+    force_final_nls=True)
+  if retval is None:
+    return
+  sections, j, secbody, sectail, has_non_lang = retval
+
+  subsections = re.split("(^==+[^=\n]+==+\n)", secbody, 0, re.M)
+  for k in xrange(2, len(subsections), 2):
+    if "==Pronunciation==" in subsections[k - 1]:
+      parsed = blib.parse_text(subsections[k])
+      for t in parsed.filter_templates():
+        def getp(param):
+          return getparam(t, param)
+        origt = unicode(t)
+        tn = tname(t)
+        if tn != "cu-IPA":
+          pagemsg("WARNING: Saw non-{{cu-IPA}} template in Old Church Slavonic pronunciation section: %s" % unicode(t))
+          break
+      else: # no break
+        subsections[k - 1] = ""
+        subsections[k] = ""
+        notes.append("remove Pronunciation section with bad {{cu-IPA}} from Old Church Slavonic term")
+
+  secbody = "".join(subsections)
+  # Strip extra newlines added to secbody
+  sections[j] = secbody.rstrip("\n") + sectail
+  return "".join(sections), notes
+  
+parser = blib.create_argparser(u"Remove Pronunciation sections with {{cu-IPA}} from Old Church Slavonic terms",
+  include_pagefile=True, include_stdin=True)
+parser.add_argument("--partial-page", action="store_true", help="Input was generated with 'find_regex.py --lang LANG' and has no ==LANG== header.")
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True,
+    default_refs=["Template:cu-IPA"], skip_ignorable_pages=True)
