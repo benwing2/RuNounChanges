a17aa5d705d1489aa23793992e5068f0b9907c82
Ben Wing <ben@benwing.com>
2022-07-21 00:33:17 -0500

    table_of_uses: support --raw-refs

--- table_of_uses.py
+++ table_of_uses.py
@@ -7,11 +7,13 @@ import blib
 from blib import getparam, rmparam, msg, errmsg, errandmsg, site
 
 parser = blib.create_argparser(u"List pages, lemmas and/or non-lemmas")
-parser.add_argument('--tempfile', help="Templates and aliases to do")
-parser.add_argument('--include-refs', help="Include column for template references",
+parser.add_argument("--tempfile", help="Templates and aliases to do")
+parser.add_argument("--include-refs", help="Include column for template references",
     action="store_true")
-parser.add_argument('--ref-namespaces', help="List of namespaces to restrict references to")
-parser.add_argument('--include-disposition', help="Include column for disposition",
+parser.add_argument("--ref-namespaces", help="List of namespaces to restrict references to")
+parser.add_argument("--include-disposition", help="Include column for disposition",
+    action="store_true")
+parser.add_argument("--raw-refs", help="References are raw instead of in Template space",
     action="store_true")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
@@ -23,11 +25,15 @@ msg("! Aliased template !! Canonical template !! #Uses%s%s" %
    " !! Suggested disposition" if args.include_disposition else ""))
 for lineno, ref_and_aliases in blib.iter_items_from_file(args.tempfile):
   split_refs = re.split(",", ref_and_aliases)
-  mainref = "Template:%s" % split_refs[0]
+  mainref = split_refs[0]
+  if not args.raw_refs:
+    mainref = "Template:%s" % mainref
   aliases = split_refs[1:]
   refs = [(mainref, None)]
   for alias in aliases:
-    refs.append(("Template:%s" % alias, mainref))
+    if not args.raw_refs:
+      alias = "Template:%s" % alias
+    refs.append((alias, mainref))
   for alias, mainref in refs:
     def errandpagemsg(txt):
       errandmsg("Page %s %s: %s" % (lineno, alias, txt))
