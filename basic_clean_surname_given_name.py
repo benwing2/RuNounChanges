c60929360eca06706089b074282ce475240fe207
Ben Wing <ben@benwing.com>
2022-08-28 21:26:30 -0500

    basic_clean_surname_given_name,clean_etym_text: new scripts

---
+++ basic_clean_surname_given_name.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, site
+
+name_templates = ["surname", "given name"]
+name_template_re = "(?:%s)" % "|".join(name_templates)
+
+def process_text_on_page(index, pagetitle, text):
+  global args
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+  origtext = text
+  notes = []
+
+  if blib.page_should_be_ignored(pagetitle):
+    return
+
+  parsed = blib.parse_text(text)
+
+  for t in parsed.filter_templates():
+    tn = tname(t)
+    if tn in name_templates:
+      origt = unicode(t)
+      def getp(param):
+        return getparam(t, param).strip()
+      lang = getp("1")
+      aval = getp("A")
+      if lang != "en":
+        if aval in ["A", "a", "An", "an"]:
+          pagemsg("Remove redundant article A=%s in {{%s|%s}}: %s" % (aval, tn, lang, unicode(t)))
+          notes.append("remove redundant article A=%s in {{%s|%s}}" % (aval, tn, lang))
+          rmparam(t, "A")
+        else:
+          m = re.search("^(A|An) +(.*)$", aval)
+          if m:
+            pagemsg("Change explicit article A=%s to lowercase in non-English {{%s|%s}}: %s" % (aval, tn, lang, unicode(t)))
+            notes.append("change explicit article A=%s to lowercase in non-English {{%s|%s}}" % (aval, tn, lang))
+            aval = "a" + aval[1:]
+            t.add("A", aval)
+
+      if origt != unicode(t):
+        pagemsg("Replaced %s with %s" % (origt, unicode(t)))
+
+  text = unicode(parsed)
+  lines = text.split("\n")
+  for lineno, line in enumerate(lines):
+    if re.search(r"\{\{%s\|" % name_template_re, line):
+      parsed = blib.parse_text(line)
+      lang = None
+      namet = None
+      must_continue = False
+      for t in parsed.filter_templates():
+        tn = tname(t)
+        if tn in name_templates:
+          def getp(param):
+            return getparam(t, param).strip()
+          if lang is not None:
+            pagemsg("WARNING: Saw more than one {{surname}}/{{given name}} template on line: <from> %s <to> %s <end>" %
+                (line, line))
+            must_continue = True
+            break
+          lang = getp("1")
+          namet = t
+      if must_continue:
+        continue
+      if lang is None:
+        pagemsg("WARNING: No templates on line with {{surname}}/{{given name}}?: %s" % line)
+        continue
+      if lang != "en":
+        origline = line
+        line = line.strip()
+        if line.endswith(".") and not line.endswith("etc."):
+          line = line[:-1]
+          pagemsg("Replaced line #%s <%s> with <%s>" % (lineno + 1, origline, line))
+          lines[lineno] = line
+          notes.append("remove final dot from non-English {{%s|%s}} line" % (tn, lang))
+      m = re.search(r"^(#\s*)\{\{[lm]\|([^{}|]*)\|([^{}|]*)\}\}[;:,]\s*(\{\{%s\|([^{}|]*)[^{}]*)\}\}(.*)$" % name_template_re,
+          line)
+      if m:
+        beginning, en_lang, en_name, name_template, name_lang, rest = m.groups()
+        name_lang = name_lang.strip()
+        en_lang = en_lang.strip()
+        if name_lang == "en":
+          pagemsg("WARNING: Saw English {{surname}}/{{given name}} line with English-equivalent name: <from> %s <to> %s <end>" %
+              (line, line))
+          continue
+        if name_lang == en_lang:
+          pagemsg("WARNING: Saw non-English {{surname}}/{{given name}} line with English-equivalent name using the same language '%s', assuming should be English: %s" %
+            (en_lang, line))
+          en_lang = "en"
+        if en_lang == "en":
+          xlit_param = "xlit"
+          xlit = en_name
+        else:
+          xlit_param = "eq"
+          xlit = "%s:%s" % (en_lang, en_name)
+        if namet.has(xlit_param):
+          pagemsg("WARNING: Would incorporate English or other-language borrowing '%s' as %s=%s but param already exists: %s" %
+            (en_name, xlit_param, xlit, line))
+          continue
+        origline = line
+        line = "%s%s|%s=%s}}%s" % (beginning, name_template, xlit_param, xlit, rest)
+        pagemsg("Replaced line #%s <%s> with <%s>" % (lineno + 1, origline, line))
+        lines[lineno] = line
+        notes.append("incorporate English or other-language borrowing '%s' of non-English {{%s|%s}} line" %
+          (en_name, tn, lang))
+        parsed = blib.parse_text(line)
+
+      templates = list(parsed.filter_templates())
+      if len(templates) == 2 and tname(templates[0]) in ["lb", "label"] and tname(templates[1]) in name_templates:
+        # this is common enough and should remain
+        continue
+      if len(templates) > 1:
+        pagemsg("WARNING: Saw multiple templates on line with {{surname}}/{{given name}}: <from> %s <to> %s <end>" %
+            (line, line))
+
+  text = "\n".join(lines)
+
+  return text, notes
+
+parser = blib.create_argparser("Do basic clean up of {{surname}} and {{given name}}, removing redundant articles and final periods", include_pagefile=True, include_stdin=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, default_refs=["Template:surname", "Template:given name"],
+  edit=True, stdin=True)
---
+++ clean_etym_text.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pywikibot, re, sys, codecs, argparse
+
+import blib
+from blib import getparam, rmparam, tname, pname, msg, site
+from collections import defaultdict
+
+lastcats_seen = defaultdict(int)
+
+def process_text_on_page(index, pagetitle, text):
+  global args
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+  origtext = text
+  notes = []
+
+  if blib.page_should_be_ignored(pagetitle):
+    return
+
+  text, texttail = blib.force_two_newlines_in_secbody(text, "")
+
+  newtext = re.sub("\n\n\n+", "\n\n", text)
+  if newtext != text:
+    notes.append("replace 3+ newlines with 2")
+    text = newtext
+  newtext = re.sub(u"[ \t\u00A0]+\n", "\n", text)
+  if newtext != text:
+    notes.append("remove extraneous spaces at end of line")
+    text = newtext
+
+  def replace_after_from(m):
+    fromtext = m.group(1)
+    m = re.search("^(.*)&nbsp;$", fromtext)
+    if m:
+      fromtext = m.group(1).rstrip()
+      notes.append("remove extraneous final NBSP")
+    m = re.search(r"^(.*)(<br\s*/?>)$", fromtext)
+    if m:
+      fromtext = m.group(1).rstrip()
+      removed_br = m.group(2)
+      notes.append("remove extraneous final %s" % removed_br)
+    if not re.search(ur"([.?!:]|etc\.|</\s*[Rr]ef[a-z]*>|</\s*hiero>|-->|\[http[^\[\]]*\]|<[Rr]ef[^<>]*/>|<br\s*/?>|<br [^<>]*>|\{\{(cln|catlangname|C|c|top|topic|topics|catlangcode|rfv-etym|etystub|rfe|rfe-lite|rfref|root|dercat|defdate|etydate|C\.|hanja-[a-z]+|ja-rendaku2|rendaku2|ja-renj[oō]|renj[oō]|ja-from-kaminidan|ja-etym-renyokei|attn|attention|lena|Pigafetta|hu-langref|U:hu:postpositional-adjective|ru-cform-impfv-length|htetylz|ko-etym-[a-zA-Z-]*|tl-irregular-verb|sv-verb-form-pastpart|progreso|nonlemma|ref)(?:\|(?:[^{}]|\{\{[^{}]*\}\})*)?\}\}|\{\{ar-root[^{}]*\|notext=1[^{}]*\}\}|\[\[(?:Category|CAT):[^\[\]]*\]\]|\.[" + '"' + ur"’”'»„›)])$", fromtext):
+      mm = re.search(r"\{\{([^{}|]*)(?:\|(?:[^{}]|\{\{[^{}]*\}\})*)?\}\}$", fromtext)
+      if mm:
+        lastcat = mm.group(1)
+        lastcats_seen[lastcat] += 1
+      fromtext = fromtext + "."
+      fromtext = re.sub(r"[;,]\.$", ".", fromtext)
+      notes.append("add missing final period (full stop) in 'From ...' line")
+    return fromtext + "\n\n"
+
+  text = re.sub(r"^((?:Inherited from |Borrowed from |From |\{\{(?:bor\+|inh\+)\|).*)\n\n", replace_after_from, text, 0, re.M)
+
+  text = text.rstrip("\n") + texttail
+
+  return text, notes
+
+parser = blib.create_argparser("Clean up etym-section text, adding missing periods, removing spaces at EOL and 3+ newlines", include_pagefile=True, include_stdin=True)
+args = parser.parse_args()
+start, end = blib.parse_start_end(args.start, args.end)
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True)
+
+for template, count in sorted(lastcats_seen.items(), key=lambda x: -x[1]):
+  msg("%-50s = %s" % (template, count))
