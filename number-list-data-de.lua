86183b9f43c96d8b322cf32c8e5fdc7797d7c279
Ben Wing <ben@benwing.com>
2022-08-28 21:27:07 -0500

    number-list-data-de,number-list-data-pt: rewrite modules

--- number-list-data-de.lua
+++ number-list-data-de.lua
@@ -6,15 +6,20 @@ local power_of = m_numutils.power_of
 
 export.additional_number_types = {
 	{key = "sequence_adverb", after = "ordinal"},
+	{key = "adverbial_abbreviation", after = "adverbial"},
+	{key = "multiplier_abbreviation", after = "multiplier"},
 	{key = "polygon"},
+	{key = "polygon_abbreviation"},
 	{key = "polygonal_adjective"},
+	{key = "polygonal_adjective_abbreviation"},
 	{key = "polyhedron"},
+	{key = "polyhedron_abbreviation"},
 }
 
 local numbers = export.numbers
 
-local lcfirst = mw.getContentLanguage():lcfirst
-local ucfirst = mw.getContentLanguage():ucfirst
+local lcfirst = function(text) return mw.getContentLanguage():lcfirst(text) end
+local ucfirst = function(text) return mw.getContentLanguage():ucfirst(text) end
 
 local function add_ordinal_suffix(num, term)
 	term = lcfirst(term):gsub("e$", "")
@@ -34,35 +39,57 @@ local function add_suffix(numstr, term, suffix)
 	return term .. suffix
 end
 
+local function has_polygon(num)
+	if type(num) == "string" then
+		return false
+	end
+	return num >= 3 and num <= 22 or num == 24 or num == 27 or num == 28 or num == 30 or num == 31 or num == 32 or
+		num == 34 or num == 36 or num == 40 or num == 48 or num == 50 or num == 51 or num == 60 or num == 70 or
+		num == 80
+end
+
 local function make_number(num, props, card_base)
 	local numstr = m_numutils.format_fixed(num)
-	local with_thousands_dot = #numstr < 10 and m_numutils.add_thousands_separator(numstr, " ") or nil
-	local with_thousands_space = #numstr < 10 and m_numutils.add_thousands_separator(numstr, " ") or nil
 	local ordinal_abbr
-	if with_thousands then
-		if type(num) == "number" and num >= 20 or type(num) == "string" then
-			ordinal_abbr = {with_thousands .. ".", with_thousands .. "ste"}
+	if #numstr < 10 then
+		local with_thousands_dot = m_numutils.add_thousands_separator(numstr, ".") .. "."
+		local with_thousands_space = m_numutils.add_thousands_separator(numstr, " ") .. "."
+		if with_thousands_dot == with_thousands_space then
+			ordinal_abbr = with_thousands_dot
 		else
-			ordinal_abbr = with_thousands .. "."
+			ordinal_abbr = {with_thousands_dot, with_thousands_space}
 		end
 	end
 
 	card_base = card_base or props.cardinal
 	props.ordinal = props.ordinal or map(function(card) return add_ordinal_suffix(num, card) end, card_base)
+	props.ordinal_abbr = ordinal_abbr
 	if props.wplink == true then
 		props.wplink = card_base
 	end
 	if props.adverbial == true then
 		props.adverbial = map(function(card) return add_suffix(numstr, card, "mal") end, card_base)
 	end
+	if props.adverbial and type(num) == "number" and num <= 100 then
+		props.adverbial_abbreviation = num .. "-mal"
+	end
 	if props.multiplier == true then
 		props.multiplier = map(function(card) return add_suffix(numstr, card, "fach") end, card_base)
+	end
+	if props.multiplier and type(num) == "number" and num <= 100 then
+		props.multiplier_abbreviation = num .. "-fach"
 	end
 	if props.fractional == true then
 		props.fractional = map(function(ord) return ucfirst(ord) .. "l" end, props.ordinal)
 	end
 	if props.sequence_adverb == true then
 		props.sequence_adverb = map(function(ord) return ord .. "ns" end, props.ordinal)
+	end
+	if has_polygon(num) then
+		props.polygon = map(function(card) return ucfirst(card) .. "eck" end, card_base)
+		props.polygon_abbreviation = num .. "-Eck"
+		props.polygonal_adjective = map(function(polygon) return lcfirst(polygon) .. "ig" end, props.polygon)
+		props.polygonal_adjective_abbreviation = num .. "-eckig"
 	end
 
 	numbers[num] = props
@@ -93,8 +120,8 @@ make_number(2, {
 })
 
 -- Do numbers 3 through 12.
-for i, cardinal in ipairs { "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf", "zwölf" } do
-	local num = i + 1
+for i, cardinal in ipairs { "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf", "zwölf" } do
+	local num = i + 2
 	local ordinal
 	if num == 3 then
 		ordinal = "dritte"
@@ -145,7 +172,7 @@ for i, tens_cardinal in ipairs {
 		elseif ones == 1 then
 			ones_prefix = "einund"
 		else
-			ones_prefix = number[ones].cardinal .. "und"
+			ones_prefix = numbers[ones].cardinal .. "und"
 		end
 		local cardinal = map(function(tens_card) return ones_prefix .. tens_card end, tens_cardinal)
 		local wplink
@@ -176,10 +203,19 @@ make_number(100, {
 	sequence_adverb = true,
 }, "hundert")
 
+make_number(101, {
+	cardinal = {"hunderteins", "einhunderteins"},
+	ordinal = {"hunderterste", "einhunderterste"},
+	adverbial = true,
+	multiplier = true,
+	fractional = true,
+	sequence_adverb = true,
+}, {"hundertein", "einhundertein"})
+
 -- Do numbers 200 through 900 by 100.
 for i=200, 900, 100 do
 	make_number(i, {
-		cardinal = number[i / 100].cardinal .. "hundert",
+		cardinal = numbers[i / 100].cardinal .. "hundert",
 		-- no wplink for any of these numbers
 		adverbial = true,
 		multiplier = true,
@@ -197,29 +233,78 @@ make_number(1000, {
 	sequence_adverb = true,
 }, "tausend")
 
--- Do numbers 2000 through 9000 by 1000.
-for i=2000, 9000, 1000 do
-	make_number(i, {
-		cardinal = number[i / 1000].cardinal .. "tausend",
-		-- no wplink for any of these numbers
+local function make_large_number(num, cardinal, card_base, wplink)
+	make_number(num, {
+		cardinal = cardinal,
+		wplink = wplink,
 		adverbial = true,
 		multiplier = true,
 		fractional = true,
 		-- sequence adverbs this large can't easily be attested.
-	})
+	}, card_base)
 end
 
--- Do numbers 10000 through 90000 by 10000.
-for i=10000, 90000, 10000 do
-	make_number(i, {
-		-- Need to use map() because of 30.
-		cardinal = map(function(base_card) return base_card .. "tausend" end, number[i / 10000].cardinal),
-		-- no wplink for any of these numbers
-		adverbial = true,
-		multiplier = true,
-		fractional = true,
-		-- sequence adverbs this large can't easily be attested.
-	})
+-- Do numbers 2000 through 9000 by 1000.
+for i=2000, 9000, 1000 do
+	-- no wplink for any of these numbers
+	make_large_number(i, numbers[i / 1000].cardinal .. "tausend")
 end
+
+-- Do numbers 10,000 through 90,000 by 10,000.
+for i=10000, 90000, 10000 do
+	-- no wplink for any of these numbers
+	make_large_number(i,
+		-- Need to use map() because of 30.
+		map(function(base_card) return base_card .. "tausend" end, numbers[i / 1000].cardinal)
+	)
+end
+
+-- Do numbers 100,000 through 900,000 by 100,000.
+for i=100000, 900000, 100000 do
+	-- no wplink for any of these numbers
+	make_large_number(i,
+		-- Need to use map() because of 100.
+		map(function(base_card) return base_card .. "tausend" end, numbers[i / 1000].cardinal)
+	)
+end
+
+make_large_number(1000000, "[[ein|eine]] [[Million]]<link:Million>", "million", "Million")
+make_large_number(2000000, "[[zwei]] [[Million]]en", "zweimillion")
+make_large_number(power_of(7), "[[zehn]] [[Million]]en", "zehnmillion")
+make_large_number(power_of(8), "[[hundert]] [[Million]]en", "hundertmillion")
+make_large_number(power_of(9), "[[ein|eine]] [[Milliarde]]<link:Milliarde>", "milliard", "Milliarde")
+make_large_number(power_of(9, 2), "[[zwei]] [[Milliarde]]n", "zweimilliard")
+make_large_number(power_of(10), "[[zehn]] [[Milliarde]]n", "zehnmilliard")
+make_large_number(power_of(11), "[[hundert]] [[Milliarde]]n", "hundertmilliard")
+make_large_number(power_of(12), "[[ein|eine]] [[Billion]]<link:Billion>", "billion", "Billion")
+make_large_number(power_of(13), "[[zehn]] [[Billion]]en", "zehnbillion")
+make_large_number(power_of(14), "[[hundert]] [[Billion]]en", "hundertbillion")
+make_large_number(power_of(15), "[[ein|eine]] [[Billiarde]]<link:Billiarde>", "billiard", "Billiarde")
+make_large_number(power_of(18), "[[ein|eine]] [[Trillion]]<link:Trillion>", "trillion", "Trillion")
+make_large_number(power_of(21), "[[ein|eine]] [[Trilliarde]]<link:Trilliarde>", "trilliard", "Trilliarde")
+make_large_number(power_of(24), "[[ein|eine]] [[Quadrillion]]<link:Quadrillion>", "quadrillion", "Quadrillion")
+make_large_number(power_of(27), "[[ein|eine]] [[Quadrilliarde]]<link:Quadrilliarde>", "quadrilliard", "Quadrilliarde")
+make_large_number(power_of(30), "[[ein|eine]] [[Quintillion]]<link:Quintillion>", "quintillion", "Quintillion")
+make_large_number(power_of(33), "[[ein|eine]] [[Quintilliarde]]<link:Quintilliarde>", "quintilliard", "Quintilliarde")
+make_large_number(power_of(36), "[[ein|eine]] [[Sextillion]]<link:Sextillion>", "sextillion", "Sextillion")
+make_large_number(power_of(39), "[[ein|eine]] [[Sextilliarde]]<link:Sextilliarde>", "sextilliard", "Sextilliarde")
+make_large_number(power_of(42), "[[ein|eine]] [[Septillion]]<link:Septillion>", "septillion", "Septillion")
+make_large_number(power_of(45), "[[ein|eine]] [[Septilliarde]]<link:Septilliarde>", "septilliard", "Septilliarde")
+make_large_number(power_of(48), "[[ein|eine]] [[Oktillion]]<link:Oktillion>", "oktillion", "Oktillion")
+make_large_number(power_of(51), "[[ein|eine]] [[Oktilliarde]]<link:Oktilliarde>", "oktilliard", "Oktilliarde")
+make_large_number(power_of(54), "[[ein|eine]] [[Nonillion]]<link:Nonillion>", "nonillion", "Nonillion")
+make_large_number(power_of(57), "[[ein|eine]] [[Nonilliarde]]<link:Nonilliarde>", "nonilliard", "Nonilliarde")
+make_large_number(power_of(60), "[[ein|eine]] [[Dezillion]]<link:Dezillion>", "dezillion", "Dezillion")
+make_large_number(power_of(63), "[[ein|eine]] [[Dezilliarde]]<link:Dezilliarde>", "dezilliard", "Dezilliarde")
+make_large_number(power_of(66), "[[ein|eine]] [[Undezillion]]<link:Undezillion>", "undezillion", "Undezillion")
+make_large_number(power_of(69), "[[ein|eine]] [[Undezilliarde]]<link:Undezilliarde>", "undezilliard", "Undezilliarde")
+make_large_number(power_of(72), "[[ein|eine]] [[Duodezillion]]<link:Duodezillion>", "duodezillion", "Duodezillion")
+make_large_number(power_of(75), "[[ein|eine]] [[Duodezilliarde]]<link:Duodezilliarde>", "duodezilliard", "Duodezilliarde")
+make_large_number(power_of(78), "[[ein|eine]] [[Tredezillion]]<link:Tredezillion>", "tredezillion", "Tredezillion")
+make_large_number(power_of(81), "[[ein|eine]] [[Tredezilliarde]]<link:Tredezilliarde>", "tredezilliard", "Tredezilliarde")
+make_large_number(power_of(84), "[[ein|eine]] [[Quattuordezillion]]<link:Quattuordezillion>", "quattuordezillion", "Quattuordezillion")
+make_large_number(power_of(87), "[[ein|eine]] [[Quattuordezilliarde]]<link:Quattuordezilliarde>", "quattuordezilliard", "Quattuordezilliarde")
+make_large_number(power_of(120), "[[ein|eine]] [[Vigintillion]]<link:Vigintillion>", "vigintillion", "Vigintillion")
+make_large_number(power_of(123), "[[ein|eine]] [[Vigintilliarde]]<link:Vigintilliarde>", "vigintilliard", "Vigintilliarde")
 
 return export
--- number-list-data-pt.lua
+++ number-list-data-pt.lua
@@ -1,119 +1,100 @@
 local export = {numbers = {}}
 
+local m_numutils = require("Module:number list/utils")
+local map = m_numutils.map
+local power_of = m_numutils.power_of
+
+export.additional_number_types = {
+	{key = "group"},
+}
+
 local numbers = export.numbers
 
-numbers[0] = {
-	cardinal = "zero",
-	ordinal = "zerésimo",
-}
-
-numbers[1] = {
-	cardinal = "um",
-	ordinal = "primeiro",
-	-- adverbial = "",
-	-- multiplier = "",
-	-- distributive = "",
-	-- collective = "",
-	-- fractional = "",
-}
-
-numbers[2] = {
-	cardinal = "dois",
-	ordinal = "segundo",
-}
-
-numbers[3] = {
-	cardinal = "três",
-	ordinal = "terceiro",
-}
-
-numbers[4] = {
-	cardinal = "quatro",
-	ordinal = "quarto",
-}
-
-numbers[5] = {
-	cardinal = "cinco",
-	ordinal = "quinto",
-}
-
-numbers[6] = {
-	cardinal = "seis",
-	ordinal = "sexto",
-}
-
-numbers[7] = {
-	cardinal = "sete",
-	ordinal = "sétimo",
-}
-
-numbers[8] = {
-	cardinal = "oito",
-	ordinal = "oitavo",
-}
-
-numbers[9] = {
-	cardinal = "nove",
-	ordinal = { "nono", "noveno" },
-}
-
-numbers[10] = {
-	cardinal = "dez",
-	ordinal = "décimo",
-}
-
-numbers[11] = {
-	cardinal = "onze",
-	ordinal = { "décimo primeiro", "undécimo" },
-}
-
-numbers[12] = {
-	cardinal = "doze",
-	ordinal = { "décimo segundo", "duodécimo" },
-}
-
-numbers[13] = {
-	cardinal = "treze",
-	ordinal = { "décimo terceiro", "tredécimo" },
-}
-
-numbers[14] = {
-	cardinal = { "quatorze", "catorze" },
-	ordinal = "décimo quarto",
-}
-
-numbers[15] = {
-	cardinal = "quinze",
-	ordinal = "décimo quinto",
-}
-
-numbers[16] = {
-	cardinal = {"dezesseis<tag:Brazil>", "dezasseis<tag:Portugal>"},
-	ordinal = "décimo sexto",
-}
-
-numbers[17] = {
-	cardinal = {"dezessete<tag:Brazil>", "dezassete<tag:Portugal>"},
-	ordinal = "décimo sétimo",
-}
-
-numbers[18] = {
-	cardinal = "dezoito",
-	ordinal = "décimo oitavo",
-}
-
-numbers[19] = {
-	cardinal = {"dezenove<tag:Brazil>", "dezanove<tag:Portugal>"},
-	ordinal = "décimo nono",
-}
-
-local function first_element_if_table(t)
-	if type(t) == "table" then
-		return t[1]
-	else
-		return t
+local function make_number(num, cardinal, ordinal, wplink, fractional)
+	local numstr = m_numutils.format_fixed(num)
+	local thousands_numstr = #numstr < 10 and m_numutils.add_thousands_separator(numstr, ".") or nil
+	if not fractional then
+		-- exceptions to the following rules need to be given explicitly
+		if num == 0 or num == 1 then
+			-- no fractional
+		elseif type(num) == "number" and num <= 10 then
+			fractional = ordinal
+		else
+			local avos_form = map(function(card)
+				if not card:find("%[") then
+					card = ("[[%s]]"):format(card)
+				end
+				return ("%s [[avo]]s"):format(card)
+			end, cardinal)
+			if fractional == true then
+				-- both ordinal and cardinal + avos are possible
+				local combined = {}
+				local function insert_one_or_more(els)
+					if type(els) == "table" then
+						for _, el in ipairs(els) do
+							table.insert(combined, el)
+						end
+					else
+						table.insert(combined, els)
+					end
+				end
+				insert_one_or_more(ordinal)
+				insert_one_or_more(avos_form)
+				fractional = combined
+			else
+				fractional = avos_form
+			end
+		end
 	end
+	numbers[num] = {
+		cardinal = cardinal,
+		ordinal = ordinal,
+		ordinal_abbr = thousands_numstr and {thousands_numstr .. "º", thousands_numstr .. ".º"} or nil,
+		fractional = fractional,
+		wplink = wplink,
+	}
 end
+
+make_number(0, "zero", "zerésimo", "0 (número)")
+make_number(1, "um", "primeiro", "um")
+numbers[1].multiplier = {"único", "singular"}
+make_number(2, "dois", "segundo", "dois", {"meio", "mitade"})
+numbers[2].multiplier = {"dobro", "duplo"}
+numbers[2].group = {"dupla", "par", "duo"}
+make_number(3, "três", "terceiro", "três")
+numbers[3].multiplier = {"triplo", "tríplice"}
+numbers[3].group = {"trio", "trinca", "terceto", "tríade"}
+make_number(4, "quatro", "quarto", "quatro")
+numbers[4].multiplier = "quádruplo"
+numbers[4].group = "quarteto"
+make_number(5, "cinco", "quinto", "cinco")
+numbers[5].multiplier = "quíntuplo"
+numbers[5].group = "quinteto"
+make_number(6, "seis", "sexto", "seis")
+numbers[6].multiplier = "sêxtuplo"
+numbers[6].group = "sexteto"
+make_number(7, "sete", "sétimo", "sete")
+numbers[7].multiplier = {"sétuplo", "séptuplo"}
+numbers[7].group = "septeto"
+make_number(8, "oito", "oitavo", "oito")
+numbers[8].multiplier = "óctuplo"
+numbers[8].group = "octeto"
+make_number(9, "nove", {"nono", "noveno"}, "nove")
+numbers[9].multiplier = {"nônuplo<tag:Brazil>", "nónuplo<tag:Portugal>"}
+numbers[9].group = "noneto"
+make_number(10, "dez", "décimo", "dez")
+numbers[10].multiplier = "décuplo"
+make_number(11, "onze", {"décimo primeiro", "undécimo"}, nil, {"undécimo", "[[onze]] [[avo]]s"})
+numbers[11].multiplier = "undécuplo"
+make_number(12, "doze", {"décimo segundo", "duodécimo"}, nil, {"duodécimo", "[[doze]] [[avo]]s"})
+numbers[12].multiplier = "duodécuplo"
+make_number(13, "treze", "décimo terceiro", "treze")
+make_number(14, {"catorze", "quatorze"}, "décimo quarto", "catorze")
+make_number(15, "quinze", "décimo quinto")
+make_number(16, {"dezesseis<tag:Brazil>", "dezasseis<tag:Portugal>"}, "décimo sexto")
+make_number(17, {"dezessete<tag:Brazil>", "dezassete<tag:Portugal>"}, "décimo sétimo", "17 (número)")
+make_number(18, "dezoito", "décimo oitavo", "dezoito")
+make_number(19, {"dezenove<tag:Brazil>", "dezanove<tag:Portugal>"}, "décimo nono")
 
 for i, vals in ipairs {
 	{ "vinte", "vigésimo" },
@@ -121,46 +102,70 @@ for i, vals in ipairs {
 	{ "quarenta", "quadragésimo" },
 	{ "cinquenta", "quinquagésimo" },
 	{ "sessenta", "sexagésimo" },
-	{ "setenta", "septagésimo" },
-	{ "oitenta", "octagésimo" },
+	{ "setenta", {"septuagésimo", "setuagésimo"}, },
+	{ "oitenta", "octogésimo" },
 	{ "noventa", "nonagésimo" },
 } do
 	local tens = (i + 1) * 10
 	local tens_cardinal, tens_ordinal = unpack(vals)
-	numbers[tens] = {
-		cardinal = tens_cardinal,
-		ordinal = tens_ordinal,
-	}
-	
+	local function has_wplink(num)
+		-- random collection of numbers with Portuguese Wikipedia entries; update as appropriate
+		return num == 23 or num == 25 or num == 30 or num == 35 or num == 36 or num == 37
+	end
+
+	-- true here means both ordinal and cardinal + avos are possible fractional forms
+	make_number(tens, tens_cardinal, tens_ordinal, has_wplink(tens) and tens_cardinal or nil, true)
+
 	for ones = 1, 9 do
+		local num = tens + ones
 		local ones_numeral = numbers[ones]
-		numbers[tens + ones] = {
-			cardinal = tens_cardinal .. " e " .. first_element_if_table(ones_numeral.cardinal),
-			ordinal = tens_ordinal .. " " .. first_element_if_table(ones_numeral.ordinal),
-		}
+		local ones_ordinal = ones_numeral.ordinal
+		if ones == 9 then
+			ones_ordinal = "nono"
+		end
+		local cardinal = tens_cardinal .. " e " .. ones_numeral.cardinal
+		-- Use map() because of 70th.
+		local ordinal = map(function(tens_ord) return tens_ord .. " " .. ones_ordinal end, tens_ordinal)
+		local wplink = has_wplink(num) and tens_cardinal or nil
+		make_number(num, cardinal, ordinal, wplink)
 	end
 end
 
-local function add_number(number, cardinal, ordinal)
-	numbers[number] = {
-		cardinal = cardinal,
-		ordinal = ordinal,
-	}
-end
-
-add_number(100, "cem", "centésimo")
-add_number(200, "duzentos", "ducentésimo")
-add_number(300, "trezentos", "trecentésimo")
-add_number(400, "quatrocentos", "quadrigentésimo")
-add_number(500, "quinhentos", "quingentésimo")
-add_number(600, "seiscentos", "sexcentésimo")
-add_number(700, "setecentos", "septicentésimo")
-add_number(800, "oitocentos", "octigentésimo")
-add_number(900, "novecentos", "nongentésimo")
-add_number(1000, "mil", "milésimo")
-add_number(1e6, "um milhão", "milionésimo")
-add_number(1e6, "um milhão", "milionésimo")
-add_number(1e9, { "um bilhão", "mil milhões" }, "bilionésimo")
-add_number(1e12, { "um trilhão", "um bilhão" }, { "trilionésimo", "bilionésimo" })
+-- Ordinals from https://www.normaculta.com.br/numerais-ordinais/
+-- Fractionals from https://www.normaculta.com.br/numerais-fracionarios/
+-- Note that the above site says 1/100 = only "um centésimo", but in fact "um cem avos" is also common and endorsed by
+-- other sites such as http://www.uel.br/projetos/matessencial/basico/fundamental/fracoes.html
+make_number(100, {"cem<q:alone or followed by a noun or higher numeral>", "cento<q:followed by a lower numeral>"}, "centésimo", nil, {"centésimo", "[[cem]] [[avo]]s"})
+numbers[100].multiplier = "cêntuplo"
+make_number(200, "duzentos", "ducentésimo", nil, true)
+make_number(300, "trezentos", {"trecentésimo", "tricentésimo"}, nil, true)
+make_number(400, "quatrocentos", "quadringentésimo", nil, true)
+make_number(500, "quinhentos", "quingentésimo", "quinhentos", true)
+make_number(600, "seiscentos", {"sexcentésimo", "seiscentésimo"}, "seiscentos", true)
+make_number(700, "setecentos", {"septingentésimo", "setingentésimo"}, "setecentos", true)
+make_number(800, "oitocentos", "octingentésimo", nil, true)
+make_number(900, "novecentos", {"noningentésimo", "nongentésimo"}, "novecentos", true)
+make_number(1000, "mil", "milésimo", nil, true)
+make_number(10000, "[[dez]] [[mil]]", {"[[décimo]] [[milésimo]]", "[[décimo]] [[de]] [[milésimo]]"}, "dez mil", true)
+make_number(100000, "[[cem]] [[mil]]", {"[[centésimo]] [[milésimo]]", "[[centésimo]] [[de]] [[milésimo]]"}, nil, true)
+make_number(1000000, "[[um]] [[milhão]]<link:milhão>", "milionésimo", "milhão", true)
+make_number(10000000, "[[dez]] [[milhão|milhões]]", {"[[décimo]] [[milionésimo]]", "[[décimo]] [[de]] [[milionésimo]]"}, nil, true)
+make_number(100000000, "[[cem]] [[milhão|milhões]]", {"[[centésimo]] [[milionésimo]]", "[[centésimo]] [[de]] [[milionésimo]]"}, nil, true)
+make_number(power_of(9), {"[[um]] [[bilhão]]<link:bilhão><tag:Brazil>", "[[mil]] [[milhão|milhões]]<tag:Portugal>"}, {"bilionésimo<tag:Brazil>", "[[milésimo]] [[milionésimo]]<tag:Portugal>", "[[milésimo]] [[de]] [[milionésimo]]<tag:Portugal>"},
+	"1 000 000 000", true)
+make_number(power_of(12), {"[[um]] [[trilhão]]<link:trilhão><tag:Brazil>", "[[um]] [[bilião]]<link:bilião><tag:Portugal>"}, {"trilionésimo<tag:Brazil>", "bilionésimo<tag:Portugal>"},
+	"1 000 000 000 000", true)
+make_number(power_of(15), {"[[um]] [[quatrilhão]]<link:quatrilhão><tag:Brazil>", "[[um]] [[quadrilhão]]<link:quadrilhão><tag:Brazil>", "[[mil]] [[bilião|biliões]]<tag:Portugal>"}, {"quatrilionésimo<tag:Brazil>", "quadrilionésimo<tag:Brazil>", "[[milésimo]] [[bilionésimo]]<tag:Portugal>", "[[milésimo]] [[de]] [[bilionésimo]]<tag:Portugal>"},
+	"1000000000000000", true)
+make_number(power_of(18), {"[[um]] [[quintilhão]]<link:quintilhão><tag:Brazil>", "[[um]] [[trilião]]<link:trilião><tag:Portugal>"}, {"quintilionésimo<tag:Brazil>", "trilionésimo<tag:Portugal>"},
+	"1000000000000000000", true)
+make_number(power_of(21), {"[[um]] [[sextilhão]]<link:sextilhão><tag:Brazil>", "[[mil]] [[trilião|triliões]]<tag:Portugal>"}, {"sextilionésimo<tag:Brazil>", "[[milésimo]] [[trilionésimo]]<tag:Portugal>", "[[milésimo]] [[de]] [[trilionésimo]]<tag:Portugal>"},
+	nil, true)
+make_number(power_of(24), {"[[um]] [[septilhão]]<link:septilhão><tag:Brazil>", "[[um]] [[quatrilião]]<link:quatrilião><tag:Portugal>", "[[um]] [[quadrilião]]<link:quadrilião><tag:Portugal>"}, {"septilionésimo<tag:Brazil>", "quatrilionésimo<tag:Portugal>", "quadrilionésimo<tag:Portugal>"},
+	nil, true)
+make_number(power_of(27), {"[[um]] [[octilhão]]<link:octilhão><tag:Brazil>", "[[mil]] [[quatrilião|quatriliões]]<tag:Portugal>", "[[mil]] [[quadrilião|quadriliões]]<tag:Portugal>"}, {"octilionésimo<tag:Brazil>", "[[milésimo]] [[quatrilionésimo]]<tag:Portugal>", "[[milésimo]] [[quadrilionésimo]]<tag:Portugal>", "[[milésimo]] [[de]] [[quatrilionésimo]]<tag:Portugal>", "[[milésimo]] [[de]] [[quadrilionésimo]]<tag:Portugal>"},
+	nil, true)
+make_number(power_of(30), {"[[um]] [[nonilhão]]<link:nonilhão><tag:Brazil>", "[[um]] [[quintilião]]<link:quintilião><tag:Portugal>"}, {"nonilionésimo<tag:Brazil>", "quintilionésimo<tag:Portugal>"},
+	nil, true)
 
 return export
