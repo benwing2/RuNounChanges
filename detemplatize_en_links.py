c858d82bb42ece4e2d0a1803b929555e270ec3a0
Ben Wing <ben@benwing.com>
2022-09-05 22:55:13 -0500

    detemplatize_en_links: always both templatize and detemplatize

--- detemplatize_en_links.py
+++ detemplatize_en_links.py
@@ -23,37 +23,37 @@ def process_text_on_page(index, pagetitle, text):
     lines = sectext.split("\n")
     new_lines = []
     for line in lines:
-      if line.startswith("#"):
-        if args.convert_raw_self_links:
-          template_split_re = r"(\{\{(?:[^{}]|\{\{[^{}]*\}\})*\}\})"
-          # Split templates and only change non-template text
-          split_templates = re.split(template_split_re, line)
-          for l in xrange(0, len(split_templates), 2):
-            while True:
-              newtext = re.sub(r"^#(.*?)\[\[%s\]\]" % pagetitle, r"#\1" + get_templated_self_link(pagetitle),
-                  split_templates[l], 0, re.M)
-              if newtext == split_templates[l]:
-                break
-              changed = True
-              notes.append("replace raw self link to English terms with templated one")
-              split_templates[l] = newtext
-          line = "".join(split_templates)
-        else:
-          def replace_templated(m):
-            origm1 = m.group(1)
-            m1 = origm1
-            if "[[" not in m1:
-              m1 = "[[%s]]" % m1
-            m1_new = m1.replace("[[%s]]" % pagetitle, get_templated_self_link(pagetitle))
-            saw_self_link = False
-            if m1_new != m1:
-              saw_self_link = True
-              m1 = m1_new
-            if m1 != get_templated_self_link(origm1):
-              notes.append("replace templated link to English terms in defns with raw link(s)" + (
-                ", keeping self-links templated" if saw_self_link else ""))
-            return m1
-          line = re.sub(r"\{\{l\|en\|((?:[^{}|]|\[\[[^{}\[\]]*\]\])*?)\}\}", replace_templated, line)
+      if re.search("^#+[^*:#]", line):
+        # First, replace templated English links in definitions with raw ones.
+        def replace_templated(m):
+          origm1 = m.group(1)
+          m1 = origm1
+          if "[[" not in m1:
+            m1 = "[[%s]]" % m1
+          m1_new = m1.replace("[[%s]]" % pagetitle, get_templated_self_link(pagetitle))
+          saw_self_link = False
+          if m1_new != m1:
+            saw_self_link = True
+            m1 = m1_new
+          if m1 != get_templated_self_link(origm1):
+            notes.append("replace templated link to English term(s) in defns with raw link(s)" + (
+              ", keeping self-links templated" if saw_self_link else ""))
+          return m1
+        # Replace only one-part templated links (no vertical bars outside of raw links) but allow two-part raw links
+        # inside of a one-part templated link.
+        line = re.sub(r"\{\{l\|en\|((?:[^{}|\[\]]|\[\[[^{}\[\]]*\]\])*?)\}\}", replace_templated, line)
+
+        # Now, replace raw English self-links with templated ones.
+        template_split_re = r"(\{\{(?:[^{}]|\{\{[^{}]*\}\})*\}\})"
+        # Split templates and only change non-template text
+        split_templates = re.split(template_split_re, line)
+        for l in xrange(0, len(split_templates), 2):
+          def replace_raw(m):
+            notes.append("replace raw self link to English term with templated one")
+            return get_templated_self_link(pagetitle)
+          split_templates[l] = re.sub(r"\[\[(?:#English\|)?%s\]\]" % re.escape(pagetitle), replace_raw, split_templates[l])
+        line = "".join(split_templates)
+
       new_lines.append(line)
     return "\n".join(new_lines)
 
