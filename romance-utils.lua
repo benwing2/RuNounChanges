f414c4367bd4b22c61112fda7d4540a618fd4ca7
Ben Wing <ben@benwing.com>
2022-11-06 18:24:10 -0600

    romance-utils: support 'include_hyphen_prefixes' (NOT TESTED)

--- romance-utils.lua
+++ romance-utils.lua
@@ -165,59 +165,75 @@ end
 
 
 -- Auto-add links to a word that should not have spaces but may have hyphens and/or apostrophes. We split off final
--- punctuation, then split on hyphens if `split_dash` is given, and also split on apostrophes. The apostrophe is
--- included in the link to its left (so we auto-split "l'eau" as "[[l']][[eau]]). `no_split_apostrophe_words`, if
--- given, is a set of words that contain apostrophes but which should not be split on the apostrophes, such as
--- "[[c'est]]" and "[[quelqu'un]]".
-local function add_single_word_links(space_word, split_dash, no_split_apostrophe_words)
+-- punctuation, then split on hyphens if `splithyph` is given, and also split on apostrophes. We only split on hyphens
+-- and apostrophes if they are in the middle of the word, not at the beginning of end (hyphens at the beginning or end
+-- indicate suffixes or prefixes, respectively, and apostrophes at the beginning or end are also possible, as in
+-- Italian [['ndrangheta]] or [[po']]). The apostrophe is included in the link to its left (so we auto-split French
+-- [[l'eau]] as [[l']][[eau]]). `no_split_apostrophe_words`, if given, is a set of words that contain apostrophes but
+-- which should not be split on the apostrophes, such as French [[c'est]] and [[quelqu'un]]. `include_hyphen_prefixes`,
+-- if given, is a set of prefixes (not including the final hyphen) where we should include the final hyphen in the
+-- prefix. Hence, e.g. if "anti" is in the set, a Portuguese word like [[anti-herói]] "anti-hero" will be split
+-- [[anti-]][[herói]] (whereas a word like [[código-fonte]] "source code" will be split as [[código]]-[[fonte]]).
+local function add_single_word_links(space_word, splithyph, no_split_apostrophe_words, include_hyphen_prefixes)
 	local space_word_no_punct, punct = rmatch(space_word, "^(.*)([,;:?!])$")
 	space_word_no_punct = space_word_no_punct or space_word
 	punct = punct or ""
 	local words
 	-- don't split prefixes and suffixes
-	if not split_dash or space_word_no_punct:find("^%-") or space_word_no_punct:find("%-$") then
+	if not splithyph or space_word_no_punct:find("^%-") or space_word_no_punct:find("%-$") then
 		words = {space_word_no_punct}
 	else
 		words = rsplit(space_word_no_punct, "%-")
 	end
 	local linked_words = {}
-	for _, word in ipairs(words) do
-		-- Don't split on apostrophes if the word is in `no_split_apostrophe_words` or begins or ends with an apostrophe
-		-- (e.g. [['ndrangheta]] or [[po']]). Handle multiple apostrophes correctly, e.g. [[l'altr'ieri]].
-		if (not no_split_apostrophe_words or not no_split_apostrophe_words[word]) and word:find("'")
-			and not word:find("^'") and not word:find("'$") then
-			local apostrophe_parts = rsplit(word, "'")
-			for i, apostrophe_part in ipairs(apostrophe_parts) do
-				if i == #apostrophe_parts then
-					apostrophe_parts[i] = "[[" .. apostrophe_part .. "]]"
-				else
-					apostrophe_parts[i] = "[[" .. apostrophe_part .. "']]"
-				end
-			end
-			word = table.concat(apostrophe_parts)
+	for j, word in ipairs(words) do
+		if j < #words and include_hyphen_prefixes and include_hyphen_prefixes[word] then
+			word = "[[" .. word .. "-]]"
 		else
-			word = "[[" .. word .. "]]"
+			-- Don't split on apostrophes if the word is in `no_split_apostrophe_words` or begins or ends with an apostrophe
+			-- (e.g. [['ndrangheta]] or [[po']]). Handle multiple apostrophes correctly, e.g. [[l'altr'ieri]].
+			if (not no_split_apostrophe_words or not no_split_apostrophe_words[word]) and word:find("'")
+				and not word:find("^'") and not word:find("'$") then
+				local apostrophe_parts = rsplit(word, "'")
+				for i, apostrophe_part in ipairs(apostrophe_parts) do
+					if i == #apostrophe_parts then
+						apostrophe_parts[i] = "[[" .. apostrophe_part .. "]]"
+					else
+						apostrophe_parts[i] = "[[" .. apostrophe_part .. "']]"
+					end
+				end
+				word = table.concat(apostrophe_parts)
+			else
+				word = "[[" .. word .. "]]"
+			end
+			if j < #words then
+				word = word .. "-"
+			end
 		end
 		table.insert(linked_words, word)
 	end
-	return table.concat(linked_words, "-") .. punct
+	return table.concat(linked_words) .. punct
 end
 
 -- Auto-add links to a lemma. Links are not added to single-word lemmas. We split on spaces, and also on hyphens if
--- split_dash is given or the word has no spaces. In addition, we split on apostrophes, including the apostrophe in
+-- `splithyph` is given or the word has no spaces. In addition, we split on apostrophes, including the apostrophe in
 -- the link to its left (so we auto-split "de l'eau" "[[de]] [[l']][[eau]]"). We don't always split on hyphens because
 -- of cases like "boire du petit-lait" where "petit-lait" should be linked as a whole, but provide the option to do it
 -- for cases like "croyez-le ou non". If there's no space, however, then it makes sense to split on hyphens by default
 -- (e.g. for "avant-avant-hier"). Cases where only some of the hyphens should be split can always be handled by
 -- explicitly specifying the head (e.g. "Nord-Pas-de-Calais" given as head=[[Nord]]-[[Pas-de-Calais]]).
-function export.add_lemma_links(lemma, split_dash, no_split_apostrophe_words)
+--
+-- `no_split_apostrophe_words` and `include_hyphen_prefixes` allow for special-case handling of particular words and
+-- are as described in the comment above add_single_word_links().
+function export.add_lemma_links(lemma, splithyph, no_split_apostrophe_words, include_hyphen_prefixes)
 	if not rfind(lemma, " ") then
-		split_dash = true
+		splithyph = true
 	end
 	local words = rsplit(lemma, " ")
 	local linked_words = {}
 	for _, word in ipairs(words) do
-		table.insert(linked_words, add_single_word_links(word, split_dash, no_split_apostrophe_words))
+		table.insert(linked_words, add_single_word_links(word, splithyph, no_split_apostrophe_words,
+			include_hyphen_prefixes))
 	end
 	local retval = table.concat(linked_words, " ")
 	-- If we ended up with a single link consisting of the entire lemma,
