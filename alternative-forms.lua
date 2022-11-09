a23e710498858ac8b25d073900f03b5dd0df2f2a
Ben Wing <ben@benwing.com>
2022-11-09 04:22:39 -0600

    alternative-forms: support q=, qq=, semicolon separator (NOT TESTED)

--- alternative-forms.lua
+++ alternative-forms.lua
@@ -29,13 +29,13 @@ function export.make_dialects(raw, lang)
 	if raw[1] then
 		dialect_info = mw.title.new(dialect_page).exists and mw.loadData(dialect_page) or false
 	end
-		
+
 	local dialects = {}
-	
+
 	for _, dialect in ipairs(raw) do
 		table.insert(dialects, dialect_info and export.getLabel(dialect, dialect_info) or dialect)
 	end
-	
+
 	return dialects
 end
 
@@ -52,11 +52,12 @@ end
 --                  is {list = true, allow_holes = true}.
 -- * `convert`: An optional function to convert the raw argument into the form passed to [[Module:links]].
 --              This function takes three parameters: (1) `arg` (the raw argument); (2) `inline` (true if we're
---              processing an inline modifier, false otherwise); (4) `i` (the logical index of the term being processed,
---              starting from 1).
--- * `item_dest`: The name of the key used when storing the parameter's value into the processed `part` object.
---                Normally the same as the parameter's name. Different in the case of "t", where we store the gloss in
---                "gloss", and "g", where we store the genders in "genders".
+--              processing an inline modifier, false otherwise); (4) `termno` (the logical index of the term being
+--              processed, starting from 1).
+-- * `item_dest`: The name of the key used when storing the parameter's value into the processed `term` or `termobj`
+--                object. Normally the same as the parameter's name. Different in the case of "t", where we store the
+--                gloss in "gloss", and "g", where we store the genders in "genders".
+-- * `outer`: If true, store the value into `termobj` rather than `term`.
 local param_mods = {
 	t = {
 		-- We need to store the t1=/t2= param and the <t:...> inline modifier into the "gloss" key of the parsed term,
@@ -73,16 +74,18 @@ local param_mods = {
 		-- We need to store the g1=/g2= param and the <g:...> inline modifier into the "genders" key of the parsed term,
 		-- because that is what [[Module:links]] expects.
 		item_dest = "genders",
-		convert = function(arg, inline, i)
+		convert = function(arg, inline, termno)
 			return rsplit(arg, ",")
 		end,
 	},
 	id = {},
 	alt = {},
-	-- Not yet supported. FIXME: Support me.
-	-- q = {},
-	-- Not yet supported. FIXME: Support me.
-	-- qq = {},
+	q = {
+		outer = true,
+	},
+	qq = {
+		outer = true,
+	},
 	lit = {},
 	pos = {},
 }
@@ -100,14 +103,14 @@ function export.create(frame)
 	local title = mw.title.getCurrentTitle()
 	local NAMESPACE = title.nsText
 	local PAGENAME = title.text
-	
+
 	local list_with_holes = { list = true, allow_holes = true }
 	local params = {
 		[1] = { required = true, default = "und" },
 		[2] = list_with_holes,
 		["sc"] = {},
 	}
-	
+
 	for param_mod, param_mod_spec in pairs(param_mods) do
 		if not param_mod_spec.extra_specs then
 			params[param_mod] = list_with_holes
@@ -125,7 +128,7 @@ function export.create(frame)
 	local sc = args["sc"] and require("Module:scripts").getByCode(args["sc"], "sc") or nil
 
 	local rawDialects = {}
-	local links = {}
+	local items = {}
 
 	-- Find the maximum index among any of the list parameters.
 	local maxmaxindex = 0
@@ -142,7 +145,9 @@ function export.create(frame)
 	-- Is set to true if there is a term (entry link, alt text, transliteration, transcription) at the previous index.
 	local prev = false
 	local use_semicolon = false
+	local put
 
+	local termno = 0
 	for i = 1, maxmaxindex do
 		-- If the previous term parameter was empty and we're not on the first term parameter,
 		-- this term parameter and any others contain dialect or other labels.
@@ -151,24 +156,23 @@ function export.create(frame)
 			break
 		end
 
-		-- Compute whether any of the separate indexed params exist for this index.
-		local any_param_at_index = false
-		for k, v in pairs(args) do
-			if type(v) == "table" and v[i] then
-				any_param_at_index = true
-				break
-			end
-		end
+		local term = args[2][i]
 
-		-- If any of the params used for formatting this term is present, create a term and add it to the list.
-		if any_param_at_index then
-			-- Tracking for use of any specific indexed parameter. FIXME: Do we still need this?
+		if term ~= ";" then
+			-- Compute whether any of the separate indexed params exist for this index.
+			local any_param_at_index = term ~= nil
 			for k, v in pairs(args) do
 				-- Look for named list parameters. We check:
-				-- (1) key is a string (excludes 2=, a numbered rather than named list param);
+				-- (1) key is a string (excludes 2=, a numbered rather than named list param, because it needs to
+				--     be indexed using `i` instead of `termno`);
 				-- (2) value is a table (1= and sc= are converted into strings or nil rather than lists);
-				-- (3) the value has an entry at index `i` (the current index).
-				if type(k) == "string" and type(v) == "table" and v[i] then
+				-- (3) the value has an entry at index `termno` (the current logical index).
+				if type(k) == "string" and type(v) == "table" and v[termno] then
+					any_param_at_index = true
+					-- Tracking for use of any specific indexed parameter. FIXME: Do we still need this?
+					-- FIXME: If we don't need it, remove the call to track() below, add `break` below, and wrap
+					-- the `for` loop in `if not any_param_at_index then` for efficiency purposes.
+					-- break
 					-- [[Special:WhatLinksHere/Template:tracking/alter/alt]]
 					-- [[Special:WhatLinksHere/Template:tracking/alter/id]]
 					-- [[Special:WhatLinksHere/Template:tracking/alter/tr]]
@@ -176,143 +180,172 @@ function export.create(frame)
 					track(k)
 				end
 			end
-			
-			-- Initialize the `part` object passed to full_link() in [[Module:links]].
-			local term = args[2][i]
-			local part = {
-				lang = lang,
-				sc = sc,
-				term = term,
-			}
 
-			-- Parse all the term-specific parameters and store in `part`.
-			for param_mod, param_mod_spec in pairs(param_mods) do
-				local dest = param_mod_spec.item_dest or param_mod
-				local arg = args[param_mod][i]
-				if arg then
-					if param_mod_spec.convert then
-						arg = param_mod_spec.convert(arg, false, i)
+			-- If any of the params used for formatting this term is present, create a term and add it to the list.
+			if any_param_at_index then
+				termno = ternmo + 1
+
+				-- Initialize the `termobj` object and the `term` object passed to full_link() in [[Module:links]].
+				local termobj = {
+					joiner = i > 1 and (args[2][i - 1] == ";" and "; " or ", ") or "",
+					term = {
+						lang = lang,
+						sc = sc,
+						term = term,
+					},
+				}
+
+				-- Parse all the term-specific parameters and store in `term` or `termobj`.
+				for param_mod, param_mod_spec in pairs(param_mods) do
+					local dest = param_mod_spec.item_dest or param_mod
+					local arg = args[param_mod][termmno]
+					if arg then
+						if param_mod_spec.convert then
+							arg = param_mod_spec.convert(arg, false, termno)
+						end
+						local obj = param_mod_spec.outer and termobj or termobj.term
+						obj[dest] = arg
 					end
-					part[dest] = arg
 				end
-			end
 
-			-- Check for inline modifier, e.g. מרים<tr:Miryem>. But exclude HTML entry with <span ...>, <i ...>, <br/>
-			-- or similar in it, caused by wrapping an argument in {{l|...}}, {{af|...}} or similar. Basically, all tags
-			-- of the sort we parse here should consist of a less-than sign, plus letters, plus a colon, e.g. <tr:...>,
-			-- so if we see a tag on the outer level that isn't in this format, we don't try to parse it. The
-			-- restriction to the outer level is to allow generated HTML inside of e.g. qualifier tags, such as
-			-- foo<q:similar to {{m|fr|bar}}>.
-			if term and term:find("<") and not term:find("^[^<]*<[a-z]*[^a-z:]") then
-				if not put then
-					put = require("Module:parse utilities")
+				-- Check for inline modifier, e.g. מרים<tr:Miryem>. But exclude HTML entry with <span ...>, <i ...>, <br/>
+				-- or similar in it, caused by wrapping an argument in {{l|...}}, {{af|...}} or similar. Basically, all tags
+				-- of the sort we parse here should consist of a less-than sign, plus letters, plus a colon, e.g. <tr:...>,
+				-- so if we see a tag on the outer level that isn't in this format, we don't try to parse it. The
+				-- restriction to the outer level is to allow generated HTML inside of e.g. qualifier tags, such as
+				-- foo<q:similar to {{m|fr|bar}}>.
+				if term and term:find("<") and not term:find("^[^<]*<[a-z]*[^a-z:]") then
+					if not put then
+						put = require("Module:parse utilities")
+					end
+					local run = put.parse_balanced_segment_run(term, "<", ">")
+					local function parse_err(msg)
+						-- Add 1 before first term index starts at 2.
+						error(msg .. ": " .. (i + 1) .. "=" .. table.concat(run))
+					end
+					part.term = run[1]
+
+					for j = 2, #run - 1, 2 do
+						if run[j + 1] ~= "" then
+							parse_err("Extraneous text '" .. run[j + 1] .. "' after modifier")
+						end
+						local modtext = run[j]:match("^<(.*)>$")
+						if not modtext then
+							parse_err("Internal error: Modifier '" .. modtext .. "' isn't surrounded by angle brackets")
+						end
+						local prefix, arg = modtext:match("^([a-z]+):(.*)$")
+						if not prefix then
+							parse_err(("Modifier %s lacks a prefix, should begin with one of %s followed by a colon"):format(
+								run[j], table.concat(get_valid_prefixes(), ",")))
+						end
+						local param_mod_spec = param_mods[prefix]
+						if not param_mod_spec then
+							parse_err(("Unrecognized prefix '%s' in modifier %s, should be one of %s"):format(
+								prefix, run[j], table.concat(get_valid_prefixes(), ",")))
+						end
+						local dest = param_mod_spec.item_dest or prefix
+						if part[dest] then
+							parse_err("Modifier '" .. prefix .. "' occurs twice, second occurrence " .. run[j])
+						end
+						if param_mod_spec.convert then
+							arg = param_mod_spec.convert(arg, true, termno)
+						end
+						local obj = param_mod_spec.outer and termobj or termobj.term
+						obj[dest] = arg
+					end
 				end
-				local run = put.parse_balanced_segment_run(term, "<", ">")
-				local function parse_err(msg)
-					error(msg .. ": " .. (i + 1) .. "=" .. table.concat(run))
+
+				-- FIXME: Either we should have a general mechanism in `param_mods` for default values, or (better) modify
+				-- [[Module:links]] so it can handle nil for .genders.
+				termobj.term.genders = termobj.term.genders or {}
+
+				-- If the displayed term (from .term or .alt) has an embedded comma, use a semicolon to join the terms.
+				local term_text = termobj.term.term or termobj.term.alt
+				if not use_semicolon and term_text then
+					if term_text:find(",", 1, true) then
+						use_semicolon = true
+					end
 				end
-				part.term = run[1]
 
-				for j = 2, #run - 1, 2 do
-					if run[j + 1] ~= "" then
-						parse_err("Extraneous text '" .. run[j + 1] .. "' after modifier")
-					end
-					local modtext = run[j]:match("^<(.*)>$")
-					if not modtext then
-						parse_err("Internal error: Modifier '" .. modtext .. "' isn't surrounded by angle brackets")
-					end
-					local prefix, arg = modtext:match("^([a-z]+):(.*)$")
-					if not prefix then
-						parse_err(("Modifier %s lacks a prefix, should begin with one of %s followed by a colon"):format(
-							run[j], table.concat(get_valid_prefixes(), ",")))
-					end
-					if not param_mods[prefix] then
-						parse_err(("Unrecognized prefix '%s' in modifier %s, should be one of %s"):format(
-							prefix, run[j], table.concat(get_valid_prefixes(), ",")))
-					end
-					local dest = param_mods[prefix].item_dest or prefix
-					if part[dest] then
-						parse_err("Modifier '" .. prefix .. "' occurs twice, second occurrence " .. run[j])
-					end
-					if param_mods[prefix].convert then
-						arg = param_mods[prefix].convert(arg, true, i)
-					end
-					part[dest] = arg
+				-- If the to-be-linked term is the same as the pagename, display it unlinked.
+				if termobj.term.term and lang:makeEntryName(termobj.term.term) == PAGENAME then
+					track("term is pagename")
+					termobj.term.alt = termobj.term.alt or termobj.term.term
+					termobj.term.term = nil
 				end
-			end
 
-			-- FIXME: Either we should have a general mechanism in `param_mods` for default values, or (better) modify
-			-- [[Module:links]] so it can handle nil for .genders.
-			part.genders = part.genders or {}
-
-			-- If the displayed term (from .term or .alt) has an embedded comma, use a semicolon to join the terms.
-			local term_text = part.term or part.alt
-			if not use_semicolon and term_text then
-				if term_text:find(",", 1, true) then
-					use_semicolon = true
+				table.insert(items, termobj)
+				prev = true
+			else
+				if math.max(args.alt.maxindex, args.id.maxindex, args.tr.maxindex, args.ts.maxindex) >= i then
+					track("too few terms")
 				end
-			end
 
-			-- If the to-be-linked term is the same as the pagename, display it unlinked.
-			if part.term and lang:makeEntryName(part.term) == PAGENAME then
-				track("term is pagename")
-				part.alt = part.alt or part.term
-				part.term = nil
+				prev = false
 			end
-
-			table.insert(links, m_links.full_link(part))
-			prev = true
-		else
-			if math.max(args.alt.maxindex, args.id.maxindex, args.tr.maxindex, args.ts.maxindex) >= i then
-				track("too few terms")
-			end
-			
-			prev = false
 		end
 	end
-	
-	-- The template must have either links or dialect labels.
-	if links[1] == nil and rawDialects[1] == nil then error("No terms found!") end
 
-	local dialects  = export.make_dialects(rawDialects, lang)
-	
-	local output = { table.concat(links, use_semicolon and '; ' or ', ') }
+	-- The template must have either items or dialect labels.
+	if items[1] == nil and rawDialects[1] == nil then error("No terms found!") end
+
+	-- If any term had an embedded comma, override all joiners to be semicolons.
+	if use_semicolon then
+		for i, item in ipairs(items) do
+			if i > 1 then
+				item.joiner = "; "
+			end
+		end
+	end
+
+	-- Format all the items, including joiners, pre-qualifiers and post-qualifiers.
+	for i, item in ipairs(items) do
+		local preq_text = item.q and require("Module:qualifier").format_qualifier(item.q) .. " " or ""
+		items[i] = item.joiner .. preq_text .. m_links.full_link(item.term)
+			.. (item.qq and " " .. require("Module:qualifier").format_qualifier(item.qq) or "")
+	end
+
+	-- Construct the final output.
+	local output = { table.concat(items) }
+
+	-- If there are dialect or similar tags, construct them now and append to final output.
+	local dialects = export.make_dialects(rawDialects, lang)
 	if #dialects > 0 then
 		local dialect_label
 		if lang:hasTranslit() then
-			dialect_label = " &ndash; ''" .. table.concat(dialects, ", ") .. "''" 
+			dialect_label = " &ndash; ''" .. table.concat(dialects, ", ") .. "''"
 		else
 			dialect_label = " (''" .. table.concat(dialects, ", ") .. "'')"
 		end
-		
+
 		-- Fixes the problem of '' being added to '' at the end of last dialect parameter
 		dialect_label = mw.ustring.gsub(dialect_label, "''''", "")
 		table.insert(output, dialect_label)
 	end
-	
+
 	return table.concat(output)
 end
 
 function export.categorize(frame)
 	local content = {}
-	
+
 	local title = mw.title.getCurrentTitle()
 	local titletext = title.text
 	local namespace = title.nsText
 	local subpagename = title.subpageText
-	
+
 	-- subpagename ~= titletext if it is a documentation page
 	if namespace == "Module" and subpagename == titletext then
 		local langCode = mw.ustring.match(titletext, "^([^:]+):")
 		local lang = m_languages.getByCode(langCode) or error('"' .. langCode .. '" is not a valid language code.')
 		content.canonicalName = lang:getCanonicalName()
-		
+
 		local categories =
 [=[
 [[Category:<canonicalName> modules|dialects]]
 [[Category:Dialectal data modules|<canonicalName>]]
 ]=]
-		
+
 		categories = mw.ustring.gsub(categories, "<([^>]+)>", content)
 		return categories
 	end
