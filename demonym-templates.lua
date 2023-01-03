79ce5a2af9944d3d2124fcec4acd7fa8ede34f6d
Ben Wing <ben@benwing.com>
2023-01-03 02:53:56 -0600

    demonym,demonym-templates: mostly done

---
+++ demonym-templates.lua
@@ -0,0 +1,204 @@
+local export = {}
+
+local m_languages = require("Module:languages")
+local m_demonym = require("Module:demonym")
+
+local rsplit = mw.text.split
+local u = mw.ustring.char
+
+
+-- Ensure that brackets display literally in error messages. Replacing with equivalent HTML escapes doesn't work
+-- because they are displayed literally; but inserting a Unicode word-joiner symbol works.
+local function escape_brackets(term)
+	term = term:gsub("%[%[", "[" .. u(0x2060) .. "[")
+	return term
+end
+
+
+-- Return a param_mods structure as required by parse_inline_modifiers() in [[Module:parse utilities]]:
+-- * `convert`: An optional function to convert the raw argument into the form needed for further processing.
+--              This function takes two parameters: (1) `arg` (the raw argument); (2) `parse_err` (a function to
+--              generate an error).
+-- * `item_dest`: The name of the key used when storing the parameter's value into the processed object.
+--                Normally the same as the parameter's name. Different in the case of "t", where we store the gloss in
+--                "gloss", and "g", where we store the genders in "genders".
+local function get_param_mods(termparam)
+	return {
+		t = {
+			-- We need to store the <t:...> inline modifier into the "gloss" key of the parsed part, because that is what
+			-- [[Module:links]] expects.
+			item_dest = "gloss",
+		},
+		gloss = {},
+		tr = {},
+		ts = {},
+		g = {
+			-- We need to store the <g:...> inline modifier into the "genders" key of the parsed part, because that is what
+			-- [[Module:links]] expects.
+			item_dest = "genders",
+			convert = function(arg, parse_err)
+				return rsplit(arg, ",")
+			end,
+		},
+		id = {},
+		alt = {},
+		q = {},
+		qq = {},
+		lit = {},
+		pos = {},
+		sc = {
+			-- We need a conversion function to convert from script codes to script objects, which needs to know the name
+			-- of the parameter we're modifying. (This only affects the error message.)
+			convert = function(arg, parse_err)
+				return require("Module:scripts").getByCode(arg, "" .. termparam .. ":sc")
+			end,
+		}
+	}
+end
+
+
+local function get_valid_prefixes(param_mods)
+	local valid_prefixes = {}
+	for param_mod, _ in pairs(param_mods) do
+		table.insert(valid_prefixes, param_mod)
+	end
+	table.sort(valid_prefixes)
+	return valid_prefixes
+end
+
+
+local function fetch_script(sc, param)
+	return sc and require("Module:scripts").getByCode(sc, param) or nil
+end
+
+
+local function parse_args(args, hack_params)
+	local params = {
+		[1] = {required = true, default = "und"},
+		[2] = {list = true, required = true},
+		
+		["t"] = {list = true},
+		["sort"] = {},
+		["nocat"] = {type = "boolean"},
+		["nocap"] = {type = "boolean"},
+		["notext"] = {type = "boolean"},
+	}
+
+	if hack_params then
+		hack_params(params)
+	end
+
+	args = require("Module:parameters").process(args, params)
+	local lang = m_languages.getByCode(args[1], 1)
+	return args, lang
+end
+
+
+local function parse_term_with_modifiers(paramname, val)
+	local function parse_err(msg)
+		error(msg .. ": " .. paramname .. "=" .. escape_brackets(val))
+	end
+
+	local function generate_obj(term)
+		local obj = {}
+		-- Parse off an initial language code (e.g. 'la:minūtia' or 'grc:[[σκῶρ|σκατός]]'). Also handle Wikipedia prefixes
+		-- ('w:Abatemarco' or 'w:it:Colle Val d'Elsa').
+		local termlang, actual_term = term:match("^([A-Za-z0-9._-]+):(.*)$")
+		if termlang == "w" then
+			local foreign_wikipedia, foreign_term = actual_term:match("^([A-Za-z0-9._-]+):(.*)$")
+			if foreign_wikipedia then
+				termlang = termlang .. ":" .. foreign_wikipedia
+				actual_term = foreign_term
+			end
+			if actual_term:find("[%[%]]") then
+				parse_err("Cannot have brackets following a Wikipedia (w:...) link; place the Wikipedia link inside the brackets")
+			end
+			term = ("[[%s:%s|%s]]"):format(termlang, actual_term, actual_term)
+			termlang = nil
+		elseif termlang then
+			termlang = m_languages.getByCode(termlang, paramname, "allow etym")
+			term = actual_term
+		end
+
+		obj.lang = termlang
+		obj.term = term
+		return obj
+	end
+
+	-- Check for inline modifier, e.g. מרים<tr:Miryem>. But exclude HTML entry with <span ...>, <i ...>, <br/> or
+	-- similar in it, caused by wrapping an argument in {{l|...}}, {{m|...}} or similar. Basically, all tags of
+	-- the sort we parse here should consist of a less-than sign, plus letters, plus a colon, e.g. <tr:...>, so if
+	-- we see a tag on the outer level that isn't in this format, we don't try to parse it. The restriction to the
+	-- outer level is to allow generated HTML inside of e.g. qualifier tags, such as foo<q:similar to {{m|fr|bar}}>.
+	if val:find("<") and not val:find("^[^<]*<[a-z]*[^a-z:]") then
+		if not put then
+			put = require("Module:parse utilities")
+		end
+		local param_mods = get_param_mods(paramname)
+		return put.parse_inline_modifiers(val, param_mods, generate_obj, parse_err)
+	else
+		return generate_obj(val)
+	end
+
+	return part
+end
+
+
+local function get_terms_and_glosses(args)
+	for i, term in ipairs(args[2]) do
+		args[2][i] = parse_term_with_modifiers(i + 1, term)
+	end
+	for i, gloss in ipairs(args.t) do
+		args.t[i] = parse_term_with_modifiers("t" .. (i == 1 and "" or i), gloss)
+	end
+
+	return args[2], args.t
+end
+
+
+function export.demonym_adj(frame)
+	local args, lang = parse_args(frame:getParent().args)
+
+	local terms, glosses = get_terms_and_glosses(args)
+
+	return m_demonym.show_demonym_adj {
+		lang = lang,
+		parts = terms,
+		gloss = glosses,
+		sort = args.sort,
+		nocat = args.nocat,
+		nocap = args.nocap,
+		notext = args.notext,
+	}
+end
+
+
+function export.demonym_noun(frame)
+	local function hack_params(params)
+		params.g = {}
+		params.m = {list = true}
+	end
+
+	local args, lang = parse_args(frame:getParent().args)
+
+	local terms, glosses = get_terms_and_glosses(args)
+
+	for i, m in ipairs(args.m) do
+		args.m[i] = parse_term_with_modifiers("m" .. (i == 1 and "" or i), m)
+	end
+
+	return m_demonym.show_demonym_noun {
+		lang = lang,
+		parts = terms
+		gloss = glosses,
+		m = args.m,
+		g = args.g,
+		sort = args.sort,
+		nocat = args.nocat,
+		nocap = args.nocap,
+		notext = args.notext,
+	}
+end
+
+
+return export
---
+++ demonym.lua
@@ -0,0 +1,294 @@
+local export = {}
+
+local m_links = require("Module:links")
+local m_languages = require("Module:languages")
+local lang_en = m_languages.getByCode("en")
+
+local rfind = mw.ustring.find
+local rsubn = mw.ustring.gsub
+
+local force_cat = false -- set to true for testing
+
+-- version of rsubn() that discards all but the first return value
+local function rsub(term, foo, bar)
+	local retval = rsubn(term, foo, bar)
+	return retval
+end
+
+
+local function link_with_qualifiers(part, pretext)
+	local partparts = {}
+	if part.q then
+		table.insert(partparts, require("Module:qualifier").format_qualifier(part.q) .. " ")
+	end
+	if pretext then
+		table.insert(partparts, pretext)
+	end
+	table.insert(partparts, m_links.full_link(part, nil, "allow self link"))
+	if part.qq then
+		table.insert(partparts, " " .. require("Module:qualifier").format_qualifier(part.qq))
+	end
+	return table.concat(partparts)
+end
+
+
+local function format_glosses(data)
+	local has_gloss = data.gloss and #data.gloss > 0
+
+	if data.lang:getCode() == "en" then
+		if has_gloss then
+			local glosstext
+			if #data.gloss == 1 then
+				glosstext = ("an English gloss '%s'"):format(data.gloss[1].term)
+			else
+				for i, term in ipairs(data.gloss) do
+					data.gloss[i] = "'" .. term.term .. "'"
+				end
+				glosstext = ("English glosses %s"):format(table.concat(data.gloss, ", "))
+			end
+			error(("Can't specify %s for the term when the language is already English"):format(glosstext))
+		end
+		if not has_femeq and not data.nocap then
+			ins("A ")
+		else
+			ins("a ")
+		end
+	elseif has_gloss then
+		if data.notext then
+			error("Can't specify gloss along with notext=")
+		end
+		for i, gloss in ipairs(data.gloss) do
+			gloss.lang = gloss.lang or lang_en
+			data.gloss[i] = link_with_qualifiers(gloss,
+				data.g == "f" and not gloss.gloss_is_gendered and "[[female]] " or nil)
+		end
+		ins(table.concat(data.t, ", "))
+		ins(" (")
+	end
+
+end
+
+
+-- WARNING: This overwrites objects in `data`.
+function export.format_demonym_adj(data)
+	result = {}
+
+	local function ins(text)
+		table.insert(result, text)
+	end
+
+	local langcode = data.lang:getCode()
+	local lang_is_en = langcode == "en"
+
+	local has_gloss = data.gloss and #data.gloss > 0
+
+	if lang_is_en then
+		if has_gloss then
+			local glosstext
+			if #data.t == 1 then
+				glosstext = ("an English gloss '%s'"):format(data.t[1].term)
+			else
+				for i, term in ipairs(data.t) do
+					data.t[i] = "'" .. term.term .. "'"
+				end
+				glosstext = ("English glosses %s"):format(table.concat(data.t, ", "))
+			end
+			error(("Can't specify %s for the term when the language is already English"):format(glosstext))
+		end
+		if not has_femeq and not data.nocap then
+			ins("A ")
+		else
+			ins("a ")
+		end
+	elseif has_gloss then
+		if data.notext then
+			error("Can't specify gloss along with notext=")
+		end
+		for i, gloss in ipairs(data.gloss) do
+			gloss.lang = gloss.lang or lang_en
+			data.gloss[i] = link_with_qualifiers(gloss,
+				data.g == "f" and not gloss.gloss_is_gendered and "[[female]] " or nil)
+		end
+		ins(table.concat(data.t, ", "))
+		ins(" (")
+	end
+
+	if not data.notext then
+		if data.g == "f" then
+			ins("[[female]] ")
+		end
+
+		ins("[[native]] or [[inhabitant]] of ")
+	end
+
+	for i, part in ipairs(data.parts) do
+		part.lang = part.lang or lang_en
+		if part.gloss then
+			-- move gloss to 'pos' so it doesn't have quotes around it
+			if part.pos then
+				part.pos = part.gloss .. ", " .. part.pos
+			else
+				part.pos = part.gloss
+			end
+			part.gloss = nil
+		end
+		parts[i] = link_with_qualifiers(part)
+	end
+
+	if #parts == 1 then
+		ins(parts[1])
+	else
+		ins(require("Module:table").serialCommaJoin(parts, {conj = "or"}))
+	end
+
+	if has_gloss then
+		ins(")")
+	end
+
+	if data.g == "m" and not data.notext then
+		ins(" ")
+		ins(require("Module:qualifier").format_qualifier("male or of unspecified gender"))
+	end
+
+	if lang_is_en and not data.nodot and not data.notext then
+		ins(".")
+	end
+
+	if not data.nocat then
+		local cats = {}
+		table.insert(cats, langcode .. ":Demonyms")
+		if data.g == "m" then
+			table.insert(cats, langcode .. ":Male people")
+		elseif data.g == "f" then
+			table.insert(cats, langcode .. ":Female people")
+		end
+		if has_femeq then
+			table.insert(cats, data.lang:getCanonicalName() .. " female equivalent nouns")
+		end
+		ins(require("Module:utilities").format_categories(cats, data.lang, data.sort, nil, force_cat))
+	end
+
+	return table.concat(result)
+end
+
+-- WARNING: This overwrites objects in `data`.
+function export.format_demonym_noun(data)
+	result = {}
+
+	local function ins(text)
+		table.insert(result, text)
+	end
+
+	local langcode = data.lang:getCode()
+	local lang_is_en = langcode == "en"
+
+	local has_femeq = data.m and #data.m > 0
+	data.g = data.g or has_femeq and "f" or nil
+	local has_gloss = data.gloss and #data.gloss > 0
+
+	if has_femeq then
+		if data.notext then
+			error("Can't specify m= along with notext=")
+		end
+		for i, m in ipairs(data.m) do
+			data.m[i].lang = data.m[i].lang or data.lang
+		end
+		local femeq_data = {
+			text = (lang_is_en and not data.nocap and "Female" or "female") .. " equivalent of",
+			terminfos = data.m,
+			terminfo_face = "term",
+		}
+		ins(require("Module:form of").format_form_of(femeq_data))
+		ins("; ")
+	end
+
+	if lang_is_en then
+		if has_gloss then
+			local glosstext
+			if #data.t == 1 then
+				glosstext = ("an English gloss '%s'"):format(data.t[1].term)
+			else
+				for i, term in ipairs(data.t) do
+					data.t[i] = "'" .. term.term .. "'"
+				end
+				glosstext = ("English glosses %s"):format(table.concat(data.t, ", "))
+			end
+			error(("Can't specify %s for the term when the language is already English"):format(glosstext))
+		end
+		if not has_femeq and not data.nocap then
+			ins("A ")
+		else
+			ins("a ")
+		end
+	elseif has_gloss then
+		if data.notext then
+			error("Can't specify gloss along with notext=")
+		end
+		for i, gloss in ipairs(data.gloss) do
+			gloss.lang = gloss.lang or lang_en
+			data.gloss[i] = link_with_qualifiers(gloss,
+				data.g == "f" and not gloss.gloss_is_gendered and "[[female]] " or nil)
+		end
+		ins(table.concat(data.t, ", "))
+		ins(" (")
+	end
+
+	if not data.notext then
+		if data.g == "f" then
+			ins("[[female]] ")
+		end
+
+		ins("[[native]] or [[inhabitant]] of ")
+	end
+
+	for i, part in ipairs(data.parts) do
+		part.lang = part.lang or lang_en
+		if part.gloss then
+			-- move gloss to 'pos' so it doesn't have quotes around it
+			if part.pos then
+				part.pos = part.gloss .. ", " .. part.pos
+			else
+				part.pos = part.gloss
+			end
+			part.gloss = nil
+		end
+		parts[i] = link_with_qualifiers(part)
+	end
+
+	if #parts == 1 then
+		ins(parts[1])
+	else
+		ins(require("Module:table").serialCommaJoin(parts, {conj = "or"}))
+	end
+
+	if has_gloss then
+		ins(")")
+	end
+
+	if data.g == "m" and not data.notext then
+		ins(" ")
+		ins(require("Module:qualifier").format_qualifier("male or of unspecified gender"))
+	end
+
+	if lang_is_en and not data.nodot and not data.notext then
+		ins(".")
+	end
+
+	if not data.nocat then
+		local cats = {}
+		table.insert(cats, langcode .. ":Demonyms")
+		if data.g == "m" then
+			table.insert(cats, langcode .. ":Male people")
+		elseif data.g == "f" then
+			table.insert(cats, langcode .. ":Female people")
+		end
+		if has_femeq then
+			table.insert(cats, data.lang:getCanonicalName() .. " female equivalent nouns")
+		end
+		ins(require("Module:utilities").format_categories(cats, data.lang, data.sort, nil, force_cat))
+	end
+
+	return table.concat(result)
+end
+
+return export
