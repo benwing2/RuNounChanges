b78bb22a13cf9e4eff9d89473df5188c1bb6e2be
Ben Wing <ben@benwing.com>
2023-01-06 02:00:44 -0600

    parse-utilities: more work (NOT TESTED OR DONE)

--- parse-utilities.lua
+++ parse-utilities.lua
@@ -251,8 +251,36 @@ function export.split_alternating_runs_and_frob_raw_text(run, splitchar, frob, p
 end
 
 
+-- Split text on comma, but not on comma+whitespace. This is similar to `mw.text.split(text, ",")` but will not split
+-- on commas directly followed by whitespace, to handle embedded commas in terms (which are almost always followed by
+-- a space). `tempcomma` is the Unicode character to temporarily use when doing the splitting; normally U+FFF0, but
+-- you can specify a different character if you use U+FFF0 for some internal purpose.
+function export.split_on_comma(text, tempcomma)
+	tempcomma = tempcomma or u(0xFFF0)
+
+	-- First replace comma with a temporary character in comma+whitespace sequences.
+	local need_tempcomma_undo = false
+	if val:find(",%s") then
+		val = val:gsub(",(%s)", tempcomma .. "%1")
+		need_tempcomma_undo = true
+	end
+
+	-- Now split on comma.
+	val = rsplit(val, ",")
+
+	-- Now undo the replacement of comma with a temporary char if needed.
+	if need_tempcomma_undo then
+		for i, v in ipairs(val) do
+			val[i] = v:gsub(tempcomma, ",")
+		end
+	end
+
+	return val
+end
+
+
 -- Split the non-modifier parts of an alternating run (after parse_balanced_segment_run() is called) on comma, but not
--- on comma+whitespace.
+-- on comma+whitespace. See `split_on_comma()` above for more information and the meaning of `tempcomma`.
 function export.split_alternating_runs_on_comma(run, tempcomma)
 	tempcomma = tempcomma or u(0xFFF0)
 
@@ -280,6 +308,101 @@ function export.split_alternating_runs_on_comma(run, tempcomma)
 end
 
 
+-- Ensure that brackets display literally in error messages. Replacing with equivalent HTML escapes doesn't work
+-- because they are displayed literally; but inserting a Unicode word-joiner symbol works.
+function export.escape_brackets(term)
+	term = term:gsub("%[%[", "[" .. u(0x2060) .. "[")
+	return term
+end
+
+
+-- Parse a term that may have a language code preceding it (e.g. 'la:minūtia' or 'grc:[[σκῶρ|σκατός]]'). Return
+-- two arguments, the term minus the language code and the language object corresponding to the language code.
+-- Etymology-only languages are allowed. This function also correctly handles Wikipedia prefixes (e.g. 'w:Abatemarco'
+-- or 'w:it:Colle Val d'Elsa') and converts them into two-part links, with the display form not including the Wikipedia
+-- prefix. `parse_err` should be a function of one or two arguments to display an error (the second argument is the
+-- number of stack frames to ignore when calling error(); if you declare your error function with only one argument,
+-- things will still work fine).
+function export.parse_term_with_lang(term, parse_err)
+	-- Parse off an initial language code (e.g. 'la:minūtia' or 'grc:[[σκῶρ|σκατός]]'). Also handle Wikipedia prefixes
+	-- ('w:Abatemarco' or 'w:it:Colle Val d'Elsa').
+	local termlang, actual_term = term:match("^([A-Za-z0-9._-]+):(.*)$")
+	if termlang == "w" then
+		local foreign_wikipedia, foreign_term = actual_term:match("^([A-Za-z0-9._-]+):(.*)$")
+		if foreign_wikipedia then
+			termlang = termlang .. ":" .. foreign_wikipedia
+			actual_term = foreign_term
+		end
+		if actual_term:find("[%[%]]") then
+			parse_err("Cannot have brackets following a Wikipedia (w:...) link; place the Wikipedia link inside the brackets")
+		end
+		term = ("[[%s:%s|%s]]"):format(termlang, actual_term, actual_term)
+		termlang = nil
+	elseif termlang then
+		termlang = require("Module:languages").getByCode(termlang, parse_err, "allow etym")
+		term = actual_term
+	end
+
+	return term, termlang
+end
+
+
+--[=[
+Parse a term that may have inline modifiers attached (e.g. 'rifiuti<q:plural-only>' or
+'rinfusa<t:bulk cargo><lit:resupplying><qq:more common in the plural {{m|it|rinfuse}}>').
+  * `arg` is the term to parse.
+  * `param_mods` is a table describing the allowed inline modifiers (see below).
+  * `generate_obj` is a function of one or two arguments that should parse the argument minus the inline modifiers
+     and return a corresponding parsed  object (into which the inline modifiers will be rewritten). If declared with
+     one argument, that will be the raw value to parse; if declared with two arguments, the second argument will be
+     the `parse_err` function (see below).
+  * `parse_err` is an optional function of one argument (an error message) and should display the error message,
+    along with any desired contextual text (e.g. the argument name and value that triggered the error). If omitted,
+    a default function will be generated which displays the error along with the original value of `arg` (passed
+    through escape_brackets() above to ensure that bracketed text is displayed literally).
+  * `split_on_comma` is an optional Boolean argument. If true, `arg` can consist of multiple comma-separated terms,
+    each of which may be followed by inline modifiers, and the return value will be a list of parsed objects instead
+    of a single object. Note that splitting on commas will not happen if there is whitespace directly following the
+    comma, to allow for terms with embedded commas in them. The algorithm to split on commas is sensitive to inline
+    modifier syntax and will not be confused by commas inside of inline modifiers, which do not trigger splitting
+    (whether or not followed by whitespace).
+
+`param_mods` is a table describing allowed modifiers. The keys of the table are modifier prefixes and the values are
+tables describing how to parse and store the associated modifier values. Here is a typical example:
+
+local param_mods = {
+	t = {
+		item_dest = "gloss",
+	},
+	gloss = {},
+	pos = {},
+	alt = {},
+	lit = {},
+	id = {},
+	g = {
+		item_dest = "genders",
+		convert = function(arg)
+			return rsplit(arg, ",")
+		end,
+	},
+}
+
+In the table values:
+* `item_dest` specifies the destination key to store the object into (if not the same as the modifier key itself).
+* `convert` is a function of one or two arguments (the modifier value and optionally the `parse_err` function as passed
+  in or generated), and should parse and convert the value into the appropriate object. If omitted, the string value is
+  stored unchanged.
+* `store` describes how to store the converted modifier value into the parsed object. If omitted, the converted value
+  is simply written into the parsed object under the appropriate key (but an error is generated if the key already has
+  a value). If `store` == "insert", the converted value is appended to the key's value using table.insert();
+  if the key has no value, it is first converted to an empty list. `store` == "insertIfNot" is similar but appends the
+  value using insertIfNot() in [[Module:table]]. If `store == "insert-flattened", the converted value is assumed to be
+  a list and the objects are appended one-by-one into the key's existing value using table.insert(). `store` ==
+  "insertIfNot-flattened" is similar but appends using insertIfNot() in [[Module:table]]. WARNING: When using
+  "insert-flattened" and "insertIfNot-flattened", if there is no existing value for the key, the converted value is
+  just stored directly. This means that future appends will side-effect that value, so make sure that the return
+  value of the conversion function for this key generates a fresh list each time.
+]=]
 function export.parse_inline_modifiers(arg, param_mods, generate_obj, parse_err, split_on_comma)
 	local function get_valid_prefixes()
 		local valid_prefixes = {}
@@ -292,14 +415,14 @@ function export.parse_inline_modifiers(arg, param_mods, generate_obj, parse_err,
 
 	if not parse_err then
 		parse_err = function(msg)
-			error(msg .. ": " .. arg)
+			error(msg .. ": " .. export.escape_brackets(arg))
 		end
 	end
 
 	local segments = export.parse_balanced_segment_run(arg, "<", ">")
 
 	local function parse_group(group)
-		local dest_obj = generate_obj(group[1])
+		local dest_obj = generate_obj(group[1], parse_err)
 		for k = 2, #group - 1, 2 do
 			if group[k + 1] ~= "" then
 				parse_err("Extraneous text '" .. group[k + 1] .. "' after modifier")
@@ -334,15 +457,31 @@ function export.parse_inline_modifiers(arg, param_mods, generate_obj, parse_err,
 					dest_obj[key] = converted
 				elseif store == "insert" then
 					if not dest_obj[key] then
-						dest_obj[key] = {}
+						dest_obj[key] = {converted}
+					else
+						table.insert(dest_obj[key], converted)
 					end
-					table.insert(dest_obj[key], converted)
+				elseif store == "insertIfNot" then
+					if not dest_obj[key] then
+						dest_obj[key] = {converted}
+					else
+						require("Module:table").insertIfNot(dest_obj[key], converted)
+					end
 				elseif store == "insert-flattened" then
 					if not dest_obj[key] then
-						dest_obj[key] = {}
+						dest_obj[key] = obj
+					else
+						for _, obj in ipairs(converted) do
+							table.insert(dest_obj[key], obj)
+						end
 					end
-					for _, obj in ipairs(converted) do
-						table.insert(dest_obj[key], obj)
+				elseif store == "insertIfNot-flattened" then
+					if not dest_obj[key] then
+						dest_obj[key] = obj
+					else
+						for _, obj in ipairs(converted) do
+							require("Module:table").insertIfNot(dest_obj[key], obj)
+						end
 					end
 				else
 					store(dest_obj, key, converted, parse_err)
