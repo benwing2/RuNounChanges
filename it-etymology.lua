60975437adabd760f934159699557911e90938dd
Ben Wing <ben@benwing.com>
2023-01-07 02:52:35 -0600

    it-etymology: latest from Wiktionary

--- it-etymology.lua
+++ it-etymology.lua
@@ -1,9 +1,9 @@
 local export = {}
 
 local m_links = require("Module:links")
-local put = require("Module:parse utilities")
 local m_languages = require("Module:languages")
-local com_module = "Module:it-common"
+local put_module = "Module:User:Benwing2/parse utilities"
+local com_module = "Module:User:Benwing2/it-common"
 local compound_module = "Module:compound"
 local lang = m_languages.getByCode("it")
 
@@ -11,6 +11,8 @@ local rfind = mw.ustring.find
 local rsubn = mw.ustring.gsub
 local rsplit = mw.text.split
 local u = mw.ustring.char
+-- Assigned to `require("Module:parse utilities")` as necessary.
+local put
 
 local force_cat = false -- set to true for testing
 
@@ -30,7 +32,7 @@ local function link_with_qualifiers(part, pretext)
 	if pretext then
 		table.insert(partparts, pretext)
 	end
-	table.insert(partparts, m_links.full_link(part, nil, "allow self link"))
+	table.insert(partparts, m_links.full_link(part, "term", "allow self link"))
 	if part.qq then
 		table.insert(partparts, " " .. require("Module:qualifier").format_qualifier(part.qq))
 	end
@@ -38,15 +40,7 @@ local function link_with_qualifiers(part, pretext)
 end
 
 
--- Return a param_mods structure as required by parse_inline_modifiers() in [[Module:parse utilities]]:
--- * `convert`: An optional function to convert the raw argument into the form needed for further processing.
---              This function takes two parameters: (1) `arg` (the raw argument); (2) `parse_err` (a function to
---              generate an error).
--- * `item_dest`: The name of the key used when storing the parameter's value into the processed object.
---                Normally the same as the parameter's name. Different in the case of "t", where we store the gloss in
---                "gloss", and "g", where we store the genders in "genders".
-local param_mods = 
-{
+local param_mods = {
 	t = {
 		-- We need to store the <t:...> inline modifier into the "gloss" key of the parsed part, because that is what
 		-- [[Module:links]] expects.
@@ -96,37 +90,27 @@ local function parse_args(args, hack_params)
 		["notext"] = {type = "boolean"},
 	}
 
-	args = require("Module:parameters").process(args, params)
-	local lang = m_languages.getByCode(args[1], 1)
-	return args, lang
+	if hack_params then
+		hack_params(params)
+	end
+
+	return require("Module:parameters").process(args, params)
 end
 
 
 local function parse_term_with_modifiers(paramname, val)
-	local function generate_obj(term)
+	local function generate_obj(term, parse_err)
 		local obj = {}
-		local term, lang = put.parse_term_with_modifiers(paramname, ...)
-		-- Parse off an initial language code (e.g. 'la:minūtia' or 'grc:[[σκῶρ|σκατός]]'). Also handle Wikipedia prefixes
-		-- ('w:Abatemarco' or 'w:it:Colle Val d'Elsa').
-		local termlang, actual_term = term:match("^([A-Za-z0-9._-]+):(.*)$")
-		if termlang == "w" then
-			local foreign_wikipedia, foreign_term = actual_term:match("^([A-Za-z0-9._-]+):(.*)$")
-			if foreign_wikipedia then
-				termlang = termlang .. ":" .. foreign_wikipedia
-				actual_term = foreign_term
+		if term:find(":") then
+			if not put then
+				put = require(put_module)
 			end
-			if actual_term:find("[%[%]]") then
-				parse_err("Cannot have brackets following a Wikipedia (w:...) link; place the Wikipedia link inside the brackets")
-			end
-			term = ("[[%s:%s|%s]]"):format(termlang, actual_term, actual_term)
-			termlang = nil
-		elseif termlang then
-			termlang = m_languages.getByCode(termlang, paramname, "allow etym")
-			term = actual_term
+			local actual_term, termlang = put.parse_term_with_lang(term, parse_err)
+			obj.term = actual_term
+			obj.lang = termlang
+		else
+			obj.term = term
 		end
-
-		obj.lang = termlang
-		obj.term = term
 		return obj
 	end
 
@@ -135,12 +119,11 @@ local function parse_term_with_modifiers(paramname, val)
 	-- the sort we parse here should consist of a less-than sign, plus letters, plus a colon, e.g. <tr:...>, so if
 	-- we see a tag on the outer level that isn't in this format, we don't try to parse it. The restriction to the
 	-- outer level is to allow generated HTML inside of e.g. qualifier tags, such as foo<q:similar to {{m|fr|bar}}>.
-	if val:find("<") and not val:find("^[^<]*<[a-z]*[^a-z:]") then
+	if val:find("<") then
 		if not put then
-			put = require("Module:parse utilities")
+			put = require(put_module)
 		end
-		local param_mods = get_param_mods(paramname)
-		return put.parse_inline_modifiers(val, param_mods, generate_obj, parse_err)
+		return put.parse_inline_modifiers(val, paramname, param_mods, generate_obj, parse_err)
 	else
 		return generate_obj(val)
 	end
@@ -162,26 +145,13 @@ local function form_imperative(inf, parse_err)
 end
 
 
-function export.it_verb_obj(frame)
-	local args = parse_args(frame:getParent().args)
-
-	if #args[1] < 2 then
-		error("Need at least two numbered arguments to [[Template:it-verb-obj]]")
-	end
-	for i, term in ipairs(args[1]) do
-		local parsed = parse_term_with_modifiers(i + 1, term)
+local function parse_and_format_parts(parts, get_part_type)
+	for i, term in ipairs(parts) do
+		local parsed = parse_term_with_modifiers(i, term)
 		if not parsed.term:find("%[") then
 			local parttype = parsed.type
 			if not parttype then
-				if i == 1 then
-					parttype = "verb"
-				elseif i == #args[1] then
-					parttype = "object"
-				elseif term:find("[aei]re$") then
-					parttype = "verb"
-				else
-					parttype = "connector"
-				end
+				parttype = get_part_type(i, term)
 			end
 			if parttype ~= "object" and parsed.pl then
 				parse_err(("Can't specify <pl:...> with an argument that is not an object (argument type is '%s')"):
@@ -201,7 +171,10 @@ function export.it_verb_obj(frame)
 						parse_err(("Can't form default plural of term '%s' given with explicit language code prefix '%s'"):
 							format(parsed.term, parsed.lang:getCode()))
 					end
-					pl = require(com_module).make_plural(parsed.term, "?")
+					-- "Guess" a gender based on the ending. We can't pass in a "?" because then we'll get an error on terms ending in
+					-- -a because they have different plurals depending on the gender.
+					local gender = parsed.term:find("a$") and "f" or "m"
+					pl = require(com_module).make_plural(parsed.term, gender)
 					if not pl then
 						parse_err(("Default plural algorithm was unable to form the plural of term '%s'"):format(parsed.term))
 					end
@@ -215,11 +188,34 @@ function export.it_verb_obj(frame)
 		end
 
 		parsed.lang = parsed.lang or lang
-		args[1][i] = link_with_qualifiers(parsed)
+		parts[i] = link_with_qualifiers(parsed)
 	end
 
-	result = {}
+end
 
+
+function export.it_verb_obj(frame)
+	local args = parse_args(frame:getParent().args)
+
+	if #args[1] < 2 then
+		error("Need at least two numbered arguments to [[Template:it-verb-obj]]")
+	end
+
+	parse_and_format_parts(args[1], function(i, term)
+		local parttype
+		if i == 1 then
+			parttype = "verb"
+		elseif i == #args[1] then
+			parttype = "object"
+		elseif term:find("[aei]re$") then
+			parttype = "verb"
+		else
+			parttype = "connector"
+		end
+		return parttype
+	end)
+
+	result = {}
 	local function ins(text)
 		table.insert(result, text)
 	end
@@ -239,31 +235,34 @@ function export.it_verb_obj(frame)
 end
 
 
-function export.demonym_noun(frame)
-	local function hack_params(params)
-		params.g = {}
-		params.m = {list = true}
+function export.it_verb_verb(frame)
+	local args = parse_args(frame:getParent().args)
+
+	if #args[1] < 2 then
+		error("Need at least two numbered arguments to [[Template:it-verb-verb]]")
 	end
 
-	local args, lang = parse_args(frame:getParent().args)
+	parse_and_format_parts(args[1], function(i, term)
+		return "verb"
+	end)
 
-	local terms, glosses = get_terms_and_glosses(args)
-
-	for i, m in ipairs(args.m) do
-		args.m[i] = parse_term_with_modifiers("m" .. (i == 1 and "" or i), m)
+	result = {}
+	local function ins(text)
+		table.insert(result, text)
 	end
 
-	return m_demonym.show_demonym_noun {
-		lang = lang,
-		parts = terms
-		gloss = glosses,
-		m = args.m,
-		g = args.g,
-		sort = args.sort,
-		nocat = args.nocat,
-		nocap = args.nocap,
-		notext = args.notext,
-	}
+	if not args.notext then
+		if args.nocap then
+			ins("verb-verb")
+		else
+			ins("Verb-verb")
+		end
+		ins(" compound, composed of ")
+	end
+
+	ins(require(compound_module).concat_parts(lang, args[1], {"verb-verb compounds"}, args.nocat, args.sort, args.lit,
+		force_cat))
+	return table.concat(result)
 end
 
 
