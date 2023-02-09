1c23ade6c2c7d416c2597abe3220987302a6c03e
Ben Wing <ben@benwing.com>
2023-02-09 15:51:45 -0600

    it-common: move some pronunciation stuff here from [[Module:it-pronunciation]], for use in [[Module:it-headword]]

--- it-common.lua
+++ it-common.lua
@@ -1,9 +1,14 @@
-local export = {}
+local ex = {} -- normally called `export` but there are so many references to exported functions in this module
 
-local romut_module = "Module:romance utilities"
 local put_module = "Module:parse utilities"
+local romut_module = "Module:romance utilities"
+local strutil_module = "Module:string utilities"
 
+local u = mw.ustring.char
+local rfind = mw.ustring.find
+local rsubn = mw.ustring.gsub
 local rsplit = mw.text.split
+
 
 local prepositions = {
 	-- a, da + optional article
@@ -36,7 +41,143 @@ local prepositions = {
 	"fra ",
 }
 
-function export.parse_abbreviated_references_spec(spec)
+
+-- version of rsubn() that discards all but the first return value
+function ex.rsub(term, foo, bar)
+	local retval = rsubn(term, foo, bar)
+	return retval
+end
+
+-- version of rsubn() that returns a 2nd argument boolean indicating whether
+-- a substitution was made.
+function ex.rsubb(term, foo, bar)
+	local retval, nsubs = rsubn(term, foo, bar)
+	return retval, nsubs > 0
+end
+
+-- apply rsub() repeatedly until no change
+function ex.rsub_repeatedly(term, foo, bar)
+	while true do
+		local new_term = ex.rsub(term, foo, bar)
+		if new_term == term then
+			return term
+		end
+		term = new_term
+	end
+end
+
+
+---------------------- Pronunciation -----------------
+
+ex.AC = u(0x301)
+ex.GR = u(0x300)
+ex.CFLEX = u(0x302)
+ex.DOTOVER = u(0x0307) -- dot over =  ̇ = signal unstressed word
+ex.DOTUNDER = u(0x0323) -- dot under =  ̣ = unstressed vowel with quality marker
+ex.LINEUNDER = u(0x0331) -- line under =  ̱ = secondary-stressed vowel with quality marker
+ex.DIA = u(0x0308) -- diaeresis = ̈
+ex.TIE = u(0x0361) -- tie =  ͡
+ex.stress = "ˈˌ"
+ex.stress_c = "[" .. ex.stress .. "]"
+ex.quality = ex.AC .. ex.GR
+ex.quality_c = "[" .. ex.quality .. "]"
+ex.accent = ex.stress .. ex.quality .. ex.CFLEX .. ex.DOTOVER .. ex.DOTUNDER .. ex.LINEUNDER
+ex.accent_c = "[" .. ex.accent .. "]"
+
+-- Apply canonical Unicode decomposition to text, e.g. è → e + ◌̀. But recompose ö and ü so we can treat them as single
+-- vowels, and put ex.LINEUNDER/ex.DOTUNDER/ex.DOTOVER after acute/grave (canonical decomposition puts ex.LINEUNDER and ex.DOTUNDER
+-- first).
+function ex.decompose(text)
+	text = mw.ustring.toNFD(text)
+	text = ex.rsub(text, "." .. ex.DIA, {
+		["o" .. ex.DIA] = "ö",
+		["O" .. ex.DIA] = "Ö",
+		["u" .. ex.DIA] = "ü",
+		["U" .. ex.DIA] = "Ü",
+	})
+	text = ex.rsub(text, "([" .. ex.LINEUNDER .. ex.DOTUNDER .. ex.DOTOVER .. "])(" .. ex.quality_c .. ")", "%2%1")
+	return text
+end
+
+-- Apply canonical Unicode composition to text, e.g. e + ◌̀ → è.
+function ex.compose(text)
+	return mw.ustring.toNFC(text)
+end
+
+-- Split into words. Hyphens separate words but not when used to denote affixes, i.e. hyphens between non-spaces
+-- separate words. Return value includes alternating words and separators. Use table.concat(words) to reconstruct
+-- the initial text.
+function ex.split_but_rejoin_affixes(text)
+	if not rfind(text, "[%s%-]") then
+		return {text}
+	end
+	-- First replace hyphens separating words with a special character. Remaining hyphens denote affixes and don't
+	-- get split. After splitting, replace the special character with a hyphen again.
+	local TEMP_HYPH = u(0xFFF0)
+	text = ex.rsub_repeatedly(text, "([^%s])%-([^%s])", "%1" .. TEMP_HYPH .. "%2")
+	local words = require(strutil_module).capturing_split(text, "([%s" .. TEMP_HYPH .. "]+)")
+	for i, word in ipairs(words) do
+		if word == TEMP_HYPH then
+			words[i] = "-"
+		end
+	end
+	return words
+end
+
+function ex.remove_secondary_stress(text)
+	local words = ex.split_but_rejoin_affixes(text)
+	for i, word in ipairs(words) do
+		if (i % 2) == 1 then -- an actual word, not a separator
+			-- Remove unstressed quality marks.
+			word = ex.rsub(word, ex.quality_c .. ex.DOTUNDER, "")
+			-- Remove secondary stresses. Specifically:
+			-- (1) Remove secondary stresses marked with ex.LINEUNDER if there's a previously stressed vowel.
+			-- (2) Otherwise, just remove the ex.LINEUNDER, leaving the accent mark, which will then be removed if there's
+			--     a following stressed vowel, but left if it's the only stress in the word, as in có̱lle = con le.
+			--     (In the process, we remove other non-stress marks.)
+			-- (3) Remove stress mark if there's a following stressed vowel.
+			word = ex.rsub_repeatedly(word, "(" .. ex.quality_c .. ".*)" .. ex.quality_c .. ex.LINEUNDER, "%1")
+			word = ex.rsub(word, "[" .. ex.CFLEX .. ex.DOTOVER .. ex.DOTUNDER .. ex.LINEUNDER .. "]", "")
+			word = ex.rsub_repeatedly(word, ex.quality_c .. "(.*" .. ex.quality_c .. ")", "%1")
+			words[i] = word
+		end
+	end
+	return table.concat(words)
+end
+
+-- Remove all accents. NOTE: `text` on entry must be decomposed using decompose().
+function ex.remove_accents(text)
+	return ex.rsub(text, ex.accent_c, "")
+end
+
+-- Remove non-word-final accents. NOTE: `text` on entry must be decomposed using decompose().
+function ex.remove_non_final_accents(text)
+	local words = ex.split_but_rejoin_affixes(text)
+	for i, word in ipairs(words) do
+		if (i % 2) == 1 then -- an actual word, not a separator
+			word = ex.rsub_repeatedly(word, ex.accent_c .. "(.)", "%1")
+			words[i] = word
+		end
+	end
+	return table.concat(words)
+end
+
+-- Remove word-final accents on monosyllabic words. NOTE: `text` on entry must be decomposed using decompose().
+function ex.remove_final_monosyllabic_accents(text)
+	local words = ex.split_but_rejoin_affixes(text)
+	for i, word in ipairs(words) do
+		if (i % 2) == 1 then -- an actual word, not a separator
+			word = ex.rsub(word, "^(" .. NV .. "*" .. V .. ")" .. ex.accent_c .. "$", "%1")
+			words[i] = word
+		end
+	end
+	return table.concat(words)
+end
+
+
+---------------------- References -----------------
+
+function ex.parse_abbreviated_references_spec(spec)
 	local spec_before_modifiers, modifiers = spec:match("^(.-)(<<.*>>)$")
 	if spec_before_modifiers then
 		spec = spec_before_modifiers
@@ -70,6 +211,9 @@ function export.parse_abbreviated_references_spec(spec)
 	end
 end
 
+
+---------------------- Inflection -----------------
+
 -- Given a term `term`, if the term is multiword (either through spaces or hyphens), handle inflection of the term by
 -- calling handle_multiword() in [[Module:romance utilities]]. `special` indicates which parts of the multiword term to
 -- inflect, and `inflect` is a function of one argument to inflect the individual parts of the term. As an optimization,
@@ -89,13 +233,13 @@ local function call_handle_multiword(term, special, inflect)
 end
 
 -- Generate a default plural form, which is correct for most regular nouns and adjectives.
-function export.make_plural(term, gender, special)
+function ex.make_plural(term, gender, special)
 	local plspec
 	if special == "cap*" or special == "cap*+" then
 		plspec = special
 		special = nil
 	end
-	local retval = call_handle_multiword(term, special, function(term) return export.make_plural(term, gender, plspec) end)
+	local retval = call_handle_multiword(term, special, function(term) return ex.make_plural(term, gender, plspec) end)
 	if retval then
 		return retval
 	end
@@ -171,8 +315,8 @@ function export.make_plural(term, gender, special)
 end
 
 -- Generate a default feminine form.
-function export.make_feminine(term, special)
-	local retval = call_handle_multiword(term, special, export.make_feminine)
+function ex.make_feminine(term, special)
+	local retval = call_handle_multiword(term, special, ex.make_feminine)
 	if retval then
 		return retval
 	end
@@ -190,8 +334,8 @@ function export.make_feminine(term, special)
 end
 
 -- Generate a default masculine form.
-function export.make_masculine(term, special)
-	local retval = call_handle_multiword(term, special, export.make_masculine)
+function ex.make_masculine(term, special)
+	local retval = call_handle_multiword(term, special, ex.make_masculine)
 
 	-- Don't directly return gsub() because then there will be multiple return values.
 	if term:find("a$") then
@@ -203,4 +347,4 @@ function export.make_masculine(term, special)
 	return term
 end
 
-return export
+return ex
