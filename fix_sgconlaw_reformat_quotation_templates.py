ce1a6fab089e9d72a29dd6d0a74d8b7104ca3486
Ben Wing <ben@benwing.com>
2023-02-19 19:19:13 -0600

    fix_sgconlaw_reformat_quotation_templates: allow from-template parameters

--- fix_sgconlaw_reformat_quotation_templates.py
+++ fix_sgconlaw_reformat_quotation_templates.py
@@ -38,13 +38,19 @@ def process_text_on_page(index, pagename, text):
 
   curtext = text + "\n"
 
-  for fromtemp, (totemp, params) in templates_to_rename:
+  for (fromtemp, from_params), (totemp, to_params) in templates_to_rename:
     def reformat_template(m):
       template, text = m.groups()
       parsed = blib.parse_text(template)
       t = list(parsed.filter_templates())[0]
       if tname(t) != fromtemp:
         return m.group(0)
+      # If from-template params given, make sure they all match.
+      for param, value in from_params:
+        if getparam(t, param).strip() != value.strip():
+          pagemsg("Skipping template because expected param %s=%s doesn't match: %s" % (
+            param, value, unicode(t)))
+          return m.group(0)
       for existing_param in ["passage", "text"]:
         if getparam(t, existing_param):
           pagemsg("WARNING: Can't incorporate raw passage text into {{%s}} because already has %s=: %s" %
@@ -54,10 +60,13 @@ def process_text_on_page(index, pagename, text):
       text = re.sub(r"^''(.*)''$", r"\1", text)
       t.add("passage", text)
       blib.set_template_name(t, totemp)
-      if params:
-        add_params_to_template(t, params)
-      notes.append("reformat {{%s}} into {{%s%s}}, incorporating following raw passage text into passage=" %
-          (fromtemp, totemp, "".join("|%s=%s" % (param, value) for param, value in params)))
+      for param, value in from_params:
+        rmparam(t, param)
+      if to_params:
+        add_params_to_template(t, to_params)
+      notes.append("reformat {{%s%s}} into {{%s%s}}, incorporating following raw passage text into passage=" %
+          (fromtemp, "".join("|%s=%s" % (param, value) for param, value in from_params),
+            totemp, "".join("|%s=%s" % (param, value) for param, value in to_params)))
       return unicode(t) + "\n"
 
     curtext = re.sub(r"(\{\{%s.*?\}\})\n#+\*:\s*(.*?)\n" % re.escape(fromtemp),
@@ -66,13 +75,27 @@ def process_text_on_page(index, pagename, text):
   parsed = blib.parse_text(curtext)
   for t in parsed.filter_templates():
     tn = tname(t)
-    if tn in templates_to_rename_dict:
-      totemp, params = templates_to_rename_dict[tn]
+    for (fromtemp, from_params), (totemp, to_params) in templates_to_rename:
+      if tn != fromtemp:
+        continue
+      # If from-template params given, make sure they all match.
+      must_continue = False
+      for param, value in from_params:
+        if getparam(t, param).strip() != value.strip():
+          pagemsg("Skipping template because expected param %s=%s doesn't match: %s" % (
+            param, value, unicode(t)))
+          must_continue = True
+          break
+      if must_continue:
+        continue
       blib.set_template_name(t, totemp)
-      if params:
-        add_params_to_template(t, params)
-      notes.append("rename {{%s}} to {{%s%s}}" % (tn, totemp,
-        "".join("|%s=%s" % (param, value) for param, value in params)))
+      for param, value in from_params:
+        rmparam(t, param)
+      if to_params:
+        add_params_to_template(t, to_params)
+      notes.append("rename {{%s%s}} to {{%s%s}}" %
+          (fromtemp, "".join("|%s=%s" % (param, value) for param, value in from_params),
+            totemp, "".join("|%s=%s" % (param, value) for param, value in to_params)))
   curtext = unicode(parsed)
 
   return curtext.rstrip("\n"), notes
@@ -90,19 +113,29 @@ for lineno, line in blib.iter_items_from_file(args.direcfile):
     msg("Line %s: WARNING: Saw bad line in --from-to-pagefile: %s" % (lineno, line))
     continue
   fromtemp, totemp = line.split(" ||| ")
-  if "|" in totemp:
-    totemp, combined_params = totemp.split("|", 1)
+  if "|" in fromtemp:
+    fromtemp, combined_params = fromtemp.split("|", 1)
     combined_params = combined_params.split("|")
-    params = []
+    from_params = []
     for combined_param in combined_params:
       if "=" not in combined_param:
         raise ValueError("Param %s doesn't have an = sign" % combined_param)
       param, value = combined_param.split("=")
-      params.append((param, value))
+      from_params.append((param, value))
   else:
-    params = []
-  templates_to_rename.append((fromtemp, (totemp, params)))
-templates_to_rename_dict = dict(templates_to_rename)
+    from_params = []
+  if "|" in totemp:
+    totemp, combined_params = totemp.split("|", 1)
+    combined_params = combined_params.split("|")
+    to_params = []
+    for combined_param in combined_params:
+      if "=" not in combined_param:
+        raise ValueError("Param %s doesn't have an = sign" % combined_param)
+      param, value = combined_param.split("=")
+      to_params.append((param, value))
+  else:
+    to_params = []
+  templates_to_rename.append(((fromtemp, from_params), (totemp, to_params)))
 blib.do_pagefile_cats_refs(args, start, end, process_text_on_page,
-    default_refs=["Template:%s" % fromtemp for fromtemp, totemp in templates_to_rename],
-    edit=True, stdin=True)
+    default_refs=["Template:%s" % fromtemp for (fromtemp, from_params), (totemp, to_params) in templates_to_rename],
+    edit=True, stdin=True, skip_ignorable_pages=True)
