6d52e668841727e43be64fe276a2ebc2875a17af
Ben Wing <ben@benwing.com>
2023-02-21 21:34:37 -0600

    push_manual_changes: support running in --stdin mode

--- push_manual_changes.py
+++ push_manual_changes.py
@@ -5,6 +5,7 @@ import re, codecs
 
 import blib, pywikibot
 from blib import msg, getparam, addparam
+from collections import defaultdict
 
 site = pywikibot.Site()
 
@@ -15,7 +16,7 @@ def truncate(text):
     return text
   return text[0:max_truncate_len] + "..."
 
-def push_manual_changes(save, verbose, diff, direcfile, comment, start, end):
+def read_direcfile(direcfile, start, end):
   template_changes = []
   for lineno, line in blib.iter_items_from_file(direcfile, start, end):
     def linemsg(txt):
@@ -59,70 +60,101 @@ def push_manual_changes(save, verbose, diff, direcfile, comment, start, end):
           template_changes.append((pagename, repl, curr))
         else:
           linemsg("WARNING: Ignoring line with from=to: %s" % line)
+  return template_changes
 
-  for index, (pagename, repl_template, curr_template) in blib.iter_items(template_changes, get_name = lambda x: x[0]):
-    def push_one_manual_change(page, index, text):
-      def pagemsg(txt):
-        msg("Page %s %s: %s" % (index, unicode(page.title()), txt))
-      #template = blib.parse_text(template_text).filter_templates()[0]
-      #orig_template = unicode(template)
-      #if getparam(template, "sc") == "polytonic":
-      #  template.remove("sc")
-      #to_template = unicode(template)
-      #param_value = getparam(template, removed_param)
-      #template.remove(removed_param)
-      #from_template = unicode(template)
-      text = unicode(text)
-      found_repl_template = repl_template in text
-      newtext = text.replace(curr_template, repl_template)
-      changelog = ""
-      if newtext == text:
-        if not found_repl_template:
-          pagemsg("WARNING: Unable to locate current template: %s"
-              % curr_template)
-        else:
-          pagemsg("Replacement template already found, taking no action")
+def template_changes_to_dict(template_changes):
+  retval = defaultdict(list)
+  for pagename, repl_template, curr_template in template_changes:
+    retval[pagename].append((repl_template, curr_template))
+  return retval
+
+def push_one_manual_change(pagetitle, index, text, curr_template, repl_template):
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+  #template = blib.parse_text(template_text).filter_templates()[0]
+  #orig_template = unicode(template)
+  #if getparam(template, "sc") == "polytonic":
+  #  template.remove("sc")
+  #to_template = unicode(template)
+  #param_value = getparam(template, removed_param)
+  #template.remove(removed_param)
+  #from_template = unicode(template)
+  text = unicode(text)
+  found_repl_template = repl_template in text
+  newtext = text.replace(curr_template, repl_template)
+  changelog = ""
+  if newtext == text:
+    if not found_repl_template:
+      pagemsg("WARNING: Unable to locate current template: %s"
+          % curr_template)
+    else:
+      pagemsg("Replacement template already found, taking no action")
+  else:
+    if found_repl_template:
+      pagemsg("WARNING: Made change, but replacement template %s already present!" %
+          repl_template)
+    repl_curr_diff = len(repl_template) - len(curr_template)
+    newtext_text_diff = len(newtext) - len(text)
+    if newtext_text_diff == repl_curr_diff:
+      pass
+    elif repl_curr_diff == 0:
+      if newtext_text_diff != 0:
+        pagemsg("WARNING: Something wrong, no change in text length during replacement but expected change: Expected length change=%s, actual=%s, curr=%s, repl=%s"
+            % (repl_curr_diff, newtext_text_diff, curr, repl))
+    else:
+      ratio = float(newtext_text_diff) / repl_curr_diff
+      if ratio == int(ratio):
+        pagemsg("WARNING: Replaced %s occurrences of curr=%s with repl=%s"
+            % (int(ratio), curr_template, repl_template))
       else:
-        if found_repl_template:
-          pagemsg("WARNING: Made change, but replacement template %s already present!" %
-              repl_template)
-        repl_curr_diff = len(repl_template) - len(curr_template)
-        newtext_text_diff = len(newtext) - len(text)
-        if newtext_text_diff == repl_curr_diff:
-          pass
-        elif repl_curr_diff == 0:
-          if newtext_text_diff != 0:
-            pagemsg("WARNING: Something wrong, no change in text length during replacement but expected change: Expected length change=%s, actual=%s, curr=%s, repl=%s"
-                % (repl_curr_diff, newtext_text_diff, curr, repl))
-        else:
-          ratio = float(newtext_text_diff) / repl_curr_diff
-          if ratio == int(ratio):
-            pagemsg("WARNING: Replaced %s occurrences of curr=%s with repl=%s"
-                % (int(ratio), curr_template, repl_template))
-          else:
-            pagemsg("WARNING: Something wrong, length mismatch during replacement: Expected length change=%s, actual=%s, ratio=%.2f, curr=%s, repl=%s"
-                % (repl_curr_diff, newtext_text_diff, ratio, curr_template,
-                  repl_template))
-        changelog = "replace <%s> with <%s> (%s)" % (truncate(curr_template),
-            truncate(repl_template), comment)
-        pagemsg("Change log = %s" % changelog)
-      return newtext, changelog
+        pagemsg("WARNING: Something wrong, length mismatch during replacement: Expected length change=%s, actual=%s, ratio=%.2f, curr=%s, repl=%s"
+            % (repl_curr_diff, newtext_text_diff, ratio, curr_template,
+              repl_template))
+    changelog = "replace <%s> with <%s> (%s)" % (truncate(curr_template),
+        truncate(repl_template), comment)
+    pagemsg("Change log = %s" % changelog)
+  return newtext, changelog
 
+def push_manual_changes(save, verbose, diff, template_changes, start, end):
+  for index, (pagename, repl_template, curr_template) in blib.iter_items(template_changes, get_name = lambda x: x[0]):
     page = pywikibot.Page(site, pagename)
     if not page.exists():
       msg("Page %s %s: WARNING, something wrong, does not exist" % (
         index, pagename))
     else:
-      blib.do_edit(page, index, push_one_manual_change, save=save,
+      def do_push_one_manual_change(page, index, text):
+        return push_one_manual_change(unicode(page.title()), index, text, curr_template, repl_template)
+      blib.do_edit(page, index, do_push_one_manual_change, save=save,
           verbose=verbose, diff=diff)
 
-params = blib.create_argparser("Push manual changes to Wiktionary")
-params.add_argument("--file", help="File containing templates to change, as output by parse_log_file.py",
+def process_text_on_page_pushing_manual_changes(index, pagetitle, text):
+  global args
+  def pagemsg(txt):
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  if pagetitle not in direcfile_changes_dict:
+    return
+  notes = []
+  for repl_template, curr_template in direcfile_changes_dict[pagetitle]:
+    text, this_changelog = push_one_manual_change(pagetitle, index, text, curr_template, repl_template)
+    notes.append(this_changelog)
+
+  return text, notes
+
+params = blib.create_argparser("Push manual changes to Wiktionary",
+  include_pagefile=True, include_stdin=True)
+params.add_argument("--direcfile", help="File containing templates to change, as output by parse_log_file.py",
     required=True)
 params.add_argument("--comment", default="manually",
     help="Comment in change log message used to indicate source of changes (default 'manually')")
 
 args = params.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
+comment = args.comment.decode("utf-8")
+direcfile_changes = read_direcfile(args.direcfile, start, end)
 
-push_manual_changes(args.save, args.verbose, args.diff, args.file, args.comment.decode("utf-8"), start, end)
+if args.stdin:
+  direcfile_changes_dict = template_changes_to_dict(direcfile_changes)
+  blib.do_pagefile_cats_refs(args, start, end, process_text_on_page_pushing_manual_changes, edit=True, stdin=True)
+else:
+  push_manual_changes(args.save, args.verbose, args.diff, direcfile_changes, start, end)
