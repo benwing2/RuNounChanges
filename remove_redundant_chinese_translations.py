61296cdf69c8dfc1ac05d9e78604e13e6b17527e
Ben Wing <ben@benwing.com>
2023-03-03 03:01:58 -0600

    remove_redundant_chinese_translations: allow simp/trad to be reversed, look for mismatching simp/trad

--- remove_redundant_chinese_translations.py
+++ remove_redundant_chinese_translations.py
@@ -50,12 +50,12 @@ def process_text_on_page(index, pagetitle, text):
 
   notes = []
 
-  if not re.search(r"^\* *Chinese:*$", text, re.M):
+  if not re.search(r"^\* *Chinese:*", text, re.M):
     return
 
-  subsections = re.split("(^==+[^=\n]+==+\n)", text, 0, re.M)
+  subsections = re.split(r"(^\s*==+[^=\n]+==+\s*\n)", text, 0, re.M)
 
-  for k in xrange(2, len(subsections) - 2, 2):
+  for k in xrange(2, len(subsections), 2):
     if re.search(r"==\s*Translations\s*==", subsections[k - 1]):
       lines = subsections[k].split("\n")
       in_chinese = False
@@ -65,7 +65,7 @@ def process_text_on_page(index, pagetitle, text):
         if re.search(r"^\* *Chinese:* *[^: ]", line):
           line_pagemsg("WARNING: Chinese: line with junk after it")
           in_chinese = True
-        elif re.search(r"^\* *Chinese:*$", line):
+        elif re.search(r"^\* *Chinese:* *$", line):
           in_chinese = True
         elif re.search(r"^\*:", line) and in_chinese:
           m = re.search(r"^\*: *(.*?): *(.*)$", line)
@@ -108,108 +108,135 @@ def process_text_on_page(index, pagetitle, text):
             prevt = None
             text_to_remove = []
             this_notes = []
+
+            def combine_traditional_simplified(tradt, simpt, reversed=False):
+              warnings = []
+              def append_line_pagemsg(txt):
+                warnings.append("Page %s %s: %s: <from> %s <to> %s <end>" % (index, pagetitle, txt, line, line))
+              get = getparam
+              lang = get(simpt, "1")
+              if lang == "zh":
+                append_line_pagemsg("WARNING: Internal error: Unconverted 'zh' in Chinese translation template %s" % unicode(simpt))
+                return False, warnings
+              if lang != lectcode:
+                append_line_pagemsg("WARNING: Strange language in %s translation template %s" % (lect, unicode(simpt)))
+                return False, warnings
+              trad = get(tradt, "2")
+              simp = get(simpt, "2")
+              trad_to_simp = convert_traditional_to_simplified(lectcode, trad)
+              if trad_to_simp == simp:
+                if tname(tradt) != tname(simpt):
+                  append_line_pagemsg("Traditional translation template %s and corresponding simplified translation template %s have different template names, will still combine"
+                    % (unicode(tradt), unicode(simpt)))
+                simp_alt = get(simpt, "alt")
+                simp_tr = get(simpt, "tr")
+                simp_lit = get(simpt, "lit")
+                for param in simpt.params:
+                  pn = pname(param)
+                  pv = unicode(param.value)
+                  if pn not in ["1", "2", "3", "tr", "alt", "sc", "lit"]:
+                    append_line_pagemsg("WARNING: Unrecognized parameter %s=%s in simplified translation template %s, can't combine"
+                      % (pn, pv, unicode(simpt)))
+                    return False, warnings
+                  if pn == "3" and pv:
+                    append_line_pagemsg("WARNING: Gender %s=%s specified in simplified translation template %s, can't combine"
+                      % (pn, pv, unicode(simpt)))
+                    return False, warnings
+                else: # no break
+                  for param in tradt.params:
+                    pn = pname(param)
+                    pv = unicode(param.value)
+                    if pn not in ["1", "2", "3", "tr", "alt", "sc", "lit"]:
+                      append_line_pagemsg("WARNING: Unrecognized parameter %s=%s in traditional translation template %s, can't combine"
+                        % (pn, pv, unicode(tradt)))
+                      return False, warnings
+                    if pn == "3" and pv:
+                      append_line_pagemsg("WARNING: Gender %s=%s specified in simplified translation template %s, can't combine"
+                        % (pn, pv, unicode(simpt)))
+                      return False, warnings
+                  else: # no break
+                    trad_tr = get(tradt, "tr")
+                    if trad_tr and not simp_tr:
+                      pass # leave traditional tr= alone
+                    elif trad_tr and trad_tr != simp_tr:
+                      append_line_pagemsg("WARNING: Saw transliteration parameter '%s' in traditional translation template %s different from simplified transliteration '%s', can't combine"
+                        % (trad_tr, unicode(tradt), simp_tr))
+                      return False, warnings
+                    trad_lit = get(tradt, "lit")
+                    if trad_lit and not simp_lit:
+                      pass # leave traditional lit= alone
+                    elif trad_lit and trad_lit != simp_lit:
+                      append_line_pagemsg("WARNING: Saw literal parameter '%s' in traditional translation template %s different from simplified literal '%s', can't combine"
+                        % (trad_lit, unicode(tradt), simp_lit))
+                      return False, warnings
+                    trad_alt = get(tradt, "alt")
+                    if trad_alt and not simp_alt:
+                      append_line_pagemsg("WARNING: Traditional translation template %s has alt= but corresponding simplified template %s doesn't, can't combine"
+                        % (unicode(tradt), unicode(simpt)))
+                      return False, warnings
+                    if not trad_alt and simp_alt:
+                      append_line_pagemsg("WARNING: Traditional translation template %s doesn't have alt= but corresponding simplified template %s does, can't combine"
+                        % (unicode(tradt), unicode(simpt)))
+                      return False, warnings
+                    if trad_alt and simp_alt:
+                      auto_simp_alt = convert_traditional_to_simplified(lectcode, trad_alt)
+                      if auto_simp_alt != simp_alt:
+                        append_line_pagemsg("WARNING: Traditional translation template %s has alt= which converts to %s != %s from corresponding simplified template %s, can't combine"
+                          % (unicode(tradt), auto_simp_alt, simp_alt, unicode(simpt)))
+                        return False, warnings
+                    if tradt.has("3"):
+                      if get(tradt, "3"):
+                        append_line_pagemsg("WARNING: Internal error: Non-blank gender 3= in traditional template %s" % unicode(tradt))
+                        return False, warnings
+                      rmparam(tradt, "3")
+                      notes.append("remove blank 3= in traditional %s template" % lect)
+                    if simp_tr:
+                      tradt.add("tr", simp_tr)
+                      #this_notes.append("move tr=%s from simplified %s template to traditional equivalent" % (simp_tr, lect))
+                    for delim in [", ", ",", " / ", "/", " "]:
+                      this_text_to_remove = "%s%s" % (unicode(simpt), delim) if reversed else "%s%s" % (delim, unicode(simpt))
+                      if this_text_to_remove in unicode(parsed):
+                        break
+                    if trad == simp:
+                      # just trying to remove 'this_text_to_remove' may match twice; include the preceding traditional template in the line
+                      text_to_remove.append((
+                        "%s%s" % (this_text_to_remove, unicode(tradt)) if reversed else "%s%s" % (unicode(tradt), this_text_to_remove),
+                        unicode(tradt)
+                      ))
+                    else:
+                      text_to_remove.append((this_text_to_remove, ""))
+                    either_tr = simp_tr or trad_tr
+                    either_lit = simp_lit or trad_lit
+                    this_notes.append("remove redundant simplified %s translation template (trad=%s, simp=%s%s%s)"
+                      % (lect, trad, simp, ", tr=%s" % either_tr if either_tr else "", ", lit=%s" % either_lit if either_lit else ""))
+                    return True, warnings
+              elif not get(tradt, "tr") and get(simpt, "tr"):
+                append_line_pagemsg("WARNING: Traditional translation template %s has translation %s which simplifies to %s != %s from corresponding simplified template %s, can't combine"
+                  % (unicode(tradt), trad, trad_to_simp, simp, unicode(simpt)))
+                return False, warnings
+              else:
+                # could just be for different translations
+                return None, warnings
+
+
             for t in parsed.filter_templates():
-              def getp(param):
-                return getparam(t, param)
-              tn = tname(t)
-              if tn in translation_templates:
-                lang = getp("1")
-                if lang == "zh":
-                  line_pagemsg("WARNING: Internal error: Unconverted 'zh' in Chinese translation template %s" % unicode(t))
-                  must_continue = True
-                  break
-                elif lang != lectcode:
-                  line_pagemsg("WARNING: Strange language in %s translation template %s" % (lect, unicode(t)))
-                  must_continue = True
-                  break
-                if prevt and tname(prevt) == tn:
-                  trad = getparam(prevt, "2")
-                  simp = getp("2")
-                  trad_to_simp = convert_traditional_to_simplified(lectcode, trad)
-                  if trad_to_simp == simp:
-                    alt = getp("alt")
-                    tr = getp("tr")
-                    lit = getp("lit")
-                    for param in t.params:
-                      pn = pname(param)
-                      pv = unicode(param.value)
-                      if pn not in ["1", "2", "3", "tr", "alt", "sc", "lit"]:
-                        line_pagemsg("WARNING: Unrecognized parameter %s=%s in simplified translation template %s, can't combine"
-                          % (pn, pv, unicode(t)))
-                        break
-                      if pn == "3" and pv:
-                        line_pagemsg("WARNING: Gender %s=%s specified in simplified translation template %s, can't combine"
-                          % (pn, pv, unicode(t)))
-                        break
-                    else: # no break
-                      for param in prevt.params:
-                        pn = pname(param)
-                        pv = unicode(param.value)
-                        if pn not in ["1", "2", "3", "tr", "alt", "sc", "lit"]:
-                          line_pagemsg("WARNING: Unrecognized parameter %s=%s in traditional translation template %s, can't combine"
-                            % (pn, pv, unicode(prevt)))
-                          break
-                        if pn == "3" and pv:
-                          line_pagemsg("WARNING: Gender %s=%s specified in simplified translation template %s, can't combine"
-                            % (pn, pv, unicode(t)))
-                          break
-                      else: # no break
-                        prevtr = getparam(prevt, "tr")
-                        if prevtr and not tr:
-                          pass # leave traditional tr= alone
-                        elif prevtr and prevtr != tr:
-                          line_pagemsg("WARNING: Saw transliteration parameter '%s' in traditional translation template %s different from simplified transliteration '%s', can't combine"
-                            % (prevtr, unicode(prevt), tr))
-                          continue
-                        prevlit = getparam(prevt, "lit")
-                        if prevlit and not lit:
-                          pass # leave traditional lit= alone
-                        elif prevlit and prevlit != lit:
-                          line_pagemsg("WARNING: Saw literal parameter '%s' in traditional translation template %s different from simplified literal '%s', can't combine"
-                            % (prevlit, unicode(prevt), lit))
-                          continue
-                        prev_alt = getparam(prevt, "alt")
-                        if prev_alt and not alt:
-                          line_pagemsg("WARNING: Traditional translation template %s has alt= but corresponding simplified template %s doesn't, can't combine"
-                            % (unicode(prevt), unicode(t)))
-                          continue
-                        if not prev_alt and alt:
-                          line_pagemsg("WARNING: Traditional translation template %s doesn't have alt= but corresponding simplified template %s does, can't combine"
-                            % (unicode(prevt), unicode(t)))
-                          continue
-                        if prev_alt and alt:
-                          simp_alt = convert_traditional_to_simplified(lectcode, prev_alt)
-                          if simp_alt != alt:
-                            line_pagemsg("WARNING: Traditional translation template %s has alt= which converts to %s != %s from corresponding simplified template %s, can't combine"
-                              % (unicode(prevt), simp_alt, alt, unicode(t)))
-                            continue
-                        if prevt.has("3"):
-                          if getparam(prevt, "3"):
-                            line_pagemsg("WARNING: Internal error: Non-blank gender 3= in traditional template %s" % unicode(prevt))
-                            continue
-                          rmparam(prevt, "3")
-                          notes.append("remove blank 3= in traditional %s template" % lect)
-                        if tr:
-                          prevt.add("tr", tr)
-                          #this_notes.append("move tr=%s from simplified %s template to traditional equivalent" % (tr, lect))
-                        this_text_to_remove = ", %s" % unicode(t)
-                        if this_text_to_remove not in unicode(parsed):
-                          this_text_to_remove = ",%s" % unicode(t)
-                        if this_text_to_remove not in unicode(parsed):
-                          this_text_to_remove = " %s" % unicode(t)
-                        if trad == simp:
-                          # just trying to remove 'this_text_to_remove' may match twice; include the preceding traditional template in the line
-                          text_to_remove.append(("%s%s" % (unicode(prevt), this_text_to_remove), unicode(prevt)))
-                        else:
-                          text_to_remove.append((this_text_to_remove, ""))
-                        either_tr = tr or prevtr
-                        either_lit = lit or prevlit
-                        this_notes.append("remove redundant simplified %s translation template (trad=%s, simp=%s%s%s)"
-                          % (lect, trad, simp, ", tr=%s" % either_tr if either_tr else "", ", lit=%s" % either_lit if either_lit else ""))
+              if prevt and tname(t) in translation_templates and tname(prevt) in translation_templates:
+                combinable, warnings = combine_traditional_simplified(prevt, t)
+                if combinable is False:
+                  line_pagemsg("Can't combine traditional template %s and simplified template %s, trying reversed"
+                    % (unicode(prevt), unicode(t)))
+                  rev_combinable, rev_warnings = combine_traditional_simplified(t, prevt, reversed=True)
+                  if rev_combinable:
+                    line_pagemsg("Able to combine simplified template %s with traditional template %s by assuming backwards order"
+                      % (unicode(prevt), unicode(t)))
+                    for warning in rev_warnings:
+                      msg(warning)
+                    prevt = t
+                    continue
+                for warning in warnings:
+                  msg(warning)
               prevt = t
 
-            if must_continue:
-              continue
             line = unicode(parsed)
             for this_text_to_remove, this_repl in text_to_remove:
               newline, replaced = blib.replace_in_text(line, this_text_to_remove, this_repl, line_pagemsg, abort_if_warning=True,
