c2b9a3d25d3e088265fcb8a76583719c9c73aa66
Ben Wing <ben@benwing.com>
2023-03-15 02:43:10 -0500

    canon_persian: get working in non-test mode

--- canon_persian.py
+++ canon_persian.py
@@ -6,10 +6,9 @@ import pywikibot, re, sys, codecs, argparse
 import blib
 from blib import getparam, rmparam, tname, pname, msg, site
 import fa_translit
-from canon_foreign import canon_one_page_links
+from canon_foreign import canon_one_page_links, show_failure
 
-parser = blib.create_argparser("Clean up Persian transliterations",
-    include_pagefile=True)
+parser = blib.create_argparser("Clean up Persian transliterations", include_pagefile=True, include_stdin=True)
 parser.add_argument("--direcfile", help="File containing output from find_regex.py, to process")
 parser.add_argument("--test", help="Test fa_translit.py", action="store_true")
 parser.add_argument("--no-vocalize", help="Disable vocalization of Persian script", action="store_true")
@@ -19,11 +18,6 @@ start, end = blib.parse_start_end(args.start, args.end)
 templates_seen = {}
 templates_changed = {}
 printed_succeeded_failed = False
-
-def show_failure(pagemsg):
-  total = fa_translit.num_failed + fa_translit.num_succeeded
-  pagemsg("Failure = %s/%s = %.1f%%" % (fa_translit.num_failed, total,
-    100.0 * fa_translit.num_failed / total))
 
 def process_text_on_page(index, pagetitle, text):
   def pagemsg(txt):
@@ -37,16 +31,11 @@ def process_text_on_page(index, pagetitle, text):
         if int(index) % 100 == 0:
           if not printed_succeeded_failed:
             printed_succeeded_failed = True
-            show_failure(pagemsg)
+            show_failure(pagemsg, fa_translit.num_succeeded, fa_translit.num_failed)
         else:
           printed_succeeded_failed = False
         pagemsg("Processing %s" % unicode(obj.t))
         return fa_translit.test_with_obj(obj, latin, foreign, "matched")
-      def do_multiple_translits(foreign, latins):
-        # Since there are different vocalizations associated with different translits.
-        obj.addl_params["no_vocalize"] = True
-        for this_latin in latins:
-          test(obj, foreign, this_latin)
       foreign = None
       latin = None
       if obj.param[0] == "separate":
@@ -64,34 +53,20 @@ def process_text_on_page(index, pagetitle, text):
       obj.addl_params["no_vocalize"] = args.no_vocalize
       if not foreign or not latin or latin in ["-", "?"]:
         pagemsg("Skipped: foreign=%s, latin=%s" % (foreign, latin))
-      elif "," in latin and not re.search(u"[,،]", foreign):
-        # Check for multiple translits opposite a single Arabic-script term.
-        # If processing for changing, we need to paste the results back together.
-        latins = re.split(r",\s*", latin)
-        do_multiple_translits(foreign, latins)
-      elif "~" in latin and "~" not in foreign:
-        # Similar to comma-separated translits.
-        latins = re.split(r"\s*~\s*", latin)
-        do_multiple_translits(foreign, latins)
-      elif "/" in latin and "/" not in foreign:
-        # Similar to comma-separated translits.
-        latins = re.split(r"\s*/\s*", latin)
-        do_multiple_translits(foreign, latins)
-      elif " ''or'' " in latin and " " not in foreign:
-        # Similar to comma-separated translits.
-        latins = re.split(r"\s+''or''\s+", latin)
-        do_multiple_translits(foreign, latins)
-      elif " or " in latin and " " not in foreign:
-        # Similar to comma-separated translits.
-        latins = re.split(r"\s+or\s+", latin)
-        do_multiple_translits(foreign, latins)
       else:
-        test(obj, foreign, latin)
+        latins = fa_translit.split_multiple_translits(latin, foreign)
+        if latins is not None:
+          # Since there are different vocalizations associated with different translits.
+          obj.addl_params["no_vocalize"] = True
+          for this_latin in latins:
+            test(obj, foreign, this_latin)
+        else:
+          test(obj, foreign, latin)
     return blib.process_one_page_links(index, pagetitle, text, ["fa"], process_param,
         templates_seen, templates_changed)
   else:
     return canon_one_page_links(pagetitle, index, text, "fa", "fa-Arab", fa_translit,
-        templates_seen, templates_changed)
+        templates_seen, templates_changed, {"no_vocalize": args.no_vocalize})
 
 if args.direcfile:
   for lineindex, line in blib.iter_items_from_file(args.direcfile, start, end):
@@ -106,5 +81,10 @@ if args.direcfile:
       process_text_on_page(index, pagetitle, text)
 else:
   blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True)
-show_failure(msg)
+# If in --test mode, we need to use the num_succeeded/num_failed from fa_translit as the ones in canon_foreign aren't
+# set.
+if args.test:
+  show_failure(msg, fa_translit.num_succeeded, fa_translit.num_failed)
+else:
+  show_failure(msg)
 blib.output_process_links_template_counts(templates_seen, templates_changed)
