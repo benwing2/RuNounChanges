64040657bf2b64758dd480114203d50d4cb44830
Ben Wing <ben@benwing.com>
2023-03-29 04:37:57 -0500

    analyze_cs_nouns: lots more work

--- analyze_cs_nouns.py
+++ analyze_cs_nouns.py
@@ -11,16 +11,31 @@ import cslib as cs
 def is_undefined(word):
   return word in ["", "-", u"-", u"—"]
 
-genitive_singular_endings = ["a", "u", "e", u"ě", "y", "i", u"í"]
-dative_singular_endings = ["u", "ovi", "i", u"í", "e", u"ě"]
-vocative_singular_endings = ["e", u"ě", "u", "i", "o", u"í", ""]
-locative_singular_endings = ["u", "ovi", "e", u"ě", "i", u"í"]
-instrumental_singular_endings = ["em", u"ěm", "ou", u"ím", u"í"]
-nominative_plural_endings = ["i", "y", u"é", u"ové", "e", u"ě", "a", u"í"]
-genitive_plural_endings = [u"ů", u"í", ""]
-dative_plural_endings = [u"ům", u"ím", u"ám", "em", u"ěm"]
-locative_plural_endings = ["ech", u"ěch", u"ích", u"ách"]
-instrumental_plural_endings = [u"ími", "ami", "emi", u"ěmi", "mi", "y", "i"]
+noun_endings = {
+  "gen_sg": ["a", "u", "e", u"ě", "y", "i", u"í"],
+  "dat_sg": ["u", "ovi", "i", u"í", "e", u"ě"],
+  "voc_sg": ["e", u"ě", "u", "i", "o", u"í", ""],
+  "loc_sg": ["u", "ovi", "e", u"ě", "i", u"í"],
+  "ins_sg": ["em", u"ěm", "ou", u"ím", u"í"],
+  "nom_pl": ["i", "y", u"é", u"ové", "e", u"ě", "a", u"í"],
+  "gen_pl": [u"ů", u"í", ""],
+  "dat_pl": [u"ům", u"ím", u"ám", "em", u"ěm"],
+  "loc_pl": ["ech", u"ěch", u"ích", u"ách"],
+  "ins_pl": [u"ími", "ami", "emi", u"ěmi", "mi", "y", "i"],
+}
+
+adj_endings = {
+  "gen_sg": [u"ého", u"é", u"ího", u"í"],
+  "dat_sg": [u"ému", u"é", u"ímu", u"í"],
+  "voc_sg": [u"ý", u"á", u"é"],
+  "loc_sg": [u"ém", u"é", u"ím", u"í"],
+  "ins_sg": [u"ým", "ou", u"ím", u"í"],
+  "nom_pl": [u"í", u"é", u"á"],
+  "gen_pl": [u"ých", u"ích"],
+  "dat_pl": [u"ým", u"ím"],
+  "loc_pl": [u"ých", u"ích"],
+  "ins_pl": [u"ými", u"ími"],
+}
 
 slot_to_param = {
   "nom_sg":"1",
@@ -37,6 +52,16 @@ slot_to_param = {
   "voc_pl":"12",
   "loc_pl":"13",
   "ins_pl":"14",
+}
+
+pl_tantum_slot_to_param = {
+  "nom_pl":"1",
+  "gen_pl":"2",
+  "dat_pl":"3",
+  "acc_pl":"4",
+  "voc_pl":"5",
+  "loc_pl":"6",
+  "ins_pl":"7",
 }
 
 def process_text_on_page(index, pagetitle, text):
@@ -90,8 +115,9 @@ def process_text_on_page(index, pagetitle, text):
           pagemsg("WARNING: Unknown gender: %s" % gender)
           gender = "unknown"
 
-    def fetch(param):
-      param = slot_to_param.get(param, param)
+    def fetch(param, pl_tantum=False):
+      slot_map = pl_tantum_slot_to_param if pl_tantum else slot_to_param
+      param = slot_map.get(param, param)
       val = getparam(t, param).strip()
       val = blib.remove_links(val)
       vals = re.split(r",\s*", val)
@@ -102,15 +128,18 @@ def process_text_on_page(index, pagetitle, text):
         retval.append(v)
       return ", ".join(retval)
 
-    def fetch_endings(param, endings):
+    def fetch_endings(param, is_adj=False, pl_tantum=False, uniquify=True):
+      ending_set = adj_endings if is_adj else noun_endings
+      endings = ending_set[param]
       endings = sorted(endings, key=lambda x:-len(x))
-      paramval = fetch(param)
+      paramval = fetch(param, pl_tantum)
       values = re.split(", *", paramval)
       found_endings = []
       for v in values:
         for ending in endings:
           if v.endswith(ending):
-            found_endings.append(ending)
+            if not uniquify or ending not in found_endings:
+              found_endings.append(ending)
             break
         else: # no break
           pagemsg("WARNING: Couldn't recognize ending for %s=%s: %s" % (
@@ -123,7 +152,9 @@ def process_text_on_page(index, pagetitle, text):
     def truncate_extra_forms(form):
       return re.sub(",.*", "", form)
 
-    def infer_decl(lemma):
+    def infer_basic_decl(lemma):
+      overrides = []
+      decl = None
       # * Nouns ending in a consonant are generally masculine, but can be feminine of the -e/ě type (e.g. [[dlaň]]
       #   "palm (of the hand) or the i-stem type (e.g. [[kost]] "bone"), both of which have instrumental singular in
       #   -í instead of -em. Masculine nouns are animate if acc_sg = gen_sg, inanimate if acc_sg = nom_sg. Nouns
@@ -143,68 +174,101 @@ def process_text_on_page(index, pagetitle, text):
       # * Nouns in -á are feminine adjectival.
       # * Nouns in -é are neuter adjectival.
       if lemma.endswith("o"):
-        return "n"
-      if lemma.endswith("a"):
+        decl = "n"
+      elif lemma.endswith("a"):
         if fetch("acc_sg") == fetch("nom_sg"):
-          return "n.foreign"
-        if fetch("dat_pl").endswith(u"ům"):
-          return "m.an"
-        return "f"
-      if re.search(u"[eě]$", lemma):
+          decl = "n.foreign"
+        elif fetch("dat_pl").endswith(u"ům"):
+           decl = "m.an"
+        else:
+          decl = "f"
+      elif re.search(u"[eě]$", lemma):
         if re.search(u"[eě]m$", fetch("ins_sg")):
           if fetch("nom_pl").endswith("i"):
-            return "m.an"
+            decl = "m.an"
           elif fetch("nom_pl").endswith("ata"):
-            return "n.tstem"
+            decl = "n.tstem"
           else:
-            return "n"
+            decl = "n"
         else:
-          return "f"
-      if lemma.endswith(u"í"):
+          decl = "f"
+      elif lemma.endswith(u"í"):
         if fetch("gen_sg").endswith(u"ího"):
           if fetch("acc_sg").endswith(u"ího"):
-            return "m.an.+"
+            decl = "m.an.+"
           else:
-            return "n.+"
+            decl = "n.+"
         elif fetch("ins_sg").endswith(u"í"):
-          return "f.+"
+          decl = "f.+"
         else:
-          return "n"
-      if lemma.endswith(u"ý"):
+          decl = "n"
+      elif lemma.endswith(u"ý"):
         if fetch("acc_sg") == fetch("gen_sg"):
-          return "m.an"
+          decl = "m.an"
         else:
-          return "m"
-      if lemma.endswith(u"á"):
-        return "f.+"
-      if lemma.endswith(u"é"):
-        return "f.+"
-      if re.search(cs.cons_c + "$", lemma):
+          decl = "m"
+      elif lemma.endswith(u"á"):
+        decl = "f.+"
+      elif lemma.endswith(u"é"):
+        decl = "n.+"
+      elif re.search(cs.cons_c + "$", lemma):
         if fetch("ins_sg").endswith(u"í"):
           if fetch("gen_sg").endswith("i"):
-            return "f.istem"
+            decl = "f.istem"
           else:
-            return "f"
+            decl = "f"
         else:
           if re.search("(on|um)$", lemma):
             # [[enklitikon]], [[museum]]
             stem = lemma[:-2]
             if fetch("ins_sg") == stem + "em":
-              return "n.foreign"
-          if re.search("[ueo]s$", lemma):
+              decl = "n.foreign"
+          elif re.search("[ueo]s$", lemma):
             # [[komunismus]], [[hádes]], [[kosmos]]
             stem = lemma[:-2]
             if fetch("ins_sg") == stem + "em":
               if fetch("acc_sg") == fetch("gen_sg"):
-                return "m.an.foreign"
+                decl = "m.an.foreign"
               else:
-                return "m.foreign"
-          if fetch("acc_sg") == fetch("gen_sg"):
-            return "m.an"
-          else:
-            return "m"
-      pagemsg("WARNING: Unrecognized lemma ending: %s" % lemma)
-      return None
+                decl = "m.foreign"
+          if not decl:
+            gen_sg_endings = fetch_endings("gen_sg")
+            dat_sg_endings = fetch_endings("dat_sg")
+            voc_sg_endings = fetch_endings("voc_sg")
+            nom_pl_endings = fetch_endings("nom_pl")
+            loc_pl_endings = fetch_endings("loc_pl")
+            ins_pl_endings = fetch_endings("ins_pl")
+            is_soft = ins_pl_endings == "i" or dat_sg_endings == "i"
+            if fetch("acc_sg") == fetch("gen_sg"):
+              decl = "m.an"
+            else:
+              decl = "m"
+            if not is_soft:
+              if decl == "m" and gen_sg_endings != "u":
+                overrides.append("gen" + gen_sg_endings)
+              velar = re.search("[ghk]$", lemma)
+              expected_voc_sg_ending = "u" if velar else "e"
+              if voc_sg_endings != expected_voc_sg_ending:
+                overrides.append("voc" + voc_sg_endings)
+              if decl == "m.an" and nom_pl_endings != "i":
+                overrides.append("nompl" + nom_pl_endings)
+              if loc_pl_endings != "ech":
+                overrides.append("locpl" + loc_pl_endings)
+
+      if not decl:
+        pagemsg("WARNING: Unrecognized lemma ending: %s" % lemma)
+      return decl, ".".join(overrides)
+
+    def infer_decl(lemma):
+      decl, overrides = infer_basic_decl(lemma)
+      if decl is None:
+        return None
+      alternation = infer_alternations(decl, fetch("nom_sg"), fetch("gen_sg"), fetch("gen_pl"))
+      if alternation:
+        decl += "." + alternation
+      if overrides:
+        decl += "." + overrides
+      return decl
 
     # Determine the default value for the 'reducible' flag.
     def determine_default_reducible(lemma):
@@ -299,33 +363,37 @@ def process_text_on_page(index, pagetitle, text):
           return None
 
     if tn == "cs-decl-noun":
-      nom_sg = fetch("1")
-      gen_sg = fetch("2")
-      dat_sg = fetch("3")
-      acc_sg = fetch("4")
-      voc_sg = fetch("5")
-      loc_sg = fetch("6")
-      ins_sg = fetch("7")
-      nom_pl = fetch("8")
-      gen_pl = fetch("9")
-      dat_pl = fetch("10")
-      acc_pl = fetch("11")
-      voc_pl = fetch("12")
-      loc_pl = fetch("13")
-      ins_pl = fetch("14")
-      gen_sg_endings = fetch_endings("2", genitive_singular_endings)
-      dat_sg_endings = fetch_endings("3", dative_singular_endings)
-      voc_sg_endings = fetch_endings("5", vocative_singular_endings)
-      loc_sg_endings = fetch_endings("6", locative_singular_endings)
-      ins_sg_endings = fetch_endings("7", instrumental_singular_endings)
-      nom_pl_endings = fetch_endings("8", nominative_plural_endings)
-      gen_pl_endings = fetch_endings("9", genitive_plural_endings)
-      dat_pl_endings = fetch_endings("10", dative_plural_endings)
-      loc_pl_endings = fetch_endings("13", locative_plural_endings)
-      ins_pl_endings = fetch_endings("14", instrumental_plural_endings)
-
       if not heads:
         heads = [pagetitle]
+      lemma = heads[0]
+      decl = infer_decl(lemma)
+
+      nom_sg = fetch("nom_sg")
+      gen_sg = fetch("gen_sg")
+      dat_sg = fetch("dat_sg")
+      acc_sg = fetch("acc_sg")
+      voc_sg = fetch("voc_sg")
+      loc_sg = fetch("loc_sg")
+      ins_sg = fetch("ins_sg")
+      nom_pl = fetch("nom_pl")
+      gen_pl = fetch("gen_pl")
+      dat_pl = fetch("dat_pl")
+      acc_pl = fetch("acc_pl")
+      voc_pl = fetch("voc_pl")
+      loc_pl = fetch("loc_pl")
+      ins_pl = fetch("ins_pl")
+      is_adj = decl and "+" in decl
+      gen_sg_endings = fetch_endings("gen_sg", is_adj, uniquify=False)
+      dat_sg_endings = fetch_endings("dat_sg", is_adj, uniquify=False)
+      voc_sg_endings = fetch_endings("voc_sg", is_adj, uniquify=False)
+      loc_sg_endings = fetch_endings("loc_sg", is_adj, uniquify=False)
+      ins_sg_endings = fetch_endings("ins_sg", is_adj, uniquify=False)
+      nom_pl_endings = fetch_endings("nom_pl", is_adj, uniquify=False)
+      gen_pl_endings = fetch_endings("gen_pl", is_adj, uniquify=False)
+      dat_pl_endings = fetch_endings("dat_pl", is_adj, uniquify=False)
+      loc_pl_endings = fetch_endings("loc_pl", is_adj, uniquify=False)
+      ins_pl_endings = fetch_endings("ins_pl", is_adj, uniquify=False)
+
       cases = [
         "gen_sg", "dat_sg", "voc_sg", "loc_sg", "ins_sg",
         "nom_pl", "gen_pl", "dat_pl", "loc_pl", "ins_pl"
@@ -339,27 +407,17 @@ def process_text_on_page(index, pagetitle, text):
         canon(nom_sg), canon(gen_sg), canon(voc_sg), canon(loc_sg), canon(ins_sg),
         canon(nom_pl), canon(gen_pl), canon(dat_pl), canon(loc_pl), canon(ins_pl)))
       if len(heads) > 1:
-        pagemsg("WARNING: Multiple heads, not inferring declension: %s" % ",".join(heads))
-        continue
-      lemma = heads[0]
-      parts = []
-      decl = infer_decl(lemma)
+        pagemsg("WARNING: Multiple heads: %s" % ",".join(heads))
       if decl is None:
         pagemsg("Unable to infer declension")
         continue
+      pagemsg("Inferred declension %s<%s>" % (lemma, decl))
       declgender = decl[0]
       declan = "an" if ".an" in decl else "in"
       if animacy != "unknown" and (gender == "m" or declgender == "m") and declan != animacy:
         pagemsg("WARNING: Headword animacy %s differs from inferred declension animacy %s" % (animacy, declan))
       if gender != "unknown" and gender != declgender:
         pagemsg("WARNING: Headword gender %s differs from inferred declension gender %s" % (gender, declgender))
-      alternation = infer_alternations(decl, nom_sg, gen_sg, gen_pl)
-      if alternation:
-        decl += "." + alternation
-      if gender == "m":
-        if declan == "in" and gen_sg.endswith("a"):
-          decl += ".gena"
-      pagemsg("Inferred declension %s<%s>" % (lemma, decl))
 
     elif tn == "cs-decl-noun-unc":
       pagemsg("WARNING: Singular-only unimplemented")
