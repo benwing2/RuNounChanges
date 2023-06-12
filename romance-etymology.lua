81d9d74f72d376abdf0b455811e27ba9a796a6d7
Ben Wing <ben@benwing.com>
2023-06-12 09:25:11 -0700

    romance-etymology: finish (NOT TESTED)

--- romance-etymology.lua
+++ romance-etymology.lua
@@ -1,11 +1,8 @@
 local export = {}
 
 local m_links = require("Module:links")
-local m_languages = require("Module:languages")
 local put_module = "Module:parse utilities"
-local com_module = "Module:it-common"
 local compound_module = "Module:compound"
-local lang = m_languages.getByCode("it")
 
 local rfind = mw.ustring.find
 local rsplit = mw.text.split
@@ -46,7 +43,7 @@ local function get_param_mods(include_pl_and_type)
 			item_dest = "gloss",
 		},
 		gloss = {},
-		-- no 'tr' or 'ts', doesn't make sense for Italian
+		-- no 'tr' or 'ts', doesn't make sense for Romance langs
 		g = {
 			-- We need to store the <g:...> inline modifier into the "genders" key of the parsed part, because that is what
 			-- [[Module:links]] expects.
@@ -61,10 +58,11 @@ local function get_param_mods(include_pl_and_type)
 		qq = {},
 		lit = {},
 		pos = {},
-		-- no 'sc', doesn't make sense for Italian
+		-- no 'sc', doesn't make sense for Romance langs
 	}
 	if include_pl_and_type then
 		param_mods.pl = {}
+		param_mods.imp = {}
 		param_mods.type = {
 			convert = function(arg, parse_err)
 				if arg ~= "verb" and arg ~= "object" and arg ~= "connector" then
@@ -130,20 +128,7 @@ local function parse_term_with_modifiers(paramname, val, pre_initialized_obj, in
 end
 
 
-local function form_imperative(inf, parse_err)
-	local stem, ending = inf:match("^(.*)([aei]re)$")
-	if not stem then
-		parse_err(("Unrecognized infinitive '%s', doesn't end in -are, -ere or -ire"):format(inf))
-	end
-	if ending == "are" then
-		return stem .. "a"
-	else
-		return stem .. "i"
-	end
-end
-
-
-local function parse_and_format_parts(parts, get_part_type)
+local function parse_and_format_parts(data, parts, get_part_type)
 	for i, term in ipairs(parts) do
 		local parsed = parse_term_with_modifiers(i, term, nil, "include pl and type")
 		if not parsed.term:find("%[") then
@@ -155,24 +140,36 @@ local function parse_and_format_parts(parts, get_part_type)
 				parse_err(("Can't specify <pl:...> with an argument that is not an object (argument type is '%s')"):
 					format(parttype))
 			end
+			if parttype ~= "verb" and parsed.imp then
+				parse_err(("Can't specify <imp:...> with an argument that is not a verb (argument type is '%s')"):
+					format(parttype))
+			end
 			if parttype == "verb" then
-				if parsed.lang then
-					parse_err(("Can't form imperative of term '%s' given with explicit language code prefix '%s'"):
-						format(parsed.term, parsed.lang:getCode()))
+				local imp
+				parsed.imp = parsed.imp or "+"
+				if rfind(parsed.imp, "^%+") then
+					if parsed.lang then
+						parse_err(("Can't form default imperative of term '%s' given with explicit language code prefix '%s'"):
+							format(parsed.term, parsed.lang:getCode()))
+					end
+					imp = data.make_imperative(parsed.imp, parsed.term, parse_err)
+					if not imp then
+						parse_err(("Default imperative algorithm was unable to form the imperative of term '%s'"):format(parsed.term))
+					end
+				elseif parsed.imp then
+					imp = parsed.imp
 				end
-				local imperative = form_imperative(parsed.term, parse_err)
-				parsed.term = ("[[%s|%s]]"):format(parsed.term, imperative)
+				if imp then
+					parsed.term = ("[[%s|%s]]"):format(parsed.term, imp)
+				end
 			elseif parttype == "object" then
 				local pl
-				if parsed.pl == "1" then
+				if parsed.pl == "1" or rfind(parsed.pl, "^%+") then
 					if parsed.lang then
 						parse_err(("Can't form default plural of term '%s' given with explicit language code prefix '%s'"):
 							format(parsed.term, parsed.lang:getCode()))
 					end
-					-- "Guess" a gender based on the ending. We can't pass in a "?" because then we'll get an error on terms ending in
-					-- -a because they have different plurals depending on the gender.
-					local gender = parsed.term:find("a$") and "f" or "m"
-					pl = require(com_module).make_plural(parsed.term, gender)
+					pl = data.make_plural(parsed.pl, parsed.term, parse_err)
 					if not pl then
 						parse_err(("Default plural algorithm was unable to form the plural of term '%s'"):format(parsed.term))
 					end
@@ -185,7 +182,7 @@ local function parse_and_format_parts(parts, get_part_type)
 			end
 		end
 
-		parsed.lang = parsed.lang or lang
+		parsed.lang = parsed.lang or data.lang
 		parts[i] = link_with_qualifiers(parsed)
 	end
 
@@ -282,95 +279,6 @@ function export.verb_verb(data, frame)
 
 	ins(require(compound_module).concat_parts(data.lang, args[1], {"verb-verb compounds"}, args.nocat, args.sort, args.lit,
 		force_cat))
-	return table.concat(result)
-end
-
-
-function export.it_deverbal(frame)
-	local list_with_holes = { list = true, allow_holes = true }
-	local params = {
-		[1] = {required = true, list = true},
-		
-		["alt"] = list_with_holes,
-		["t"] = list_with_holes,
-		["gloss"] = {alias_of = "t", list = true, allow_holes = true},
-		["g"] = list_with_holes,
-		["id"] = list_with_holes,
-		["lit"] = list_with_holes,
-		["pos"] = list_with_holes,
-		["q"] = list_with_holes,
-		["qq"] = list_with_holes,
-		-- no tr, ts or sc; not relevant for Italian
-
-		["nocap"] = {type = "boolean"},
-		["notext"] = {type = "boolean"},
-		["nocat"] = {type = "boolean"},
-		["sort"] = {},
-		["pagename"] = {}, -- for testing
-	}
-
-	local args = require("Module:parameters").process(frame:getParent().args, params)
-
-	if #args[1] == 0 then
-	    local NAMESPACE = mw.title.getCurrentTitle().nsText
-		if NAMESPACE == "Template" then
-			table.insert(args[1], "cozzare<t:to collide, to crash>")
-			args.pagename = "cozzo"
-		else
-			error("Internal error: Something went wrong with [[Module:parameters]]; it should not allow zero numbered arguments")
-		end
-	end
-
-	local pagename = args.pagename or mw.title.getCurrentTitle().subpageText
-	local suffix = pagename:match("([aeo])$")
-	if not suffix then
-		error(("Pagename '%s' does not end in a recognizable deverbal suffix -a, -e or -o"):format(pagename))
-	end
-
-	local suffix_obj = m_links.full_link({
-		term = "-" .. suffix,
-		lang = lang,
-		id = "deverbal",
-	}, "term")
-
-	for i, term in ipairs(args[1]) do
-		local parsed = parse_term_with_modifiers(i, term, {
-			alt = args.alt[i],
-			gloss = args.t[i],
-			genders = args.g[i] and rsplit(args.g[i], ",") or nil,
-			id = args.id[i],
-			pos = args.pos[i],
-			lit = args.lit[i],
-			q = args.q[i],
-			qq = args.qq[i],
-		}, false)
-		parsed.lang = parsed.lang or lang
-		local formatted_part = link_with_qualifiers(parsed)
-		args[1][i] = require(compound_module).concat_parts(lang, {formatted_part, suffix_obj},
-			{"deverbals", "terms suffixed with -" .. suffix .. " (deverbal)"},
-			args.nocat, args.sort, nil, force_cat) -- FIXME: should we support lit= here?
-	end
-
-	result = {}
-	local function ins(text)
-		table.insert(result, text)
-	end
-
-	if not args.notext then
-		if args.nocap then
-			ins(glossary_link("Appendix:deverbal", "deverbal"))
-		else
-			ins(glossary_link("Appendix:deverbal", "Deverbal"))
-		end
-		ins(" from ")
-	end
-
-	if #args[1] == 1 then
-		ins(args[1][1])
-	else
-		ins(require("Module:table").serialCommaJoin(args[1], {conj = "or"}))
-	end
-
 	return table.concat(result)
 end
 
