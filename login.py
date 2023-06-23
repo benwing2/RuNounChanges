fe2c67f87546b5be34e24b9f34591ab66524ed7a
Ben Wing <ben@benwing.com>
2023-06-23 02:35:14 -0500

    login: --sysop not supported any more

--- login.py
+++ login.py
@@ -6,7 +6,8 @@ import pywikibot, re, sys, argparse
 import blib
 
 parser = blib.create_argparser("Login to sysop or no-sysop")
-parser.add_argument('--sysop', help="Login to sysop", action="store_true")
+#parser.add_argument('--sysop', help="Login to sysop", action="store_true")
 args = parser.parse_args()
 
-pywikibot.Site().login(sysop=args.sysop)
+#pywikibot.Site().login(sysop=args.sysop)
+pywikibot.Site().login()
