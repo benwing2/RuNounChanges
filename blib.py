1aaf73da7e91212d73feb65b4fc8aa8cbbfa0fb6
Ben Wing <ben@benwing.com>
2023-06-23 02:34:30 -0500

    blib: Upon JSONDecodeError, display raw JSON string

--- blib.py
+++ blib.py
@@ -10,6 +10,7 @@ import xml.sax
 import difflib
 import traceback
 import multiprocessing as mp
+from json.decoder import JSONDecodeError
 
 site = pywikibot.Site()
 
@@ -1449,11 +1450,18 @@ def getData():
   getScriptData()
   getEtymLanguageData()
 
+def json_loads(data):
+  try:
+    return json.loads(data)
+  except JSONDecodeError:
+    print("JSON decode error processing the following: %s" % data)
+    raise
+
 def getLanguageData():
   global languages, languages_byCode, languages_byCanonicalName
 
   jsondata = site.expand_text("{{#invoke:User:MewBot|getLanguageData}}")
-  languages = json.loads(jsondata)
+  languages = json_loads(jsondata)
   languages_byCode = {}
   languages_byCanonicalName = {}
 
@@ -1465,7 +1473,7 @@ def getLanguageData():
 def getFamilyData():
   global families, families_byCode, families_byCanonicalName
 
-  families = json.loads(site.expand_text("{{#invoke:User:MewBot|getFamilyData}}"))
+  families = json_loads(site.expand_text("{{#invoke:User:MewBot|getFamilyData}}"))
   families_byCode = {}
   families_byCanonicalName = {}
 
@@ -1477,7 +1485,7 @@ def getFamilyData():
 def getScriptData():
   global scripts, scripts_byCode, scripts_byCanonicalName
 
-  scripts = json.loads(site.expand_text("{{#invoke:User:MewBot|getScriptData}}"))
+  scripts = json_loads(site.expand_text("{{#invoke:User:MewBot|getScriptData}}"))
   scripts_byCode = {}
   scripts_byCanonicalName = {}
 
@@ -1489,7 +1497,7 @@ def getScriptData():
 def getEtymLanguageData():
   global etym_languages, etym_languages_byCode, etym_languages_byCanonicalName
 
-  etym_languages = json.loads(site.expand_text("{{#invoke:User:MewBot|getEtymLanguageData}}"))
+  etym_languages = json_loads(site.expand_text("{{#invoke:User:MewBot|getEtymLanguageData}}"))
   etym_languages_byCode = {}
   etym_languages_byCanonicalName = {}
 
