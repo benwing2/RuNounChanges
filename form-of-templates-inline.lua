f538b39811f179ba60fcbb9db50b98fbd991e16d
Ben Wing <ben@benwing.com>
2023-07-01 03:18:57 -0500

    form-of-templates-inline: rewrite

--- form-of-templates-inline.lua
+++ form-of-templates-inline.lua
@@ -1,11 +1,11 @@
 local export = {}
 
-local m_form_of = require("Module:form of")
+local m_form_of = require("Module:User:Benwing2/form of")
 local m_languages = require("Module:languages")
 
 function export.inflection_inline_t(frame)
 	local params = {
-		[1] = {required = true},
+		[1] = {required = true, default = "und"},
 		[2] = {required = true, list = true},
 	}
 	
@@ -13,24 +13,20 @@ function export.inflection_inline_t(frame)
 	
 	local lang = m_languages.getByCode(args[1], 1)
 
-	local outputs = {}
-	local inputs = {}
-	
-	for _, tag in ipairs(args[2]) do
-		if tag == ";" then
-			if #inputs > 0 then
-				table.insert(outputs, m_form_of.tagged_inflections({ tags = inputs, lang = lang }))
-			end
-			inputs = {}
-		else
-			table.insert(inputs, tag)
-		end
+	local tags = args[2]
+	if #tags == 0 and mw.title.getCurrentTitle().nsText == "Template" then
+		tags = {"m", "acc", "s"}
 	end
 
-	if #inputs > 0 then
-		table.insert(outputs, m_form_of.tagged_inflections({ tags = inputs, lang = lang }))
+	local outputs = {}
+	local tag_sets = m_form_of.split_tags_into_tag_sets(tags)
+
+	-- Set nocat = true so we don't generate any categories; {{qinfl}} is used to generate qualifier tags and any
+	-- associated categories may or may not apply to the page as a whole.
+	for _, tag_set in ipairs(tag_sets) do
+		table.insert(outputs, (m_form_of.tagged_inflections({ tags = tag_set, lang = lang, nocat = true })))
 	end
-	
+
 	return table.concat(outputs, "; ")
 end
 
