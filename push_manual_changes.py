6ae8eb6412f802a205bc4f6c2fbb09a86f482487
Ben Wing <ben@benwing.com>
2023-08-18 02:25:16 -0500

    push_manual_changes: allow embedded comment in <from>-<to>-<end> format, do all changes for a page at once

--- push_manual_changes.py
+++ push_manual_changes.py
@@ -11,13 +11,23 @@ site = pywikibot.Site()
 
 max_truncate_len = 80
 
-def truncate(text):
+# Truncate a string so its length is <= max_truncate_len including a ... added at the end.
+def truncate(text, truncate_len=max_truncate_len):
   text = text.replace("\n", r"\n") # escape newlines
-  if len(text) < max_truncate_len:
+  if len(text) <= truncate_len:
     return text
-  return text[0:max_truncate_len] + "..."
+  return text[0:truncate_len - 3] + "..."
+
+# Truncate a string so its UTF-8 length is <= max_truncate_len including a ... added at the end.
+def truncate_bytes(text, truncate_len=max_truncate_len):
+  text = text.replace("\n", r"\n") # escape newlines
+  textbytes = text.encode("utf-8")
+  if len(textbytes) <= truncate_len:
+    return text
+  return textbytes[0:truncate_len - 3].decode("utf-8", "ignore") + "..."
 
 def read_direcfile(direcfile, start, end):
+  comment = None
   template_changes = []
   for lineno, line in blib.iter_items_from_file(direcfile, start, end):
     def linemsg(txt):
@@ -44,9 +54,10 @@ def read_direcfile(direcfile, start, end):
           template_changes.pop()
         if repl_on_right:
           pagename, curr, repl = m.groups()
-          template_changes.append((pagename, repl, curr))
+          template_changes.append((pagename, [(repl, curr)], None))
         else:
-          template_changes.append(m.groups())
+          pagename, repl, curr = m.groups()
+          template_changes.append((pagename, [(repl, curr)], None))
       else:
         linemsg("WARNING: Ignoring line with from=to: %s" % line)
     else:
@@ -55,21 +66,27 @@ def read_direcfile(direcfile, start, end):
         linemsg("WARNING: Unable to parse line: [%s]" % line)
         continue
       pagename, directives = mpage.groups()
+      m = re.search("<comment> (.*?) <endcom>", directives)
+      if m:
+        comment = m.group(1)
+      this_template_changes = []
       for m in re.finditer("<from> (.*?) <to> (.*?) <end>", directives):
         curr, repl = m.groups()
         if curr != repl:
-          template_changes.append((pagename, repl, curr))
+          this_template_changes.append((repl, curr))
         else:
           linemsg("WARNING: Ignoring line with from=to: %s" % line)
+      if this_template_changes:
+        template_changes.append((pagename, this_template_changes, comment))
   return template_changes
 
 def template_changes_to_dict(template_changes):
   retval = defaultdict(list)
-  for pagename, repl_template, curr_template in template_changes:
-    retval[pagename].append((repl_template, curr_template))
+  for pagename, repl_curr_changes, comment in template_changes:
+    retval[pagename].append((repl_curr_changes, comment))
   return retval
 
-def push_one_manual_change(pagetitle, index, text, curr_template, repl_template):
+def push_one_set_of_manual_changes(pagetitle, index, text, repl_curr_changes, comment):
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagetitle, txt))
   #template = blib.parse_text(template_text).filter_templates()[0]
@@ -81,56 +98,66 @@ def push_one_manual_change(pagetitle, index, text, curr_template, repl_template)
   #template.remove(removed_param)
   #from_template = str(template)
   text = str(text)
-  found_repl_template = repl_template in text
-  newtext = text.replace(curr_template, repl_template)
-  changelog = ""
-  if newtext == text:
-    if not found_repl_template:
-      pagemsg("WARNING: Unable to locate current template: %s (would replace with %s)" % (curr_template, repl_template))
-    else:
-      pagemsg("Replacement template already found, taking no action")
-  else:
-    if found_repl_template:
-      pagemsg("WARNING: Made change, but replacement template %s already present!" % repl_template)
-    repl_curr_diff = len(repl_template) - len(curr_template)
-    newtext_text_diff = len(newtext) - len(text)
-    if newtext_text_diff == repl_curr_diff:
-      pass
-    elif repl_curr_diff == 0:
-      if newtext_text_diff != 0:
-        pagemsg("WARNING: Something wrong, no change in text length during replacement but expected change: Expected length change=%s, actual=%s, curr=%s, repl=%s"
-            % (repl_curr_diff, newtext_text_diff, curr, repl))
-    else:
-      ratio = float(newtext_text_diff) / repl_curr_diff
-      if ratio == int(ratio):
-        pagemsg("WARNING: Replaced %s occurrences of curr=%s with repl=%s"
-            % (int(ratio), curr_template, repl_template))
+  changelogs = []
+  for repl_template, curr_template in repl_curr_changes:
+    repl_template = undo_slash_newline(repl_template)
+    curr_template = undo_slash_newline(curr_template)
+    found_repl_template = repl_template in text
+    newtext = text.replace(curr_template, repl_template)
+    if newtext == text:
+      if not found_repl_template:
+        pagemsg("WARNING: Unable to locate current template: %s (would replace with %s)" % (curr_template, repl_template))
       else:
-        pagemsg("WARNING: Something wrong, length mismatch during replacement: Expected length change=%s, actual=%s, ratio=%.2f, curr=%s, repl=%s"
-            % (repl_curr_diff, newtext_text_diff, ratio, curr_template,
-              repl_template))
-    changelog = args.full_comment or "replace <%s> with <%s> (%s)" % (truncate(curr_template),
-        truncate(repl_template), args.annotation)
-    pagemsg("Change log = %s" % changelog)
-  return newtext, changelog
+        pagemsg("Replacement template already found, taking no action")
+    else:
+      if found_repl_template:
+        pagemsg("WARNING: Made change, but replacement template %s already present!" % repl_template)
+      repl_curr_diff = len(repl_template) - len(curr_template)
+      newtext_text_diff = len(newtext) - len(text)
+      if newtext_text_diff == repl_curr_diff:
+        pass
+      elif repl_curr_diff == 0:
+        if newtext_text_diff != 0:
+          pagemsg("WARNING: Something wrong, no change in text length during replacement but expected change: Expected length change=%s, actual=%s, curr=%s, repl=%s"
+              % (repl_curr_diff, newtext_text_diff, curr, repl))
+      else:
+        ratio = float(newtext_text_diff) / repl_curr_diff
+        if ratio == int(ratio):
+          pagemsg("WARNING: Replaced %s occurrences of curr=%s with repl=%s"
+              % (int(ratio), curr_template, repl_template))
+        else:
+          pagemsg("WARNING: Something wrong, length mismatch during replacement: Expected length change=%s, actual=%s, ratio=%.2f, curr=%s, repl=%s"
+              % (repl_curr_diff, newtext_text_diff, ratio, curr_template,
+                repl_template))
+      changelog = "replace <%s> with <%s>" % (truncate(curr_template), truncate(repl_template))
+      pagemsg("Change log = %s" % changelog)
+      if args.include_what_changed:
+        changelogs.append(changelog)
+
+  if comment:
+    changelogs = [comment]
+  return newtext, changelogs
 
 def undo_slash_newline(txt):
   if not args.undo_slash_newline:
     return txt
   return blib.undo_escape_newline(txt)
 
-def push_manual_changes(save, verbose, diff, template_changes, start, end):
-  for index, (pagename, repl_template, curr_template) in blib.iter_items(template_changes, get_name = lambda x: x[0]):
-    page = pywikibot.Page(site, pagename)
-    if not page.exists():
-      msg("Page %s %s: WARNING, something wrong, does not exist" % (
-        index, pagename))
+def combine_notes_with_comment(notes):
+  if notes:
+    if args.comment:
+      # Wiktionary comments can be at most 500 bytes when encoded in UTF-8. If longer, they get truncated, which we
+      # don't want, because we want the comment at the end to display in full, so we need to do the truncation
+      # ourselves before adding the comment.
+      grouped_notes = "; ".join(blib.group_notes(notes))
+      comment_suffix = " (%s)" % args.comment
+      comment_suffix_len = len(comment_suffix.encode("utf-8"))
+      grouped_notes = truncate_bytes(grouped_notes, 500 - comment_suffix_len)
+      return grouped_notes + comment_suffix
     else:
-      def do_push_one_manual_change(page, index, text):
-        return push_one_manual_change(str(page.title()), index, text, undo_slash_newline(curr_template),
-                                      undo_slash_newline(repl_template))
-      blib.do_edit(page, index, do_push_one_manual_change, save=save,
-          verbose=verbose, diff=diff)
+      return notes
+  else:
+    return args.comment or "push manual changes"
 
 def process_text_on_page_pushing_manual_changes(index, pagetitle, text):
   global args
@@ -140,27 +167,24 @@ def process_text_on_page_pushing_manual_changes(index, pagetitle, text):
   if pagetitle not in direcfile_changes_dict:
     return
   notes = []
-  for repl_template, curr_template in direcfile_changes_dict[pagetitle]:
-    text, this_changelog = push_one_manual_change(pagetitle, index, text, curr_template, repl_template)
-    notes.append(this_changelog)
+  for repl_curr_changes, comment in direcfile_changes_dict[pagetitle]:
+    text, this_changelogs = push_one_set_of_manual_changes(pagetitle, index, text, repl_curr_changes, comment)
+    notes.extend(this_changelogs)
 
-  return text, notes
+  return text, combine_notes_with_comment(notes)
 
 params = blib.create_argparser("Push manual changes to Wiktionary",
   include_pagefile=True, include_stdin=True)
 params.add_argument("--direcfile", help="File containing templates to change, as output by various scripts with --from-to",
     required=True)
 params.add_argument("--undo-slash-newline", action="store_true", help=r"Undo replacement of newlines with \n")
-params.add_argument("--annotation", default="manually",
-    help="Comment in change log message used to indicate source of changes, in addition to a message about what exactly changed (default 'manually')")
-params.add_argument("--full-comment", help="Full comment of change log message")
+params.add_argument("--comment", help="Comment of change log message (included in addition to any comments embedded in the manual changes)")
+params.add_argument("--include-what-changed", action="store_true", help="If no comment embedded in manual changes, include what changed in the changelog")
 
 args = params.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 direcfile_changes = read_direcfile(args.direcfile, start, end)
 
-if args.stdin:
-  direcfile_changes_dict = template_changes_to_dict(direcfile_changes)
-  blib.do_pagefile_cats_refs(args, start, end, process_text_on_page_pushing_manual_changes, edit=True, stdin=True)
-else:
-  push_manual_changes(args.save, args.verbose, args.diff, direcfile_changes, start, end)
+direcfile_changes_dict = template_changes_to_dict(direcfile_changes)
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page_pushing_manual_changes, edit=True, stdin=True,
+                           default_pages=list(direcfile_changes_dict.keys()))
