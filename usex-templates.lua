4eeef68b4f7bdc7d26846c6643a62876096dc0ee
Ben Wing <ben@benwing.com>
2023-08-27 13:34:33 -0500

    usex-templates: support original text

--- usex-templates.lua
+++ usex-templates.lua
@@ -2,7 +2,7 @@ local export = {}
 
 local m_languages = require("Module:languages")
 local table_module = "Module:table"
-local usex_module = "Module:usex"
+local usex_module = "Module:User:Benwing2/usex"
 
 local rsplit = mw.text.split
 local rfind = mw.ustring.find
@@ -13,55 +13,86 @@ local function track(page)
 end
 
 function export.usex_t(frame)
+	-- Invocation arguments (passed in the template #invoke call).
+	local iparams = {
+		["quote"] = {},
+		["compat"] = {type = "boolean"},
+		["inline"] = {type = "boolean"},
+		["nocat"] = {type = "boolean"},
+		["class"] = {},
+	}
+
+	local iargs = require("Module:parameters").process(frame.args, iparams)
+	local compat = iargs.compat
+
+	-- Template (parent) arguments.
 	local params = {
+		-- Usex/quotation text parameters
 		[1] = {required = true},
 		[2] = {},
-		[3] = {},
-		
-		["inline"] = {type = "boolean"},
-		["noenum"] = {type = "boolean"},
-		["ref"] = {},
-		["lit"] = {},
-		["q"] = {list = true},
-		["qq"] = {list = true},
-		["sc"] = {},
-		["source"] = {},
-		["footer"] = {},
-		["subst"] = {},
-		["t"] = {alias_of = 3},
-		["translation"] = {alias_of = 3},
 		["tr"] = {},
 		["transliteration"] = {alias_of = "tr"},
 		["ts"] = {},
 		["transcription"] = {alias_of = "ts"},
+		["sc"] = {},
 		["norm"] = {},
 		["normalization"] = {alias_of = "norm"},
 		["normsc"] = {},
-		["nocat"] = {type = "boolean"},
+		["subst"] = {},
+		["q"] = {list = true},
+		["qq"] = {list = true},
+		["ref"] = {},
+
+		-- Translation of usex text
+		[3] = {},
+		["t"] = {alias_of = 3},
+		["translation"] = {alias_of = 3},
+		["lit"] = {},
+
+		-- Original text, if the usex/quotation is a translation
+		["orig"] = {},
+		["origlang"] = {},
+		["origtr"] = {},
+		["origts"] = {},
+		["origsc"] = {},
+		["orignorm"] = {},
+		["orignormsc"] = {},
+		["origsubst"] = {},
+		["origq"] = {list = true},
+		["origqq"] = {list = true},
+		["origref"] = {},
+
+		-- Citation-related parameters; for anything more complex, usex {{quote-*}}
+		["source"] = {},
+		["footer"] = {},
+
+		-- Formatting parameters
+		["inline"] = {type = "boolean"},
+		["noenum"] = {type = "boolean"},
 		["brackets"] = {type = "boolean"},
+
+		-- Categorization parameters
+		["nocat"] = {type = "boolean"},
 		["sort"] = {},
 	}
-	
-	local quote = (frame.args["quote"] or "") ~= ""
-	local compat = (frame.args["compat"] or "") ~= ""
-	local template_inline = (frame.args["inline"] or "") ~= ""
-	local template_nocat = (frame.args["nocat"] or "") ~= ""
-	local class = frame.args["class"]
-	
+
 	if compat then
 		params["lang"] = {required = true}
 		params["t"].alias_of = 2
 		params["translation"].alias_of = 2
 		table.remove(params, 1)
 	end
-	
+
 	local args = require("Module:parameters").process(frame:getParent().args, params)
-	
-	local lang = args[compat and "lang" or 1] or "und"
-	local sc = args["sc"]
+
+	local lang = m_languages.getByCode(args[langparam] or "und", langparam, "allow etym")
+	local sc = args.sc
 	sc = sc and require("Module:scripts").getByCode(sc, "sc") or nil
-	local normsc = args["normsc"]
+	local normsc = args.normsc
 	normsc = normsc == "auto" and normsc or normsc and require("Module:scripts").getByCode(normsc, "normsc") or nil
+	if normsc and not args.norm then
+		error("Cannot specify normsc= without norm=")
+	end
 
 	if #args.qq > 0 then
 		track("qq")
@@ -69,30 +100,69 @@ function export.usex_t(frame)
 	if #args.q > 0 then
 		track("q")
 	end
-	
+
+	local origlang, origsc, orignormsc
+	if args.orig then
+		origlang = m_languages.getByCode(args.origlang, "origlang", "allow etym")
+		origsc = args.origsc
+		origsc = origsc and require("Module:scripts").getByCode(origsc, "origsc") or nil
+		orignormsc = args.orignormsc
+		orignormsc = orignormsc == "auto" and orignormsc or
+			orignormsc and require("Module:scripts").getByCode(orignormsc, "normsc") or nil
+		if orignormsc and not args.orignorm then
+			error("Cannot specify orignormsc= without orignorm=")
+		end
+	else
+		for _, noparam in ipairs { "origlang", "origtr", "origts", "origsc", "orignorm", "orignormsc", "origsubst",
+			"origref" } do
+			if args[noparam] then
+				error(("Cannot specify %s= without orig="):format(noparam))
+			end
+		end
+		if #args.origq > 0 then
+			error("Cannot specify origq= without orig=")
+		end
+		if #args.origqq > 0 then
+			error("Cannot specify origqq= without orig=")
+		end
+	end
+
 	local data = {
-		lang = m_languages.getByCode(lang, compat and "lang" or 1),
+		lang = lang,
 		sc = sc,
 		normsc = normsc,
 		usex = args[compat and 1 or 2],
 		translation = args[compat and 2 or 3],
-		transliteration = args["tr"],
-		transcription = args["ts"],
-		normalization = args["norm"],
-		noenum = args["noenum"],
-		inline = args["inline"] or template_inline,
-		ref = args["ref"],
-		quote = quote,
-		lit = args["lit"],
-		substs = args["subst"],
+		transliteration = args.tr,
+		transcription = args.ts,
+		normalization = args.norm,
+		noenum = args.noenum,
+		inline = args.inline or iargs.inline,
+		ref = args.ref,
+		quote = iargs.quote,
+		lit = args.lit,
+		subst = args.subst,
 		-- FIXME, change to left and right qualifiers
 		qq = #args.qq > 0 and args.qq or args.q,
-		source = args["source"],
-		footer = args["footer"],
-		nocat = args["nocat"] or template_nocat,
-		brackets = args["brackets"],
-		sortkey = args["sort"],
-		class = class,
+		source = args.source,
+		footer = args.footer,
+		nocat = args.nocat or iargs.nocat,
+		brackets = args.brackets,
+		sortkey = args.sort,
+		class = iargs.class,
+
+		-- Original text, if the usex/quotation is a translation
+		orig = args.orig,
+		origlang = origlang,
+		origtr = args.origtr,
+		origts = args.origts,
+		origsc = origsc,
+		orignorm = args.orignorm,
+		orignormsc = orignormsc,
+		origsubst = args.origsubst,
+		origq = args.origq,
+		origqq = args.origqq,
+		origref = args.origref,
 	}
 	
 	return require(usex_module).format_usex(data)
