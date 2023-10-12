befa60cda2b8bf91d151c3c7b7234f69465f656f
Ben Wing <ben@benwing.com>
2023-10-11 19:36:59 -0500

    labels-ancillary: latest from Wiktionary (further generalize to include regional and plain categories; add a help message indicating how to add the label also for umbrella categories)

--- labels-ancillary.lua
+++ labels-ancillary.lua
@@ -1,29 +1,39 @@
 local export = {}
 
+-- This module contains ancillary functions for manipulating labels. Currently the supported functions are for finding
+-- the labels that generate a given category.
+
 local labels_module = "Module:labels"
 
 -- Find the labels matching category `cat` of type `cat_type` (currently supported values are "topic" for topic
--- categories and "pos" for POS categories) for language `lang` (which may be nil for umbrella categories). The
--- format of `cat` depends on `cat_type`; for topic categories it should be e.g. "water" or "Water" (either form
--- works), and for POS categories it should be e.g. "attenuative verbs".
+-- categories, e.g. 'en:Water'; "pos" for POS categories, e.g. 'English attenuative verbs'; "regional" for regional
+-- categories, e.g. 'Ghanaian English; and "plain" for plain categories, e.g. 'Issime Walser') for language `lang`
+-- (which may be nil for umbrella or plain categories). The format of `cat` depends on `cat_type`, but in general is
+-- the portion of the category minus the language prefix or suffix. For topic categories it should be e.g. "water" or
+-- "Water" (either form works); for POS categories it should be e.g. "attenuative verbs"; for regional categories it
+-- should be e.g. "Ghanaian"; and for plain categories it should be e.g. "Issime Walser" (the actual category name).
 function export.find_labels_for_category(cat, cat_type, lang)
 	local function ucfirst(txt)
 		return mw.getContentLanguage():ucfirst(txt)
 	end
-	if cat_type == "topic" then
+	if cat_type ~= "pos" then
 		cat = ucfirst(cat)
 	end
 
 	local function should_error_on_cat(cat)
 		if cat_type == "topic" then
 			return cat:find("^[Aa]ll ")
-		else
+		elseif cat_type == "pos" then
 			return cat == "lemmas"
+		elseif cat_type == "regional" then
+			return "British English" -- an arbitrary known regional category
+		else
+			return "Issime Walser" -- an arbitrary known plain category
 		end
 	end
 
 	local function labcat_matches(labcat)
-		if cat_type == "topic" then
+		if cat_type ~= "pos" then
 			return ucfirst(labcat) == cat
 		else
 			return labcat == cat
@@ -33,8 +43,12 @@ function export.find_labels_for_category(cat, cat_type, lang)
 	local function fetch_labdata_cats(labdata)
 		if cat_type == "topic" then
 			return labdata.topical_categories
-		else
+		elseif cat_type == "pos" then
 			return labdata.pos_categories
+		elseif cat_type == "regional" then
+			return labdata.regional_categories
+		else
+			return labdata.plain_categories
 		end
 	end
 
@@ -113,13 +127,14 @@ function export.find_labels_for_category(cat, cat_type, lang)
 	return cat_labels_found
 end
 
-function export.get_labels_categorizing(cat, cat_type, lang)
-	local cat_producing_labels = export.find_labels_for_category(cat, cat_type, lang)
+-- Format the labels that categorize into some category for display in the text for that category. `lang` is the
+-- language of the category, or nil. Returns nil if there are no labels.
+function export.format_labels_categorizing(labels, lang)
 	local function make_code(txt)
 		return ("<code>%s</code>"):format(txt)
 	end
 	local formatted_labels = {}
-	for label, aliases in pairs(cat_producing_labels) do
+	for label, aliases in pairs(labels) do
 		if #aliases == 0 then
 			table.insert(formatted_labels, make_code(label))
 		elseif #aliases == 1 then
@@ -140,9 +155,19 @@ function export.get_labels_categorizing(cat, cat_type, lang)
 		if lang then
 			retval = retval .. (" To generate this category using a label, use {{tl|lb|%s|<var>label</var>}}."):format(
 				lang:getCode())
+		else
+			retval = retval .. " To generate this category using a label, use {{tl|lb|<var>langcode</var>|<var>label</var>}}, " ..
+				"where <code><var>langcode</var></code> is the appropriate language code for the language in question " ..
+				"(see [[Wiktionary:List of languages]])."
 		end
 		return retval
 	end
+end
+
+-- Generate a message indicating what labels categorize into the specified category `cat` of type `cat_type`, for
+-- language `lang` (a language object, may be nil). Parameters are the same as for find_labels_for_category() above.
+function export.get_labels_categorizing(cat, cat_type, lang)
+	return export.format_labels_categorizing(export.find_labels_for_category(cat, cat_type, lang), lang)
 end
 
 return export
