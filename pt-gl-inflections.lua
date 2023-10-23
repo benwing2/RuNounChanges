120fa9dd8c7c74ed8a275577edd2f42e4c37b065
Ben Wing <ben@benwing.com>
2023-10-22 23:46:28 -0500

    pt-inflections -> pt-gl-inflections and generalize for Galician (standard and reintegrationist) (latest from Wiktionary)

--- pt-gl-inflections.lua
+++ pt-gl-inflections.lua
@@ -4,30 +4,85 @@ local export = {}
 
 Authorship: Ben Wing <benwing2>
 
-This module implements {{pt-verb form of}}, which automatically generates the appropriate inflections of a non-lemma
-verb form given the form and the conjugation spec for the verb (which is usually just the infinitive; see {{pt-conj}}
-and {{pt-verb}}).
+This module implements {{pt-verb form of}}, {{gl-verb form of}} and {{gl-reinteg-verb form of}}, which automatically
+generate the appropriate inflections of a non-lemma verb form given the form and the conjugation spec for the verb
+(which is usually just the infinitive; see {{pt-conj}}/{{pt-verb}}, {{gl-conj}}/{{gl-verb}} and {{gl-reinteg-conj}}/
+{{gl-reinteg-verb}}).
 ]=]
 
 local m_links = require("Module:links")
 local m_table = require("Module:table")
 local m_form_of = require("Module:form of")
-local m_pt_verb = require("Module:pt-verb")
 local accel_module = "Module:accel"
-
-local lang = require("Module:languages").getByCode("pt")
+local pt_verb_module = "Module:pt-verb"
+local gl_verb_module = "Module:gl-verb"
+local gl_reinteg_verb_module = "Module:gl-reinteg-verb"
 
 local rmatch = mw.ustring.match
 local rfind = mw.ustring.find
 local rsplit = mw.text.split
 local usub = mw.ustring.sub
 
+local function pt_extract_labels(formobj)
+	local labels = {}
+	local form = formobj.form
+	local footnotes = formobj.footnotes
+	local m_pt_verb = require(pt_verb_module)
+	if not not form:find(m_pt_verb.VAR_BR) or footnotes and m_table.contains(footnotes, "[Brazil]") or
+		footnotes and m_table.contains(footnotes, "[Brazil only]") then
+		table.insert(labels, "Brazilian Portuguese verb form")
+	end
+	if not not form:find(m_pt_verb.VAR_PT) or footnotes and m_table.contains(footnotes, "[Portugal]") or
+		footnotes and m_table.contains(footnotes, "[Portugal only]") then
+		table.insert(labels, "European Portuguese verb form")
+	end
+	if not not form:find(m_pt_verb.VAR_SUPERSEDED) or footnotes and m_table.contains(footnotes, "[superseded]") then
+		table.insert(labels, "superseded")
+	end
+	return labels
+end
+
+local function gl_reinteg_extract_labels(formobj)
+	local labels = {"[[Appendix:Reintegrationism|reintegrationist]] norm"}
+	local form = formobj.form
+	local footnotes = formobj.footnotes
+	local m_gl_reinteg_verb = require(gl_reinteg_verb_module)
+	if not not form:find(m_gl_reinteg_verb.ALTVAR) or footnotes and m_table.contains(footnotes, "[less common]") then
+		table.insert(labels, "less common")
+	end
+	return labels
+end
+
+local var_properties = {
+	pt = {
+		verb_module = pt_verb_module,
+		lang = require("Module:languages").getByCode("pt"),
+		langcode = "pt",
+		extract_labels = pt_extract_labels,
+		remove_variant_codes = true,
+	},
+	gl = {
+		verb_module = gl_verb_module,
+		lang = require("Module:languages").getByCode("gl"),
+		langcode = "gl",
+		remove_variant_codes = false,
+	},
+	["gl-reinteg"] = {
+		verb_module = gl_reinteg_verb_module,
+		lang = require("Module:languages").getByCode("gl"),
+		langcode = "gl",
+		extract_labels = gl_reinteg_extract_labels,
+		remove_variant_codes = true,
+	},
+}
+
 local function track(page)
-	require("Module:debug/track")("pt-inflections/" .. page)
+	require("Module:debug/track")("pt-gl-inflections/" .. page)
 	return true
 end
 
-local function generate_inflection_of(tag_sets, lemma, pretext, args)
+local function generate_inflection_of(tag_sets, lemma, pretext, args, varcode)
+	local lang = var_properties[varcode].lang
 	for _, tag_set in ipairs(tag_sets) do
 		tag_set.tags = rsplit(tag_set.tag, "|")
 		tag_set.tag = nil
@@ -35,7 +90,7 @@ local function generate_inflection_of(tag_sets, lemma, pretext, args)
 	local has_multiple_tag_sets = #tag_sets > 1
 
 	-- If only one tag, extract out the "combined with ..." text and move into posttext=, which goes after the lemma.
-	-- FIXME: No support for clitic combinations currently for Portuguese, although maybe we should add it.
+	-- FIXME: No support for clitic combinations currently for Portuguese or Galician, although maybe we should add it.
 	local posttext
 	if #tag_sets == 1 then
 		local tags = tag_sets[1].tags
@@ -78,27 +133,36 @@ local function generate_inflection_of(tag_sets, lemma, pretext, args)
 	}
 end
 
-local function extract_labels(formobj)
-	local labels = {}
-	local form = formobj.form
-	local footnotes = formobj.footnotes
-	if not not form:find(m_pt_verb.VAR_BR) or footnotes and m_table.contains(footnotes, "[Brazil]") or
-		footnotes and m_table.contains(footnotes, "[Brazil only]") then
-		table.insert(labels, "Brazilian Portuguese verb form")
-	end
-	if not not form:find(m_pt_verb.VAR_PT) or footnotes and m_table.contains(footnotes, "[Portugal]") or
-		footnotes and m_table.contains(footnotes, "[Portugal only]") then
-		table.insert(labels, "European Portuguese verb form")
-	end
-	if not not form:find(m_pt_verb.VAR_SUPERSEDED) or footnotes and m_table.contains(footnotes, "[superseded]") then
-		table.insert(labels, "superseded")
-	end
-	return labels
-end
-
 function export.verb_form_of(frame)
+	local varcode = frame.args.varcode
+	if not varcode then
+		error("Frame (invocation) argument varcode= must be specified")
+	end
+	if not var_properties[varcode] then
+		local valid_varcodes = {}
+		for k, v in pairs(var_properties) do
+			table.insert(valid_varcodes, k)
+		end
+		table.sort(valid_varcodes)
+		error("Unrecognized value for varcode=; possible values are " .. table.concat(valid_varcodes, ", "))
+	end
 	local parargs = frame:getParent().args
-	local alternant_multiword_spec = m_pt_verb.do_generate_forms(parargs, false, "from verb form of")
+	local m_verb_module = require(var_properties[varcode].verb_module)
+	local function remove_variant_codes(form)
+		if var_properties[varcode].remove_variant_codes then
+			return m_verb_module.remove_variant_codes(form)
+		else
+			return form
+		end
+	end
+	local function extract_labels(formobj)
+		if var_properties[varcode].extract_labels then
+			return var_properties[varcode].extract_labels(formobj)
+		else
+			return {}
+		end
+	end
+	local alternant_multiword_spec = m_verb_module.do_generate_forms(parargs, false, "from verb form of")
 	local non_lemma_form = alternant_multiword_spec.verb_form_of_form
 
 	local lemmas = {}
@@ -126,8 +190,7 @@ function export.verb_form_of(frame)
 				if forms then
 					for _, formobj in ipairs(forms) do
 						local labels = extract_labels(formobj)
-						local form =
-							m_pt_verb.remove_variant_codes(m_links.remove_links(mw.ustring.toNFC(formobj.form)))
+						local form = remove_variant_codes(m_links.remove_links(mw.ustring.toNFC(formobj.form)))
 						if non_lemma_form == form then
 							if (not slot_restrictions or slot_restrictions[slot]) then
 								m_table.insertIfNot(tags, {tag = accel, labels = labels})
@@ -163,7 +226,7 @@ function export.verb_form_of(frame)
 		check_slot_restrictions_against_slots_seen()
 	end
 	if #tags > 0 then
-		return generate_inflection_of(tags, lemma, nil, alternant_multiword_spec.args)
+		return generate_inflection_of(tags, lemma, nil, alternant_multiword_spec.args, varcode)
 	end
 
 	-- If we don't find any matches, we try again, looking for non-reflexive forms of reflexive-only verbs.
@@ -182,12 +245,12 @@ function export.verb_form_of(frame)
 				-- and accumulate properties into refl_forms_to_tags.
 				for i, part_form in ipairs(part_forms) do
 					local labels = extract_labels(part_form)
-					local form = m_pt_verb.remove_variant_codes(m_links.remove_links(mw.ustring.toNFC(part_form.form)))
+					local form = remove_variant_codes(m_links.remove_links(mw.ustring.toNFC(part_form.form)))
 					if non_lemma_form == form then
 						if (not slot_restrictions or slot_restrictions[slot]) then
 							local saw_existing = false
 							for _, refl_form_to_tags in ipairs(refl_forms_to_tags) do
-								if refl_form_to_tags.form == m_pt_verb.remove_variant_codes(full_forms[i].form) then
+								if refl_form_to_tags.form == remove_variant_codes(full_forms[i].form) then
 									table.insert(refl_form_to_tags.tags, {tag = accel, labels = labels})
 									saw_existing = true
 									break
@@ -195,9 +258,9 @@ function export.verb_form_of(frame)
 							end
 							if not saw_existing then
 								table.insert(refl_forms_to_tags, {
-									form = m_pt_verb.remove_variant_codes(full_forms[i].form),
+									form = remove_variant_codes(full_forms[i].form),
 									tags = {{tag = accel, labels = labels}},
-									variant = variant_forms and m_pt_verb.remove_variant_codes(variant_forms[i].form),
+									variant = variant_forms and remove_variant_codes(variant_forms[i].form),
 								})
 							end
 						end
@@ -214,21 +277,22 @@ function export.verb_form_of(frame)
 		end
 		if #refl_forms_to_tags > 0 then
 			local parts = {}
+			local langcode = var_properties[varcode].langcode
 			for _, refl_form_to_tags in ipairs(refl_forms_to_tags) do
 				local only_used_in
 				if refl_form_to_tags.variant then
 					only_used_in =
-						frame:preprocess(("{{only used in|pt|%s|nocap=1}}, <span class='use-with-mention'>syntactic variant of {{m|pt|%s}}</span>"):format(
-							refl_form_to_tags.variant, refl_form_to_tags.form))
+						frame:preprocess(("{{only used in|%s|%s|nocap=1}}, <span class='use-with-mention'>syntactic variant of {{m|%s|%s}}</span>"):format(
+							langcode, refl_form_to_tags.variant, langcode, refl_form_to_tags.form))
 				else
 					only_used_in =
-						frame:preprocess(("{{only used in|pt|%s|nocap=1}}"):format(refl_form_to_tags.form))
+						frame:preprocess(("{{only used in|%s|%s|nocap=1}}"):format(langcode, refl_form_to_tags.form))
 				end
 				if refl_form_to_tags.form == lemma then
 					table.insert(parts, only_used_in)
 				else
 					local infl = generate_inflection_of(refl_form_to_tags.tags, lemma, only_used_in .. ", ",
-						alternant_multiword_spec.args)
+						alternant_multiword_spec.args, varcode)
 					table.insert(parts, infl)
 				end
 			end
