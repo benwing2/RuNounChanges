8ce7c2707c69850b3c586f953a9c1864abb06eca
Ben Wing <ben@benwing.com>
2023-12-14 04:18:20 -0600

    extract_pagename.sh: allow page numbers with periods in them

--- extract_pagename.sh
+++ extract_pagename.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-perl -pe 's/^.*?:Page /Page /' "$@" | grep '^Page ' |perl -pe 's/^Page [0-9]+ (.*?): .*$/$1/'|uniq
+perl -pe 's/^.*?:Page /Page /' "$@" | grep '^Page ' |perl -pe 's/^Page [0-9.]+ (.*?): .*$/$1/'|uniq
