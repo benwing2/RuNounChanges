0525983111af22d38bed1657589ed5c30b16af03
Ben Wing <ben@benwing.com>
2024-03-09 05:22:31 -0600

    module-categorization: latest from Wiktionary

--- module-categorization.lua
+++ module-categorization.lua
@@ -1,20 +1,32 @@
 local export = {}
 
-local m_table = require("Module:table")
-
 local rsplit = mw.text.split
+local rfind = mw.ustring.find
 
 local keyword_to_module_type = {
-	common = "Utility",
-	utilities = "Utility",
+	common = "Language-specific utility",
+	utilities = "Language-specific utility",
 	headword = "Headword-line",
 	translit = "Transliteration",
+	infl = "Inflection",
+	inflection = "Inflection",
 	decl = "Inflection",
+	declension = "Inflection",
+	adecl = "Inflection",
 	conj = "Inflection",
+	conjugation = "Inflection",
 	noun = "Inflection",
+	nouns = "Inflection",
+	pronoun = "Inflection",
+	pronouns = "Inflection",
 	verb = "Inflection",
+	verbs = "Inflection",
 	adjective = "Inflection",
+	adjectives = "Inflection",
+	adj = "Inflection",
 	nominal = "Inflection",
+	nominals = "Inflection",
+	pron = "Pronunciation",
 	pronun = "Pronunciation",
 	pronunc = "Pronunciation",
 	pronunciation = "Pronunciation",
@@ -29,16 +41,16 @@ local module_type_generates_lang_specific_cat = {
 	["Inflection"] = true,
 	["Data"] = true,
 	["Testcase"] = true,
-	["Pronunciation testcase"] = true,
-	["Transliteration testcase"] = true,
 }
 
 -- If a module type is here, we will generate a lang-specific module-type category such as
--- [[:Category:Pali inflection modules]].
-local module_type_script_specific = {
-	["Transliteration"] = true,
-	["Entry name-generating"] = true,
-	["Sortkey-generating"] = true,
+-- [[:Category:Pali inflection modules]]. The value is a module that returns a function that fetches all the
+-- languages that use a given module for transliteration/entry-name generation/sortkey generation.
+local languages_from_module_name = {
+	["Transliteration"] = "Module:languages/byTranslitModule",
+	["Transliteration testcase"] = "Module:languages/byTranslitModule",
+	["Entry name-generating"] = "Module:languages/byEntryNameModule",
+	["Sortkey-generating"] = "Module:languages/bySortkeyModule",
 }
 
 local module_type_patterns = {
@@ -46,7 +58,7 @@ local module_type_patterns = {
 	{"/testcases%f[-/%z]", function(typ)
 		if typ == "Pronunciation" then
 			return "Pronunciation testcase"
-		elseif type == "Transliteration" then
+		elseif typ == "Transliteration" then
 			return "Transliteration testcase"
 		else
 			return "Testcase"
@@ -54,44 +66,81 @@ local module_type_patterns = {
 	end},
 }
 
--- return_raw set to true makes function return table of categories with
--- "[[Category:" and "]]" stripped away. It is used by [[Module:documentation]].
-function export.categorize(frame, return_raw)
-	local title = mw.title.getCurrentTitle()
-	local subpage = title.subpageText
+local function get_lang_or_script(code)
+	return code == "-" and code or
+		require("Module:languages").getByCode(code, nil, "allow etym") or
+		require("Module:languages").getByCode(code .. "-pro", nil, "allow etym") or
+		require("Module:scripts").getByCode(code)
+end
 
-	-- To ensure no categories are added on documentation pages.
-	if subpage == "documentation" then
-		return ""
+local function obj_code(obj)
+	if obj == "-" then
+		return obj
+	end
+	return obj:getCode()
+end
+
+local function infer_lang_or_script_code(name)
+	local hyphen_parts = rsplit(name, "%-")
+	for i = #hyphen_parts - 1, 1, -1 do
+		local code = table.concat(hyphen_parts, "-", 1, i)
+		local obj = get_lang_or_script(code)
+		if obj then
+			local rest = table.concat(hyphen_parts, "-", i + 1)
+			return obj, rest
+		end
+	end
+	return nil, nil
+end
+
+local function infer_lang_and_script_codes(name)
+	local objs = {}
+	while true do
+		local obj, rest = infer_lang_or_script_code(name)
+		if not obj then
+			return objs, name
+		end
+		if #objs > 0 and obj:getCode() == "to" then
+			-- skip 'to' in e.g. [[Module:ks-Arab-to-Deva-translit]]; it's not Tongan
+		else
+			table.insert(objs, obj)
+		end
+		name = rest
+	end
+end
+
+--[==[
+Main entry point. Can be called from Lua or another module.
+
+`return_raw` set to true makes function return a table of categories with {"[[Category:"} and {"]]"}
+stripped away. It is used by [[Module:documentation]].
+]==]
+function export.categorize(frame, return_raw, noerror)
+	local categories = {}
+
+	local function insert_cat(cat, sortkey)
+		for _, existing_cat in ipairs(categories) do
+			if existing_cat.name == cat then
+				return
+			end
+		end
+		table.insert(categories, {name = cat, sort = sortkey})
 	end
 
-	local output, categories = {}, {}
-	local namespace = title.nsText
-	local pagename, mode
+	local pagename
 
 	if frame.args[1] then
 		pagename = frame.args[1]
-		pagename = pagename:gsub("^Module:", "")
-		mode = "testing"
-		mw.log("arg", pagename)
-	else
-		if namespace ~= "Module" then
-			error("This template should only be used in the Module namespace.")
-		end
-
-		pagename = title.text
-
-		if subpage ~= pagename then
-			pagename = title.rootText
-		end
 	end
 
 	local args
 	if frame.args.is_template then
 		local params = {
 			[1] = {}, -- comma-separated list of languages; by default, inferred from module name
-			[2] = {}, -- FIXME: used in several modules saying e.g. "per the Paiboon scheme"; ignored
 			["type"] = {},
+			[2] = {alias_of = "type"},
+			["pagename"] = {}, -- for testing
+			["return_cats"] = {type = "boolean"}, -- for testing
 		}
 
 		local parent_args = frame:getParent().args
@@ -100,137 +149,227 @@ function export.categorize(frame, return_raw)
 		args = {}
 	end
 
-	--[[
-		If this is a transliteration, entry name-generating or sortkey-generating module, then parameter 1 is used as the code rather than the code in the page title.
-	]]
-	local inferred_code, module_type_keyword = pagename:match("([-%a]+)[- ]([^/]+)%f[/%z]")
+	pagename = pagename or args.pagename
+	local title
+	if pagename then
+		title = mw.title.new(pagename)
+	else
+		title = mw.title.getCurrentTitle()
+		-- Fuckme, sometimes this function is called with a faked frame and a title with the namespace already chopped out,
+		-- so this test cannot be done in that case.
+		if title.nsText ~= "Module" then
+			error(("This template should only be used in the Module namespace, not on page '%s'."):format(title.fullText))
+		end
+		pagename = title.fullText
+	end
+	
+	local subpage = title.subpageText
 
-	if not inferred_code then
-		error(("Could not infer language/script code and module type from page name '%s'"):format(pagename))
+	local null_return_value = return_raw and {} or ""
+
+	-- To ensure no categories are added on documentation pages.
+	if subpage == "documentation" then
+		return null_return_value
 	end
 
-	if subpage == "sandbox" then
-		table.insert(categories, "Sandbox modules")
+	local root_pagename
+	if subpage ~= pagename then
+		root_pagename = title.rootText
 	else
-		local module_type = args.type or keyword_to_module_type[module_type_keyword]
-		local langcodes = rsplit(args[1] or inferred_code, ",")
-		for _, code in ipairs(langcodes) do
-			local lang, sc
-			local origcode = code
+		root_pagename = pagename
+	end
+	root_pagename = root_pagename:gsub("^Module:", "")
 
-			if module_type then
-
-				local getByCode = require("Module:languages").getByCode
-
-				for stage=1,2 do
-					lang = getByCode(code, nil, "allow etym") or getByCode(code .. "-pro", nil, "allow etym")
-
-					if module_type_script_specific[module_type] then
-						if not lang then
-							sc = require("Module:scripts").getByCode(code)
-
-							if sc then
-								table.insert(categories, module_type .. " modules by script|" .. sc:getCanonicalName())
-							end
-						end
-					end
-
-					if lang or sc then
-						break
-					end
-
-					-- Some modules have names like [[Module:bho-Kthi-translit]] or
-					-- [[Module:Deva-Kthi-translit]]. If we didn't recognize the code the
-					-- first time, try chopping off the attached script and try again.
-					code = code:gsub("%-[A-Z].*", "")
-				end
-
-				if not (sc or lang) then
-					if module_type_script_specific[module_type] then
-						error('The language or script code "' .. origcode ..
-							'" in the page title is not recognized by [[Module:languages]] or [[Module:scripts]].')
-					else
-						error('The language code "' .. origcode ..
-							'" in the page title is not recognized by [[Module:languages]].')
-					end
-				end
-
-				local function insert_overall_cat(lang, sortkey)
-					m_table.insertIfNot(categories, lang:getNonEtymologicalName() .. " modules|" .. sortkey)
-				end
-
-				local function insert_overall_module_type_cat(module_type, sortkey)
-					m_table.insertIfNot(categories, module_type .. " modules|" .. sortkey)
-				end
-
-				local function insert_module_type_cat(lang, module_type)
-					insert_overall_module_type_cat(module_type, lang:getCanonicalName())
-					if module_type_generates_lang_specific_cat[module_type] then
-						m_table.insertIfNot(categories, lang:getNonEtymologicalName() .. " " ..
-							mw.getContentLanguage():lcfirst(module_type) .. " modules")
-					end
-				end
-
-				if module_type_script_specific[module_type] then
-					local langs
-					if module_type == "Transliteration" then
-						langs = require("Module:languages/byTranslitModule")(pagename)
-					elseif module_type == "Entry name-generating" then
-						langs = require("Module:languages/byEntryNameModule")(pagename)
-					elseif module_type == "Sortkey-generating" then
-						langs = require("Module:languages/bySortkeyModule")(pagename)
-					end
-
-					local sortkey = module_type
-
-					if sc then
-						sortkey = sortkey .. ", " .. sc:getCanonicalName()
-					end
-
-					if langs[1] then
-						for _, lang in ipairs(langs) do
-							insert_overall_cat(lang, sortkey)
-						end
-					elseif lang then
-						insert_overall_cat(lang, sortkey)
-					end
-
-					if sc then
-						insert_overall_module_type_cat(module_type, sc:getCanonicalName())
-					else
-						insert_module_type_cat(lang, module_type)
-					end
-				else
-					insert_overall_cat(lang, module_type)
-					insert_module_type_cat(lang, module_type, lang:getCanonicalName())
-				end
+	-- Take the module type(s) from type= if given, or infer from the pagename.
+	local module_types
+	if args.type then
+		module_types = rsplit(args.type, ",")
+	else
+		local module_type_keyword = root_pagename:match("[-%a]+[- ]([^/]+)%f[/%z]")
+		if not module_type_keyword then
+			if noerror then
+				return null_return_value
 			else
-				error('The module-type keyword "' .. module_type_keyword .. '" was not recognized.')
+				error(("Could not extract module type from root pagename '%s'"):format(root_pagename))
+			end
+		end
+		local module_type = keyword_to_module_type[module_type_keyword]
+		if not module_type then
+			if noerror then
+				return null_return_value
+			else
+				error(("Did not recognize inferred module-type keyword '%s' from root pagename '%s'"):format(
+					module_type_keyword, root_pagename))
+			end
+		end
+		module_types = {module_type}
+	end
+
+	-- Look for additional module type(s) inferred by pattern.
+	for _, pattern_spec in ipairs(module_type_patterns) do
+		local pattern, inferred_type = unpack(pattern_spec)
+		if rfind(pagename, pattern) then
+			local insertIfNot = require("Module:table").insertIfNot
+			if type(inferred_type) == "string" then
+				insertIfNot(module_types, inferred_type)
+			else
+				local addl_types = {}
+				for _, typ in ipairs(module_types) do
+					table.insert(addl_types, inferred_type(typ))
+				end
+				for _, typ in ipairs(addl_types) do
+					insertIfNot(module_types, typ)
+				end
 			end
 		end
 	end
 
-	if return_raw then
+	-- If 1= specified, take the languages/scripts directly from there. Otherwise, (a) try to extract one or more
+	-- languages/scripts from the pagename (e.g. [[Module:uk-be-headword]] -> Ukrainian and Belarusian (languages);
+	-- [[Module:bho-Kthi-translit]] -> Bhojpuri (language) and Kaithi (script); [[Module:Deva-Kthi-translit]] ->
+	-- Devanagari and Kaithi (scripts)); and (b) if the specified or inferred module type(s) contain a type listed in
+	-- languages_from_module_name[], use the function referenced there to extract additional languages (i.e. all the
+	-- languages that use the module we are processing).
+	local inferred_objs
+	if args[1] then
+		inferred_objs = {}
+		for _, code in ipairs(rsplit(args[1], ",")) do
+			-- We need to have an indicator of families because we allow bare family codes to stand for proto-languages.
+			if code:find("^fam:") then
+				code = code:gsub("^fam:", "")
+				local family = require("Module:families").getByCode(code) or
+					error(("Unrecognized family code '%s' in [[Module:module categorization]]"):format(code))
+				local descendants = family:getDescendantCodes()
+				for _, desc in ipairs(descendants) do
+					local obj = get_lang_or_script(desc)
+					if obj then
+						-- make sure we skip families without proto-languages
+						table.insert(inferred_objs, obj)
+					end
+				end
+			else
+				local obj = get_lang_or_script(code)
+				if not obj then
+					error(("Unrecognized language or script code '%s'"):format(code))
+				end
+				table.insert(inferred_objs, obj)
+			end
+		end
+	else
+		inferred_objs = infer_lang_and_script_codes(root_pagename)
+		local function insert_obj_if_not(obj)
+			local saw_obj = false
+			for _, existing_obj in ipairs(inferred_objs) do
+				if obj_code(obj) == obj_code(existing_obj) then
+					saw_obj = true
+					break
+				end
+			end
+			if not saw_obj then
+				table.insert(inferred_objs, obj)
+			end
+		end
+
+		for _, module_type in ipairs(module_types) do
+			local languages_extractor = languages_from_module_name[module_type]
+			if languages_extractor then
+				local langs = require(languages_extractor)(root_pagename)
+				if langs then
+					for _, obj in ipairs(langs) do
+						insert_obj_if_not(obj)
+					end
+				end
+			end
+		end
+
+		if #inferred_objs == 0 then
+			if noerror then
+				return null_return_value
+			else
+				error(("Could not infer any languages or scripts from root pagename '%s'"):format(root_pagename))
+			end
+		end
+	end
+
+	if pagename:find("^Module:User:") then
+		insert_cat("User sandbox modules")
+	elseif pagename:find("/sandbox") then
+		insert_cat("Sandbox modules")
+	else
+		for _, module_type in ipairs(module_types) do
+			for _, obj in ipairs(inferred_objs) do
+				local function insert_overall_module_type_cat(module_type, sortkey)
+					if module_type ~= "-" then
+						insert_cat(module_type .. " modules", sortkey)
+					end
+				end
+
+				if obj == "-" then
+					insert_overall_module_type_cat(module_type)
+				else
+					if obj:hasType("script") then
+						insert_cat(module_type .. " modules by script", obj:getCanonicalName())
+					end
+	
+					local function construct_lang_or_sc_cat(obj, suffix)
+						local prefix
+						if obj:hasType("language") then
+							prefix = obj:getNonEtymologicalName()
+						else
+							prefix = obj:getCategoryName()
+						end
+						return prefix .. " " .. suffix
+					end
+
+					insert_cat(construct_lang_or_sc_cat(obj, "modules"), module_type)
+					insert_overall_module_type_cat(module_type, obj:getCanonicalName())
+					if module_type_generates_lang_specific_cat[module_type] then
+						insert_cat(construct_lang_or_sc_cat(obj, mw.getContentLanguage():lcfirst(module_type) .. " modules"))
+					end
+				end
+			end
+		end
+	end
+
+	for i, catspec in ipairs(categories) do
+		if catspec.sort then
+			categories[i] = ("%s|%s"):format(catspec.name, catspec.sort)
+		else
+			categories[i] = catspec.name
+		end
+	end
+
+	if args.return_cats then
+		return table.concat(categories, ",")
+	elseif return_raw then
 		return categories
 	else
 		for i, cat in ipairs(categories) do
 			categories[i] = "[[Category:" .. cat .. "]]"
 		end
-		categories = table.concat(categories)
+		return table.concat(categories)
 	end
+end
 
-	if testing then
-		table.insert(output, pagename)
-
-		if categories == "" then
-			categories = '<span class="error">failed to generate categories for ' .. pagename .. '</span>'
-		else
-			categories = mw.ustring.gsub(categories, "%]%]%[%[", "]]\n[[")
-			categories = frame:extensionTag{ name = "syntaxhighlight", content = categories }
-		end
+--[==[Table used in the documentation to {{tl|module cat}}.]==]
+function export.keyword_to_module_type_table()
+	local parts = {}
+	local function ins(text)
+		table.insert(parts, text)
 	end
-
-	return table.concat(output) .. categories
+	ins('{|class="wikitable"')
+	ins("! Keyword !! Inferred module type")
+	local keywords = {}
+	for k, v in pairs(keyword_to_module_type) do
+		table.insert(keywords, k)
+	end
+	table.sort(keywords)
+	for _, keyword in ipairs(keywords) do
+		ins("|-")
+		ins(("| <code>%s</code> || <code>%s</code>"):format(keyword, keyword_to_module_type[keyword]))
+	end
+	ins("|}")
+	return table.concat(parts, "\n")
 end
 
 return export
