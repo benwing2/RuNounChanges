2a5d7c2a9632823a981c51b2abaaf805723cc13e
Ben Wing <ben@benwing.com>
2024-03-19 06:12:16 -0500

    labels-data-lang-zh.lua: latest from Wiktionary

---
+++ labels-data-lang-zh.lua
@@ -0,0 +1,503 @@
+local labels = {}
+
+labels["American (&ndash;1980)"] = {
+	aliases = {"America 1", "United States 1", "USA 1", "US 1"},
+	Wikipedia = "History of Chinese Americans",
+	regional_categories = true,
+}
+
+labels["American (1980&ndash;)"] = {
+	aliases = {"America 2", "United States 2", "USA 2", "US 2"},
+	Wikipedia = "History of Chinese Americans",
+	regional_categories = true,
+}
+
+labels["Beijing"] = {
+	aliases = {"Peking", "Pekingese", "Beijing Mandarin"},
+	Wikipedia = "Beijing dialect",
+	plain_categories = "Beijing Mandarin",
+}
+
+labels["Cantonese"] = {
+	Wikipedia = true,
+	regional_categories = true,
+}
+
+labels["dialectal Cantonese"] = {
+	display = "dialectal [[w:Cantonese|Cantonese]]",
+	regional_categories = "Cantonese",
+}
+
+labels["Central Min"] = {
+	aliases = {"Min Zhong"},
+	Wikipedia = true,
+	regional_categories = true,
+}
+
+labels["Central Plains Mandarin"] = {
+	aliases = {"Zhongyuan Mandarin"},
+	Wikipedia = true,
+	plain_categories = true,
+}
+
+labels["Changzhounese"] = {
+	aliases = {"Changzhou Wu", "Changzhounese Wu"},
+	Wikipedia = "Changzhou dialect",
+	plain_categories = true,
+}
+
+labels["Chuzhou Wu"] = {
+	aliases = {"Lishuinese", "Fujian Wu", "Lishui Wu"},
+	Wikipedia = "Lishui dialect",
+	plain_categories = true,
+}
+
+labels["Coastal Min"] = {
+	aliases = {"coastal Min"},
+	Wikipedia = true,
+}
+
+labels["Datian Min"] = {
+	Wikipedia = true,
+	regional_categories = true,
+}
+
+labels["Dungan"] = {
+	Wikipedia = "Dungan language",
+	regional_categories = true,
+}
+
+labels["Eastern Min"] = {
+	aliases = {"Min Dong"},
+	Wikipedia = true,
+	regional_categories = true,
+}
+
+labels["dialectal Eastern Min"] = {
+	aliases = {"dialectal Min Dong"},
+	display = "dialectal [[w:Eastern Min|Eastern Min]]",
+	regional_categories = "Eastern Min",
+}
+
+labels["Gansu Dungan"] = {
+	display = "[[Gansu]] [[w:Dungan language|Dungan]]",
+	plain_categories = true,
+}
+
+labels["Gan"] = {
+	Wikipedia = "Gan Chinese",
+	regional_categories = true,
+}
+
+labels["dialectal Gan"] = {
+	display = "dialectal [[w:Gan Chinese|Gan]]",
+	regional_categories = "Gan",
+}
+
+labels["Guangxi Mandarin"] = {
+	display = "Guangxi [[w:Southwestern Mandarin|Mandarin]]",
+	plain_categories = true,
+}
+
+labels["dialectal Guangxi Mandarin"] = {
+	display = "dialectal Guangxi [[w:Southwestern Mandarin|Mandarin]]",
+	plain_categories = "Guangxi Mandarin",
+}
+
+labels["Guilin Mandarin"] = {
+	display = "Guilin [[w:Southwestern Mandarin|Mandarin]]",
+	plain_categories = true,
+}
+
+labels["Hainanese"] = {
+	aliases = {"Hainan Min", "Hainan Min Chinese"},
+	Wikipedia = "Hainanese",
+	regional_categories = true,
+}
+
+labels["Hakka"] = {
+	Wikipedia = "Hakka Chinese",
+	regional_categories = true,
+}
+
+labels["dialectal Hakka"] = {
+	display = "dialectal [[w:Hakka Chinese|Hakka]]",
+	regional_categories = "Hakka",
+}
+
+labels["Hangzhounese Wu"] = {
+	aliases = {"Hangzhounese", "Hangzhou Wu"},
+	display = "Hangzhounese",
+	Wikipedia = "Hangzhou dialect",
+	plain_categories = true,
+}
+
+labels["Hokkien"] = {
+	Wikipedia = true,
+	regional_categories = true,
+}
+
+labels["Hong Kong Cantonese"] = {
+	aliases = {"HKC"},
+	Wikipedia = true,
+	plain_categories = true,
+}
+
+labels["Hong Kong Hakka"] = {
+	display = "Hong Kong [[w:Hakka language|Hakka]]",
+	plain_categories = true,
+}
+
+labels["Huizhou"] = {
+	display = "[[w:Huizhou Chinese|Huizhou]]",
+	regional_categories = true,
+}
+
+labels["Huzhounese"] = {
+	aliases = {"Huzhou Wu", "Huzhounese Wu"},
+	Wikipedia = "zh:湖州話",
+	regional_categories = true,
+}
+
+labels["Inland Min"] = {
+	aliases = {"inland Min"},
+	Wikipedia = true,
+}
+
+labels["Jianghuai Mandarin"] = {
+	aliases = {"Jiang-Huai Mandarin", "Lower Yangtze Mandarin", "Huai"},
+	Wikipedia = "Lower Yangtze Mandarin",
+	regional_categories = "Mandarin",
+}
+
+labels["Jiaoliao Mandarin"] = {
+	aliases = {"Jiao-Liao Mandarin"},
+	Wikipedia = true,
+	regional_categories = "Mandarin",
+}
+
+labels["Jilu Mandarin"] = {
+	aliases = {"Ji-Lu Mandarin"},
+	Wikipedia = true,
+	regional_categories = "Mandarin",
+}
+
+labels["Jin"] = {
+	Wikipedia = "Jin Chinese",
+	regional_categories = true,
+}
+
+labels["dialectal Jin"] = {
+	display = "dialectal [[w:Jin Chinese|Jin]]",
+	regional_categories = "Jin",
+}
+
+labels["Korean Classical Chinese"] = {
+	Wikipedia = "Chinese-language literature of Korea",
+	plain_categories = true,
+}
+
+labels["Lanyin Mandarin"] = {
+	aliases = {"Lan-Yin Mandarin"},
+	Wikipedia = true,
+	plain_categories = true,
+}
+
+labels["Leizhou Min"] = {
+	Wikipedia = true,
+	regional_categories = true,
+}
+
+labels["Linshao Wu"] = {
+	aliases = {"Linshao", "Lin-Shao Wu", "Lin-Shao"},
+	Wikipedia = "Shaoxing dialect",
+}
+
+labels["Liuzhou Mandarin"] = {
+	display = "Liuzhou [[w:Southwestern Mandarin|Mandarin]]",
+	plain_categories = true,
+}
+
+labels["Mandarin"] = {
+	Wikipedia = "Mandarin Chinese",
+	regional_categories = true,
+}
+
+labels["dialectal Mandarin"] = {
+	display = "dialectal [[w:Mandarin Chinese|Mandarin]]",
+	regional_categories = "Mandarin",
+}
+
+labels["Min"] = {
+	Wikipedia = "Min Chinese",
+	regional_categories = true,
+}
+
+labels["Nanning Pinghua"] = {
+	display = "Nanning [[w:Pinghua|Pinghua]]",
+	plain_categories = true,
+}
+
+labels["Ningbonese Wu"] = {
+	aliases = {"Ningbonese", "Ningbo Wu", "Ningbo"},
+	display = "Ningbonese",
+	Wikipedia = "Ningbo dialect",
+	plain_categories = true,
+}
+
+labels["Northeastern Mandarin"] = {
+	aliases = {"northeastern Mandarin", "NE Mandarin"},
+	Wikipedia = true,
+	plain_categories = true,
+}
+
+labels["North America"] = {
+	aliases = {"North American"},
+	display = "[[Canada]], [[American English|US]]",
+	regional_categories = {"Canadian", "American"},
+}
+
+labels["Northern Min"] = {
+	aliases = {"Min Bei"},
+	Wikipedia = true,
+	regional_categories = true,
+}
+
+labels["Northern Wu"] = {
+	aliases = {"Taihu", "Taihu Wu"},
+	Wikipedia = true,
+	plain_categories = true,
+}
+
+labels["Penang Hokkien"] = {
+	Wikipedia = true,
+	plain_categories = true,
+}
+
+labels["Philippine Hokkien"] = {
+	aliases = {"PH Hokkien", "Ph Hokkien", "PH", "PHH"},
+	Wikipedia = true,
+	regional_categories = "Philippine",
+}
+
+labels["Pinghua"] = {
+	Wikipedia = true,
+	regional_categories = true,
+}
+
+labels["Puxian Min"] = {
+	aliases = {"Puxian", "Pu-Xian Min", "Pu-Xian", "Xinghua", "Hinghwa"},
+	Wikipedia = "Pu-Xian Min",
+	regional_categories = true,
+}
+
+labels["Quanzhou"] = {
+	aliases = {"Quanzhou Hokkien", "Chinchew", "Choanchew"},
+	Wikipedia = "Quanzhou dialect",
+	plain_categories = "Quanzhou Hokkien",
+}
+
+labels["Shanghainese Wu"] = {
+	aliases = {"Shanghai Wu", "Shanghainese"},
+	display = "Shanghainese",
+	Wikipedia = "Shanghainese",
+	plain_categories = true,
+}
+
+labels["Shaoxing Wu"] = {
+	aliases = {"Shaoxingnese", "Shaoxingnese Wu", "Shaoxingese", "Shaoxingese Wu"},
+	Wikipedia = "Shaoxing dialect",
+	plain_categories = true,
+}
+
+labels["Shehua"] = {
+	Wikipedia = "Shehua",
+	regional_categories = true,
+}
+
+labels["Shuangfeng"] = {
+	aliases = {"Shuangfeng Xiang"},
+	Wikipedia = "Shuangfeng dialect",
+	plain_categories = "Shuangfeng Xiang",
+}
+
+labels["Sichuanese"] = {
+	aliases = {"Sichuan"},
+	Wikipedia = "Sichuanese dialect",
+	regional_categories = true,
+}
+
+labels["Singaporean Hokkien"] = {
+	aliases = {"Singapore Hokkien"},
+	Wikipedia = "Singaporean Hokkien",
+	plain_categories = true,
+}
+
+labels["Siyi"] = {
+	aliases = {"Siyi Yue"},
+	Wikipedia = "Siyi Yue",
+}
+
+labels["Southern Min"] = {
+	aliases = {"Min Nan"},
+	Wikipedia = true,
+	regional_categories = true,
+	track = true,
+}
+
+labels["dialectal Southern Min"] = {
+	aliases = {"dialectal Min Nan"},
+	display = "dialectal [[w:Southern Min|Southern Min]]",
+	regional_categories = "Southern Min",
+}
+
+labels["Southern Wu"] = {
+	display = "Southern [[w:Wu Chinese|Wu]]",
+	plain_categories = true,
+}
+
+labels["Southwestern Mandarin"] = {
+	aliases = {"southwestern Mandarin", "Upper Yangtze Mandarin", "Southwest Mandarin"},
+	Wikipedia = true,
+	plain_categories = true,
+}
+
+labels["Standard Written Chinese"] = {
+	aliases = {"SWC"},
+	Wikipedia = "Written vernacular Chinese",
+}
+
+labels["Sujiahu"] = {
+	aliases = {"Su-Jia-Hu Wu", "Sujiahu Wu", "Su-Jia-Hu"}, -- not Suzhou-Jiaxing-Huzhou
+	display = "[[w:Suzhou dialect|Su]][[w:zh:嘉興話|jia]][[w:Shanghainese|hu]]",
+	plain_categories = "Northern Wu",
+}
+
+labels["Suzhounese Wu"] = {
+	aliases = {"Suzhounese", "Suzhou Wu"},
+	display = "Suzhounese",
+	Wikipedia = "Suzhou dialect",
+	plain_categories = true,
+}
+
+labels["Taishanese"] = {
+	aliases = {"Toishanese", "Hoisanese"},
+	Wikipedia = true,
+	regional_categories = true,
+}
+
+labels["Taiwanese Hakka"] = {
+	aliases = {"Taiwan Hakka"},
+	Wikipedia = true,
+	plain_categories = true,
+}
+
+labels["Taiwanese Hokkien"] = {
+	aliases = {"Taiwanese Southern Min", "Taiwanese Min Nan", "Taiwan Hokkien", "Taiwan Southern Min", "Taiwan Min Nan"},
+	Wikipedia = "Taiwanese Hokkien",
+	plain_categories = true,
+	track = true,
+}
+
+labels["Taiwanese Hokkien and Hakka"] = {
+	aliases = {"Taiwanese Hokkien & Hakka", "Taiwanese Hakka and Hokkien", "Taiwanese Hakka & Hokkien", "Taiwanese Southern Min and Hakka", "Taiwanese Southern Min & Hakka", "Taiwanese Hakka and Southern Min", "Taiwanese Hakka & Southern Min", "Taiwanese Min Nan and Hakka", "Taiwanese Min Nan & Hakka", "Taiwanese Hakka and Min Nan", "Taiwanese Hakka & Min Nan", "Taiwan Hokkien and Hakka", "Taiwan Hokkien & Hakka", "Taiwan Hakka and Hokkien", "Taiwan Hakka & Hokkien", "Taiwan Southern Min and Hakka", "Taiwan Southern Min & Hakka", "Taiwan Hakka and Southern Min", "Taiwan Hakka & Southern Min", "Taiwan Min Nan and Hakka", "Taiwan Min Nan & Hakka", "Taiwan Hakka and Min Nan", "Taiwan Hakka & Min Nan"},
+	display = "[[w:Taiwanese Hokkien|Taiwanese Hokkien]] and [[w:Taiwanese Hakka|Hakka]]",
+	plain_categories = {"Taiwanese Hokkien", "Taiwanese Hakka"},
+	track = true,
+}
+
+labels["Taiwanese Mandarin"] = {
+	aliases = {"Taiwan Mandarin"},
+	Wikipedia = true,
+	plain_categories = true,
+}
+
+labels["Teochew"] = {
+	Wikipedia = "Teochew dialect",
+	regional_categories = true,
+	track = true,
+}
+
+labels["Tianjin"] = {
+	aliases = {"Tianjin Mandarin", "Tianjinese", "Tianjinese Mandarin"},
+	Wikipedia = "Tianjin dialect",
+	plain_categories = "Tianjin Mandarin",
+}
+
+labels["Vietnamese Classical Chinese"] = {
+	Wikipedia = "Literary Chinese in Vietnam",
+	plain_categories = true,
+}
+
+labels["Waxiang"] = {
+	Wikipedia = "Waxiang Chinese",
+	regional_categories = true,
+}
+
+labels["Wenzhou Wu"] = {
+	aliases = {"Wenzhounese", "Wenzhounese Wu", "Wenzhou", "Oujiang"},
+	display = "[[w:Wenzhou dialect|Wenzhounese]]",
+	plain_categories = "Wenzhounese Wu",
+}
+
+labels["Wu"] = {
+	Wikipedia = "Wu Chinese",
+	regional_categories = true,
+}
+
+labels["dialectal Wu"] = {
+	display = "dialectal [[w:Wu Chinese|Wu]]",
+	regional_categories = "Wu",
+}
+
+labels["Wuhan"] = {
+	aliases = {"Wuhan Mandarin", "Hankou", "Hankow"},
+	Wikipedia = "Wuhan dialect",
+	plain_categories = "Wuhan Mandarin",
+}
+
+labels["Wuzhou Wu"] = {
+	aliases = {"Jinhua Wu", "Jinhuanese", "Jinhuanese Wu", "Wuzhou"},
+	Wikipedia = "Jinhua dialect",
+	plain_categories = true,
+}
+
+labels["Xiamen"] = {
+	aliases = {"Xiamen Hokkien", "Amoy"},
+	Wikipedia = "Amoy dialect",
+	plain_categories = "Xiamen Hokkien",
+}
+
+labels["Xiang"] = {
+	Wikipedia = "Xiang Chinese",
+	regional_categories = true,
+}
+
+labels["dialectal Xiang"] = {
+	display = "dialectal [[w:Xiang Chinese|Xiang]]",
+	regional_categories = "Xiang",
+}
+
+labels["Xinjiang"] = {
+	aliases = {"Xinjiang Mandarin"},
+	display = "[[w:Xinjiang Mandarin|Xinjiang]]",
+	plain_categories = "Xinjiang Mandarin",
+}
+
+labels["Xinqu Wu"] = {
+	aliases = {"Quzhou Wu", "Quzhounese", "Quzhounese Wu", "Shangrao Wu", "Shangraonese", "Shangraonese Wu", "Xinzhou Wu"},
+	Wikipedia = "Quzhou dialect",
+}
+
+labels["Zhongshan Min"] = {
+	Wikipedia = true,
+	regional_categories = true,
+}
+
+labels["Zhangzhou"] = {
+	aliases = {"Zhangzhou Hokkien", "Changchew"},
+	Wikipedia = "Zhangzhou dialect",
+	plain_categories = "Zhangzhou Hokkien",
+}
+
+return require("Module:labels").finalize_data(labels)
