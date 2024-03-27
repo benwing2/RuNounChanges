bf055e07488b3a3c0e48ebba2216b964d343b627
Ben Wing <ben@benwing.com>
2024-03-27 01:17:56 -0500

    push_find_regex_changes: use blib.normalize_text_for_save(); show diff when can't save; comment process_text_on_page()

--- push_find_regex_changes.py
+++ push_find_regex_changes.py
@@ -7,13 +7,23 @@ from blib import getparam, rmparam, msg, errmsg, errandmsg, site
 import pywikibot, re, sys, argparse
 import unicodedata
 
+# process_text_on_page() callback. `index` is the index of the page whose title is `pagetitle`. `curtext` is the
+# actual current text of the page. `contents` is the desired text of the page (or of the specific language section if
+# --lang-only or --subset-of-langs), and `origcontents` is the previous text of the page (or of the specific language
+# section) from which `contents` was derived. We need `origcontents` so we can check to see if the page (or specific
+# language section) was changed by someone else in the meantime; if so, we can't save.
 def process_text_on_page(index, pagetitle, curtext, contents, prev_comment, origcontents):
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagetitle, txt))
   def errandpagemsg(txt):
     errandmsg("Page %s %s: %s" % (index, pagetitle, txt))
 
-  if contents == origcontents:
+  def normalize_text(text):
+    if text is None:
+      return text
+    return blib.normalize_text_for_save(text).rstrip("\n")
+
+  if normalize_text(contents) == normalize_text(origcontents):
     pagemsg("Skipping contents because no change")
     return
   if args.verbose:
@@ -35,8 +45,11 @@ def process_text_on_page(index, pagetitle, curtext, contents, prev_comment, orig
       sections, sections_by_lang, _ = blib.split_text_into_sections(curtext, pagemsg)
 
       def replace_lang_section(lang, newsectext, origsectext):
+        newsectext = unicodedata.normalize("NFC", newsectext)
+        supposed_cursectext = unicodedata.normalize("NFC", origsectext)
         if lang not in sections_by_lang:
-          errandpagemsg("WARNING: Couldn't find %s section, skipping page" % lang)
+          errandpagemsg("WARNING: Couldn't find %s section, skipping page; showing our changes:" % lang)
+          blib.show_diff(supposed_cursectext, newsectext)
           return False
         langsec = sections_by_lang[lang]
         cursectext = unicodedata.normalize("NFC", sections[langsec])
@@ -45,12 +58,12 @@ def process_text_on_page(index, pagetitle, curtext, contents, prev_comment, orig
         # an extra newline at the end of the page because it will be stripped by MediaWiki.
         if not cursectext.endswith("\n"):
           cursectext += "\n"
-        supposed_cursectext = unicodedata.normalize("NFC", origsectext)
         if cursectext != supposed_cursectext:
           if cursectext == newsectext:
             pagemsg("%s section has already been changed to new text, not saving" % lang)
           else:
-            errandpagemsg("WARNING: %s text has changed from supposed original text, not saving" % lang)
+            errandpagemsg("WARNING: %s text has changed from supposed original text, not saving; showing our changes:" % lang)
+            blib.show_diff(supposed_cursectext, newsectext)
           return False
         sections[langsec] = newsectext
         return True
@@ -74,14 +87,15 @@ def process_text_on_page(index, pagetitle, curtext, contents, prev_comment, orig
             return
       contents = "".join(sections)
     else:
-      nfc_curtext = unicodedata.normalize("NFC", curtext)
-      # MediaWiki strips newlines from the end of the page so we need to do the same for comparison.
-      supposed_nfc_curtext = unicodedata.normalize("NFC", origcontents.rstrip("\n"))
+      nfc_curtext = normalize_text(curtext)
+      supposed_nfc_curtext = normalize_text(origcontents)
+      contents = normalize_text(contents)
       if nfc_curtext != supposed_nfc_curtext:
-        if nfc_curtext == contents.rstrip("\n"):
+        if nfc_curtext == contents:
           pagemsg("Page has already been changed to new text, not saving")
         else:
-          errandpagemsg("WARNING: Text has changed from supposed original text, not saving")
+          errandpagemsg("WARNING: Text has changed from supposed original text, not saving; showing our changes:")
+          blib.show_diff(supposed_nfc_curtext, contents)
         return
   if not prev_comment and not args.comment:
     errandpagemsg("WARNING: Trying to save page and neither previous comment not --comment available")
