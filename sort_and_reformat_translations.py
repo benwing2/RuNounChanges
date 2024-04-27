d71d5980bc7eea2fa842dd77433ca803774c0c52
Ben Wing <ben@benwing.com>
2024-04-27 02:16:50 -0500

    sort_and_reformat_translations: partial rewrite

--- sort_and_reformat_translations.py
+++ sort_and_reformat_translations.py
@@ -18,16 +18,51 @@ language_sets = {
   },
   "Apache": {
     "recognize": lambda lang: lang.endswith("Apache") or lang in ["Jicarilla", "Lipan", "Chiricahua"], # not Navajo
+    "rename": {
+      "Chiricahua Apache": "Chiricahua",
+      "Jicarilla Apache": "Jicarilla",
+      "Lipan Apache": "Lipan",
+      "Mescalero": "Chiricahua",
+      "Western": "Western Apache",
+    },
   },
   "Arabic": {
-    "recognize": lambda lang: lang.endswith("Arabic") or lang in ["Hassaniya", "Nubi"], # not Maltese
+    "recognize": lambda lang: lang.endswith("Arabic") or lang in ["Hassaniya"], # not Maltese or Nubi (creole)
+    "add_lang": {"Algerian", "Andalusian", "Baharna", "Chadian", "Cypriot", "Dhofari", "Egyptian", "Gulf", "Hassaniya",
+                 "Hijazi", "Iraqi", "Juba", "Levantine", "Libyan", "Mesopotamian", "Moroccan", "Najdi",
+                 "North Levantine", "Omani", "South Levantine", "Sudanese", "Tunisian", "Yemeni"},
+    "rename": {
+      "Andalusi Arabic": "Andalusian Arabic",
+      "Bahrani Arabic": "Baharna Arabic",
+      "Hadrami Arabic": "Yemeni Arabic (Hadrami)",
+      "Lebanese": "North Levantine Arabic (Lebanese)",
+      "Lebanese Arabic": "North Levantine Arabic (Lebanese)",
+      "Levantine, North": "North Levantine Arabic",
+      "Levantine, South": "South Levantine Arabic",
+      "Morocco": "Moroccan Arabic",
+      "Palestine": "South Levantine Arabic (Palestinian)",
+      "Palestinian": "South Levantine Arabic (Palestinian)",
+      "Palestinian Arabic": "South Levantine Arabic (Palestinian)",
+      "San'ani Arabic": "Yemeni Arabic (San'ani)",
+      "San'ani Yemeni Arabic": "Yemeni Arabic (San'ani)",
+      "Syrian": "North Levantine Arabic (Syrian)",
+      "Syrian Arabic": "North Levantine Arabic (Syrian)",
+      "Yemen": "Yemeni Arabic",
+    },
   },
   "Aramaic": {
     "recognize": lambda lang: lang.endswith("Aramaic") or lang in [
       "Mlahsö", "Turoyo", "Classical Syriac", "Hulaulá", "Hértevin", "Koy Sanjaq Surat", "Lishana Deni",
       "Lishanid Noshan", "Lishán Didán", "Senaya", "Classical Mandaic", "Mandaic"],
-    "rename_lang_map": {
+    "rename": {
       "Assyrian Neo Aramaic": "Assyrian Neo-Aramaic",
+      "Babylonian": "Jewish Babylonian Aramaic",
+      "Jewish Babylonian": "Jewish Babylonian Aramaic",
+      "Jewish Baylonian Aramaic": "Jewish Babylonian Aramaic",
+      #"Palestinian": "Jewish Palestinian Aramaic",
+      #"Palestinian Aramaic": "Jewish Palestinian Aramaic",
+      "Syriac": "Classical Syriac",
+      "Syriac, Classical": "Classical Syriac",
     },
   },
   "Armenian": {},
@@ -40,33 +75,88 @@ language_sets = {
       "Chinese", "Cantonese", "Yue", "Dungan", "Gan", "Hakka", "Huizhou", "Jin", "Min", "Min Nan", "Wu",
       "Hangzhounese", "Ningbonese", "Shanghainese", "Suzhounese", "Wenzhounese", "Xiang",
       "Pinghua", "Waxiang", "Hokkien", "Hainanese", "Teochew", "Shaozhou Tuhua", "Sichuanese", "Taishanese",
-      "Tangwang", "Wutunhua"]) or lang in ["Ci"],
+      "Tangwang"]) or lang in ["Ci"], # not Wutunhua, a Mandarin-Amdo-Bonan creole
+    "rename": {
+      "Madarin": "Mandarin",
+      "Min Bei": "Northern Min",
+      "Min Dong": "Eastern Min",
+      "Min Nan": "Hokkien", # correct
+      "Min Zhong": "Central Min",
+      "Puxian": "Puxian Min",
+    }
   },
   "French": {
     "recognize": lambda lang: lang in ["Middle French", "Old French"], # not Louisiana Creole French?
+    "add_lang": {"Canadian"},
+    "rename": {
+      "Canada": "Canadian French",
+    },
   },
   "Georgian": {},
   # inconsistent nesting currently, issues with "Low German"
   "German": {
     "recognize": lambda lang: lang in ["Alemannic German", "Kölsch", "Palatinate German"],
+    "add_lang": {"Alemannic", "East Central"},
+    "unindent": {"Luxembourgish"},
+    "rename": {
+      "Alemannic": "Alemannic German",
+      "Alsace": "Alsatian Alemannic German",
+      "Alsatian <small>(Low Alemannic German)</small>": "Alsatian Alemannic German",
+      "Bavaria": "Bavarian",
+      "Silesian": "Silesian East Central German",
+      "Silesian German": "Silesian East Central German",
+    },
   },
   "Greek": {
     "recognize": lambda lang: lang.endswith("Greek") or lang in [
       "Kaliarda", "Katharevousa", "Yevanic", "Tsakonian", "Opuntian Locrian", "Ozolian Locrian"],
+    "add_lang": {"Aeolic", "Ancient", "Arcadian", "Arcadocypriot", "Attic", "Byzantine", "Cappadocian", "Classical",
+                 "Cretan", "Cypriot", "Doric", "Epic", "Hellenistic", "Ionic", "Italiot", "Koine", "Laconian",
+                 "Medieval", "Mycenaean", "Pamphylian", "Pontic", "Thessalian"},
+    "rename": {
+      "Mycenean Greek": "Mycenaean Greek",
+      "Mycenean": "Mycenaean Greek",
+      "Griko": "Italiot Greek",
+    },
   },
   "Irish": {},
   "Khanty": {},
-  "Kurdish": {},
+  "Kurdish": {
+    "rename": {
+      "Cnetral Kurdish": "Central Kurdish",
+      "Laki Kurdish": "Laki",
+      "Sorani": "Central Kurdish",
+      "Kurmanji": "Northern Kurdish",
+    },
+  },
   "Lawa": {},
   "Low German": {
     "recognize": lambda lang: lang != "Middle Low German" and (lang.endswith("Low German") or lang in ["Dutch Low Saxon"]),
+    "rename": {
+      "Dutch Low German": "Dutch Low Saxon",
+      "Mennonite Plautdietsch": "Plautdietsch",
+      "Plauttdietsch": "Plautdietsch",
+      "Plauttdietsch (Mennonite Low German)": "Plautdietsch",
+    },
   },
-  "Mari": {},
+  "Mari": {
+    "add_lang": {"Eastern", "Western"},
+  },
   "Mansi": {},
-  "Mongolian": {},
+  "Mongolian": {
+    "rename": {
+      "Roman": "Latin",
+    },
+  },
+  "Nahuatl": {
+    # FIXME: Make sure it's OK to move "Foo Nahuatl" under "Nahuatl"
+    "add_lang": {"Central", "Central Huasteca", "Central Puebla", "Classical", "Coatepec", "Cosoleacaque",
+                 "Eastern Durango", "Eastern Huasteca", "Guerrero", "Highland Puebla", ...},
+    ...,
+  },
   "Norwegian": {
     "recognize": lambda lang: lang.startswith("Norwegian") or lang in ["Bokmål", "Bokmal", "Nynorsk"],
-    "rename_lang_map": {
+    "rename": {
       "Norwegian Bokmål": "Bokmål",
       "Norwegian (Bokmål)": "Bokmål",
       "Norwegian (bokmål)": "Bokmål",
@@ -75,23 +165,83 @@ language_sets = {
       "Norwegian Nynorsk": "Nynorsk",
       "Norwegian (Nynorsk)": "Nynorsk",
       "Norwegian (nynorsk)": "Nynorsk",
+      "Nynorak": "Nynorsk",
+      "Nynorskl": "Nynorsk",
     },
   },
   "Ohlone": {},
   "Persian": {
     # not Middle Persian/Old Persian?
-    "recognize": lambda lang: lang in ["Dari", "Classical Persian", "Iranian Persian"],
+    "recognize": lambda lang: lang in ["Dari", "Dari Persian", "Classical Persian", "Iranian Persian"],
+    "add_lang": {"Classical", "Iranian"},
+    "rename": {
+      "Dari Persian": "Dari",
+    },
+  },
+  "Portuguese": {
+    # FIXME: Make sure entries in this section are kosher
+    "add_lang": {"Brazilian", "European"},
+    "rename": {
+      "Brazil": "Brazilian Portuguese",
+      "European": "European Portuguese",
+      "Portugal": "European Portuguese",
+      "Iberian": "European Portuguese",
+      "{{qualifier|Brazil}}": "Brazilian Portuguese",
+      "{{qualifier|Portugal}}": "European Portuguese",
+    },
+  },
+  "Punjabi": {
+    # FIXME: Make sure it's OK to move "Foo Punjabi" under "Punjabi"
+    "add_lang": {"Eastern", "Western"},
+    "rename": {
+      "Eastern Panjabi": "Eastern Punjabi",
+      "Western Panjabi": "Western Punjabi",
+      "shahmukhi": "Shahmukhi",
+    },
   },
   "Roglai": {},
   "Romani": {},
   "Sama": {},
-  "Sami": {},
-  "Sorbian": {},
+  "Sami": {
+    "add_lang": {"Akkala", "Inari", "Kemi", "Kildin", "Lule", "Northern", "Pite", "Skolt", "Southern", "Ter", "Ume" },
+    "rename": {
+      "Kola": "Kildin Sami",
+    },
+  },
+  "Serbo-Croatian": {
+    "rename": {
+      "Cyrilli": "Cyrillic",
+      "Cyrillic script": "Cyrillic",
+      "Cyrillic spelling": "Cyrillic",
+      "Latin script": "Latin",
+      "Latın": "Latin",
+      "Roman": "Latin",
+      "Roman script": "Latin",
+      "Roman spelling": "Latin",
+    }
+  },
+  "Sorbian": {
+    "rename": {
+      "Low Sorbian": "Lower Sorbian",
+      "Lower": "Lower Sorbian",
+      "Upper": "Upper Sorbian",
+    },
   "Spanish": {},
   "Tujia": {},
   "Welsh": {},
   "Yokuts": {},
 }
+
+# Keys are languages. The value for a key is a dictionary where keys are the language header the given language is
+# indented under, or "" for a non-indented language.
+lang_counts = defaultdict(lambda: defaultdict(int))
+# Keys are languages. Values are total counts of appearances, indented or not.
+total_lang_counts = defaultdict(int)
+# Keys are non-indented languages ("headers"). The value for a key is a dictionary where keys are indented languages
+# and the values are counts.
+header_counts = defaultdict(lambda: defaultdict(int))
+# Keys are headers. Values are total counts of cases where there's at least one language indented.
+total_header_counts = defaultdict(int)
 
 def process_text_on_page(index, pagename, text):
   def pagemsg(txt):
@@ -120,6 +270,7 @@ def process_text_on_page(index, pagename, text):
       prev_indented_lang = ""
       translation_lines = []
       new_lines.append(line)
+      saw_indented_lang = False
     elif line.startswith("{{trans-bottom"):
       if not in_translation_section:
         pagemsg("WARNING: Found {{trans-bottom}} not in a translation section")
@@ -165,7 +316,7 @@ def process_text_on_page(index, pagename, text):
           pagemsg("WARNING: Unrecognized langcode %s in {{ttbc}}: %s" % (langcode, line))
           return m.group(0)
         line = re.sub(r"\{\{ttbc\|([^{}|=]*)\}\}", replace_ttbc, line)
-        langname_regex = "(?:'Are'are|\w[^:;{}]*?)"
+        langname_regex = r"(?:'Are'are|\w[^:;{}]*?)"
         m = re.search(r"^([:*]+ *)(%s)(;?)((?: *\{\{.*)?)$" % langname_regex, line)
         if m:
           init, potential_lang, semicolon, rest = m.groups()
@@ -184,9 +335,26 @@ def process_text_on_page(index, pagename, text):
           init_star, rest = m.groups()
           line = "*:" + rest
           notes.append("replace %s with *: in translation section" % init_star)
-        m = re.search(r"^(\* *: *)([^:]*)(:.*)$", line)
+        m = re.search(r"^(\* *:? *)(%s) *(:.*)$" % langname_regex, line)
+        if m:
+          init_star, lang, rest = m.groups()
+          if ":" in init_star:
+            init_star = "*: "
+          else:
+            init_star = "* "
+          newline = init_star + lang + rest
+          if newline != line:
+            line = newline
+            notes.append("remove extraneous spaces in translation section")
+        m = re.search(r"^(\* *: *)([^:]+)(:.*)$", line)
         if m:
           init_star, indented_lang, rest = m.groups()
+          lang_counts[indented_lang][prev_lang] += 1
+          total_lang_counts[indented_lang] += 1
+          header_counts[prev_lang][indented_lang] += 1
+          if not saw_indented_lang:
+            total_header_counts[prev_lang] += 1
+            saw_indented_lang = True
           if indented_lang in ["Acehnese", "Ambonese Malay", "Baba Malay", "Balinese", "Banda", "Banjarese", "Buginese", "Brunei", "Brunei Malay", "Ende", "Indonesia", "Indonesian", "Jambi Malay", "Javanese", "Kelantan-Pattani Malay", "Madurese", "Makasar", "Minangkabau", "Nias", "Sarawak Malay", "Sarawakian", "Sikule", "Simeulue", "Singkil", "Sundanese", "Terengganu Malay"]:
             # Javanese variants: Central Javanese, Western Javanese, Kaili, Krama, Ngoko, Old Javanese
             pagemsg("Found %s translation indented under %s, unindenting: %s" % (indented_lang, prev_lang, line))
@@ -285,6 +453,9 @@ def process_text_on_page(index, pagename, text):
             translation_lines.append((prev_lang, prev_indented_lang, lineind, line, origline))
           else:
             rest, lang, after_lang = m.groups()
+            lang_counts[lang][""] += 1
+            total_lang_counts[lang] += 1
+            saw_indented_lang = False
             for langset, langset_props in language_sets.items():
               recognize = langset_props.get("recognize", lambda lang: lang.endswith(langset))
               if lang != langset and recognize(lang):
@@ -330,7 +501,34 @@ def process_text_on_page(index, pagename, text):
 parser = blib.create_argparser("Sort translations, unindent translations under Malayic, rename Min Chinese varieties and correct misc translation table issues",
                                include_pagefile=True, include_stdin=True)
 parser.add_argument("--rename-min", action="store_true", help="Rename Min varieties.")
+parser.add_argument("--analyze", action="store_true", help="Analyze existing indented and non-indented language use and output a table at the end.")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
 blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, edit=True, stdin=True)
+
+if args.analyze:
+  def output_table(key):
+    msg("%-40s %5s: %s" % ("Language", "Count", "Count-by-header"))
+    msg("---------------------------------------------------------")
+    for lang, count in sorted(list(total_lang_counts.items()), key=key):
+      by_header = "; ".join("%s (%s)" % (header or "unindented", headercount)
+                            for header, headercount in sorted(list(lang_counts[lang].items()), key=lambda x: -x[1]))
+      msg("%-40s %5s: %s" % (lang, count, by_header))
+
+  msg("Sorted by total count:")
+  msg("----------------------")
+  output_table(lambda x: -x[1])
+  msg("")
+  msg("Sorted by language, from the end:")
+  msg("---------------------------------")
+  output_table(lambda x: (x[0][::-1], -x[1]))
+  msg("")
+  msg("By header:")
+  msg("----------")
+  msg("%-50s %8s %s" % ("Header", "Indented", "Total"))
+  msg("------------------------------------------------------------------")
+  for header, indented_dict in sorted(list(header_counts.items()), key=lambda x: -total_header_counts[x[0]]):
+    msg("%-50s %8s %5s" % (header, total_header_counts[header], total_lang_counts[header]))
+    for lang, langcount in sorted(list(indented_dict.items()), key=lambda x: x[0]):
+      msg("%-50s %8s %5s" % ("  * %s" % (lang), langcount, total_lang_counts[lang]))
