8a698b33d5b3a371f72deb1730979fb2b38cdd36
Ben Wing <ben@benwing.com>
2024-05-20 02:56:58 -0500

    analyze_accent_qualifier: warn about first (accent) param looking like a lang code when more than one accent param present

--- analyze_accent_qualifier.py
+++ analyze_accent_qualifier.py
@@ -10,6 +10,8 @@ from collections import defaultdict
 total_qualifiers = defaultdict(int)
 qualifiers_by_lang = defaultdict(lambda: defaultdict(int))
 
+blib.getData()
+
 def process_text_on_page(index, pagename, text):
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagename, txt))
@@ -28,8 +30,24 @@ def process_text_on_page(index, pagename, text):
       tn = tname(t)
       if tn in ["a", "accent"]:
         params = blib.fetch_param_chain(t, "1")
-        for param in params:
+        for index, param in enumerate(params):
           param = param.strip()
+          if index == 0:
+            pseudo_langname = None
+            pseudo_langtype = None
+            if param in blib.languages_byCode:
+              pseudo_langname = blib.languages_byCode[param]["canonicalName"]
+              pseudo_langtype = "full"
+            elif param in blib.etym_languages_byCode:
+              pseudo_langname = blib.etym_languages_byCode[param]["canonicalName"]
+              pseudo_langtype = "etym-only"
+            if pseudo_langtype:
+              if len(params) == 1:
+                pagemsg("WARNING: Saw qualifier '%s' same as language code for %s language '%s' in lang section '%s' but only one qualifier: %s" % (
+                  param, pseudo_langtype, pseudo_langname, lang, str(t)))
+              else:
+                pagemsg("WARNING: Saw qualifier '%s' same as language code for %s language '%s' in lang section '%s' and multiple qualifiers: %s" % (
+                  param, pseudo_langtype, pseudo_langname, lang, str(t)))
           total_qualifiers[param] += 1
           qualifiers_by_lang[param][lang] += 1
 
