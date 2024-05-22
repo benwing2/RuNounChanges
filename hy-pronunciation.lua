b7db2d97403bab67793c2f4dc5849df38c472530
Ben Wing <ben@benwing.com>
2024-05-21 22:32:36 -0500

    hy-pronunciation: latest from Wiktionary

--- hy-pronunciation.lua
+++ hy-pronunciation.lua
@@ -1,70 +1,368 @@
 local export = {}
 
 local m_str_utils = require("Module:string utilities")
+local lang = require("Module:languages").getByCode("hy")
+local m_IPA = require("Module:IPA")
 
 local rsubn = m_str_utils.gsub
 local toNFC = mw.ustring.toNFC
 local rlower = m_str_utils.lower
 local rfind = m_str_utils.find
 
--- single characters that map to IPA sounds   
+-- single characters that map to IPA sounds
+local phonemic_chars_map = {
+	-- Eastern Armenian
+	east = {
+		["ա"] = "ɑ",
+		["բ"] = "b",
+		["գ"] = "ɡ",
+		["դ"] = "d",
+		["ե"] = "e",
+		["զ"] = "z",
+		["է"] = "e",
+		["ը"] = "ə",
+		["թ"] = "tʰ",
+		["ժ"] = "ʒ",
+		["ի"] = "i",
+		["լ"] = "l",
+		["խ"] = "χ",
+		["ծ"] = "t͡s",
+		["կ"] = "k",
+		["հ"] = "h",
+		["ձ"] = "d͡z",
+		["ղ"] = "ʁ",
+		["ճ"] = "t͡ʃ",
+		["մ"] = "m",
+		["յ"] = "j",
+		["ն"] = "n",
+		["շ"] = "ʃ",
+		["ո"] = "o",
+		["չ"] = "t͡ʃʰ",
+		["պ"] = "p",
+		["ջ"] = "d͡ʒ",
+		["ռ"] = "r",
+		["ս"] = "s",
+		["վ"] = "v",
+		["տ"] = "t",
+		["ր"] = "ɾ",
+		["ց"] = "t͡sʰ",
+		["ւ"] = "v",
+		["փ"] = "pʰ",
+		["ք"] = "kʰ",
+		["և"] = "ev",
+		["օ"] = "o",
+		["ֆ"] = "f",
+		["-"] = " ",
+		["’"] = "",
+		["-"] = "",
+		["."] = "·",
+	},
+	-- Western Armenian
+	west = {
+		["ա"] = "ɑ",
+		["բ"] = "p",
+		["գ"] = "k",
+		["դ"] = "t",
+		["ե"] = "e",
+		["զ"] = "z",
+		["է"] = "e",
+		["ը"] = "ə",
+		["թ"] = "t",
+		["ժ"] = "ʒ",
+		["ի"] = "i",
+		["լ"] = "l",
+		["խ"] = "χ",
+		["ծ"] = "d͡z",
+		["կ"] = "ɡ",
+		["հ"] = "h",
+		["ձ"] = "t͡s",
+		["ղ"] = "ʁ",
+		["ճ"] = "d͡ʒ",
+		["մ"] = "m",
+		["յ"] = "j",
+		["ն"] = "n",
+		["շ"] = "ʃ",
+		["ո"] = "o",
+		["չ"] = "t͡ʃ",
+		["պ"] = "b",
+		["ջ"] = "t͡ʃ",
+		["ռ"] = "r",
+		["ս"] = "s",
+		["վ"] = "v",
+		["տ"] = "d",
+		["ր"] = "ɾ",
+		["ց"] = "t͡s",
+		["ւ"] = "v",
+		["փ"] = "p",
+		["ք"] = "k",
+		["և"] = "ev",
+		["օ"] = "o",
+		["ֆ"] = "f",
+		["-"] = " ",
+		["’"] = "",
+		["-"] = "",
+		["."] = "·",
+	},
+}
+
+-- character sequences of two that map to IPA sounds
+local phonemic_2chars_map = {
+	east = {
+		{ "ու", "u" },
+	},
+	west = {
+		-- if not in the initial position and if not preceded by [ɑeəoiu]
+		{
+			"(.?.?)յու",
+			function(before)
+				if not (before == "" or mw.ustring.find(before, "[%sաեէիոօ]$") or before == "ու") then
+					return before .. "ʏ"
+				end
+			end,
+		},
+		{ "ու", "u" },
+		{ "էօ", "œ" },
+		-- Western Armenian inserts ə in the causative
+		{ "ցնել", "t͡sənel" },
+	},
+}
+
+-- single characters that map to IPA sounds
 local phonetic_chars_map = {
 	-- Eastern Armenian
 	east = {
-		["ա"]="ɑ", ["բ"]="b", ["գ"]="ɡ", ["դ"]="d", ["ե"]="e", ["զ"]="z",
-		["է"]="e", ["ը"]="ə", ["թ"]="tʰ", ["ժ"]="ʒ", ["ի"]="i", ["լ"]="l",
-		["խ"]="χ", ["ծ"]="t͡s", ["կ"]="k", ["հ"]="h", ["ձ"]="d͡z", ["ղ"]="ʁ", 
-		["ճ"]="t͡ʃ", ["մ"]="m", ["յ"]="j", ["ն"]="n", ["շ"]="ʃ", ["ո"]="o",
-		["չ"]="t͡ʃʰ", ["պ"]="p", ["ջ"]="d͡ʒ", ["ռ"]="r", ["ս"]="s", ["վ"]="v", 
-		["տ"]="t", ["ր"]="ɾ", ["ց"]="t͡sʰ", ["ւ"]="v", ["փ"]="pʰ", ["ք"]="kʰ",
-		["և"]="ev", ["օ"]="o", ["ֆ"]="f", ["-"]=" ", ["’"]="", ["-"]=""
+		["ա"] = "ɑ",
+		["բ"] = "b",
+		["գ"] = "ɡ",
+		["դ"] = "d",
+		["ե"] = "e",
+		["զ"] = "z",
+		["է"] = "e",
+		["ը"] = "ə",
+		["թ"] = "tʰ",
+		["ժ"] = "ʒ",
+		["ի"] = "i",
+		["լ"] = "l",
+		["խ"] = "χ",
+		["ծ"] = "t͡s",
+		["կ"] = "k",
+		["հ"] = "h",
+		["ձ"] = "d͡z",
+		["ղ"] = "ʁ",
+		["ճ"] = "t͡ʃ",
+		["մ"] = "m",
+		["յ"] = "j",
+		["ն"] = "n",
+		["շ"] = "ʃ",
+		["ո"] = "o",
+		["չ"] = "t͡ʃʰ",
+		["պ"] = "p",
+		["ջ"] = "d͡ʒ",
+		["ռ"] = "r",
+		["ս"] = "s",
+		["վ"] = "v",
+		["տ"] = "t",
+		["ր"] = "ɾ",
+		["ց"] = "t͡sʰ",
+		["ւ"] = "v",
+		["փ"] = "pʰ",
+		["ք"] = "kʰ",
+		["և"] = "ev",
+		["օ"] = "o",
+		["ֆ"] = "f",
+		["-"] = " ",
+		["’"] = "",
+		["-"] = "",
 	},
 	-- note that the default pronunciation of ostensible /ɾ/ is [ɹ]
 	-- Western Armenian
 	west = {
-		["ա"]="ɑ", ["բ"]="pʰ", ["գ"]="kʰ", ["դ"]="tʰ", ["ե"]="e", ["զ"]="z",
-		["է"]="e", ["ը"]="ə", ["թ"]="tʰ", ["ժ"]="ʒ", ["ի"]="i", ["լ"]="l",
-		["խ"]="χ", ["ծ"]="d͡z", ["կ"]="ɡ", ["հ"]="h", ["ձ"]="t͡sʰ", ["ղ"]="ʁ", 
-		["ճ"]="d͡ʒ", ["մ"]="m", ["յ"]="j", ["ն"]="n", ["շ"]="ʃ", ["ո"]="o",
-		["չ"]="t͡ʃʰ", ["պ"]="b", ["ջ"]="t͡ʃʰ", ["ռ"]="r", ["ս"]="s", ["վ"]="v", 
-		["տ"]="d", ["ր"]="ɾ", ["ց"]="t͡sʰ", ["ւ"]="v", ["փ"]="pʰ", ["ք"]="kʰ",
-		["և"]="ev", ["օ"]="o", ["ֆ"]="f", ["-"]=" ", ["’"]="", ["-"]=""
+		["ա"] = "ɑ",
+		["բ"] = "pʰ",
+		["գ"] = "kʰ",
+		["դ"] = "tʰ",
+		["ե"] = "e",
+		["զ"] = "z",
+		["է"] = "e",
+		["ը"] = "ə",
+		["թ"] = "tʰ",
+		["ժ"] = "ʒ",
+		["ի"] = "i",
+		["լ"] = "l",
+		["խ"] = "χ",
+		["ծ"] = "d͡z",
+		["կ"] = "ɡ",
+		["հ"] = "h",
+		["ձ"] = "t͡sʰ",
+		["ղ"] = "ʁ",
+		["ճ"] = "d͡ʒ",
+		["մ"] = "m",
+		["յ"] = "j",
+		["ն"] = "n",
+		["շ"] = "ʃ",
+		["ո"] = "o",
+		["չ"] = "t͡ʃʰ",
+		["պ"] = "b",
+		["ջ"] = "t͡ʃʰ",
+		["ռ"] = "r",
+		["ս"] = "s",
+		["վ"] = "v",
+		["տ"] = "d",
+		["ր"] = "ɾ",
+		["ց"] = "t͡sʰ",
+		["ւ"] = "v",
+		["փ"] = "pʰ",
+		["ք"] = "kʰ",
+		["և"] = "ev",
+		["օ"] = "o",
+		["ֆ"] = "f",
+		["-"] = " ",
+		["’"] = "",
+		["-"] = "",
 	},
 }
 
 -- character sequences of two that map to IPA sounds
 local phonetic_2chars_map = {
 	east = {
-		{ 'ու', 'u' },
+		{ "ու", "u" },
 	},
 	west = {
 		-- if not in the initial position and if not preceded by [ɑeəoiu]
-		{ '(.?.?)յու', function(before)
-			if not (before == '' or rfind(before, '[%sաեէիոօ]$')
-			or before == "ու") then
-				return before .. 'ʏ'
-			end
-		end },
-		{ 'ու', 'u' },
-		{ 'էօ', 'œ' },
+		{
+			"(.?.?)յու",
+			function(before)
+				if not (before == "" or rfind(before, "[%sաեէիոօ]$") or before == "ու") then
+					return before .. "ʏ"
+				end
+			end,
+		},
+		{ "ու", "u" },
+		{ "էօ", "œ" },
 		-- պ, տ, կ are not voiced after ս and շ
-		{ 'սպ', 'sp' },
-		{ 'ստ', 'st' },
-		{ 'սկ', 'sk' },
-		{ 'շպ', 'ʃp' },
-		{ 'շտ', 'ʃt' },
-		{ 'շկ', 'ʃk' },
+		{ "սպ", "sp" },
+		{ "ստ", "st" },
+		{ "սկ", "sk" },
+		{ "շպ", "ʃp" },
+		{ "շտ", "ʃt" },
+		{ "շկ", "ʃk" },
 		-- Western Armenian inserts ə in the causative
-		{ 'ցնել', 't͡sʰənel' },
-
+		{ "ցնել", "t͡sʰənel" },
 	},
 }
 
-function export._pronunciation(word, system)
+function export.phonemic_IPA(word, system)
+	if not (phonemic_chars_map[system] and phonemic_2chars_map[system]) then
+		error("Invalid system " .. tostring(system))
+	end
+
+	word = mw.ustring.lower(word)
+
+	local phonemic = word
+
+	-- then long consonants that are orthographically geminated.
+
+	for _, replacement in ipairs(phonemic_2chars_map[system]) do
+		phonemic = mw.ustring.gsub(phonemic, unpack(replacement))
+	end
+
+	-- ոու is pronounced ou
+	phonemic = mw.ustring.gsub(phonemic, "ոːւ", "օու")
+
+	-- ե and ո are pronounced as je and vo word-initially.
+	phonemic = mw.ustring.gsub(phonemic, "^ե", "յէ")
+	phonemic = mw.ustring.gsub(phonemic, "^ո", "վօ")
+	-- except when followed by another վ.
+	phonemic = mw.ustring.gsub(phonemic, "^վօվ", "օվ")
+
+	--final ք, from the ancient plural, is extrasyllabic and should be marked.
+	phonemic = mw.ustring.gsub(phonemic, "([^ɑeiouəœʏ])ք$", "%1·ք")
+
+	-- ոու is pronounced oov
+	phonemic = mw.ustring.gsub(phonemic, "ոու", "օու")
+
+	phonemic = mw.ustring.gsub(phonemic, ".", phonemic_chars_map[system])
+
+	--oov is actually ou
+	phonemic = mw.ustring.gsub(phonemic, "oov", "ou")
+
+	-- palatalization in the Eastern Armenian sequence -ությ-, especially in the suffix -ություն [considered non-standard by strict prescriptivists]
+	if system == "east" then
+		phonemic = mw.ustring.gsub(phonemic, "utʰj", "ut͡sʰj")
+
+		phonemic = mw.ustring.gsub(phonemic, "b(.͡?.?ʰ)", "pʰ%1")
+		phonemic = mw.ustring.gsub(phonemic, "d(.͡?.?ʰ)", "tʰ%1")
+		phonemic = mw.ustring.gsub(phonemic, "ɡ(.͡?.?ʰ)", "kʰ%1")
+		phonemic = mw.ustring.gsub(phonemic, "d͡z(.͡?.?ʰ)", "t͡sʰ%1")
+		phonemic = mw.ustring.gsub(phonemic, "d͡ʒ(.͡?.?ʰ)", "t͡ʃʰ%1")
+		phonemic = mw.ustring.gsub(phonemic, "ʒ(.͡?.?ʰ)", "ʃ%1")
+
+		phonemic = mw.ustring.gsub(phonemic, "z(.)ʰ", "s%1ʰ")
+	end
+
+	if system == "west" then
+		phonemic = mw.ustring.gsub(phonemic, "b([ptk])", "p%1")
+		phonemic = mw.ustring.gsub(phonemic, "d([ptk])", "t%1")
+		phonemic = mw.ustring.gsub(phonemic, "ɡ([ptk])", "k%1")
+		phonemic = mw.ustring.gsub(phonemic, "d͡z([ptk])", "t͡s%1")
+		phonemic = mw.ustring.gsub(phonemic, "d͡ʒ([ptk])", "t͡ʃ%1")
+		phonemic = mw.ustring.gsub(phonemic, "z([ptk])", "s%1")
+		phonemic = mw.ustring.gsub(phonemic, "ʒ([ptk])", "ʃ%1")
+	end
+
+	phonemic = mw.ustring.gsub(phonemic, "ʁ([ptksʃ])", "χ%1")
+	phonemic = mw.ustring.gsub(phonemic, "v([ptksʃ])", "f%1")
+
+	-- generating the stress
+	phonemic = mw.ustring.gsub(phonemic, "%S+", function(word)
+		-- Do not add a stress mark for monosyllabic words. Check to see if the word contains only a single instance of [ɑeəoiuœʏ]+.
+		local numberOfVowels = select(2, mw.ustring.gsub(word, "[ɑeəoiuœʏ]", "%0"))
+
+		-- If polysyllabic, add IPA stress mark using the following rules. The stress is always on the last syllable not
+		-- formed by schwa [ə]. In some rare cases the stress is not on the last syllable. In such cases the stressed vowel
+		-- is marked by the Armenian stress character <՛>, e.g. մի՛թե. So:
+		--      1) Find the vowel followed by <՛>․ If none, jump to step 2. Else check if it is the first vowel of the word.
+		--         If true, put the IPA stress at the beginning, else do step 3.
+		--      2) Find the last non-schwa vowel, i.e. [ɑeoiuœʏ],
+		--      3) If the IPA symbol preceding it is [ɑeəoiuœʏ], i.e. a vowel, put the stress symbol between them,
+		--         if it is NOT [ɑeoiuəœʏ], i.e. it is a consonant,
+		--         put the stress before that consonant.
+		if numberOfVowels > 1 then
+			local rcount
+			word, rcount = mw.ustring.gsub(word, "([^ɑeoiuœʏə]*[ɑeoiuœʏə])՛", "ˈ%1")
+			if rcount == 0 then
+				word = mw.ustring.gsub(word, "([^ɑeoiuœʏə]*[ɑeoiuœʏ][^ɑeoiuœʏə]*)$", "ˈ%1")
+				word = mw.ustring.gsub(
+					word,
+					"([^ɑeoiuœʏə]*[ɑeəoiuœʏ]?[ɑeoiuœʏ][^ɑeoiuœʏə]*ə[^ɑeoiuœʏə]*)$",
+					"ˈ%1"
+				)
+			end
+			-- Including () in the second and third sets will only work
+			-- if () never encloses a vowel.
+			word = mw.ustring.gsub(word, "([ɑeəoiuœʏ])ˈ([^ɑeoiuœʏə()]+)([^ɑeoiuœʏəːˈʰ()j])", "%1%2ˈ%3")
+			word = mw.ustring.gsub(word, "(.)͡ˈ", "ˈ%1͡")
+			return word
+		end
+	end)
+
+	-- move stress marker out of opening/closing parentheses
+	if system == "east" or system == "west" then
+		phonemic = mw.ustring.gsub(phonemic, "ˈ%)", ")ˈ")
+		phonemic = mw.ustring.gsub(phonemic, "%(ˈ", "ˈ(")
+	end
+
+	-- certain words should not undergo voicing assimilation, namely those with stems taking the ending -ք; these were marked with "·", which must be removed.
+	phonemic = mw.ustring.gsub(phonemic, "·", "")
+
+	return phonemic
+end
+
+function export.phonetic_IPA(word, system)
 	if not (phonetic_chars_map[system] and phonetic_2chars_map[system]) then
 		error("Invalid system " .. tostring(system))
 	end
-	
+
 	word = rlower(word)
 
 	local phonetic = word
@@ -75,7 +373,7 @@ function export._pronunciation(word, system)
 	for _, replacement in ipairs(phonetic_2chars_map[system]) do
 		phonetic = rsubn(phonetic, unpack(replacement))
 	end
-	
+
 	-- ոու is pronounced ou
 	phonetic = rsubn(phonetic, "ոːւ", "օու")
 
@@ -84,11 +382,11 @@ function export._pronunciation(word, system)
 	phonetic = rsubn(phonetic, "^ո", "վօ")
 	-- except when followed by another վ.
 	phonetic = rsubn(phonetic, "^վօվ", "օվ")
-	
+
 	-- ոու is pronounced oov
 	phonetic = rsubn(phonetic, "ոու", "օու")
 
-	phonetic = rsubn(phonetic, '.', phonetic_chars_map[system])
+	phonetic = rsubn(phonetic, ".", phonetic_chars_map[system])
 
 	--oov is actually ou
 	phonetic = rsubn(phonetic, "oov", "ou")
@@ -223,9 +521,7 @@ function export._pronunciation(word, system)
 		phonetic = rsubn(phonetic, "pʰɡ", "pʰk")
 		phonetic = rsubn(phonetic, "tʰɡ", "tʰk")
 		phonetic = rsubn(phonetic, "kʰɡ", "kʰk")
-
 	end
-
 
 	-- prothetic ə before {s/ʃ/z}{p/t/k/b/d/g} in Western Armenian; this rule is not the norm in Eastern Armenian anymore
 	if system == "west" then
@@ -236,11 +532,11 @@ function export._pronunciation(word, system)
 	phonetic = rsubn(phonetic, "%S+", function(word)
 		-- Do not add a stress mark for monosyllabic words. Check to see if the word contains only a single instance of [ɑeəoiuœʏ]+.
 		local numberOfVowels = select(2, rsubn(word, "[ɑeəoiuœʏ]", "%0"))
-	
-		-- If polysyllabic, add an acute using the following rules. The stress is always on the last syllable not 
+
+		-- If polysyllabic, add an acute using the following rules. The stress is always on the last syllable not
 		-- formed by schwa [ə]. In some rare cases the stress is not on the last syllable. In such cases the stressed vowel
 		-- is marked by the Armenian stress character <՛>, e.g. մի՛թե. So:
-		--      1) Find the vowel followed by <՛> and put the acute on it․ If none, go to step 2. 
+		--      1) Find the vowel followed by <՛> and put the acute on it․ If none, go to step 2.
 		--      2) Find the last non-schwa vowel, i.e. [ɑeoiuœʏ], and put the acute on it.
 		if numberOfVowels > 1 then
 			local rcount
@@ -254,30 +550,108 @@ function export._pronunciation(word, system)
 
 	-- change phonetically-impossible ɾː to ɹː
 	if system == "east" or system == "west" then
-        phonetic = rsubn(phonetic, "ɾː", "ɹː")
-    end
+		phonetic = rsubn(phonetic, "ɾː", "ɹː")
+	end
 
 	if system == "east" or system == "west" then
-        phonetic = rsubn(phonetic, "([td])%1͡([sʃzʒ])(ʰ?)", "%1̚%1͡%2%3")
-        phonetic = rsubn(phonetic, "([td])͡([sʃzʒ])(ʰ?)ː", "%1̚%1͡%2%3")
-    end
+		phonetic = rsubn(phonetic, "([td])%1͡([sʃzʒ])(ʰ?)", "%1̚%1͡%2%3")
+		phonetic = rsubn(phonetic, "([td])͡([sʃzʒ])(ʰ?)ː", "%1̚%1͡%2%3")
+	end
 
 	phonetic = toNFC(phonetic)
 	return phonetic
 end
 
-function export.pronunciation(word, system)
-	if type(word) == "table" then
-		local frame = word
-		local invoke_args, parent_args = frame.args, frame:getParent().args
-		word = invoke_args[1] or parent_args[1]
-		system = invoke_args.system or parent_args.system or "east"
-	end
-	if not word or (word == "") then
-		error("Please put the word as the first positional parameter!")
-	end
+function export.generic_IPA(frame) 
+	local params = {
+		[1] = {},
+		["system"] = {},
+	}
 	
-	return export._pronunciation(word, system)
+	local parent_args = frame:getParent().args
+	local args = require("Module:parameters").process(parent_args, params, nil, "hy-pronunciation", "IPA")
+	
+	local pagename = mw.loadData("Module:headword/data").pagename
+
+	return m_IPA.format_IPA_full({
+			lang = lang,
+			items = {
+					{ pron = "/" .. export.phonemic_IPA(args[1] or pagename, system or "east") .. "/" },
+					{ pron = "[" .. export.phonetic_IPA(args[1] or pagename, system or "east") .. "]" },
+				},
+		})
 end
- 
+
+function export.IPA(frame)
+
+	local params = {
+		["E"] = { type = "boolean", default = "true" },
+		["W"] = { type = "boolean", default = "true" },
+		[1] = {},
+		[2] = {},
+		["e"] = {},
+		["e2"] = {},
+		["coll"] = {},
+		["w"] = {},
+		["w2"] = {},
+		["collw"] = {},
+		["pagename"] = {},
+	}
+
+	local parent_args = frame:getParent().args
+	local args = require("Module:parameters").process(parent_args, params, nil, "hy-pronunciation", "IPA")
+	local lines = {}
+	local function ins(text)
+		table.insert(lines, text)
+	end
+	local function get_pagename()
+		return args.pagename or mw.loadData("Module:headword/data").pagename
+	end
+
+	local function insert_east_or_west(prons, system, standard_accent, coll_pron, coll_qualifier)
+		local items = {}
+		for _, pron in ipairs(prons) do
+			if pron then
+				table.insert(items, { pron = "/" .. export.phonemic_IPA(pron, system) .. "/" })
+				table.insert(items, { pron = "[" .. export.phonetic_IPA(pron, system) .. "]" })
+			end
+		end
+		ins("* " .. m_IPA.format_IPA_full({
+			lang = lang,
+			items = items,
+			a = { standard_accent },
+		}))
+		if coll_pron then
+			ins("** " .. m_IPA.format_IPA_full({
+				lang = lang,
+				items = {
+					{ pron = "/" .. export.phonemic_IPA(coll_pron, system) .. "/" },
+					{ pron = "[" .. export.phonetic_IPA(coll_pron, system) .. "]" },
+				},
+				q = { coll_qualifier, "colloquial" },
+			}))
+		end
+	end
+	if args.E then
+		insert_east_or_west(
+			{ args.e or args[1] or get_pagename(), args[2], args.e2 },
+			"east",
+			"hy-E",
+			args.coll,
+			"Eastern Armenian"
+		)
+	end
+	if args.W then
+		insert_east_or_west(
+			{ args.w or args[1] or get_pagename(), args[2], args.w2 },
+			"west",
+			"hy-W",
+			args.collw,
+			"Western Armenian"
+		)
+	end
+
+	return table.concat(lines, "\n")
+end
+
 return export
