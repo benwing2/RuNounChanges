f45129c39b3c401b5cc5843aa67457e3d222326d
Ben Wing <ben@benwing.com>
2024-05-29 23:49:56 -0500

    add_lang_to_accent_qualifier: also incorporate {{a}} into {{homophones}}, and display lines where incorporation couldn't happen

--- add_lang_to_accent_qualifier.py
+++ add_lang_to_accent_qualifier.py
@@ -84,7 +84,8 @@ def process_text_on_page(index, pagetitle, text):
 
   notes = []
 
-  if not re.search(r"\{\{ *(a|accent) *\|", text):
+  accent_template_re = r"\{\{ *(a|accent) *\|"
+  if not re.search(accent_template_re, text):
     return
   def hack_templates(sectext, langname, langnamecode=None, is_citation=False):
     if langname not in blib.languages_byCanonicalName:
@@ -95,21 +96,21 @@ def process_text_on_page(index, pagetitle, text):
 
     lines = sectext.split("\n")
     for i, line in enumerate(lines):
-      def incorporate_a_into_IPA(m, accent_first):
+      def incorporate_a_into_pron_template(m, accent_first):
         if accent_first:
-          raw_accent_t, raw_ipa_t = m.groups()
+          raw_accent_t, raw_pron_t = m.groups()
         else:
-          raw_ipa_t, raw_accent_t = m.groups()
+          raw_pron_t, raw_accent_t = m.groups()
         accent_t = list(blib.parse_text(raw_accent_t).filter_templates())[0]
-        ipa_t = list(blib.parse_text(raw_ipa_t).filter_templates())[0]
-        tname_ipa = tname(ipa_t)
+        pron_t = list(blib.parse_text(raw_pron_t).filter_templates())[0]
+        tname_pron = tname(pron_t)
         if accent_templates_have_lang:
           accents = blib.fetch_param_chain(accent_t, "2")
           accent_lang = getparam(accent_t, "1")
-          ipa_lang = getparam(ipa_t, "1")
-          if accent_lang != ipa_lang:
+          pron_lang = getparam(pron_t, "1")
+          if accent_lang != pron_lang:
             pagemsg("WARNING: {{%s}} lang '%s' disagrees with {{%s}} lang '%s', not changing: %s" % (
-              tname(accent_t), accent_lang, tname_ipa, ipa_lang, line))
+              tname(accent_t), accent_lang, tname_pron, pron_lang, line))
             return m.group(0)
         else:
           accents = blib.fetch_param_chain(accent_t, "1")
@@ -117,22 +118,28 @@ def process_text_on_page(index, pagetitle, text):
           a_param_name = "a"
         else:
           a_param_name = "aa"
-        if getparam(ipa_t, a_param_name):
-          pagemsg("WARNING: Already saw %s= in {{%s}}, not changing: %s" % (a_param_name, tname_ipa, line))
+        if getparam(pron_t, a_param_name):
+          pagemsg("WARNING: Already saw %s= in {{%s}}, not changing: %s" % (a_param_name, tname_pron, line))
           return m.group(0)
         a_param = ",".join(x.strip() for x in accents)
-        ipa_t.add(a_param_name, a_param)
-        notes.append("incorporate %s=%s into {{%s|%s}}" % (a_param_name, a_param, tname_ipa, getparam(ipa_t, "1")))
-        if tname_ipa == "IPA-lite":
-          blib.set_template_name(ipa_t, "IPA")
+        pron_t.add(a_param_name, a_param)
+        notes.append("incorporate %s=%s into {{%s|%s}}" % (a_param_name, a_param, tname_pron, getparam(pron_t, "1")))
+        if tname_pron == "IPA-lite":
+          blib.set_template_name(pron_t, "IPA")
           notes.append("convert {{IPA-lite}} back to {{IPA}}")
-        return str(ipa_t)
+        return str(pron_t)
       newline = re.sub(
         r"(\{\{\s*(?:a|accent)\s*\|(?:\{\{[^{}]*\}\}|[^{}=])+\}\})[:,]*\s*(\{\{\s*(?:IPA|IPA-lite)\s*\|(?:\{\{[^{}]*\}\}|[^{}])+\}\})",
-        lambda m: incorporate_a_into_IPA(m, accent_first=True), line)
+        lambda m: incorporate_a_into_pron_template(m, accent_first=True), line)
       newline = re.sub(
         r"(\{\{\s*(?:IPA|IPA-lite)\s*\|(?:\{\{[^{}]*\}\}|[^{}])+\}\})\s*(\{\{\s*(?:a|accent)\s*\|(?:\{\{[^{}]*\}\}|[^{}=])+\}\})",
-        lambda m: incorporate_a_into_IPA(m, accent_first=False), newline)
+        lambda m: incorporate_a_into_pron_template(m, accent_first=False), newline)
+      newline = re.sub(
+        r"(\{\{\s*(?:a|accent)\s*\|(?:\{\{[^{}]*\}\}|[^{}=])+\}\})[:,]*\s*(\{\{\s*(?:homophones?|hmp)\s*\|(?:\{\{[^{}]*\}\}|[^{}])+\}\})",
+        lambda m: incorporate_a_into_pron_template(m, accent_first=True), newline)
+      newline = re.sub(
+        r"(\{\{\s*(?:homophones?|hmp)\s*\|(?:\{\{[^{}]*\}\}|[^{}])+\}\})\s*(\{\{\s*(?:a|accent)\s*\|(?:\{\{[^{}]*\}\}|[^{}=])+\}\})",
+        lambda m: incorporate_a_into_pron_template(m, accent_first=False), newline)
       def incorporate_a_into_enPR(m, accent_first):
         if accent_first:
           raw_accent_t, raw_enPR_t = m.groups()
@@ -171,58 +178,65 @@ def process_text_on_page(index, pagetitle, text):
         lines[i] = newline
     sectext = "\n".join(lines)
 
-    parsed = blib.parse_text(sectext)
-    for t in parsed.filter_templates():
-      origt = str(t)
-      tn = tname(t)
-      thislangcode = langnamecode
-      if tn in ["citation", "citations"] and is_citation:
-        thislangcode = getparam(t, "1")
-        langnamecode = thislangcode
-      elif tn in accent_templates:
-        numbered_params = blib.fetch_param_chain(t, "1")
-        if not numbered_params:
-          pagemsg("WARNING: No params: %s" % str(t))
-          continue
-        param1 = numbered_params[0]
-        if (args.skip_already_done and param1 in blib.languages_byCode and
-            len(numbered_params) > 1):
-          pagemsg("Skipping likely already-done template: %s" % str(t))
-          continue
-        must_continue = False
-        for param in t.params:
-          pn = pname(param)
-          if not re.search("^[0-9]+$", pn):
-            pagemsg("WARNING: Unrecognized param %s=%s: %s" % (pn, str(param.value), str(t)))
-            must_continue = True
-            break
-        if not thislangcode:
-          if "Todo/Westrobothnian" in pagetitle:
-            pagemsg("Westrobothnian TODO, using 'und' as langcode")
-            thislangcode = "und"
-            notes.append("use 1=%s for {{%s}} in Westrobothnian TODO page" % (thislangcode, tn))
-          elif "Appendix:Kotava" in pagetitle:
-            pagemsg("Kotava appendix, using 'en' as langcode")
-            thislangcode = "en"
-            notes.append("use 1=%s for {{%s}} in Kotava appendix page" % (thislangcode, tn))
-          elif pagetitle in lang_for_special_pages:
-            thislangcode = lang_for_special_pages[pagetitle]
-            pagemsg("Recognized special page, using '%s' as langcode" % thislangcode)
-            notes.append("use 1=%s for {{%s}} in specially recognized page" % (thislangcode, tn))
-          else:
-            pagemsg("WARNING: Unrecognized language %s, unable to add language: %s" % (langname, str(t)))
+    lines = sectext.split("\n")
+    for i, line in enumerate(lines):
+      if not re.search(accent_template_re, line):
+        continue
+      pagemsg("Accent template remains on section line %s: %s" % (i + 1, line))
+      parsed = blib.parse_text(line)
+      for t in parsed.filter_templates():
+        origt = str(t)
+        tn = tname(t)
+        thislangcode = langnamecode
+        if tn in ["citation", "citations"] and is_citation:
+          thislangcode = getparam(t, "1")
+          langnamecode = thislangcode
+        elif tn in accent_templates:
+          numbered_params = blib.fetch_param_chain(t, "1")
+          if not numbered_params:
+            pagemsg("WARNING: No params: %s" % str(t))
             continue
-        elif pagetitle.startswith("Rhymes:"):
-          notes.append("infer 1=%s for {{%s}} based on page title" % (thislangcode, tn))
-        else:
-          notes.append("infer 1=%s for {{%s}} based on section it's in" % (thislangcode, tn))
-        # Erase all params.
-        del t.params[:]
-        t.add("1", thislangcode)
-        blib.set_param_chain(t, numbered_params, "2")
-        pagemsg("Replace <%s> with <%s>" % (origt, str(t)))
+          param1 = numbered_params[0]
+          if (args.skip_already_done and param1 in blib.languages_byCode and
+              len(numbered_params) > 1):
+            pagemsg("Skipping likely already-done template: %s" % str(t))
+            continue
+          must_continue = False
+          for param in t.params:
+            pn = pname(param)
+            if not re.search("^[0-9]+$", pn):
+              pagemsg("WARNING: Unrecognized param %s=%s: %s" % (pn, str(param.value), str(t)))
+              must_continue = True
+              break
+          if not thislangcode:
+            if "Todo/Westrobothnian" in pagetitle:
+              pagemsg("Westrobothnian TODO, using 'und' as langcode")
+              thislangcode = "und"
+              notes.append("use 1=%s for {{%s}} in Westrobothnian TODO page" % (thislangcode, tn))
+            elif "Appendix:Kotava" in pagetitle:
+              pagemsg("Kotava appendix, using 'en' as langcode")
+              thislangcode = "en"
+              notes.append("use 1=%s for {{%s}} in Kotava appendix page" % (thislangcode, tn))
+            elif pagetitle in lang_for_special_pages:
+              thislangcode = lang_for_special_pages[pagetitle]
+              pagemsg("Recognized special page, using '%s' as langcode" % thislangcode)
+              notes.append("use 1=%s for {{%s}} in specially recognized page" % (thislangcode, tn))
+            else:
+              pagemsg("WARNING: Unrecognized language %s, unable to add language: %s" % (langname, str(t)))
+              continue
+          elif pagetitle.startswith("Rhymes:"):
+            notes.append("infer 1=%s for {{%s}} based on page title" % (thislangcode, tn))
+          else:
+            notes.append("infer 1=%s for {{%s}} based on section it's in" % (thislangcode, tn))
+          # Erase all params.
+          del t.params[:]
+          t.add("1", thislangcode)
+          blib.set_param_chain(t, numbered_params, "2")
+          pagemsg("Replace <%s> with <%s>" % (origt, str(t)))
+      line = str(parsed)
+    sectext = "\n".join(lines)
 
-    return str(parsed), langnamecode
+    return sectext, langnamecode
 
   pagemsg("Processing")
 
