2021311b9b3be68dbc325cd121b41d0725893f14
Ben Wing <ben@benwing.com>
2024-06-02 02:18:14 -0500

    make_parallel_run: support --overlap-offset, --no-sleep; bug fixes

--- make_parallel_run.py
+++ make_parallel_run.py
@@ -8,6 +8,8 @@ parser.add_argument('--num-parts', help="Number of parallel parts.", type=int, d
 parser.add_argument('--command', help="Command to run.", required=True)
 parser.add_argument('--output-prefix', help="Prefix for output files.")
 parser.add_argument('--num-terms', help="Approximate number of terms that will be run on.", type=int, required=True)
+parser.add_argument('--overlap-offset', help="Value to add (or subtract if negative) to the beginning of the next run to get the last term that this run will run on. Set to -1 for no overlap.", type=int, default=5)
+parser.add_argument('--no-sleep', help="Don't sleep at beginning of runs (normally done so offsets will remain true; not necessary if offsets won't change as files are saved).", action="store_true")
 args = parser.parse_args()
 
 print("#!/bin/sh")
@@ -19,12 +21,16 @@ num_terms_per_run = args.num_terms // args.num_parts
 # changes the category or references.
 sleep_increment = int(num_terms_per_run / 1000 * 1.1)
 for run in range(args.num_parts):
-  sleep = (args.num_parts - run - 1) * sleep_increment
   sleep_prefix = ""
-  if sleep > 0:
-    sleep_prefix = "sleep %s && " % sleep
+  if not args.no_sleep:
+    sleep = (args.num_parts - run - 1) * sleep_increment
+    if sleep > 0:
+      sleep_prefix = "sleep %s && " % sleep
   first_term_index = max(1, run * num_terms_per_run)
-  last_term_index = (run + 1) * num_terms_per_run + 5
+  if run + 1 == args.num_parts:
+    last_term_index = args.num_terms
+  else:
+    last_term_index = (run + 1) * num_terms_per_run + args.overlap_offset
   print("%s%s --save %s %s > %s.out.%s.%s-%s &"
     % (sleep_prefix, args.command, first_term_index, last_term_index, args.output_prefix,
        run + 1, first_term_index, last_term_index))
