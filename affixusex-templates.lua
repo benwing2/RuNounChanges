16697efa8d25fb25d810dfa891ad466899108682
Ben Wing <ben@benwing.com>
2024-06-14 19:09:30 -0500

    affixusex-templates: latest from Wiktionary

--- affixusex-templates.lua
+++ affixusex-templates.lua
@@ -1,6 +1,17 @@
-local export = {}
+local require = require
 
 local m_languages = require("Module:languages")
+
+local concat = table.concat
+local find = string.find
+local gsub = string.gsub
+local insert = table.insert
+local match = string.match
+local shallowcopy = require("Module:table").shallowcopy
+local sort = table.sort
+local sub = string.sub
+
+local export = {}
 
 -- Per-param modifiers, which can be specified either as separate parameters (e.g. t2=, pos3=) or as inline modifiers
 -- <t:...>, <pos:...>, etc. The key is the name fo the parameter (e.g. "t", "pos") and the value is a table with
@@ -16,9 +27,6 @@ local m_languages = require("Module:languages")
 -- * `item_dest`: The name of the key used when storing the parameter's value into the processed `parts` list.
 --                Normally the same as the parameter's name. Different in the case of "gloss", which is an alias for
 --                "t".
--- * `param_key`: The name of the key used in the `params` spec passed to [[Module:parameters]]. Normally the same as
---                the parameter's name. Different in the case of "sc", where we distinguish per-term parameters
---                "sc1", "sc2", etc. from the overall script parameter "sc".
 local param_mods = {
 	t = {},
 	gloss = {
@@ -37,24 +45,19 @@ local param_mods = {
 	lit = {},
 	pos = {},
 	lang = {
+		extra_specs = {type = "language", etym_lang = true},
 		convert = function(arg, inline, i)
-			-- i + 1 because we want to reference the actual term param name, which is 2= for the first term, 3= for
-			-- the second, etc.
-			return m_languages.getByCode(arg, inline and "" .. (i + 1) .. ":lang" or "lang" .. i, "allow etym")
+			-- i + 1 because we want to reference the actual term param name, which is 2= for the first term, 3= for the second, etc.
+			return inline and m_languages.getByCode(arg, (i + 1) .. ":lang", "allow etym") or arg
 		end,
 	},
 	sc = {
 		-- sc1=, sc2=, ... are different from sc=; the former apply to individual arguments when lang1=, lang2=, ...
-		-- is specified, while the latter applies to all arguments where langN=... isn't specified. To handle this in
-		-- separate parameters, we need to set the key in the `params` object passed to [[Module:parameters]] to
-		-- something else (in this case "langsc") and set `list = "sc"` in the value of the `params` object. This
-		-- causes [[Module:parameters]] to fetch parameters named sc1=, sc2= etc. but store them into "langsc", while
-		-- sc= is stored into "sc".
-		param_key = "langsc",
-		extra_specs = {list = "sc"},
+		-- is specified, while the latter applies to all arguments where langN=... isn't specified.
+		extra_specs = {type = "script", separate_no_index = true, require_index = false},
 		convert = function(arg, inline, i)
 			-- i + 1 same as above for "lang".
-			return require("Module:scripts").getByCode(arg, inline and "" .. (i + 1) .. ":sc" or "sc" .. i)
+			return inline and require("Module:scripts").getByCode(arg, (i + 1) .. ":sc") or arg
 		end,
 	},
 	arrow = {
@@ -73,53 +76,41 @@ local param_mods = {
 	fulljoiner = {},
 	accel = {
 		convert = function(arg, inline, i)
-			return arg:gsub("_", "|") -- To allow use of | in templates
+			return gsub(arg, "_", "|") -- To allow use of | in templates
 		end,
 	},
 }
 
+local default_param_spec = {list = true, allow_holes = true, require_index = true}
+
 local function get_valid_prefixes()
 	local valid_prefixes = {}
 	for param_mod, _ in pairs(param_mods) do
-		table.insert(valid_prefixes, param_mod)
+		insert(valid_prefixes, param_mod)
 	end
-	table.sort(valid_prefixes)
+	sort(valid_prefixes)
 	return valid_prefixes
-end
-
-local rfind = mw.ustring.find
-local rsubn = mw.ustring.gsub
-
--- helper functions
-
--- version of rsubn() that discards all but the first return value
-local function rsub(term, foo, bar)
-	local retval = rsubn(term, foo, bar)
-	return retval
 end
 
 function export.affixusex_t(frame)
 	local params = {
-		[1] = {required = true, default = "und"},
+		[1] = {required = true, type = "language", etym_lang = true, default = "und"},
 		[2] = {list = true, allow_holes = true},
 		
 		["altaff"] = {},
-		["sc"] = {},
 		["nointerp"] = {type = "boolean"},
 		["pagename"] = {},
 	}
-
-	local default_param_spec = {list = true, allow_holes = true, require_index = true}
+	
 	for param_mod, param_mod_spec in pairs(param_mods) do
-		local param_key = param_mod_spec.param_key or param_mod
 		if not param_mod_spec.extra_specs then
-			params[param_key] = default_param_spec
+			params[param_mod] = default_param_spec
 		else
-			local param_spec = mw.clone(default_param_spec)
+			local param_spec = shallowcopy(default_param_spec)
 			for k, v in pairs(param_mod_spec.extra_specs) do
 				param_spec[k] = v
 			end
-			params[param_key] = param_spec
+			params[param_mod] = param_spec
 		end
 	end
 
@@ -133,13 +124,12 @@ function export.affixusex_t(frame)
 	elseif aftype == "suffix" then
 		params["altsuf"] = {alias_of = "altaff"}
 	end
-
+	
 	local args = require("Module:parameters").process(frame:getParent().args, params)
-
-	local lang = m_languages.getByCode(args[1], 1)
-	local sc = args["sc"]
-	sc = sc and require("Module:scripts").getByCode(sc, "sc") or nil
-
+	
+	local lang = args[1]
+	local sc = args.sc.default
+	
 	-- Find the maximum index among any of the list parameters.
 	local maxmaxindex = 0
 	for k, v in pairs(params) do
@@ -159,8 +149,7 @@ function export.affixusex_t(frame)
 		-- Parse all the term-specific modifiers and store in `part`.
 		for param_mod, param_mod_spec in pairs(param_mods) do
 			local dest = param_mod_spec.item_dest or param_mod
-			local param_key = param_mod_spec.param_key or param_mod
-			local arg = args[param_key][i]
+			local arg = args[param_mod] and args[param_mod][i]
 			if arg then
 				if param_mod_spec.convert then
 					arg = param_mod_spec.convert(arg, false, i)
@@ -172,11 +161,11 @@ function export.affixusex_t(frame)
 		-- Remove and remember an initial exclamation point from the term, and parse off an initial language code (e.g.
 		-- 'la:minūtia' or 'grc:[[σκῶρ|σκατός]]').
 		if term then
-			if rfind(term, "^!") then
+			if sub(term, 1, 1) == "!" then
 				part.begins_with_exclamation_point = true
-				term = rsub(term, "^!", "")
+				term = gsub(term, "^!", "")
 			end
-			local termlang, actual_term = term:match("^([A-Za-z0-9._-]+):(.*)$")
+			local termlang, actual_term = match(term, "^([%w._-]+):(.*)$")
 			if termlang and termlang ~= "w" then -- special handling for w:... links to Wikipedia
 				-- i + 1 because terms begin at 2=.
 				termlang = m_languages.getByCode(termlang, i + 1, "allow etym")
@@ -196,13 +185,13 @@ function export.affixusex_t(frame)
 		-- the sort we parse here should consist of a less-than sign, plus letters, plus a colon, e.g. <tr:...>, so if
 		-- we see a tag on the outer level that isn't in this format, we don't try to parse it. The restriction to the
 		-- outer level is to allow generated HTML inside of e.g. qualifier tags, such as foo<q:similar to {{m|fr|bar}}>.
-		if term and term:find("<") and not term:find("^[^<]*<[a-z]*[^a-z:]") then
+		if term and find(term, "<", 1, true) and not match(term, "^[^<]*<%l*[^%l:]") then
 			if not put then
 				put = require("Module:parse utilities")
 			end
 			local run = put.parse_balanced_segment_run(term, "<", ">")
 			local function parse_err(msg)
-				error(msg .. ": " .. (i + 1) .. "=" .. table.concat(run))
+				error(msg .. ": " .. (i + 1) .. "=" .. concat(run))
 			end
 			part.term = run[1]
 
@@ -210,18 +199,18 @@ function export.affixusex_t(frame)
 				if run[j + 1] ~= "" then
 					parse_err("Extraneous text '" .. run[j + 1] .. "' after modifier")
 				end
-				local modtext = run[j]:match("^<(.*)>$")
+				local modtext = match(run[j], "^<(.*)>$")
 				if not modtext then
 					parse_err("Internal error: Modifier '" .. modtext .. "' isn't surrounded by angle brackets")
 				end
-				local prefix, arg = modtext:match("^([a-z]+):(.*)$")
+				local prefix, arg = match(modtext, "^(%l+):(.*)$")
 				if not prefix then
 					parse_err(("Modifier %s lacks a prefix, should begin with one of %s followed by a colon"):format(
-						run[j], table.concat(get_valid_prefixes(), ",")))
+						run[j], concat(get_valid_prefixes(), ",")))
 				end
 				if not param_mods[prefix] then
 					parse_err(("Unrecognized prefix '%s' in modifier %s, should be one of %s"):format(
-						prefix, run[j], table.concat(get_valid_prefixes(), ",")))
+						prefix, run[j], concat(get_valid_prefixes(), ",")))
 				end
 				local dest = param_mods[prefix].item_dest or prefix
 				if part[dest] then
@@ -234,7 +223,7 @@ function export.affixusex_t(frame)
 			end
 		end
 
-		table.insert(parts, part)
+		insert(parts, part)
 	end
 
 	-- Determine whether the terms in the numbered params contain a prefix or suffix. If not, we may insert one before
@@ -244,7 +233,7 @@ function export.affixusex_t(frame)
 	for i=1, maxmaxindex do
 		if parts[i].term then
 			-- Careful here, a prefix beginning with ! should be treated as a normal term.
-			if parts[i].begins_with_exclamation_point or lang:makeEntryName(parts[i].term) == SUBPAGE then
+			if parts[i].begins_with_exclamation_point or (lang:makeEntryName(parts[i].term)) == SUBPAGE then
 				affix_in_parts = true
 				if not parts[i].alt then
 					parts[i].alt = parts[i].term
@@ -260,7 +249,7 @@ function export.affixusex_t(frame)
 	-- Determine affix to interpolate if needed.
 	local affix = args["altaff"]
 	if not affix then
-		if lang:getType() == "reconstructed" then
+		if lang:hasType("reconstructed") then
 			affix = "*" .. SUBPAGE
 		else
 			affix = SUBPAGE
@@ -280,11 +269,17 @@ function export.affixusex_t(frame)
 	-- preceding.
 	if not args["nointerp"] and not affix_in_parts then
 		if aftype == "prefix" or (
-			aftype == "affix" and rfind(insertable_aff, "%-$") and not rfind(insertable_aff, "^%-")) then
-			table.insert(parts, 1, {alt = affix})
+			aftype == "affix" and
+			sub(insertable_aff, -1) == "-" and
+			sub(insertable_aff, 1, 1) ~= "-"
+		) then
+			insert(parts, 1, {alt = affix})
 		elseif aftype == "suffix" or (
-			aftype == "affix" and rfind(insertable_aff, "^%-") and not rfind(insertable_aff, "%-$")) then
-			table.insert(parts, maxmaxindex, {alt = affix})
+			aftype == "affix" and
+			sub(insertable_aff, 1, 1) == "-" and
+			sub(insertable_aff, -1) ~= "-"
+		) then
+			insert(parts, maxmaxindex, {alt = affix})
 		end
 	end
 
