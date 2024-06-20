176515f82309633a05e33e3af498b247bdc35a20
Ben Wing <ben@benwing.com>
2024-06-19 21:35:44 -0500

    affix-pseudo-loan: latest from Wiktionary

--- affix-pseudo-loan.lua
+++ affix-pseudo-loan.lua
@@ -23,9 +23,12 @@ local function get_pseudo_loan_text(lang, source, has_parts, nocap)
 	end
 	local text
 	if langcode == "ja" and sourcecode == "en" then
-		text = "[[Appendix:Japanese glossary#wasei eigo|" .. (nocap and "w" or "W") .. "asei eigo]] ({{m|ja|和製英語}}; " ..
-			glossary_pseudo_loan_link("pseudo-anglicism") .. ")"
-		text = mw.getCurrentFrame():preprocess(text)
+		local ja_link = require("Module:links").full_link({
+			term = "和製英語",
+			lang = require("Module:languages").getByCode("ja"),
+			tr = "-"
+		}, "term")
+		text = "[[Appendix:Japanese glossary#wasei eigo|" .. (nocap and "w" or "W") .. "asei eigo]] (" .. ja_link .. "; " .. glossary_pseudo_loan_link("pseudo-anglicism") .. ")"
 	elseif pseudo_loan_by_source[sourcecode] then
 		text = glossary_pseudo_loan_link((nocap and "p" or "P") .. "seudo-" .. pseudo_loan_by_source[sourcecode])
 	else
@@ -42,6 +45,8 @@ function export.show_pseudo_loan(data)
 	local parts_formatted = {}
 	local categories = {}
 
+	data.force_cat = data.force_cat or debug_force_cat
+
 	if not data.nocat then
 		table.insert(categories, "pseudo-loans from " .. data.source:getCanonicalName())
 		table.insert(categories, "terms derived from " .. data.source:getCanonicalName())
@@ -49,22 +54,23 @@ function export.show_pseudo_loan(data)
 
 	-- Make links out of all the parts
 	for i, part in ipairs(data.parts) do
-		table.insert(parts_formatted, m_affix.link_term(part,
-			-- If the part is in a language other than the source, we need to pass `lang` here and not `source`,
-			-- because the value is used as the destination language in derived-from categories. For example, in
-			-- [[Ego-Shooter]], a German pseudo-loan from English but where the first part is from Latin, we'll wrongly
-			-- get the page placed in 'English terms derived from Latin' if we always pass `source`. When the part is in
-			-- the source language, we do need to pass `source` so the part gets linked correctly.
-			part.lang and data.lang or data.source, data.sc, data.sort_key, data.force_cat or debug_force_cat,
-			data.nocat))
+		part.part_lang = part.lang
+		-- When the part is in the source language, we need to use `source` so the part gets linked correctly. Otherwise,
+		-- `data.lang` will be used, which is correct, because the value is used as the destination language in
+		-- derived-from categories. An example is [[Ego-Shooter]], a German pseudo-loan from English but where the
+		-- first part is from Latin.
+		part.lang = part.lang or data.source
+		part.sc = part.sc or data.sc
+		table.insert(parts_formatted, m_affix.link_term(part, data))
 	end
 
 	local text_sections = {}
 	if not data.notext then
 		table.insert(text_sections, get_pseudo_loan_text(data.lang, data.source, #data.parts > 0, data.nocap))
 	end
-	table.insert(text_sections, m_affix.concat_parts(data.lang, parts_formatted, categories, data.nocat, data.sort_key,
-		data.lit, data.force_cat or debug_force_cat))
+	table.insert(text_sections, m_affix.join_formatted_parts {
+		data = data, parts_formatted = parts_formatted, categories = categories}
+	)
 	return table.concat(text_sections)
 end
 
