8cf3913264c5c470a6b8cc476211f7a4bcb29de7
Ben Wing <ben@benwing.com>
2024-06-29 03:08:37 -0500

    object-usage: latest from Wiktionary (recursively parse and format case inside of parens if special characters (+, /, & etc.) are present; include associated cases in parens in the format prep (+ case), or (case +) postp for postpositions; if there's also translit, there's only one set of parens; support ; between parameters; fix spacing of + making case governance)

--- object-usage.lua
+++ object-usage.lua
@@ -134,67 +134,71 @@ function export.show_obj(frame)
 		qualifier_label_mod_with_starred_set["*" .. mod] = true
 	end
 
-	local function parse_one_form(run)
-		local function parse_err(msg)
-			error(msg .. ": '" .. table.concat(run) .. "'")
-		end
-		if #run == 1 and run[1] == "" then
-			error("Blank form not allowed")
-		end
-		local retval = {}
-		retval.form = run[1]
-		retval.form, retval.is_postposition = rsubb(retval.form, "^::", "")
-		if retval.is_postposition then
-			retval.is_term = true
-		else
-			retval.form, retval.is_term = rsubb(retval.form, "^:", "")
-		end
-
-		for i = 2, #run - 1, 2 do
-			if run[i + 1] ~= "" then
-				parse_err("Extraneous text '" .. run[i + 1] .. "' after modifier")
+	local function parse_object(object, paramno)
+		local function parse_one_form(run)
+			local function parse_err(msg)
+				error(msg .. ": '" .. table.concat(run) .. "'")
 			end
-			if run[i]:find("^%(") then
-				if not retval.is_term then
-					parse_err("Can't attach case '" .. run[i] .. "' to non-term")
-				end
-				retval.case = run[i]:gsub("^%((.*)%)$", "%1")
+			if #run == 1 and run[1] == "" then
+				error("Blank form not allowed")
+			end
+			local retval = {}
+			retval.form = run[1]
+			retval.form, retval.is_postposition = rsubb(retval.form, "^::", "")
+			if retval.is_postposition then
+				retval.is_term = true
 			else
-				local modtext = run[i]:match("^<(.*)>$")
-				if not modtext then
-					parse_err("Internal error: Modifier '" .. modtext .. "' isn't surrounded by angle brackets")
+				retval.form, retval.is_term = rsubb(retval.form, "^:", "")
+			end
+
+			for i = 2, #run - 1, 2 do
+				if run[i + 1] ~= "" then
+					parse_err("Extraneous text '" .. run[i + 1] .. "' after modifier")
 				end
-				local prefix, arg = modtext:match("^(%*?[a-z]+):(.*)$")
-				if prefix then
-					if qualifier_label_mod_with_starred_set[prefix] or prefix == "t" or prefix == "id" or
-						prefix == "tr" or prefix == "ts" or prefix == "alt" or prefix == "ref" then
-						if not retval.is_term and not qualifier_label_mod_with_starred_set[prefix] and
-							prefix ~= "ref" and prefix ~= "t" then
-							parse_err("Can't attach prefix '" .. prefix .. "' to non-term")
-						end
-						local item_dest = prefix == "ref" and "refs" or prefix
-						if retval[item_dest] then
-							parse_err("Can't set two values for prefix '" .. prefix .. "'")
-						end
-						if prefix == "l" or prefix == "ll" or prefix == "*l" or prefix == "*ll" then
-							arg = require(labels_module).split_labels_on_comma(arg)
-						elseif prefix == "ref" then
-							arg = require(references_module).parse_references(arg, parse_err)
-						end
-						retval[item_dest] = arg
+				if run[i]:find("^%(") then
+					if not retval.is_term then
+						parse_err("Can't attach case '" .. run[i] .. "' to non-term")
+					end
+					local raw_case = run[i]:gsub("^%((.*)%)$", "%1")
+					if raw_case:find("[+/&<>()%[%]]") then
+						retval.case = parse_object(raw_case, ("%s:%s(...)"):format(retval.form, paramno))
 					else
-						parse_err("Unrecognized prefix '" .. prefix .. "' in modifier " .. run[i])
+						retval.case = raw_case
 					end
 				else
-					retval.t = modtext
+					local modtext = run[i]:match("^<(.*)>$")
+					if not modtext then
+						parse_err("Internal error: Modifier '" .. modtext .. "' isn't surrounded by angle brackets")
+					end
+					local prefix, arg = modtext:match("^(%*?[a-z]+):(.*)$")
+					if prefix then
+						if qualifier_label_mod_with_starred_set[prefix] or prefix == "t" or prefix == "id" or
+							prefix == "tr" or prefix == "ts" or prefix == "alt" or prefix == "ref" then
+							if not retval.is_term and not qualifier_label_mod_with_starred_set[prefix] and
+								prefix ~= "ref" and prefix ~= "t" then
+								parse_err("Can't attach prefix '" .. prefix .. "' to non-term")
+							end
+							local item_dest = prefix == "ref" and "refs" or prefix
+							if retval[item_dest] then
+								parse_err("Can't set two values for prefix '" .. prefix .. "'")
+							end
+							if prefix == "l" or prefix == "ll" or prefix == "*l" or prefix == "*ll" then
+								arg = require(labels_module).split_labels_on_comma(arg)
+							elseif prefix == "ref" then
+								arg = require(references_module).parse_references(arg, parse_err)
+							end
+							retval[item_dest] = arg
+						else
+							parse_err("Unrecognized prefix '" .. prefix .. "' in modifier " .. run[i])
+						end
+					else
+						retval.t = modtext
+					end
 				end
 			end
+			return retval
 		end
-		return retval
-	end
 
-	local parsed_objects = {}
-	for argno, object in ipairs(args[2]) do
 		local parsed_object = {arguments = {}}
 		local orig_segments =
 			m_parse_utilities.parse_multi_delimiter_balanced_segment_run(object, {{"[", "]"}, {"(", ")"}, {"<", ">"}})
@@ -229,9 +233,9 @@ function export.show_obj(frame)
 					-- Join to the previous alternant.
 					table.insert(parsed_object.arguments[#parsed_object.arguments].alternants, form)
 				else
-					local suppress_plus = prev_joiner == "&" 
+					local suppress_with = prev_joiner == "&" 
 					-- Create a new argument.
-					table.insert(parsed_object.arguments, {alternants = {form}, suppress_plus = suppress_plus})
+					table.insert(parsed_object.arguments, {alternants = {form}, suppress_with = suppress_with})
 				end
 			end
 			i = i + 2
@@ -239,8 +243,7 @@ function export.show_obj(frame)
 
 		-- Now move qualifiers up as necessary.
 		local function parse_err(msg)
-			-- argno + 1 because object arguments begin at 2=
-			error(("%s: %s=%s"):format(msg, argno + 1, object))
+			error(("%s: %s=%s"):format(msg, paramno, object))
 		end
 		for _, argument in ipairs(parsed_object.arguments) do
 			for i, alternant in ipairs(argument.alternants) do
@@ -274,12 +277,28 @@ function export.show_obj(frame)
 			end
 		end
 
-		table.insert(parsed_objects, parsed_object)
+		return parsed_object
 	end
 
-	-- Now generate the text.
-	local object_parts = {}
-	for _, parsed_object in ipairs(parsed_objects) do
+	local parsed_objects = {}
+	for argno, object in ipairs(args[2]) do
+		if object == ";" then
+			-- bare semicolon separator, to create a higher-level separation between parameters than
+			-- the normal "; or ..." separator.
+			if not parsed_objects[1] then
+				error("Can't have bare semicolon separator parameter as first parameter")
+			end
+		else
+			-- argno + 1 because object arguments begin at 2=
+			local parsed_object = parse_object(object, argno + 1)
+			if argno > 1 and args[2][argno - 1] == ";" then
+				parsed_object.separator = ";"
+			end
+			table.insert(parsed_objects, parsed_object)
+		end
+	end
+
+	local function format_parsed_object(parsed_object, suppress_initial_with)
 		local argument_parts = {}
 
 		local multiple_alternants = false
@@ -290,9 +309,26 @@ function export.show_obj(frame)
 			end
 		end
 
+		local used_with_in_prefix = false
 		for i, argument in ipairs(parsed_object.arguments) do
 			local alternant_parts = {}
 			for _, alternant in ipairs(argument.alternants) do
+				local case_text
+				if alternant.case then
+					if type(alternant.case) == "string" then
+						case_text = require(form_of_module).tagged_inflections {
+							lang = lang, tags = {alternant.case}, text_classes = text_classes
+						}
+					else
+						case_text = format_parsed_object(alternant.case, "suppress with")
+					end
+					if alternant.is_postposition then
+						case_text = "(" .. case_text .. " +)"
+					else
+						case_text = "+ " .. case_text
+					end
+				end
+
 				local form
 				--local text_classes = "object-usage-form-of-tag"
 				local text_classes = "object-usage-tag"
@@ -302,27 +338,22 @@ function export.show_obj(frame)
 						term = nil
 					end
 					form = m_links.full_link({lang = lang, term = term, alt = alternant.alt, id = alternant.id,
-						tr = alternant.tr, ts = alternant.ts}, "bold")
+						tr = alternant.tr, ts = alternant.ts, pos = not alternant.is_postposition and case_text or nil},
+						"bold")
+					if alternant.is_postposition and case_text then
+						form = case_text .. " " .. form
+					end
 				else
 					form = require(form_of_module).tagged_inflections {
 						lang = lang, tags = {alternant.form}, text_classes = text_classes
 					}
-				end
-
-				if alternant.case then
-					local case_text = "+ " .. require(form_of_module).tagged_inflections {
-						lang = lang, tags = {alternant.case}, text_classes = text_classes
-					}
-					if alternant.is_postposition then
-						form = case_text .. " " .. form
-					else
-						form = form .. " " .. case_text
+					if case_text then
+						if alternant.is_postposition then
+							form = case_text .. " " .. form
+						else
+							form = form .. " (" .. case_text .. ")"
+						end
 					end
-				end
-
-				local qualifier_text = ""
-				if alternant.q then
-					qualifier_text = require("Module:qualifier").format_qualifier(alternant.q) .. " "
 				end
 
 				local meaning_text = ""
@@ -344,10 +375,12 @@ function export.show_obj(frame)
 				table.insert(alternant_parts, part)
 			end
 			local prefix, separator
-			if not argument.suppress_plus then
-				if i == 1 then
-					separator = ""
+			local suppress_with = argument.suppress_with or i == 1 and suppress_initial_with
+			if not suppress_with then
+				if not used_with_in_prefix then
+					separator = i > 1 and " " or ""
 					prefix = "''with'' "
+					used_with_in_prefix = true
 				elseif multiple_alternants then
 					separator = ", "
 					prefix = "''along with'' "
@@ -372,10 +405,28 @@ function export.show_obj(frame)
 			end
 			table.insert(argument_parts, separator .. part)
 		end
-		table.insert(object_parts, table.concat(argument_parts))
+		return table.concat(argument_parts)
 	end
 
-	return require("Module:TemplateStyles")("Module:User:Benwing2/object usage/style.css") .. "[" .. table.concat(object_parts, "; ''or''&ensp;") .. "]"
+	-- Now generate the text.
+	local object_parts = {}
+	local function ins(txt)
+		table.insert(object_parts, txt)
+	end
+	ins(require("Module:TemplateStyles")("Module:User:Benwing2/object usage/style.css"))
+	ins("[")
+	for i, parsed_object in ipairs(parsed_objects) do
+		if i > 1 then
+			if parsed_object.separator == ";" then
+				ins("; ''in addition,''&ensp;")
+			else
+				ins("; ''or''&ensp;")
+			end
+		end
+		ins(format_parsed_object(parsed_object, false))
+	end
+	ins("]")
+	return table.concat(object_parts)
 end
 
 
