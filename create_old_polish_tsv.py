9690e0c352e3acba84b9764aae8dcbdd7b652b90
Ben Wing <ben@benwing.com>
2024-08-01 03:58:48 -0500

    create_old_polish_tsv,create_old_polish_boilerplate: new scripts (the latter NOT READY)

---
+++ create_old_polish_tsv.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse, json, re
+
+parser = argparse.ArgumentParser(description="Create TSV from Old Polish JSON dump.")
+parser.add_argument('--direcfile', help="File containing JSON.", required=True)
+args = parser.parse_args()
+
+data = json.loads(open(args.direcfile).read())
+for k, v in data.items():
+  index = int(k) + 1
+  pagename = v["pagename"]
+  defs = v["defs"]
+  for defn, quotes in defs.items():
+    for quotenum, quote in enumerate(quotes):
+      quote_type = quote["typ"]
+      rodzaj = quote["rodzaj"]
+      numer = quote["numer"]
+      funkcja = quote.get("funkcja", "")
+      gramatyka = quote.get("gramatyka", "")
+      semantyka = quote.get("semantyka", "")
+      def clean_up_html(text):
+        return text.replace("&lt", "<").replace("&gt", ">")
+      defn = clean_up_html(defn)
+      uwagi = clean_up_html(quote.get("uwagi", ""))
+      orig = clean_up_html(quote["transliteracji"])
+      def split_quote_year(val):
+        m = re.search(r"^(.*?) (?:\(([0-9][0-9][0-9][0-9])\) )?((?:ca )?(?:[0-9][0-9][0-9][0-9](?:[-–—][0-9]+)?|X[XIV]*(?: (?:ex|med|p\. *pr|p\. *in|p\. *post))?))?\.?$", val)
+        if m:
+          val, paren_year, year = m.groups()
+          if paren_year:
+            published_year = year or ""
+            year = paren_year
+          else:
+            year = year or ""
+            published_year = ""
+        else:
+          year = ""
+          published_year = ""
+        year = re.sub("^ca ", "c. ", year)
+        published_year = re.sub("^ca ", "c. ", published_year)
+        return val, year, published_year
+      orig, origyear, origpublished_year = split_quote_year(orig)
+      norm = clean_up_html(quote["transkrypcji"])
+      norm, normyear, normpublished_year = split_quote_year(norm)
+      source = quote["lokalizacja"]
+      line = [index, pagename, defn, quotenum + 1, orig, origyear, origpublished_year, norm, normyear,
+              normpublished_year, source, quote_type, rodzaj, numer, funkcja, gramatyka, semantyka, uwagi]
+      print("\t".join("%s" % x for x in line))
