9c2efa2cc0b99c44f11f8e1aa1d7cb7e76d22e1f
Ben Wing <ben@benwing.com>
2024-10-17 02:30:28 -0500

    fix_cog_usage: allow for multiple languages converted to comma-separated {{cog|LANG1,LANG2,LANG3|...}}; remove {{etyl}} support; {{m|...}} can have etym-only langs; misc fixes

--- fix_cog_usage.py
+++ fix_cog_usage.py
@@ -110,10 +110,16 @@ for code, desc in blib.etym_languages_byCode.items():
     for othername in desc["otherNames"]:
       add_name_with_code(othername, code, False, True)
 
+# 2024-10-15: temporary hack for recently renamed language Venetian -> Venetan (still in dump)
+if "Venetan" in language_name_to_code:
+  language_name_to_code["Venetian"] = language_name_to_code["Venetan"]
+
 def process_text_on_page(index, pagetitle, pagetext):
   global args
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagetitle, txt))
+
+  notes = []
 
   if not args.stdin:
     pagemsg("Processing")
@@ -129,76 +135,148 @@ def process_text_on_page(index, pagetitle, pagetext):
     sections[-1] += splitsections[i]
 
   def replace_with_cog(m):
-    punct, cognate_with, cog, langname, tempname, langcode, vbar = m.groups()
+    at_beg, punct, cognate_with, cog, langname, tempname, langcode, vbar, at_end = m.groups()
     origtext = m.group(0)
-    if langname.startswith("{{etyl"):
-      mm = re.search(r"^\{\{etyl\|(.*?)\|-\}\}$", langname)
-      if not mm:
-        pagemsg("WARNING: Something wrong, can't match template call %s" % langname)
-        return origtext
-      etym_langcode = mm.group(1)
-      if etym_langcode != langcode and etym_language_to_parent.get(etym_langcode, "NONE") != langcode:
-        pagemsg("WARNING: Mismatched language codes, saw %s vs. %s in %s {{m|%s|...}}"
-          % (etym_langcode, langcode, langname, langcode))
-        return origtext
-      if langcode != etym_langcode:
-        pagemsg("Using etym language code %s in place of parent %s" % (
-          etym_langcode, langcode))
-      newtext = "%s%s%s{{cog|%s|" % (punct, cognate_with, cog, etym_langcode)
-      pagemsg("Replacing <%s> with <%s>" % (origtext, newtext))
-      return newtext
+    def warning(txt):
+      if args.begin_end:
+        pagemsg("WARNING: %s; <begin> %s <end>" % (txt, origtext))
+      else:
+        pagemsg("WARNING: %s" % txt)
+    # {{etyl}} is obsolete
+    #if langname.startswith("{{etyl"):
+    #  mm = re.search(r"^\{\{etyl\|(.*?)\|-\}\}$", langname)
+    #  if not mm:
+    #    warning("Something wrong, can't match template call %s" % langname)
+    #    return origtext
+    #  etym_langcode = mm.group(1)
+    #  if etym_langcode != langcode and etym_language_to_parent.get(etym_langcode, "NONE") != langcode:
+    #    pagemsg("WARNING: Mismatched language codes, saw %s vs. %s in %s {{m|%s|...}}"
+    #      % (etym_langcode, langcode, langname, langcode))
+    #    return origtext
+    #  if langcode != etym_langcode:
+    #    pagemsg("Using etym language code %s in place of parent %s" % (
+    #      etym_langcode, langcode))
+    #  newtext = "%s%s%s{{cog|%s|" % (punct, cognate_with, cog, etym_langcode)
+    #  pagemsg("Replacing <%s> with <%s>" % (origtext, newtext))
+    #  return newtext
 
-    if langname not in language_name_to_code:
-      pagemsg("WARNING: Saw unrecognized lang name <%s> (lang code=%s)" % (
-        langname, langcode))
-      return origtext
-    langcodes, etymcode, isetymcanon = language_name_to_code[langname]
-    is_etym_lang = etymcode and etym_language_to_parent[etymcode] == langcode
-    is_non_etym_lang_canon = False
-    is_non_etym_lang = False
-    for code, iscanon in langcodes:
-      if langcode == code:
-        is_non_etym_lang = True
-        is_non_etym_lang_canon = iscanon
-        break
-    if is_etym_lang and is_non_etym_lang:
-      pagemsg("NOTE: Language name could be both etym lang %s (canon=%s) and non-etym lang %s (canon=%s)" % (
-        etymcode, isetymcanon, langcode, is_non_etym_lang_canon))
-    if is_etym_lang and (not is_non_etym_lang or isetymcanon or not is_non_etym_lang_canon):
-      pagemsg("Using etym language code %s in place of parent %s for language name %s" % (
-        etymcode, langcode, langname))
-      langcode_to_use = etymcode
-    elif is_non_etym_lang:
-      langcode_to_use = langcode
+    raw_langname = langname
+    dialectal = ""
+    # Remove stray comma at end, which appears occasionally.
+    langname = re.sub(" *, *$", "", langname)
+    if " and " in langname or ", " in langname:
+      langnames = re.split("(?:,* and |, )", langname)
     else:
-      pagemsg("WARNING: lang name <%s> isn't a name of lang code %s or any etym language descending from it; expected %s" % (
-        langname, langcode,
-        langcodes and etymcode and "lang code(s) %s or etym parent lang code %s" % (
-          ",".join(code for code, _ in langcodes), etym_language_to_parent[etymcode]) or
-        langcodes and "lang code(s) %s" % ",".join(code for code, _ in langcodes) or
-        "etym parent lang code %s" % etym_language_to_parent[etymcode]))
+      m = re.search("^dialectal (.*)$", langname)
+      if m:
+        langname = m.group(1)
+        dialectal = "dialectal "
+      if not m:
+        m = re.search("^(.*) dialect(?:al)?$", langname)
+        if m:
+          langname = m.group(1)
+          dialectal = "dialectal "
+      langnames = [langname]
+    langcodes = []
+    langcodes_for_checking = []
+    if langcode in blib.language_aliases_to_canonical:
+      langcode = blib.language_aliases_to_canonical[langcode]
+    langname_code_info = []
+    for langname in langnames:
+      if langname not in language_name_to_code:
+        warning("Saw unrecognized lang name <%s> (lang code=%s)" % (langname, langcode))
+        return origtext
+      this_langcodes, etymcode, isetymcanon = language_name_to_code[langname]
+      langname_code_info.append((langname, language_name_to_code[langname]))
+      is_etym_lang = not not etymcode
+      is_etym_lang_matching_langcode = is_etym_lang and (
+        etymcode == langcode or
+        etym_language_to_parent[etymcode] == langcode
+      )
+      is_non_etym_lang = False
+      is_non_etym_lang_canon = False
+      is_non_etym_lang_matching_langcode = False
+      is_non_etym_lang_canon_matching_langcode = False
+      best_non_etym_code = None
+      for code, iscanon in this_langcodes:
+        is_non_etym_lang = True
+        is_non_etym_lang_canon = is_non_etym_lang_canon or iscanon
+        if langcode == code:
+          best_non_etym_code = code
+          is_non_etym_lang_matching_langcode = True
+          is_non_etym_lang_canon_matching_langcode = iscanon
+          break
+        elif iscanon or best_non_etym_code is None:
+          best_non_etym_code = code
+      if etymcode and best_non_etym_code:
+        pagemsg("NOTE: Language name could be both etym lang %s (canon=%s) and non-etym lang %s (canon=%s)" % (
+          etymcode, isetymcanon, best_non_etym_code, is_non_etym_lang_canon))
+      use_etym_code = False
+      if is_etym_lang_matching_langcode:
+        use_etym_code = (
+          not is_non_etym_lang_matching_langcode or isetymcanon or not is_non_etym_lang_canon_matching_langcode
+        )
+      elif is_etym_lang:
+        use_etym_code = (
+          not is_non_etym_lang or isetymcanon or not is_non_etym_lang_canon
+        )
+      if use_etym_code:
+        pagemsg("Using etym language code %s for %s language name %s" % (
+          etymcode, "canonical" if isetymcanon else "non-canonical", langname))
+        langcode_to_use = etymcode
+      else:
+        assert is_non_etym_lang
+        langcode_to_use = best_non_etym_code
+        pagemsg("Using full language code %s for %s language name %s" % (
+          best_non_etym_code, "canonical" if is_non_etym_lang_canon else "non-canonical", langname))
+      langcodes.append(langcode_to_use)
+      langcodes_for_checking.append(langcode_to_use)
+      if use_etym_code:
+        langcodes_for_checking.append(etym_language_to_parent[etymcode])
+    if langcode not in langcodes_for_checking:
+      def expected(this_langcodes, etymcode):
+        return (etymcode and "lang code(s) %s or etym-lang parent lang code %s" % (
+            ",".join([code for code, _ in this_langcodes] + ([etymcode] if etymcode else [])),
+            etym_language_to_parent[etymcode]) or
+          "lang code(s) %s" % ",".join(code for code, _ in this_langcodes))
+      if len(langnames) == 1:
+        warning("lang name <%s> isn't a name of lang code %s or any etym language descending from it; expected %s" % (
+          langname, langcode, expected(this_langcodes, etymcode)))
+      else:
+        warning("none of the lang names <%s> are a name of lang code %s or an etym language descending from it; expected %s" % (
+          ",".join(langnames), langcode, "; ".join(
+            "%s: %s" % (langname, expected(this_langcodes, etymcode))
+            for langname, (this_langcodes, etymcode, _) in langname_code_info)))
       return origtext
-    newtext = "%s%s%s{{cog|%s|" % (punct, cognate_with, cog, langcode_to_use)
+    newtext = "%s%s%s%s%s{{cog|%s|%s" % (at_beg, punct, cognate_with, cog, dialectal, ",".join(langcodes), at_end)
+    notes.append("use %s{{cog|%s|...}} for cognates in place of %s {{m|%s|...}}" % (
+      dialectal, ",".join(langcodes), raw_langname, langcode))
     pagemsg("Replacing <%s> with <%s>" % (origtext, newtext))
     return newtext
 
   # Go through each section in turn, looking for Etymology sections
+
+  #match_etyl_re_arm = r"|\{\{etyl\|[A-Za-z0-9.-]+\|-\}\}"
+  match_etyl_re_arm = ""
+  match_cognate_re = r"^(.*?)(^|[;:,.] +|\()((?:[Aa]lso +)?(?:[Cc]ognate +(?:with +|of +|to +)|[Cc]ognates +include +|(?:[Cc]ompare|[Cc]f\.) +(?:with +|to +)?)(?:also +)?(?:the +)?)((?:(?:[A-Z][A-Za-z,-]+(?: [A-Za-z,-]+)*? +)?\{\{(?:cog|[ml]|term)\|[A-Za-z0-9.,-]+\|(?:[^{}]|\{\{[^{}]*?\}\})*\}\}[,;]? *(?:(?:and|or) +(?:also +)?|/ *)?)*?)([A-Z][A-Za-z,-]+(?: [A-Za-z,-]+)*?%s)( +\{\{(?:[ml]|term)\|)([A-Za-z0-9.-]+)(\|)(.*?)$" % match_etyl_re_arm
   for i in range(len(sections)):
     if re.match("^===*Etymology( [0-9]+)?=*==", sections[i]):
       text = sections[i]
       while True:
-        new_text = re.sub(r"(^|[;:,.] +|\()((?:[Aa]lso +)?(?:[Cc]ognate +(?:with +|of +|to +)|[Cc]ognates +include +|[Cc]ompare +(?:with +|to +)?)(?:also +)?(?:the +)?)((?:\{\{(?:cog|[ml]|term)\|[A-Za-z0-9.-]+\|(?:[^{}]|\{\{[^{}]*?\}\})*\}\},? *(?:(?:and|or) +(?:also +)?|/ *)?)*?)([A-Z][A-Za-z-]+(?: [A-Za-z-]+)*?|\{\{etyl\|[A-Za-z0-9.-]+\|-\}\})( +\{\{(?:[ml]|term)\|)([A-Za-z0-9.-]+)(\|)",
-          replace_with_cog, text, 0, re.M)
+        new_text = re.sub(match_cognate_re, replace_with_cog, text, 0, re.M)
         if new_text == text:
           break
         sections[i] = new_text
         text = new_text
 
-  return pagehead + "".join(sections), "Use {{cog}} for cognates in place of LANG {{m|CODE|...}} or {{etyl|CODE|-}} {{m|CODE|...}}"
+  return pagehead + "".join(sections), notes
 
 if __name__ == "__main__":
-  parser = blib.create_argparser("Use {{cog}} for cognates in place of LANG {{m|CODE|...}} or {{etyl|CODE|-}} {{m|CODE|...}}",
+  parser = blib.create_argparser("Use {{cog}} for cognates in place of LANG {{m|CODE|...}}",
     include_pagefile=True, include_stdin=True)
+  parser.add_argument("--begin-end",
+    help="""Output in begin-end format for use with push_manual_changes.py.""",
+    action="store_true")
   args = parser.parse_args()
   start, end = blib.parse_start_end(args.start, args.end)
 
