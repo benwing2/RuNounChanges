4c599cb88882bb02533ca90f12f0bc8c1c65b0c6
Ben Wing <ben@benwing.com>
2024-10-17 04:10:00 -0500

    find_template: support --single-line, --single-line-grouped-tsv

--- find_template.py
+++ find_template.py
@@ -25,15 +25,11 @@ def process_text_on_page(index, pagetitle, text, templates, paramspecs, countpar
   paramset = paramspecs and set(paramspecs) or set()
 
   lines_output = 0
-  def output_found(txt):
-    nonlocal lines_output
-    if args.find_regex_output:
-      if lines_output == 0:
-        pagemsg("-------- begin text --------")
-      msg(txt)
-    else:
-      pagemsg(txt)
-    lines_output += 1
+  template_occurrences = None
+  if args.single_line:
+    template_occurrences = []
+  elif args.single_line_grouped_tsv:
+    template_occurrences = defaultdict(list)
   for t in parsed.filter_templates():
     if args.from_to:
       temptext = "<from> %s <to> %s <end>" % (str(t), str(t))
@@ -41,8 +37,23 @@ def process_text_on_page(index, pagetitle, text, templates, paramspecs, countpar
       temptext = str(t)
     tn = tname(t)
     if tn in templates:
+      def output_found(gloss):
+        txt = "Found %s: %s" % (gloss, temptext)
+        nonlocal lines_output
+        if args.find_regex_output:
+          if lines_output == 0:
+            pagemsg("-------- begin text --------")
+          msg(txt)
+          lines_output += 1
+        elif args.single_line:
+          template_occurrences.append((gloss, temptext))
+        elif args.single_line_grouped_tsv:
+          template_occurrences[tn].append(temptext)
+        else:
+          pagemsg(txt)
+          lines_output += 1
       if not paramspecs and not countparams:
-        output_found("Found %s template: %s" % (tn, temptext))
+        output_found("%s template" % tn)
       else:
         seen_params = set()
         counted_param_values = counted_param_values_by_template[tn]
@@ -54,11 +65,11 @@ def process_text_on_page(index, pagetitle, text, templates, paramspecs, countpar
             if pname not in counted_param_values:
               counted_param_values[pname] = defaultdict(int)
             if pvalue not in counted_param_values[pname]:
-              output_found("Found new value %s=%s for %s template: %s" % (pname, pvalue, tn, temptext))
+              output_found("new value %s=%s for %s template" % (pname, pvalue, tn))
             counted_param_values[pname][pvalue] += 1
           if args.negate:
             if pname not in paramset:
-              output_found("Found %s template with unrecognized param %s=%s: %s" % (tn, pname, pvalue, temptext))
+              output_found("%s template with unrecognized param %s=%s" % (tn, pname, pvalue))
           elif paramspecs:
             for spec in paramspecs:
               found = False
@@ -70,13 +81,13 @@ def process_text_on_page(index, pagetitle, text, templates, paramspecs, countpar
               elif pname == spec:
                 found = True
               if found:
-                output_found("Found %s template with %s=%s: %s" % (tn, pname, pvalue, temptext))
+                output_found("%s template with %s=%s" % (tn, pname, pvalue))
         # Also output occurrences of missing params when !PARAM given
         if paramspecs:
           for spec in paramspecs:
             if type(spec) is tuple and spec[0] == 'notpresent':
               if not getparam(t, spec[1]):
-                output_found("Found %s template with param %s missing or blank: %s" % (tn, spec[1], temptext))
+                output_found("%s template with param %s missing or blank" % (tn, spec[1]))
         # Also track occurrences of params in countparams not occurring
         if countparams:
           for countparam in countparams:
@@ -84,8 +95,21 @@ def process_text_on_page(index, pagetitle, text, templates, paramspecs, countpar
               if countparam not in counted_param_values:
                 counted_param_values[countparam] = defaultdict(int)
               if None not in counted_param_values[countparam]:
-                output_found("Found new value %s=(unseen) for %s template: %s" % (countparam, tn, temptext))
+                output_found("new value %s=(unseen) for %s template" % (countparam, tn))
               counted_param_values[countparam][None] += 1
+  if args.single_line:
+    if template_occurrences:
+      pagemsg("Found %s" % "; ".join("%s (%s)" % (temptext, gloss) for gloss, temptext in template_occurrences))
+      lines_output += 1
+  elif args.single_line_grouped_tsv:
+    if template_occurrences:
+      parts = []
+      parts.append(str(index))
+      parts.append(pagetitle)
+      for tn in templates:
+        parts.append(", ".join(template_occurrences[tn]))
+      msg("\t".join(parts))
+      lines_output += 1
   if lines_output > 0:
     if args.find_regex_output:
       msg("-------- end text --------")
@@ -110,6 +134,13 @@ parser.add_argument("--negate",
 parser.add_argument("--from-to",
     help="""Output in from-to format for use with push_manual_changes.py.""",
     action="store_true")
+parser.add_argument("--single-line",
+    help="""Output in single-line format, with all found templates on the same line.""",
+    action="store_true")
+parser.add_argument("--single-line-grouped-tsv",
+    help="""Output in single-line format, with all found templates on the same line, grouped.
+Templates with a specific name will be grouped together and separated by TAB for easy
+processing in a spreadsheet.""", action="store_true")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
