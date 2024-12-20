11f4d04466f0990b6f9e00091b830f5ac9b48b24
Ben Wing <ben@benwing.com>
2024-12-20 04:32:13 -0600

    gmq-headword: latest from sandbox; auto-adjective support, noun support fixes

--- gmq-headword.lua
+++ gmq-headword.lua
@@ -21,7 +21,10 @@ local m_headword_utilities = require_when_needed(headword_utilities_module)
 local glossary_link = require_when_needed(headword_utilities_module, "glossary_link")
 local inflection_utilities_module = "Module:inflection utilities"
 local m_inflection_utilities = require_when_needed(inflection_utilities_module)
-local is_noun_module = "Module:is-noun"
+local is_adjective_module = "Module:User:Benwing2/is-adjective"
+local is_common_module = "Module:User:Benwing2/is-common"
+local is_noun_module = "Module:User:Benwing2/is-noun"
+local dump = mw.dumpObject
 
 local list_param = {list = true, disallow_holes = true}
 
@@ -40,19 +43,6 @@ end
 local function track(track_id)
 	require("Module:debug/track")("gmq-headword/" .. track_id)
 	return true
-end
-
--- Parse and insert an inflection not requiring additional processing into `data.inflections`. The raw arguments come
--- from `args[field]`, which is parsed for inline modifiers. `label` is the label that the inflections are given;
--- sections enclosed in <<...>> are linked to the glossary. `accel` is the accelerator form, or nil.
-local function parse_and_insert_inflection(data, args, field, label, accel)
-	m_headword_utilities.parse_and_insert_inflection {
-		headdata = data,
-		forms = args[field],
-		paramname = field,
-		label = label,
-		accel = accel and {form = accel} or nil,
-	}
 end
 
 -- The main entry point.
@@ -153,6 +143,112 @@ function export.show(frame)
 	end
 end
 
+-- Parse user-specified arguments in 1=, 2=, etc. for @@ scraping specs. If found, look up the appropriate
+-- inflection(s). Returns two values. If there is no scraping error, the first value is a list of declension
+-- specs (either directly specified by the user or scraped if an @@ spec was given) and the second value is
+-- nil. Otherwise, the first value is nil and the second value is a scraping-error data structure, which the
+-- calling function should directly return upstream.
+local function parse_noun_adj_scraping_specs(data, declspecs, infl_template, allow_empty_infl)
+	if not declspecs[2] and declspecs[1]:find("^@@") then
+		local declid
+		if declspecs[1] ~= "@@" then
+			declid = declspecs[1]:match("^@@%s*:%s*(.+)$")
+			if not declid then
+				error(("Syntax error in self-scraping spec '%s'"):format(declspecs[1]))
+			end
+		end
+		local decls = require(is_common_module).find_inflection(data.pagename, "is-ndecl", false, false, declid)
+		if type(decls) == "string" then
+			data.alternant_multiword_spec = {scrape_errors = {decls}}
+			return nil, {data}
+		end
+		for i, declobj in ipairs(decls) do
+			decls[i] = declobj.decl
+		end
+		declspecs = decls
+	end
+	return declspecs, nil
+end
+
+local function do_noun_adj_form(this_data, alternant_multiword_spec, slot, label, label_for_not_present, accel_form)
+	local forms = alternant_multiword_spec.forms[slot]
+	local retval
+	if not forms then
+		if not label_for_not_present then
+			return
+		end
+		retval = {label = "no " .. label_for_not_present}
+	else
+		retval = {label = label, accel = accel_form and {form = accel_form} or nil}
+		local prev_footnotes
+		for _, form in ipairs(forms) do
+			local footnotes = form.footnotes
+			if footnotes and prev_footnotes and m_table.deepEquals(footnotes, prev_footnotes) then
+				footnotes = nil
+			end
+			prev_footnotes = form.footnotes
+			local quals, refs
+			if footnotes then
+				quals, refs = m_inflection_utilities.fetch_headword_qualifiers_and_references(footnotes)
+			end
+			local term = form.form
+			table.insert(retval, {term = term, q = quals, refs = refs})
+		end
+	end
+
+	table.insert(this_data.inflections, retval)
+end
+
+local function insert_noun_adj_linked_lemma_if_needed(this_data, alternant_multiword_spec, infl_module_name)
+	-- Use the "linked" form of the lemma as the head if no head= explicitly given.
+	if #this_data.heads == 0 then
+		this_data.heads = {}
+		local lemmas = require(infl_module_name).get_lemmas(alternant_multiword_spec, "linked variant")
+		for _, lemma_obj in ipairs(lemmas) do
+			local head = lemma_obj.form
+			--local head = alternant_multiword_spec.args.nolinkhead and lemma_obj.form or
+			--	m_headword_utilities.add_lemma_links(lemma_obj.form, alternant_multiword_spec.args.splithyph)
+			local quals, refs
+			if lemma_obj.footnotes then
+				quals, refs = m_inflection_utilities.fetch_headword_qualifiers_and_references(lemma_obj.footnotes)
+			end
+			table.insert(this_data.heads, {term = head, q = quals, refs = refs})
+		end
+	end
+end
+
+local function get_process_noun_adj_multiple_data(infl_template)
+	return function(multiple_data)
+		local parts = {}
+		local function ins(txt)
+			table.insert(parts, txt)
+		end
+		for i, data in ipairs(multiple_data) do
+			local alternant_multiword_spec = data.alternant_multiword_spec
+			if alternant_multiword_spec.header then
+				ins(("'''%s:'''<br />"):format(alternant_multiword_spec.header))
+			elseif alternant_multiword_spec.q then
+				ins((" ''%s''<br />"):format(alternant_multiword_spec.q))
+			elseif i > 1 then
+				ins(" ''or''<br />")
+			end
+			if alternant_multiword_spec.scrape_errors then
+				local errmsgs = {}
+				for _, scrape_error in ipairs(alternant_multiword_spec.scrape_errors) do
+					table.insert(errmsgs, '<span style="font-weight: bold; color: #CC2200;">' .. scrape_error .. "</span>")
+				end
+				ins(table.concat(errmsgs, "<br />"))
+				ins(require("Module:utilities").format_categories(
+					{("Icelandic scraping errors in Template:%s"):format(infl_template)}, data.lang, nil, nil,
+					force_cat))
+			else
+				ins(require("Module:headword").full_headword(data))
+			end
+		end
+		return table.concat(parts)
+	end
+end
+
 local function get_manual_noun_params(lang, is_proper)
 	return {
 		[1] = {alias_of = "g"},
@@ -247,24 +343,10 @@ local function do_auto_nouns(is_proper, args, data)
 	local alternant_multiword_specs = {}
 	local multiple_data = {}
 	local all_genders = {}
-	local declspecs = args[1]
-	if not declspecs[2] and declspecs[1]:find("^@@") then
-		local declid
-		if declspecs[1] ~= "@@" then
-			declid = declspecs[1]:match("^@@%s*:%s*(.+)$")
-			if not declid then
-				error(("Syntax error in self-scraping spec '%s'"):format(declspecs[1]))
-			end
-		end
-		local decls = m_is_noun.find_declension(data.pagename, false, declid)
-		if type(decls) == "string" then
-			data.alternant_multiword_spec = {scrape_errors = {decls}}
-			return {data}
-		end
-		for i, declobj in ipairs(decls) do
-			decls[i] = declobj.decl
-		end
-		declspecs = decls
+
+	local declspecs, scrape_error_data = parse_noun_adj_scraping_specs(data, args[1], "is-ndecl", false)
+	if scrape_error_data then
+		return scrape_error_data
 	end
 	for i, declspec in ipairs(declspecs) do
 		local alternant_multiword_spec =
@@ -288,63 +370,42 @@ local function do_auto_nouns(is_proper, args, data)
 			for _, gender in ipairs(this_data.genders) do
 				m_table.insertIfNot(all_genders, gender)
 			end
-	
+
 			local function do_noun_form(slot, label, label_for_not_present, accel_form)
-				local forms = alternant_multiword_spec.forms[slot]
-				local retval
-				if not forms then
-					if not label_for_not_present then
-						return
-					end
-					retval = {label = "no " .. label_for_not_present}
-				else
-					retval = {label = label, accel = accel_form and {form = accel_form} or nil}
-					local prev_footnotes
-					for _, form in ipairs(forms) do
-						local footnotes = form.footnotes
-						if footnotes and prev_footnotes and m_table.deepEquals(footnotes, prev_footnotes) then
-							footnotes = nil
-						end
-						prev_footnotes = form.footnotes
-						local quals, refs
-						if footnotes then
-							quals, refs = m_inflection_utilities.fetch_headword_qualifiers_and_references(footnotes)
-						end
-						local term = form.form
-						table.insert(retval, {term = term, q = quals, refs = refs})
-					end
-				end
-	
-				table.insert(this_data.inflections, retval)
+				do_noun_adj_form(this_data, alternant_multiword_spec, slot, label, label_for_not_present, accel_form)
 			end
-	
+
+			local function insert_label(label)
+				table.insert(this_data.inflections, {label = label})
+			end
+
 			if is_proper then
-				table.insert(this_data.inflections, {label = glossary_link("proper noun")})
+				insert_label(glossary_link("proper noun"))
 			end
 			if not alternant_multiword_spec.first_noun and alternant_multiword_spec.first_adj then
-				table.insert(this_data.inflections, {label = "adjectival"})
+				insert_label("adjectival")
 			end
 			local def_prefix
 			if alternant_multiword_spec.definiteness == "def" then
 				def_prefix = "def_"
-				table.insert(this_data.inflections, {label = glossary_link("definite") .. " only"})
+				insert_label(glossary_link("definite") .. " only")
 			else
 				def_prefix = "ind_"
 			end
 			if alternant_multiword_spec.number == "pl" then
-				table.insert(this_data.inflections, {label = glossary_link("plural only")})
+				insert_label(glossary_link("plural only"))
 			end
 			if alternant_multiword_spec.saw_indecl and not alternant_multiword_spec.saw_non_indecl then
-				table.insert(this_data.inflections, {label = glossary_link("indeclinable")})
+				insert_label(glossary_link("indeclinable"))
 			elseif alternant_multiword_spec.saw_unknown_decl and not alternant_multiword_spec.saw_non_unknown_decl then
-				table.insert(this_data.inflections, {label = "unknown declension"})
+				insert_label("unknown declension")
 			elseif alternant_multiword_spec.number == "pl" then
 				do_noun_form(def_prefix .. "gen_p", "genitive plural", "genitive plural")
 			else
 				do_noun_form(def_prefix .. "gen_s", "genitive singular", "genitive singular")
 				do_noun_form(def_prefix .. "nom_p", "nominative plural", not is_proper and "plural" or nil)
 			end
-	
+
 			if i == 1 then
 				-- Parse and insert an inflection not requiring additional processing into `this_data.inflections`. The raw
 				-- arguments come from `args[field]`, which is parsed for inline modifiers. If there is a corresponding
@@ -361,7 +422,7 @@ local function do_auto_nouns(is_proper, args, data)
 						frob = frob,
 					}
 				end
-	
+
 				handle_infl("m", "male equivalent")
 				handle_infl("f", "female equivalent")
 				handle_infl("dim", "<<diminutive>>")
@@ -375,22 +436,8 @@ local function do_auto_nouns(is_proper, args, data)
 			for _, cat in ipairs(alternant_multiword_spec.categories) do
 				table.insert(this_data.categories, cat)
 			end
-	
-			-- Use the "linked" form of the lemma as the head if no head= explicitly given.
-			if #this_data.heads == 0 then
-				this_data.heads = {}
-				local lemmas = m_is_noun.get_lemmas(alternant_multiword_spec, "linked variant")
-				for _, lemma_obj in ipairs(lemmas) do
-					local head = lemma_obj.form
-					--local head = alternant_multiword_spec.args.nolinkhead and lemma_obj.form or
-					--	m_headword_utilities.add_lemma_links(lemma_obj.form, alternant_multiword_spec.args.splithyph)
-					local quals, refs
-					if lemma_obj.footnotes then
-						quals, refs = m_inflection_utilities.fetch_headword_qualifiers_and_references(lemma_obj.footnotes)
-					end
-					table.insert(this_data.heads, {term = head, q = quals, refs = refs})
-				end
-			end
+
+			insert_noun_adj_linked_lemma_if_needed(this_data, alternant_multiword_spec, is_noun_module)
 		end
 	end
 
@@ -417,34 +464,7 @@ local function get_noun_pos_functions(is_proper)
 				return do_manual_nouns(is_proper, args, data)
 			end
 		end,
-		process_multiple_headword_data = function(multiple_data)
-			local parts = {}
-			local function ins(txt)
-				table.insert(parts, txt)
-			end
-			for i, data in ipairs(multiple_data) do
-				local alternant_multiword_spec = data.alternant_multiword_spec
-				if alternant_multiword_spec.header then
-					ins(("'''%s:'''<br />"):format(alternant_multiword_spec.header))
-				elseif alternant_multiword_spec.q then
-					ins((" ''%s''<br />"):format(alternant_multiword_spec.q))
-				elseif i > 1 then
-					ins(" ''or''<br />")
-				end
-				if alternant_multiword_spec.scrape_errors then
-					local errmsgs = {}
-					for _, scrape_error in ipairs(alternant_multiword_spec.scrape_errors) do
-						table.insert(errmsgs, '<span style="font-weight: bold; color: #CC2200;">' .. scrape_error .. "</span>")
-					end
-					ins(table.concat(errmsgs, "<br />"))
-					ins(require("Module:utilities").format_categories({"Icelandic scraping errors in Template:is-ndecl"},
-						data.lang, nil, nil, force_cat))
-				else
-					ins(require("Module:headword").full_headword(data))
-				end
-			end
-			return table.concat(parts)
-		end,
+		process_multiple_headword_data = get_process_noun_adj_multiple_data("is-ndecl"),
 	}
 end
 
@@ -478,25 +498,173 @@ local function do_comparative_superlative(args, data, plpos)
 	end
 end
 
-pos_functions["adjectives"] = {
-	params = function(lang)
-		local params = {
-			[1] = {list = "comp", disallow_holes = true},
-			[2] = {list = "sup", disallow_holes = true},
-			["adv"] = list_param,
-			["indecl"] = {type = "boolean"},
-		}
-		return params
-	end,
-	func = function(args, data)
-		if args.indecl then
-			table.insert(data.inflections, {label = glossary_link("indeclinable")})
-			table.insert(data.categories, data.langname .. " indeclinable adjectives")
+local function get_manual_adjective_params(lang)
+	return {
+		[1] = {list = "comp", disallow_holes = true},
+		[2] = {list = "sup", disallow_holes = true},
+		["adv"] = list_param,
+		["indecl"] = {type = "boolean"},
+	}
+end
+
+local function do_manual_adjectives(args, data)
+	if args.indecl then
+		table.insert(data.inflections, {label = glossary_link("indeclinable")})
+		table.insert(data.categories, data.langname .. " indeclinable adjectives")
+	end
+	do_comparative_superlative(args, data, "adjectives")
+	m_headword_utilities.parse_and_insert_inflection {
+		headdata = data,
+		forms = args.adv,
+		paramname = "adv",
+		label = "adverb",
+	}
+end
+
+local function get_auto_adjective_params(lang)
+	return {
+        [1] = {required = true, list = true, default = "djúpur<comp:^>"},
+        ["pos"] = {},
+		["adv"] = list_param,
+	}
+end
+
+local function do_auto_adjectives(args, data)
+	if data.lang:getCode() ~= "is" then
+		error("Internal error: Only Icelandic supported at the moment")
+	end
+	if args.pos then
+		data.pos_category = args.pos
+	end
+	local m_is_adjective = require(is_adjective_module)
+	local m_is_common = require(is_common_module)
+	local alternant_multiword_specs = {}
+	local multiple_data = {}
+	local declspecs, scrape_error_data = parse_noun_adj_scraping_specs(data, args[1], "is-adecl", "allow empty infl")
+	if scrape_error_data then
+		return scrape_error_data
+	end
+	for i, declspec in ipairs(declspecs) do
+		local alternant_multiword_spec = m_is_adjective.do_generate_forms(args, declspec, "is-adj")
+		alternant_multiword_specs[i] = alternant_multiword_spec
+		local this_data
+		if i < #declspecs then
+			this_data = m_table.shallowCopy(data)
+			this_data.categories = {}
+			this_data.inflections = {}
+		else
+			-- for the last (or usually only) spec, save memory by not cloning
+			this_data = data
 		end
-		do_comparative_superlative(args, data, "adjectives")
-		parse_and_insert_inflection(data, args, "adv", "adverb")
-	end,
-}
+		multiple_data[i] = this_data
+		this_data.alternant_multiword_spec = alternant_multiword_spec
+		this_data.heads = args.head
+		if not alternant_multiword_spec.scrape_errors then
+			local function do_adjective_form(slot, label, label_for_not_present, accel_form)
+				do_noun_adj_form(this_data, alternant_multiword_spec, slot, label, label_for_not_present, accel_form)
+			end
+
+			local function insert_label(label)
+				table.insert(this_data.inflections, {label = label})
+			end
+
+			if alternant_multiword_spec.saw_indecl and not alternant_multiword_spec.saw_non_indecl then
+				insert_label(glossary_link("indeclinable"))
+			elseif alternant_multiword_spec.saw_unknown_decl and not alternant_multiword_spec.saw_non_unknown_decl then
+				insert_label("unknown declension")
+			end
+
+			local function do_superlative()
+				do_adjective_form("sup_str_nom_m", glossary_link("superlative"), nil, "superlative")
+			end
+
+			if alternant_multiword_spec.haspos == "has" then
+				if alternant_multiword_spec.number.pos == "sg" then
+					insert_label(glossary_link("singular only"))
+				elseif alternant_multiword_spec.number.pos == "pl" then
+					insert_label(glossary_link("plural only"))
+				end
+				if alternant_multiword_spec.state.pos == "strong" or alternant_multiword_spec.state.pos == "weak" then
+					-- not "both" or "none"
+					insert_label(alternant_multiword_spec.state.pos .. " only")
+				end
+				if alternant_multiword_spec.hascomp == "has" then
+					do_adjective_form("comp_wk_nom_m", glossary_link("comparative"), nil, "comparative")
+					if alternant_multiword_spec.hassup == "has" then
+						do_superlative()
+					elseif alternant_multiword_spec.hassup == "hasnot" then
+						insert_label("no " .. glossary_link("superlative"))
+					end
+				elseif alternant_multiword_spec.hascomp == "hasnot" then
+					if alternant_multiword_spec.hassup == "has" then
+						insert_label("no " .. glossary_link("comparative"))
+						do_superlative()
+					else
+						insert_label("not " .. glossary_link("comparable"))
+					end
+				end
+			elseif alternant_multiword_spec.hascomp == "has" then
+				insert_label(glossary_link("comparative") .. " only")
+				if alternant_multiword_spec.hassup == "has" then
+					do_superlative()
+				end
+			else
+				insert_label(glossary_link("superlative") .. " only")
+			end
+
+			if i == 1 then
+				-- Parse and insert an inflection not requiring additional processing into `this_data.inflections`. The raw
+				-- arguments come from `args[field]`, which is parsed for inline modifiers. If there is a corresponding
+				-- qualifier field `FIELD_qual`, qualifiers may additionally come from there. `label` is the label that the
+				-- inflections are given, which is linked to the glossary if surrounded by <<..>> (which is removed).
+				-- `plpos` is the plural part of speech, used in [[Category:LANGNAME PLPOS with red links in their headword
+				-- lines]]. `accel` is the accelerator form, or nil.
+				local function handle_infl(field, label, frob)
+					m_headword_utilities.parse_and_insert_inflection {
+						headdata = this_data,
+						forms = args[field],
+						paramname = field,
+						label = label,
+						frob = frob,
+					}
+				end
+
+				handle_infl("adv", "adverb")
+			end
+
+			-- Add categories.
+			for _, cat in ipairs(alternant_multiword_spec.categories) do
+				table.insert(this_data.categories, cat)
+			end
+
+			insert_noun_adj_linked_lemma_if_needed(this_data, alternant_multiword_spec, is_adjective_module)
+		end
+	end
+
+	return multiple_data
+end
+
+local function get_adjective_pos_functions()
+	return {
+		params = function(lang)
+			if lang:getCode() == "is" then
+				return get_auto_adjective_params(lang)
+			else
+				return get_manual_adjective_params(lang)
+			end
+		end,
+		func = function(args, data)
+			if data.lang:getCode() == "is" then
+				return do_auto_adjectives(args, data)
+			else
+				return do_manual_adjectives(args, data)
+			end
+		end,
+		process_multiple_headword_data = get_process_noun_adj_multiple_data("is-adecl"),
+	}
+end
+
+pos_functions["adjectives"] = get_adjective_pos_functions()
 
 pos_functions["adverbs"] = {
 	params = {
