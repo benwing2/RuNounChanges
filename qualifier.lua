a760e8cd52f4cf57b5443a32395433c9b6672d88
Ben Wing <ben@benwing.com>
2025-02-02 17:59:56 -0600

    qualifier: latest from Wiktionary

--- qualifier.lua
+++ qualifier.lua
@@ -2,21 +2,42 @@ local export = {}
 
 local concat = table.concat
 
+--[==[
+Wrap text in one or more CSS classes. `classes` should be a string; separate multiple classes with a space.
+]==]
 function export.wrap_css(text, classes)
 	return ("<span class=\"%s\">%s</span>"):format(classes, text)
 end
 
+--[==[
+Wrap text in one or more qualifier CSS classes. `suffix` is the suffix describing the type of content, e.g. `brac`
+for parens, `content` for content, `comma` for commas. CSS classes <code>ib-<var>suffix</var></code> and
+i<code>qualifier-<var>suffix</var></code> are added.
+]==]
 function export.wrap_qualifier_css(text, suffix)
 	local css_classes = ("ib-%s qualifier-%s"):format(suffix, suffix)
 	return export.wrap_css(text, css_classes)
 end
 
-function export.format_qualifiers(list, open, close, opencontent, closecontent, no_ib_content)
-	if type(list) ~= "table" then
-		list = {list}
+--[==[
+Format one or more qualifiers. `data` is an object with the following fields:
+* `qualifiers`: A single qualifier or a list or qualifiers.
+* `open`: Override the open paren displayed before the qualifiers. If `false` or an empty string, no paren is displayed.
+* `close`: Override the close paren displayed before the qualifiers. If `false` or an empty string, no paren is
+  displayed.
+* `opencontent`: Content to display before the qualifiers, after the open paren.
+* `closecontent`: Content to display after the qualifiers, before the close paren.
+* `no_ib_content`: Suppress wrapping the content with classes `ib-content` and `qualifier-content`. Parens and commas
+  will still be wrapped in CSS.
+* `raw`: Suppress all CSS wrapping.
+]==]
+function export.format_qualifiers(data)
+	local qualifiers, open, close = data.qualifiers, data.open, data.close
+	if type(qualifiers) ~= "table" then
+		qualifiers = {qualifiers}
 	end
 
-	if not list[1] then
+	if not qualifiers[1] then
 		return ""
 	end
 
@@ -24,46 +45,76 @@ function export.format_qualifiers(list, open, close, opencontent, closecontent, 
 	local function ins(text)
 		table.insert(parts, text)
 	end
+	local function wrap_qualifier_css(text, suffix)
+		if data.raw then
+			return text
+		end
+		return export.wrap_qualifier_css(text, suffix)
+	end
 
-	if open ~= false then
-		ins(export.wrap_qualifier_css(open or "(", "brac"))
+	if open ~= false and open ~= ""then
+		ins(wrap_qualifier_css(open or "(", "brac"))
 	end
-	if opencontent then
-		ins(opencontent)
+	if data.opencontent then
+		ins(data.opencontent)
 	end
-	local content = concat(list, export.wrap_qualifier_css(",", "comma") .. " ")
-	if not no_ib_content then
-		content = export.wrap_qualifier_css(content, "content")
+	local content = concat(qualifiers, wrap_qualifier_css(",", "comma") .. " ")
+	if not data.no_ib_content then
+		content = wrap_qualifier_css(content, "content")
 	end
 	ins(content)
-	if closecontent then
-		ins(closecontent)
+	if data.closecontent then
+		ins(data.closecontent)
 	end
-	if close ~= false then
-		ins(export.wrap_qualifier_css(close or ")", "brac"))
+	if close ~= false and close ~= "" then
+		ins(wrap_qualifier_css(close or ")", "brac"))
 	end
 	return concat(parts)
 end
 
-function export.format_qualifier(list, open, close, opencontent, closecontent, no_ib_content)
-	return export.format_qualifiers(list, open, close, opencontent, closecontent, no_ib_content)
+--[==[
+An older interface onto `format_qualifiers`. Eventually code should be converted to use the new entry point.
+]==]
+function export.format_qualifier(qualifiers, open, close, opencontent, closecontent, no_ib_content)
+	return export.format_qualifiers {
+		qualifiers = qualifiers,
+		open = open,
+		close = close,
+		opencontent = opencontent,
+		closecontent = closecontent,
+		no_ib_content = no_ib_content,
+	}
 end
 
-local function format_qualifiers_with_clarification(list, clarification, open, close)
+local function format_qualifiers_with_clarification(qualifiers, clarification, openquote, closequote)
 	local opencontent = export.wrap_css(clarification, "qualifier-clarification") ..
-		export.wrap_css(open or "“", "qualifier-clarification qualifier-quote")
+		export.wrap_css(openquote or "“", "qualifier-clarification qualifier-quote")
 
-	local closecontent = export.wrap_css(close or "”", "qualifier-clarification qualifier-quote")
+	local closecontent = export.wrap_css(closequote or "”", "qualifier-clarification qualifier-quote")
 
-	return export.format_qualifiers(list, "(", ")", opencontent, closecontent)
+	return export.format_qualifiers {
+		qualifiers = qualifiers,
+		open = "(",
+		close = ")",
+		opencontent = opencontent,
+		closecontent = closecontent,
+	}
 end
 
-function export.sense(list)
-	return export.format_qualifiers(list) .. export.wrap_css(":", "ib-colon sense-qualifier-colon")
+--[==[
+Internal implementation of {{tl|sense}}.
+]==]
+function export.sense(qualifiers)
+	return export.format_qualifiers {
+		qualifiers = qualifiers
+	}.. export.wrap_css(":", "ib-colon sense-qualifier-colon")
 end
 
-function export.antsense(list)
-	return format_qualifiers_with_clarification(list, "antonym(s) of ") ..
+--[==[
+Internal implementation of {{tl|antsense}}.
+]==]
+function export.antsense(qualifiers)
+	return format_qualifiers_with_clarification(qualifiers, "antonym(s) of ") ..
 		export.wrap_css(":", "ib-colon sense-qualifier-colon")
 end
 
